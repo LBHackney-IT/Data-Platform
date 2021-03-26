e925de8959209eb3364a147f05ce11d04ba74dd1
mattbee <matt.bee@madetech.com>
2021-03-26 10:39:52 +0000

    Add Google Sheets API to project
    Co-authored-by: maysakanoni <maysa@madetech.com>

--- terraform/11-google-platform.tf
+++ terraform/11-google-platform.tf
@@ -3,3 +3,10 @@ resource "google_project" "data_platform_staging" {
   project_id = lower("${var.team}-${var.project}-${var.environment}")
   org_id  = "127397986651"
 }
+
+resource "google_project_service" "data_platform_sheets_api" {
+  project = google_project.data_platform_staging.id
+  service = "sheets.googleapis.com"
+
+  disable_dependent_services = true
+}
