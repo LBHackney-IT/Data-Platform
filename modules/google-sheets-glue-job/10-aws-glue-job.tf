159cd0458d5002e558e2878f1c00e8f5d0a4c7dc
Ben <ben.dalton@madetech.com>
2021-03-31 12:13:51 +0100

    Create module for AWS glue job
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: joates-madetech <james.oates@madetech.com>
    Co-authored-by: mattbee <matt.bee@madetech.com>

---
+++ modules/google-sheets-glue-job/10-aws-glue-job.tf
@@ -0,0 +1,24 @@
+# Import test data
+resource "aws_glue_job" "glue_job_google_sheet_import" {
+  tags = var.tags
+
+  name = "Google Sheets Import Job - ${var.glue_job_name}"
+  number_of_workers = 10
+  worker_type = "G.1X"
+  role_arn = var.glue_role_arn
+  command {
+    python_version = "3"
+    script_location = "s3://${var.glue_scripts_bucket_id}/${var.google_sheets_import_script_key}"
+  }
+
+  glue_version = "2.0"
+
+  default_arguments = {
+    "--TempDir" = "s3://${var.glue_temp_storage_bucket_id}"
+    "--additional-python-modules" = "gspread==3.7.0, google-auth==1.27.1, pyspark==3.1.1"
+    "--document_key" = var.google_sheets_document_id
+    "--worksheet_name" = var.google_sheets_worksheet_name
+    "--secret_id" = var.sheets_credentials_name
+    "--s3_bucket_target" = "s3://${var.landing_zone_bucket_id}/${var.department_folder_name}/${var.output_folder_name}"
+  }
+}
