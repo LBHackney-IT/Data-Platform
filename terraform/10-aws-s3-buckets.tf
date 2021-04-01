4d75ec070ce8abaf9cc63ba3bad7f1f8e434af82
Ben <ben.dalton@madetech.com>
2021-04-01 15:24:26 +0100

    Fix formatting
    Co-authored-by: maysakanoni <maysa@madetech.com>
        Co-authored-by: joates-madetech <james.oates@madetech.com>
        Co-authored-by: mattbee <matt.bee@madetech.com>

--- terraform/10-aws-s3-buckets.tf
+++ terraform/10-aws-s3-buckets.tf
@@ -1,41 +1,45 @@
 module "landing_zone" {
-  source = "../modules/s3-bucket"
-  tags = module.tags.values
-  project = var.project
-  environment = var.environment
-  identifier_prefix = local.identifier_prefix
-  bucket_name = "Landing Zone"
-  bucket_identifier = "landing-zone"
+  source                = "../modules/s3-bucket"
+  tags                  = module.tags.values
+  project               = var.project
+  environment           = var.environment
+  identifier_prefix     = local.identifier_prefix
+  account_configuration = local.accounts
+  bucket_name           = "Landing Zone"
+  bucket_identifier     = "landing-zone"
 }
 
 module "raw_zone" {
-  source = "../modules/s3-bucket"
-  tags = module.tags.values
-  project = var.project
-  environment = var.environment
-  identifier_prefix = local.identifier_prefix
-  bucket_name = "Raw Zone"
-  bucket_identifier = "raw-zone"
+  source                = "../modules/s3-bucket"
+  tags                  = module.tags.values
+  project               = var.project
+  environment           = var.environment
+  identifier_prefix     = local.identifier_prefix
+  account_configuration = local.accounts
+  bucket_name           = "Raw Zone"
+  bucket_identifier     = "raw-zone"
 }
 
 module "refined_zone" {
-  source = "../modules/s3-bucket"
-  tags = module.tags.values
-  project = var.project
-  environment = var.environment
-  identifier_prefix = local.identifier_prefix
-  bucket_name = "Refined Zone"
-  bucket_identifier = "refined-zone"
+  source                = "../modules/s3-bucket"
+  tags                  = module.tags.values
+  project               = var.project
+  environment           = var.environment
+  identifier_prefix     = local.identifier_prefix
+  account_configuration = local.accounts
+  bucket_name           = "Refined Zone"
+  bucket_identifier     = "refined-zone"
 }
 
 module "trusted_zone" {
-  source = "../modules/s3-bucket"
-  tags = module.tags.values
-  project = var.project
-  environment = var.environment
-  identifier_prefix = local.identifier_prefix
-  bucket_name = "Trusted Zone"
-  bucket_identifier = "trusted-zone"
+  source                = "../modules/s3-bucket"
+  tags                  = module.tags.values
+  project               = var.project
+  environment           = var.environment
+  identifier_prefix     = local.identifier_prefix
+  account_configuration = local.accounts
+  bucket_name           = "Trusted Zone"
+  bucket_identifier     = "trusted-zone"
 }
 
 
@@ -43,22 +47,22 @@ module "trusted_zone" {
 resource "aws_kms_key" "glue_scripts_key" {
   tags = module.tags.values
 
-  description = "${var.project} ${var.environment} - Glue Scripts Bucket Key"
+  description             = "${var.project} ${var.environment} - Glue Scripts Bucket Key"
   deletion_window_in_days = 10
-  enable_key_rotation = true
+  enable_key_rotation     = true
 }
 
 resource "aws_s3_bucket" "glue_scripts_bucket" {
   tags = module.tags.values
 
-  bucket = lower("${local.identifier_prefix}-glue-scripts")
+  bucket        = lower("${local.identifier_prefix}-glue-scripts")
   force_destroy = true
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = aws_kms_key.glue_scripts_key.arn
-        sse_algorithm = "aws:kms"
+        sse_algorithm     = "aws:kms"
       }
     }
   }
@@ -68,22 +72,22 @@ resource "aws_s3_bucket" "glue_scripts_bucket" {
 resource "aws_kms_key" "glue_temp_storage_bucket_key" {
   tags = module.tags.values
 
-  description = "${var.project} ${var.environment} - Glue Temp Storage Bucket Key"
+  description             = "${var.project} ${var.environment} - Glue Temp Storage Bucket Key"
   deletion_window_in_days = 10
-  enable_key_rotation = true
+  enable_key_rotation     = true
 }
 
 resource "aws_s3_bucket" "glue_temp_storage_bucket" {
   tags = module.tags.values
 
-  bucket = lower("${local.identifier_prefix}-glue-temp-storage")
+  bucket        = lower("${local.identifier_prefix}-glue-temp-storage")
   force_destroy = true
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = aws_kms_key.glue_temp_storage_bucket_key.arn
-        sse_algorithm = "aws:kms"
+        sse_algorithm     = "aws:kms"
       }
     }
   }
@@ -93,22 +97,22 @@ resource "aws_s3_bucket" "glue_temp_storage_bucket" {
 resource "aws_kms_key" "athena_storage_bucket_key" {
   tags = module.tags.values
 
-  description = "${var.project} ${var.environment} - Glue Temp Storage Bucket Key"
+  description             = "${var.project} ${var.environment} - Glue Temp Storage Bucket Key"
   deletion_window_in_days = 10
-  enable_key_rotation = true
+  enable_key_rotation     = true
 }
 
 resource "aws_s3_bucket" "athena_storage_bucket" {
   tags = module.tags.values
 
-  bucket = lower("${local.identifier_prefix}-athena-storage")
+  bucket        = lower("${local.identifier_prefix}-athena-storage")
   force_destroy = true
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = aws_kms_key.athena_storage_bucket_key.arn
-        sse_algorithm = "aws:kms"
+        sse_algorithm     = "aws:kms"
       }
     }
   }
--- terraform/50-aws-secrets-manager.tf
+++ terraform/50-aws-secrets-manager.tf
@@ -1,19 +1,19 @@
 resource "aws_kms_key" "sheets_credentials" {
   tags = module.tags.values
 
-  description = "${var.project} ${var.environment} - Sheets Credentials"
+  description             = "${var.project} ${var.environment} - Sheets Credentials"
   deletion_window_in_days = 10
-  enable_key_rotation = true
+  enable_key_rotation     = true
 }
 
 resource "aws_secretsmanager_secret" "sheets_credentials_housing" {
   tags = module.tags.values
 
-  name = "${local.identifier_prefix}-sheets-credentials-housing"
+  name       = "${local.identifier_prefix}-sheets-credentials-housing"
   kms_key_id = aws_kms_key.sheets_credentials.id
 }
 
 resource "aws_secretsmanager_secret_version" "housing_json_credentials_secret_version" {
-  secret_id = aws_secretsmanager_secret.sheets_credentials_housing.id
+  secret_id     = aws_secretsmanager_secret.sheets_credentials_housing.id
   secret_binary = google_service_account_key.housing_json_credentials.private_key
 }
