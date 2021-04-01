863ba005f33995deb07f9c5cb2bc5b336d157bfd
maysakanoni <maysa@madetech.com>
2021-04-01 16:32:29 +0100

    Add AWS trigger to run glue job
    Co-authored-by: joates-madetech <james.oates@madetech.com>
    Co-authored-by: mattbee <matt.bee@madetech.com>
    Co-authored-by: ben-d758 <ben.dalton@madetech.com>

--- modules/google-sheets-glue-job/02-inputs-optional.tf
+++ modules/google-sheets-glue-job/02-inputs-optional.tf
@@ -1,1 +1,5 @@
-
+variable "google_sheet_import_schedule" {
+  description = "Cron schedule for importing the Google sheet using AWS Glue"
+  type        = string
+  default     = "cron(30 16 * * ? *)"
+}
