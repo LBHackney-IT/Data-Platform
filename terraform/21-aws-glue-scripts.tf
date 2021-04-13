d6e9fb41ef81a46ec9f3d8a957da1220fa77e22a
Ben <ben.dalton@madetech.com>
2021-04-13 17:15:33 +0100

    Add job script using Directed Acyclic Graph (DAG)
    We hope that by using aws_glue_script data source to generate
    glue_script_template_to_raw script from a Directed Acyclic Graph (DAG),
    it will allow the script to be edited using the Visual editor oin Glue
    Studio.
    
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: mattbee <matt.bee@madetech.com>
    Co-authored-by: joates-madetech <james.oates@madetech.com>

--- terraform/21-aws-glue-scripts.tf
+++ terraform/21-aws-glue-scripts.tf
@@ -8,12 +8,66 @@ resource "aws_s3_bucket_object" "google_sheets_import_script" {
   etag   = filemd5("../scripts/google-sheets-import.py")
 }
 
-resource "aws_s3_bucket_object" "glue_job_template_script_to_raw" {
-  tags = module.tags.values
+data "aws_glue_script" "glue_script_template_to_raw" {
+  language = "PYTHON"
 
-  bucket = aws_s3_bucket.glue_scripts_bucket.id
-  key    = "scripts/glue-job-template-script-to-raw.py"
-  acl    = "private"
-  source = "../scripts/glue-job-template-script-to-raw.py"
-  etag   = filemd5("../scripts/glue-job-template-script-to-raw.py")
+  dag_edge {
+    source = "DataSource0"
+    target = "DataSink0"
+  }
+
+  dag_node {
+    id = "DataSource0"
+    node_type = "DataSource"
+
+    args {
+      name = "connection_type"
+      value = "s3"
+    }
+
+    args {
+      name = "format"
+      value = "parquet"
+    }
+
+    args {
+      name = "connection_options"
+      value = "{\"paths\": [\"s3://${module.landing_zone.bucket_id}/job-template-script.csv\"], \"recurse\":True}"
+    }
+
+    args {
+      name = "transformation_ctx"
+      value = "DataSource0"
+    }
+  }
+
+  dag_node {
+    id = "DataSink0"
+    node_type = "DataSink"
+
+    args {
+      name = "connection_type"
+      value = "s3"
+    }
+
+    args {
+      name = "format"
+      value = "csv"
+    }
+
+    args {
+      name = "connection_options"
+      value = "{\"path\": [\"s3://${module.raw_zone.bucket_id}/job-template-script/\", \"partitionKeys\": []}"
+    }
+
+    args {
+      name = "transformation_ctx"
+      value = "DataSink0"
+    }
+  }
 }
+
+output "python_script" {
+  value = data.aws_glue_script.glue_script_template_to_raw.python_script
+}
+
