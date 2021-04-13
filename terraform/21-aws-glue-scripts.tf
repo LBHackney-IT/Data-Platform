4672077a2cf5de9685e3e7708fa7d834d87a76b5
mattbee <matt.bee@madetech.com>
2021-04-13 14:52:09 +0100

    Add terraform for Glue template
    We are trying to create a Glue template for use in Studio, to allow
    easy creation of ETL processes.

--- terraform/21-aws-glue-scripts.tf
+++ terraform/21-aws-glue-scripts.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket_object" "google_sheets_import_script" {
   source = "../scripts/google-sheets-import.py"
   etag   = filemd5("../scripts/google-sheets-import.py")
 }
+
+resource "aws_s3_bucket_object" "glue_job_template_script_to_raw" {
+  tags = module.tags.values
+
+  bucket = aws_s3_bucket.glue_scripts_bucket.id
+  key    = "scripts/glue-job-template-script-to-raw.py"
+  acl    = "private"
+  source = "../scripts/glue-job-template-script-to-raw.py"
+  etag   = filemd5("../scripts/glue-job-template-script-to-raw.py")
+}
