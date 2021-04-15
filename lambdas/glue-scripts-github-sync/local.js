bd0f098bdd05d4de80d262adac88cab06acccb1b
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "add lambda to clone data platform repo to a temp directory"
    This reverts commit 0efacd936444b65aac621b998918384a1fcfa9aa.

--- lambdas/glue-scripts-github-sync/local.js
+++
@@ -1,8 +0,0 @@
-const handler = require('./index');
-const AWS = require("aws-sdk");
-
-var credentials = new AWS.SharedIniFileCredentials({profile: 'madetech'});
-AWS.config.credentials = credentials;
-
-handler.handler({})
-
