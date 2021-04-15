64d2331d64dc69d492893d7a1fc8da82fb621af9
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "Add service account to google project"
    This reverts commit a1fc74ff32b1e35f16fa660209434740a9ee4e87.

--- terraform/11-google-platform.tf
+++ terraform/11-google-platform.tf
@@ -10,23 +10,3 @@ resource "google_project_service" "data_platform_sheets_api" {
 
   disable_dependent_services = true
 }
-
-resource "google_service_account" "housing" {
-  account_id   = lower("${var.team}-${var.environment}-housing")
-  display_name = "Dataplatform housing service account"
-  project = google_project.data_platform_staging.id
-}
-
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/iam.serviceAccountUser"
-
-    members = [
-      "user:${google_service_account.housing.email}",
-    ]
-  }
-}
-resource "google_service_account_iam_policy" "admin-account-iam" {
-  service_account_id = google_service_account.housing.name
-  policy_data        = data.google_iam_policy.admin.policy_data
-}
