c659f2027aed6600045dc262e85ea456be9867f7
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "Update S3 policy"
    This reverts commit 95a2a93e8bb437141c6e226c9a72dce66a20b648.

--- terraform/10-s3.tf
+++ terraform/10-s3.tf
@@ -12,24 +12,13 @@ resource "aws_s3_bucket_policy" "aws-s3-lb-logs" {
   "Version":"2012-10-17",
   "Statement":[
     {
-      "Sid":"ListBucket",
-      "Effect":"Allow",
-      "Principal": {"AWS": ["arn:aws:iam::261219435789:root", "arn:aws:iam::261219435789:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_SandboxAdmin_772511f048f85463"]},
-      "Action":["s3:ListBucket"],
-      "Resource":"${aws_s3_bucket.dataplatform_raw.arn}"
-    },
-    {
       "Sid":"AddCannedAcl",
       "Effect":"Allow",
-      "Principal": {"AWS": ["arn:aws:iam::261219435789:root", "arn:aws:iam::261219435789:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_SandboxAdmin_772511f048f85463"]},
+    "Principal": {"AWS": ["arn:aws:iam::261219435789:root"]},
       "Action":["s3:PutObject","s3:PutObjectAcl"],
       "Resource":"${aws_s3_bucket.dataplatform_raw.arn}/social-care/*",
-      "Condition": {
-          "StringEquals": {
-              "s3:x-amz-acl": "bucket-owner-full-control"
-          }
-        },
-      }
+      "Condition":{"StringEquals":{"s3:x-amz-acl":["private"]}}
+    }
   ]
 }
 POLICY
