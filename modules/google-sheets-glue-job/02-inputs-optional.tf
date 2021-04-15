43b14803af90a482b40125c276eb62e8e8263ce7
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "Add AWS trigger to run glue job"
    This reverts commit a8bd43db6e4d470dd20cc745dd5f8449418a7017.

--- modules/google-sheets-glue-job/02-inputs-optional.tf
+++ modules/google-sheets-glue-job/02-inputs-optional.tf
@@ -1,5 +1,1 @@
-variable "google_sheet_import_schedule" {
-  description = "Cron schedule for importing the Google sheet using AWS Glue"
-  type        = string
-  default     = "cron(30 16 * * ? *)"
-}
+
