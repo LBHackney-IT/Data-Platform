5c2b565645f88a15c869b043fb0b5bd41c5ed125
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "Add terraform for creating S3 backend for project infrastructure"
    This reverts commit ffdb6c5e57611d8b9067441e06505c9e93596e7a.

--- terraform-backend-setup/99-outputs.tf
+++
@@ -1,5 +0,0 @@
-# We make any output files clear by adding them to the 99-outputs.tf, meaning anyone can quickly check if they're consuming your module
-output "terraform_state_bucket_arn" {
-  description = "Terraform bucket arn"
-  value       = aws_s3_bucket.data_platform_terraform_backend.arn
-}
