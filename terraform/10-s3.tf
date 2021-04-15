1e2ae9057affff015b79fa88c23dd359fef7f5ab
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "Attach policy to aws s3 bucket"
    This reverts commit 59db767ed0aa978c90d0cc902bb22ad0982238f0.

--- terraform/10-s3.tf
+++ terraform/10-s3.tf
@@ -1,25 +1,3 @@
 resource "aws_s3_bucket" "dataplatform_raw" {
-  provider = aws.core
   bucket = "hackney-data-platform-raw-staging-mtsandbox"
-
-}
-
-resource "aws_s3_bucket_policy" "aws-s3-lb-logs" {
-  provider = aws.core
-  bucket = aws_s3_bucket.dataplatform_raw.id
-  policy = <<POLICY
-{
-  "Version":"2012-10-17",
-  "Statement":[
-    {
-      "Sid":"AddCannedAcl",
-      "Effect":"Allow",
-    "Principal": {"AWS": ["arn:aws:iam::261219435789:root"]},
-      "Action":["s3:PutObject","s3:PutObjectAcl"],
-      "Resource":"${aws_s3_bucket.dataplatform_raw.arn}/social-care/*",
-      "Condition":{"StringEquals":{"s3:x-amz-acl":["private"]}}
-    }
-  ]
-}
-POLICY
 }
