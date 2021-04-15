f4ee765b1a26cc1041187ff63fdaefffd7c73b6f
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "re-add server side encryption for glue scripts bucket add kms decrypt for each kms key"
    This reverts commit d43f61c6a2846017d57d8fce26c8fc8dda8dd21c.

--- terraform/50-aws-secrets-manager.tf
+++ terraform/50-aws-secrets-manager.tf
@@ -6,5 +6,5 @@ resource "aws_secretsmanager_secret" "housing_json_credentials_secret" {
 resource "aws_secretsmanager_secret_version" "housing_json_credentials_secret_version" {
   provider      = aws.core
   secret_id     = aws_secretsmanager_secret.housing_json_credentials_secret.id
-  secret_binary = google_service_account_key.housing_json_credentials.private_key
+  secret_string = google_service_account_key.housing_json_credentials.private_key
 }
