6ece296e1aa5e01c7c6e077686e8a4199216f555
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "Add Iam role and s3 buckets for glue"
    This reverts commit 7814856e317308d63625bb662a84ce9fd0cb6038.

--- terraform/10-s3.tf
+++ terraform/10-s3.tf
@@ -1,99 +1,13 @@
-resource "aws_s3_bucket" "data_platform_raw" {
+resource "aws_s3_bucket" "dataplatform_raw" {
   provider = aws.core
-  bucket   = "hackney-data-platform-raw-staging-mtsandbox"
+  bucket = "hackney-data-platform-raw-staging-mtsandbox"
+
 }
 
-resource "aws_s3_bucket" "data_platform_glue_script" {
+resource "aws_s3_bucket_policy" "aws-s3-lb-logs" {
   provider = aws.core
-  bucket   = "hackney-data-platform-glue-script"
-}
-
-resource "aws_s3_bucket" "data_platform_glue_temp_storage" {
-  provider = aws.core
-  bucket   = "hackney-data-platform-glue-temp-storage"
-}
-
-resource "aws_iam_role" "glue" {
-  provider = aws.core
-  name = "data-platform-glue-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "glue.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "glue_access_policy" {
-  provider = aws.core
-  name = "data-platform-glue-access-policy"
+  bucket = aws_s3_bucket.dataplatform_raw.id
   policy = <<POLICY
-{
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "glue:*",
-                "s3:GetBucketLocation",
-                "s3:ListBucket",
-                "s3:ListAllMyBuckets",
-                "iam:ListRolePolicies",
-                "iam:GetRole",
-                "iam:GetRolePolicy",
-                "cloudwatch:PutMetricData"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": "s3:*",
-            "Resource": [
-                "${aws_s3_bucket.data_platform_raw.arn}/*",
-                "${aws_s3_bucket.data_platform_glue_temp_storage.arn}/*",
-                "${aws_s3_bucket.data_platform_glue_script.arn}/*"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents",
-                "logs:AssociateKmsKey"
-            ],
-            "Resource": [
-                "arn:aws:logs:*:*:/aws-glue/*"
-            ]
-        }
-    ]
-}
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "attach_glue_access_policy" {
-  provider = aws.core
-  role       = aws_iam_role.glue.name
-  policy_arn = aws_iam_policy.glue_access_policy.arn
-}
-
-resource "aws_s3_bucket_policy" "data_platform_raw_bucket_policy" {
-  provider = aws.core
-  bucket   = aws_s3_bucket.data_platform_raw.id
-  policy   = <<POLICY
 {
   "Version":"2012-10-17",
   "Statement":[
@@ -102,20 +16,20 @@ resource "aws_s3_bucket_policy" "data_platform_raw_bucket_policy" {
       "Effect":"Allow",
       "Principal": {"AWS": ["arn:aws:iam::261219435789:root", "arn:aws:iam::261219435789:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_SandboxAdmin_772511f048f85463"]},
       "Action":["s3:ListBucket"],
-      "Resource":"${aws_s3_bucket.data_platform_raw.arn}"
+      "Resource":"${aws_s3_bucket.dataplatform_raw.arn}"
     },
     {
       "Sid":"AddCannedAcl",
       "Effect":"Allow",
       "Principal": {"AWS": ["arn:aws:iam::261219435789:root", "arn:aws:iam::261219435789:role/aws-reserved/sso.amazonaws.com/eu-west-2/AWSReservedSSO_SandboxAdmin_772511f048f85463"]},
       "Action":["s3:PutObject","s3:PutObjectAcl"],
-      "Resource":"${aws_s3_bucket.data_platform_raw.arn}/social-care/*",
+      "Resource":"${aws_s3_bucket.dataplatform_raw.arn}/social-care/*",
       "Condition": {
           "StringEquals": {
               "s3:x-amz-acl": "bucket-owner-full-control"
           }
+        },
       }
-    }
   ]
 }
 POLICY
