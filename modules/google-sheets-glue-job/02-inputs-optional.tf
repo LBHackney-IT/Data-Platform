01d7d18d467ed348e1908d8abe4912cf52042790
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-04-15 11:08:37 +0100

    Revert "Update Google sheet import trigger to 11pm Mon-Fri"
    This reverts commit 3ba2d226dc02ca55dfb0ef53070d4a1c7cb93c8c.

--- modules/google-sheets-glue-job/02-inputs-optional.tf
+++ modules/google-sheets-glue-job/02-inputs-optional.tf
@@ -1,5 +1,5 @@
 variable "google_sheet_import_schedule" {
   description = "Cron schedule for importing the Google sheet using AWS Glue"
   type        = string
-  default     = "cron(0 23 ? * 1-5 *)"
+  default     = "cron(30 16 * * ? *)"
 }
