6faa87951143ec8a5deb295e436343ea7a243044
maysakanoni <maysa@madetech.com>
2021-04-20 13:52:25 +0100

    Export the latest snapshot to an s3 bucket
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    Co-authored-by: mattbee <matt.bee@madetech.com>

--- lambdas/rds-database-snapshot-replicator/index.js
+++ lambdas/rds-database-snapshot-replicator/index.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 
-const AWS_REGION = 'eu-west-2';
+const AWS_REGION = "eu-west-2";
 // Find newest back up
 
 // Start export task to export Snapshot to S3 (using RDS instance?)
@@ -23,5 +23,16 @@ exports.handler = async (events) => {
       marker = dbSnapshots.Marker;
     } while (marker);
     const latestSnapshot = dbSnapshots.DBSnapshots.pop();
+    var params = {
+      ExportTaskIdentifier: `${latestSnapshot.DBInstanceIdentifier}-export`,
+      IamRoleArn: "",
+      KmsKeyId:
+        "arn:aws:kms:eu-west-2:261219435789:key/60e9157b-458d-4ed7-9f5d-751769995d39",
+      S3BucketName: "hackney-jamesoates-landing-zone",
+      SourceArn: latestSnapshot.DBSnapshotArn,
+      S3Prefix: `exports/${latestSnapshot.DBInstanceIdentifier}`,
+    };
+    let response = await rds.startExportTask(params).promise();
+    console.log(response);
   }
 };
