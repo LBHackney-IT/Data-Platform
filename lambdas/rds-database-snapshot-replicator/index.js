190eccb9a7d68e54474e22f4ba4aafde79439f65
b-dalton <ben.dalton@madetech.com>
2021-04-20 17:58:53 +0100

    wrap parsing of sns message to JSON in a try catch block
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: mattbee <matt.bee@madetech.com>
    Co-authored-by: joates-madetech <james.oates@madetech.com>

--- lambdas/rds-database-snapshot-replicator/index.js
+++ lambdas/rds-database-snapshot-replicator/index.js
@@ -7,7 +7,13 @@ const AWS_REGION = "eu-west-2";
 
 exports.handler = async (events) => {
   for (const eventRecord of events.Records) {
-    const snsMessage = JSON.parse(eventRecord.Sns.Message);
+    try {
+      const snsMessage = JSON.parse(eventRecord.Sns.Message)
+    }
+    catch(err){
+      console.log("event record:", eventRecord);
+      return
+    }
     const dbInstanceId = snsMessage["Source ID"];
 
     const rds = new AWS.RDS({ region: AWS_REGION });
