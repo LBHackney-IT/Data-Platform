06b82b15ac8949ac8958b7875175d04de21eb75b
maysakanoni <maysa@madetech.com>
2021-04-21 15:31:48 +0100

    update state file

--- terraform_lambda/00-init.tf
+++ terraform_lambda/00-init.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     region  = "eu-west-2"
     key     = "tf-remote-state"
-    bucket  = "dataplatform-internal-stg-tfstate"
+    bucket  = "dataplatform-test-lambda"
     encrypt = true
   }
 }
