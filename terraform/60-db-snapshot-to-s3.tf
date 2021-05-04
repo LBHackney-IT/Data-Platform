e8a56b7fc89da3d7690afc08f3b64d2003e295b2
el <elena@madetech.com>
2021-05-04 15:31:30 +0100

    remove newly added provider from snapshot ingestion
    Co-authored-by: joates-madetech <james.oates@madetech.com>

--- terraform/60-db-snapshot-to-s3.tf
+++ terraform/60-db-snapshot-to-s3.tf
@@ -1,6 +1,6 @@
 // deploy lambda
 resource "aws_iam_role" "iam_for_lambda" {
-  provider = aws.aws_api_account
+  # provider = aws.aws_api_account
 
   name = "iam_for_lambda"
   assume_role_policy = jsonencode({
@@ -25,7 +25,7 @@ data "archive_file" "lambda_zip_file" {
 }
 
 resource "aws_s3_bucket" "s3_deployment_artefacts" {
-  provider = aws.aws_api_account
+  # provider = aws.aws_api_account
 
   bucket        = "data-platform-db-snapshot-script-${var.environment}"
   acl           = "private"
@@ -33,7 +33,7 @@ resource "aws_s3_bucket" "s3_deployment_artefacts" {
 }
 
 resource "aws_s3_bucket_object" "handler" {
-  provider = aws.aws_api_account
+  # provider = aws.aws_api_account
 
   bucket = aws_s3_bucket.s3_deployment_artefacts.bucket
   key    = "lambda_function.zip"
@@ -47,7 +47,7 @@ resource "aws_s3_bucket_object" "handler" {
 }
 
 resource "aws_lambda_function" "rds_snapshot_to_s3_lambda" {
-  provider = aws.aws_api_account
+  # provider = aws.aws_api_account
 
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "exports.handler"
@@ -119,40 +119,40 @@ resource "aws_iam_role_policy_attachment" "export_bucket_policy_attachment" {
   role = aws_iam_role.rds_export_process_role.name
   policy_arn = aws_iam_policy.export_bucket_policy_document.arn
 }
-
-resource "aws_sns_topic" "ingestion_topic" {
-  provider = aws.aws_api_account
-  name = "ingestion-topic"
-}
-
-resource "aws_sqs_queue" "ingestion_queue" {
-  provider = aws.aws_api_account
-
-  name = "ingestion-queue"
-}
-
-resource "aws_sns_topic_subscription" "ingestion_sqs_target" {
-  provider = aws.aws_api_account
-
-  topic_arn = aws_sns_topic.ingestion_topic.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.ingestion_queue.arn
-}
-
-resource "aws_lambda_permission" "with_sns" {
-  provider = aws.aws_api_account
-
-  statement_id  = "AllowExecutionFromSNS"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.rds_snapshot_to_s3_lambda.function_name
-  principal     = "sns.amazonaws.com"
-  source_arn    = aws_sns_topic.ingestion_topic.arn
-}
-
-resource "aws_sns_topic_subscription" "lambda" {
-  provider = aws.aws_api_account
-
-  topic_arn = aws_sns_topic.ingestion_topic.arn
-  protocol  = "lambda"
-  endpoint  = aws_lambda_function.rds_snapshot_to_s3_lambda.arn
-}
+#
+# resource "aws_sns_topic" "ingestion_topic" {
+#   provider = aws.aws_api_account
+#   name = "ingestion-topic"
+# }
+#
+# resource "aws_sqs_queue" "ingestion_queue" {
+#   provider = aws.aws_api_account
+#
+#   name = "ingestion-queue"
+# }
+#
+# resource "aws_sns_topic_subscription" "ingestion_sqs_target" {
+#   provider = aws.aws_api_account
+#
+#   topic_arn = aws_sns_topic.ingestion_topic.arn
+#   protocol  = "sqs"
+#   endpoint  = aws_sqs_queue.ingestion_queue.arn
+# }
+#
+# resource "aws_lambda_permission" "with_sns" {
+#   provider = aws.aws_api_account
+#
+#   statement_id  = "AllowExecutionFromSNS"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.rds_snapshot_to_s3_lambda.function_name
+#   principal     = "sns.amazonaws.com"
+#   source_arn    = aws_sns_topic.ingestion_topic.arn
+# }
+#
+# resource "aws_sns_topic_subscription" "lambda" {
+#   provider = aws.aws_api_account
+#
+#   topic_arn = aws_sns_topic.ingestion_topic.arn
+#   protocol  = "lambda"
+#   endpoint  = aws_lambda_function.rds_snapshot_to_s3_lambda.arn
+# }
