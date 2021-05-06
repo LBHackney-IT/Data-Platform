7499b05ae8c135da646765a57f57c65f113cbf47
el <elena@madetech.com>
2021-05-06 15:22:52 +0100

    add providers (All James idea)
        Co-authored-by: joates-madetech <james.oates@madetech.com>

--- terraform/60-db-snapshot-to-s3.tf
+++ terraform/60-db-snapshot-to-s3.tf
@@ -30,17 +30,17 @@ data "aws_iam_policy_document" "lambda_policy" {
 }
 
 resource "aws_iam_policy" "lambda_policy" {
+  provider = aws.aws_api_account
+
   name   = "rds-snapshot-ingestion-lambda-policy"
   policy = data.aws_iam_policy_document.lambda_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_role_attachment" {
+  provider = aws.aws_api_account
+
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = aws_iam_policy.lambda_policy.arn
-
-  depends_on = [
-    aws_iam_role.iam_for_lambda
-  ]
 }
 
 data "archive_file" "lambda_zip_file" {
