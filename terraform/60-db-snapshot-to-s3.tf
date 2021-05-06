2a3beb8763dcf9ffe6ba923251e256fea2649cae
el <elena@madetech.com>
2021-05-06 14:30:48 +0100

    fix policy name

--- terraform/60-db-snapshot-to-s3.tf
+++ terraform/60-db-snapshot-to-s3.tf
@@ -30,11 +30,11 @@ data "aws_iam_policy_document" "lambda_policy" {
 }
 
 resource "aws_iam_policy" "lambda_policy" {
-  name   = "${local.identifier_prefix}-${var.environment}-lambda-policy"
+  name   = "rds-snapshot-to-s3-lambda-policy"
   policy = data.aws_iam_policy_document.lambda_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "lambda-role-attachment" {
+resource "aws_iam_role_policy_attachment" "lambda_role_attachment" {
   role       = aws_iam_role.iam_for_lambda.arn
   policy_arn = aws_iam_policy.lambda_policy.arn
 }
