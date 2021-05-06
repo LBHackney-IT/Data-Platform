04fab558cc55886e4fedff9fd96b38aa896cf841
el <elena@madetech.com>
2021-05-06 14:22:42 +0100

    syntax fix

--- terraform/60-db-snapshot-to-s3.tf
+++ terraform/60-db-snapshot-to-s3.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "lambda_policy" {
 
     principals {
       type        = "Resource"
-      identifiers = "arn:aws:logs:*:*:*"
+      identifiers = ["arn:aws:logs:*:*:*"]
     }
   }
 }
