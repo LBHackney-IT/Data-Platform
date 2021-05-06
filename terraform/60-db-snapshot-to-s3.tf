1e7396581f47a0f8f6f1c8f24132b3cbbbd17dd2
el <elena@madetech.com>
2021-05-06 14:20:05 +0100

    allow lambda to write to cloudwatch

--- terraform/60-db-snapshot-to-s3.tf
+++ terraform/60-db-snapshot-to-s3.tf
@@ -12,10 +12,35 @@ resource "aws_iam_role" "iam_for_lambda" {
           Service : "lambda.amazonaws.com"
         },
         Effect : "Allow",
-        Sid : ""
       }
     ]
   })
+}
+
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect  = "Allow"
+
+    principals {
+      type        = "Resource"
+      identifiers = "arn:aws:logs:*:*:*"
+    }
+  }
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name   = "${local.identifier_prefix}-${var.environment}-lambda-policy"
+  policy = data.aws_iam_policy_document.lambda_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-role-attachment" {
+  role       = aws_iam_role.iam_for_lambda.arn
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
 
 data "archive_file" "lambda_zip_file" {
