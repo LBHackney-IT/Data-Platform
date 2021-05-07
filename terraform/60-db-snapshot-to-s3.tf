f4aa9858163b908d18f9f76fbdebdabae53cfab0
el <elena@madetech.com>
2021-05-07 10:21:37 +0100

    aws_lambda_event_source_mapping lies when it says it wants function name it really wants function arn!!"

--- terraform/60-db-snapshot-to-s3.tf
+++ terraform/60-db-snapshot-to-s3.tf
@@ -189,7 +189,7 @@ resource "aws_sqs_queue" "ingestion_queue" {
 resource "aws_lambda_event_source_mapping" "event_source_mapping" {
   event_source_arn = aws_sqs_queue.ingestion_queue.arn
   enabled          = true
-  function_name    = aws_lambda_function.rds_snapshot_to_s3_lambda.function_name
+  function_name    = aws_lambda_function.rds_snapshot_to_s3_lambda.arn
   batch_size       = 1
 }
 
