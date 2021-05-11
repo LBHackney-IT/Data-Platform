5ff94853629b7f73bcc61185372cdbd9d880ab6d
el <elena@madetech.com>
2021-05-11 16:21:00 +0100

    fix sqs trigger processing

--- lambdas/rds-database-snapshot-replicator/index.js
+++ lambdas/rds-database-snapshot-replicator/index.js
@@ -11,51 +11,53 @@ let s3BucketName = process.env.S3_BUCKET_NAME
 // Start export task to export Snapshot to S3 (using RDS instance?)
 
 exports.handler = async (event) => {
-  console.dir(event);
-  let sqsMessage;
-    try {
-      sqsMessage = JSON.parse(event.Message);
-      console.log("sqs message:", sqsMessage);
-    } catch (err) {
-      console.log("event error:", err);
-      console.log("sqs message:", sqsMessage);
-      return;
-    }
-    const dbSnapshotId = sqsMessage["Source ID"];
+  console.log(event);
+  event.Records.forEach(record => {
+    let sqsMessage;
+      try {
+        sqsMessage = JSON.parse(record.body.Message);
+        console.log("sqs message:", sqsMessage);
+      } catch (err) {
+        console.log("event error:", err);
+        console.log("sqs message:", sqsMessage);
+        return;
+      }
+      const dbSnapshotId = sqsMessage["Source ID"];
 
-    const rds = new AWS.RDS({ region: AWS_REGION });
-    let marker = undefined;
-    let dbSnapshots;
+      const rds = new AWS.RDS({ region: AWS_REGION });
+      let marker = undefined;
+      let dbSnapshots;
 
-    do {
-      const describeDBSnapshotsParams = {
-        DBSnapshotIdentifier: dbSnapshotId,
-        Marker: marker,
+      do {
+        const describeDBSnapshotsParams = {
+          DBSnapshotIdentifier: dbSnapshotId,
+          Marker: marker,
+        };
+
+        dbSnapshots = await rds.describeDBSnapshots(describeDBSnapshotsParams).promise();
+        console.log("All Snapshot:", dbSnapshots);
+
+        marker = dbSnapshots.Marker;
+      } while (marker);
+
+      const latestSnapshot = dbSnapshots.DBSnapshots.pop();
+      console.log("Latest Snapshot:", latestSnapshot);
+
+      const snapshotIdentifier = latestSnapshot.DBSnapshotIdentifier.replace(':', '-');
+      console.log("new snapshotIdentifier:", snapshotIdentifier);
+
+      const databaseName = latestSnapshot.DBInstanceIdentifier;
+      console.log("databaseName:", databaseName);
+
+      const startExportTaskParams = {
+        ExportTaskIdentifier: snapshotIdentifier.substr(0, 60),
+        IamRoleArn: iamRoleArn,
+        KmsKeyId: kmsKeyId,
+        S3BucketName: s3BucketName,
+        SourceArn: latestSnapshot.DBSnapshotArn,
+        S3Prefix: `${databaseName}/${snapshotIdentifier}`,
       };
-
-      dbSnapshots = await rds.describeDBSnapshots(describeDBSnapshotsParams).promise();
-      console.log("All Snapshot:", dbSnapshots);
-
-      marker = dbSnapshots.Marker;
-    } while (marker);
-
-    const latestSnapshot = dbSnapshots.DBSnapshots.pop();
-    console.log("Latest Snapshot:", latestSnapshot);
-
-    const snapshotIdentifier = latestSnapshot.DBSnapshotIdentifier.replace(':', '-');
-    console.log("new snapshotIdentifier:", snapshotIdentifier);
-
-    const databaseName = latestSnapshot.DBInstanceIdentifier;
-    console.log("databaseName:", databaseName);
-
-    const startExportTaskParams = {
-      ExportTaskIdentifier: snapshotIdentifier.substr(0, 60),
-      IamRoleArn: iamRoleArn,
-      KmsKeyId: kmsKeyId,
-      S3BucketName: s3BucketName,
-      SourceArn: latestSnapshot.DBSnapshotArn,
-      S3Prefix: `${databaseName}/${snapshotIdentifier}`,
-    };
-    let response = await rds.startExportTask(startExportTaskParams).promise();
-    console.log(response);
+      let response = await rds.startExportTask(startExportTaskParams).promise();
+      console.log(response);
+  });
 };
