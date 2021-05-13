63e66ce0a23d0576532d0d7cafff56ea57e6edc5
joates-madetech <80255046+joates-madetech@users.noreply.github.com>
2021-05-13 16:29:23 +0100

    Added new spreadsheet
    Added a new spreadsheet.

--- terraform/22-aws-glue-jobs.tf
+++ terraform/22-aws-glue-jobs.tf
@@ -33,3 +33,21 @@ module "housing_repair_data" {
   output_folder_name              = "repair-orders"
   enable_glue_trigger             = false
 }
+    
+ module "order_form_data" {
+  count = terraform.workspace == "default" ? 1 : 0
+  source                          = "../modules/google-sheets-glue-job"
+  glue_role_arn                   = aws_iam_role.glue_role.arn
+  glue_scripts_bucket_id          = module.glue_scripts.bucket_id
+  glue_temp_storage_bucket_id     = module.glue_temp_storage.bucket_arn
+  google_sheets_import_script_key = aws_s3_bucket_object.google_sheets_import_script.key
+  landing_zone_bucket_id          = module.landing_zone.bucket_id
+  sheets_credentials_name         = aws_secretsmanager_secret.sheets_credentials_housing.name
+  tags                            = module.tags.values
+  glue_job_name                   = "Order Form Example"
+  google_sheets_document_id       = "1Xc6dcQVQt1ERUjWpfxpRX5QtuSWdsgC6Swtz26VFJfI"
+  google_sheets_worksheet_name    = "Form responses 1"
+  department_folder_name          = "test"
+  output_folder_name              = "order-form"
+  enable_glue_trigger             = false
+}
