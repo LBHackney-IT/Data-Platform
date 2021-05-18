14bac681d009a3cb7226a9f60ef1bae1ab00feba
b-dalton <ben.dalton@madetech.com>
2021-05-18 09:35:04 +0100

    Create partition for database table export into s3
    Partition is target_path/database_name/table_name/import_year/import_month/import_day
    
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: joates-madetech <james.oates@madetech.com>
    Co-authored-by: elena-vi <elena@madetech.com>

--- lambdas/s3_to_s3_export_copier/index.js
+++ lambdas/s3_to_s3_export_copier/index.js
@@ -4,7 +4,7 @@ const AWS_REGION = "eu-west-2";
 
 let bucketDestination = process.env.BUCKET_DESTINATION;
 
-async function s3CopyFolder(s3Client, sourceBucketName, sourcePath, targetBucketName, targetPath) {
+async function s3CopyFolder(s3Client, sourceBucketName, sourcePath, targetBucketName, targetPath, snapshotTime) {
   console.log("sourceBucketName", sourceBucketName);
   console.log("targetBucketName", targetBucketName);
   console.log("sourcePath", sourcePath);
@@ -17,18 +17,28 @@ async function s3CopyFolder(s3Client, sourceBucketName, sourcePath, targetBucket
     Prefix: sourcePath
   };
   const listResponse = await s3Client.listObjectsV2(listObjectsParams).promise();
+  console.log("snapshot time full", snapshotTime);
+  const [year, month, day] = [snapshotTime.getFullYear(), snapshotTime.getMonth(), snapshotTime.getDay()];
 
   //console.log("list response", listResponse);
   console.log("list response contents", listResponse.Contents);
 
   await Promise.all(
     listResponse.Contents.map(async (file) => {
+      if (!file.Key.endsWith("parquet")){
+        return;
+      }
+      const [snapShotName, databaseName, tableName] = file.Key.split("/", 3);
+
+      const fileKey = file.Key.substr(`${snapShotName}/${databaseName}/${tableName}`.length + 1);
+      console.log("fileKey====", fileKey);
       const copyObjectParams = {
         Bucket: targetBucketName,
         CopySource: `${sourceBucketName}/${file.Key}`,
-        Key: `${targetPath}/${file.Key}`,
+        Key: `${targetPath}/${databaseName}/table_name=${tableName}/${year}/${month}/${day}/`,
       };
-      console.log(copyObjectParams)
+      console.log("copyObjectParams",copyObjectParams)
+
       await s3Client.copyObject(copyObjectParams).promise().catch(console.log);
     })
   );
@@ -78,9 +88,10 @@ exports.handler = async (events) => {
       const sourceBucketName = message.ExportBucket
       const targetBucketName = bucketDestination;
       const pathPrefix = `${message.ExportTaskIdentifier}`;
+      const snapshotTime = exportTaskStatus.SnapshotTime;
 
       // If it has copy the files from s3 bucket A => s3 bucket B
-      await s3CopyFolder(s3Client, sourceBucketName, pathPrefix, targetBucketName, 'rds-backups');
+      await s3CopyFolder(s3Client, sourceBucketName, pathPrefix, targetBucketName, 'housing', snapshotTime);
     })
   );
 };
