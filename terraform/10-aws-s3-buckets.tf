8d98b0f168b8e624cbb1ba8c4b066a6af963a794
maysakanoni <maysa@madetech.com>
2021-05-26 14:05:07 +0100

    remove references to db_snapshot_to_s3 module

--- terraform/10-aws-s3-buckets.tf
+++ terraform/10-aws-s3-buckets.tf
@@ -1,72 +1,72 @@
 module "landing_zone" {
-  source                         = "../modules/s3-bucket"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  bucket_name                    = "Landing Zone"
-  bucket_identifier              = "landing-zone"
-  role_arns_to_share_access_with = [
-    module.db_snapshot_to_s3.s3_to_s3_copier_lambda_role_arn
-  ]
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Landing Zone"
+  bucket_identifier = "landing-zone"
+  # role_arns_to_share_access_with = [
+  #   module.db_snapshot_to_s3.s3_to_s3_copier_lambda_role_arn
+  # ]
 }
 
 module "raw_zone" {
-  source                         = "../modules/s3-bucket"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  bucket_name                    = "Raw Zone"
-  bucket_identifier              = "raw-zone"
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Raw Zone"
+  bucket_identifier = "raw-zone"
 }
 
 module "refined_zone" {
-  source                         = "../modules/s3-bucket"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  bucket_name                    = "Refined Zone"
-  bucket_identifier              = "refined-zone"
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Refined Zone"
+  bucket_identifier = "refined-zone"
 }
 
 module "trusted_zone" {
-  source                         = "../modules/s3-bucket"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  bucket_name                    = "Trusted Zone"
-  bucket_identifier              = "trusted-zone"
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Trusted Zone"
+  bucket_identifier = "trusted-zone"
 }
 
 module "glue_scripts" {
-  source                         = "../modules/s3-bucket"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  bucket_name                    = "Glue Scripts"
-  bucket_identifier              = "glue-scripts"
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Glue Scripts"
+  bucket_identifier = "glue-scripts"
 }
 
 module "glue_temp_storage" {
-  source                         = "../modules/s3-bucket"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  bucket_name                    = "Glue Temp Storage"
-  bucket_identifier              = "glue-temp-storage"
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Glue Temp Storage"
+  bucket_identifier = "glue-temp-storage"
 }
 
 module "athena_storage" {
-  source                         = "../modules/s3-bucket"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  bucket_name                    = "Athena Storage"
-  bucket_identifier              = "athena-storage"
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Athena Storage"
+  bucket_identifier = "athena-storage"
 }
