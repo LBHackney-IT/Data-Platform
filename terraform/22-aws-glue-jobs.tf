7b2729a6d7fc1770a42fa650e2cffe9f3e7866da
daroclark <daro.clark@hackney.gov.uk>
2021-05-26 14:15:29 +0100

    Update 22-aws-glue-jobs.tf
    Added the purdy repair data

--- terraform/22-aws-glue-jobs.tf
+++ terraform/22-aws-glue-jobs.tf
@@ -68,3 +68,20 @@ module "housing_repair_data" {
   department_folder_name          = "housing"
   output_folder_name              = "test-repairs-herts-heritage"
 }
+    
+      module "test-repairs-purdy-data" {
+  count = terraform.workspace == "default" ? 1 : 0
+  source                          = "../modules/google-sheets-glue-job"
+  glue_role_arn                   = aws_iam_role.glue_role.arn
+  glue_scripts_bucket_id          = module.glue_scripts.bucket_id
+  glue_temp_storage_bucket_id     = module.glue_temp_storage.bucket_arn
+  google_sheets_import_script_key = aws_s3_bucket_object.google_sheets_import_script.key
+  landing_zone_bucket_id          = module.landing_zone.bucket_id
+  sheets_credentials_name         = aws_secretsmanager_secret.sheets_credentials_housing.name
+  tags                            = module.tags.values
+  glue_job_name                   = "Test Purdy Repair"
+  google_sheets_document_id       = "1-PpKPnaPMA6AogsNXT5seqQk3VUB-naFnFJYhROkl2o"
+  google_sheets_worksheet_name    = "FormresponsesPUR"
+  department_folder_name          = "housing"
+  output_folder_name              = "test-repairs-purdy"
+}
