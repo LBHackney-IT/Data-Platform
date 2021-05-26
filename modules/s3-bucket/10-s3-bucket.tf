7d58d82817d7cdb4f0fd815a3ccaa84b95222745
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-05-26 11:32:27 +0100

    Revert prod api deploy and remove resources with reference to account number (#70)
    * Reset deployment to StagingAPI in stg workflow
    
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    
    * Comment out resources so remove infrastructure
    
    from stg api account with references to the account number so that we
    can redeploy resources for prod api account
    
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    
    Co-authored-by: b-dalton <ben.dalton@madetech.com>

--- modules/s3-bucket/10-s3-bucket.tf
+++ modules/s3-bucket/10-s3-bucket.tf
@@ -14,13 +14,13 @@ data "aws_iam_policy_document" "key_policy" {
       "*"
     ]
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = local.default_arn
     }
   }
 
   statement {
-    sid = "CrossAccountShare"
+    sid    = "CrossAccountShare"
     effect = "Allow"
     actions = [
       "kms:Encrypt",
@@ -35,31 +35,31 @@ data "aws_iam_policy_document" "key_policy" {
       "*"
     ]
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = concat(var.role_arns_to_share_access_with, local.default_arn)
     }
   }
 }
 
-resource "aws_kms_key" "key" {
-  tags = var.tags
+# resource "aws_kms_key" "key" {
+#   tags = var.tags
 
-  description             = "${var.project} ${var.environment} - ${var.bucket_name} Bucket Key"
-  deletion_window_in_days = 10
-  enable_key_rotation     = true
+#   description             = "${var.project} ${var.environment} - ${var.bucket_name} Bucket Key"
+#   deletion_window_in_days = 10
+#   enable_key_rotation     = true
 
-  policy = data.aws_iam_policy_document.key_policy.json
-}
+#   policy = data.aws_iam_policy_document.key_policy.json
+# }
 
 
 resource "aws_kms_alias" "key_alias" {
-  name = lower("alias/${var.identifier_prefix}-s3-${var.bucket_identifier}")
+  name          = lower("alias/${var.identifier_prefix}-s3-${var.bucket_identifier}")
   target_key_id = aws_kms_key.key.key_id
 }
 
 data "aws_iam_policy_document" "bucket_policy_document" {
   statement {
-    sid = "CrossAccountShare"
+    sid    = "CrossAccountShare"
     effect = "Allow"
     actions = [
       "s3:*"
@@ -69,7 +69,7 @@ data "aws_iam_policy_document" "bucket_policy_document" {
       "${aws_s3_bucket.bucket.arn}/*"
     ]
     principals {
-      type = "AWS"
+      type        = "AWS"
       identifiers = concat(var.role_arns_to_share_access_with, local.default_arn)
     }
   }
@@ -91,7 +91,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.bucket.id
+  bucket     = aws_s3_bucket.bucket.id
   depends_on = [aws_s3_bucket.bucket]
 
   block_public_acls       = true
@@ -101,7 +101,7 @@ resource "aws_s3_bucket_public_access_block" "block_public_access" {
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.bucket_policy_document.json
+  bucket     = aws_s3_bucket.bucket.id
+  policy     = data.aws_iam_policy_document.bucket_policy_document.json
   depends_on = [aws_s3_bucket_public_access_block.block_public_access]
 }
