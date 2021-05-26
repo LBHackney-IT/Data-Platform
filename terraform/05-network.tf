c207e3a6dc2a782dacdeeed47bb1a3c05b919c4e
el <elena@madetech.com>
2021-05-26 16:49:09 +0100

    rename redshift to network as it sets up a transit gateway and make transit gateway out put vpc_id and subnet ids
    Co-authored-by: Emma <emma@madetech.com>

--- terraform/05-network.tf
+++ terraform/05-network.tf
@@ -1,10 +1,9 @@
 module "transit_gateway" {
-  count = terraform.workspace == "default" ? 1 : 0
   source               = "../modules/transit-gateway"
   core_azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
   core_cidr            = var.transit_gateway_cidr
   core_private_subnets = var.transit_gateway_private_subnets
-  application          = var.application
+  application          = local.identifier_prefix
   department           = var.department
   environment          = var.environment
 }
