bda3a36b478471aa0e44a2e6113b73a83f8f0ed3
el <elena@madetech.com>
2021-06-02 15:42:01 +0100

    asks for ecr login in a diffrent way

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,6 +4,6 @@ ecr_url=$(terraform output -raw ecr_repository_worker_endpoint)
 
 docker build -f ../docker/Dockerfile -t $ecr_url ../docker
 
-aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $ecr_url
+docker login -u AWS -p $(aws ecr get-login-password --region eu-west-2) $ecr_url
 
 docker push $ecr_url
