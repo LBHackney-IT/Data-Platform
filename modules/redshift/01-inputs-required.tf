8ffd7c8bd05d79c45c8975780f6f0f685d056566
el <elena@madetech.com>
2021-06-10 13:18:39 +0100

    Redshift (#105)
    * add redsift cluster
    
    currect error:
    Error: InvalidVPCNetworkStateFault: Cannot create a publicly accessible cluster because customer VPC vpc-076a7c128ec4a2cf9 has no internet gateway attached.
    	status code: 400, request id: 9d953180-c7de-41ab-8e67-099b3b2a969d
    
      on 70-redshift.tf line 83, in resource "aws_redshift_cluster" "redshift_cluster":
      83: resource "aws_redshift_cluster" "redshift_cluster" {
    
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    Co-authored-by: maysakanoni <maysa@madetech.com>
    
    * Create internet gateway and attach to VPC, make rds cluster not publicly accessible
    
    To do: make it so that it only deploys the rds cluster in staging
    
    Co-authored-by: elena-vi <elena@madetech.com>
    Co-authored-by: maysakanoni <maysa@madetech.com>
    
    * move redshift into module and only deploy in staging and prod
    
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    Co-authored-by: maysakanoni <maysa@madetech.com>
    
    * remove unnecessary internet gateway
    
    Co-authored-by: elena <elena@madetech.com>
    
    * Remove  unnessecary action from redshift policy
    
    Co-authored-by: elena <elena@madetech.com>
    
    * Add final snapshot identifier to redshift cluster
    
    Co-authored-by: elena <elena@madetech.com>
    
    * fix typo
    
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    Co-authored-by: maysakanoni <maysa@madetech.com>

---
+++ modules/redshift/01-inputs-required.tf
@@ -0,0 +1,59 @@
+variable "tags" {
+  description = "AWS tags"
+  type        = map(string)
+}
+
+variable "identifier_prefix" {
+  description = "Prefix"
+  type        = string
+}
+
+variable "subnet_ids_list" {
+  description = "List of subnet ids"
+  type        = list(string)
+}
+
+variable "vpc_id" {
+  description = "Id of vpc"
+  type        = string
+}
+
+variable "landing_zone_bucket_arn" {
+  description = "ARN of landing zone bucket"
+  type        = string
+}
+
+variable "refined_zone_bucket_arn" {
+  description = "ARN of refined zone bucket"
+  type        = string
+}
+
+variable "trusted_zone_bucket_arn" {
+  description = "ARN of trusted zone bucket"
+  type        = string
+}
+
+variable "raw_zone_bucket_arn" {
+  description = "ARN of raw zone bucket"
+  type        = string
+}
+
+variable "landing_zone_kms_key_arn" {
+  description = "ARN of landing zone KMS key"
+  type        = string
+}
+
+variable "refined_zone_kms_key_arn" {
+  description = "ARN of refined zone KMS key"
+  type        = string
+}
+
+variable "trusted_zone_kms_key_arn" {
+  description = "ARN of trusted zone KMS key"
+  type        = string
+}
+
+variable "raw_zone_kms_key_arn" {
+  description = "ARN of raw zone KMS key"
+  type        = string
+}
---
+++ terraform/70-redshift.tf
@@ -0,0 +1,17 @@
+module "redshift" {
+  count = terraform.workspace == "default" ? 1 : 0
+
+  source                   = "../modules/redshift"
+  tags                     = module.tags.values
+  identifier_prefix        = local.identifier_prefix
+  subnet_ids_list          = local.subnet_ids_list
+  vpc_id                   = data.aws_vpc.network.id
+  landing_zone_bucket_arn  = module.landing_zone.bucket_arn
+  refined_zone_bucket_arn  = module.refined_zone.bucket_arn
+  trusted_zone_bucket_arn  = module.trusted_zone.bucket_arn
+  raw_zone_bucket_arn      = module.raw_zone.bucket_arn
+  landing_zone_kms_key_arn = module.landing_zone.kms_key_arn
+  raw_zone_kms_key_arn     = module.raw_zone.kms_key_arn
+  refined_zone_kms_key_arn = module.refined_zone.kms_key_arn
+  trusted_zone_kms_key_arn = module.trusted_zone.kms_key_arn
+}
