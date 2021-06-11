00446ad641f84e444f4c3ad69bb821d423e2dac0
Adrian Clay <adrian@madetech.com>
2021-06-11 11:33:41 +0100

    Crawl and detect the folders to transform automatically
    Co-authored-by: Emma Corbett <emma@madetech.com>

--- scripts/manually_uploaded_parking_data_to_raw.py
+++ scripts/manually_uploaded_parking_data_to_raw.py
@@ -7,7 +7,9 @@ from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.sql import functions as f
 from awsglue.dynamicframe import DynamicFrame
+import boto3
 
+s3_client = boto3.client('s3')
 sc = SparkContext()
 glue_context = GlueContext(sc)
 
@@ -25,54 +27,56 @@ def remove_empty_columns(data_frame):
 
 def add_import_time_columns(data_frame):
     columns_added = data_frame.withColumn('import_date', f.current_timestamp())\
-      .withColumn('import_timestamp', f.lit(str(now.timestamp())))\
+      .withColumn('import_timestamp', f.lit(now.timestamp()))\
       .withColumn('import_year', f.lit(str(now.year)))\
       .withColumn('import_month', f.lit(str(now.month).zfill(2)))\
       .withColumn('import_day', f.lit(str(now.day).zfill(2)))
     return columns_added
 
-def data_source_landing_to_raw(bucketSource, bucketTarget, object_path, glue_context):
+def data_source_landing_to_raw(bucket_source, bucket_target, key, glue_context):
   data_source = glue_context.create_dynamic_frame.from_options(
     format_options = {"quoteChar":"\"","escaper":"","withHeader":True,"separator":","},
     connection_type = "s3",
     format = "csv",
-    connection_options = {"paths": [bucketSource + "/" + object_path]},
+    connection_options = {"paths": [bucket_source + "/" + key]},
     transformation_ctx = "data_source"
   )
-  logger.info(f"Retrieved data source from s3 path {bucketSource}/{object_path}")
+  logger.info(f"Retrieved data source from s3 path {bucket_source}/{key}")
 
-  logger.info("Converted into data frame")
-  data_frame = data_source.toDF()
-  data_frame = remove_empty_columns(data_frame)
-  logger.info("Using Columns:")
-  logger.info(str(data_frame.columns))
+  data_frame = remove_empty_columns(data_source.toDF())
+  logger.info("Using Columns: " + str(data_frame.columns))
   data_frame = add_import_time_columns(data_frame)
 
   data_output = DynamicFrame.fromDF(data_frame, glue_context, "cedar_data")
 
-  logger.info("Converted back into dynamic frame. Writing to s3...")
+  logger.info(f"Writing to s3 path {bucket_target}/{key}")
 
   glue_context.write_dynamic_frame.from_options(frame = data_output,
     connection_type = "s3",
     format = "parquet",
-    connection_options = {"path": bucketTarget + "/" + object_path, "partitionKeys": ["import_year" ,"import_month" ,"import_day"]},
+    connection_options = {"path": bucket_target + "/" + key, "partitionKeys": ["import_year" ,"import_month" ,"import_day"]},
     transformation_ctx = "data_sink"
   )
-  logger.info(f"Finished writing to s3 at path {bucketTarget}/{object_path}")
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
 job = Job(glue_context)
 job.init(args['JOB_NAME'], args)
 
-s3BucketTarget = get_glue_env_var('s3_bucket_target', '')
-s3BucketSource = get_glue_env_var('s3_bucket_source', '')
+s3_bucket_target = get_glue_env_var('s3_bucket_target', '')
+s3_bucket_source = get_glue_env_var('s3_bucket_source', '')
 
+folders = s3_client.list_objects_v2(
+    Bucket=s3_bucket_source,
+    Delimiter='/',
+    Prefix='parking/manual/'
+)
 
-logger.info("Starting parking/manual/cedar-markets-expenditure/")
-data_source_landing_to_raw(s3BucketSource, s3BucketTarget, "parking/manual/cedar-markets-expenditure/", glue_context)
+for folder in folders['CommonPrefixes']:
+  key = folder['Prefix']
 
-logger.info("Starting parking/manual/cedar-markets-income/")
-data_source_landing_to_raw(s3BucketSource, s3BucketTarget, "parking/manual/cedar-markets-income/", glue_context)
+  logger.info(f"Starting {key}")
+  data_source_landing_to_raw("s3://" + s3_bucket_source, "s3://" + s3_bucket_target, key, glue_context)
+  logger.info(f"Finished {key}")
 
 job.commit()
