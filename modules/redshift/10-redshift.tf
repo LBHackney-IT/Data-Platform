5b4f592ea489b76425e54d7341437224dd3a606d
b-dalton <ben.dalton@madetech.com>
2021-06-14 16:11:56 +0100

    Create security group for redshift cluster
    Allows traffic from tcp port 5439
    
    Co-authored-by: maysakanoni <maysa@madetech.com>
    Co-authored-by: joates-madetech <james.oates@madetech.com>

--- modules/redshift/10-redshift.tf
+++ modules/redshift/10-redshift.tf
@@ -79,6 +79,7 @@ resource "aws_redshift_cluster" "redshift_cluster" {
   cluster_subnet_group_name = aws_redshift_subnet_group.redshift.name
   publicly_accessible       = false
   final_snapshot_identifier = "${var.identifier_prefix}-redshift-cluster-final"
+  vpc_security_group_ids    = [aws_security_group.redshift_cluster_security_group.id]
   tags                      = var.tags
 
 }
@@ -89,4 +90,20 @@ resource "aws_redshift_subnet_group" "redshift" {
 
   tags = var.tags
 
+}
+
+resource "aws_security_group" "redshift_cluster_security_group" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 5439
+    to_port          = 5439
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
 }
