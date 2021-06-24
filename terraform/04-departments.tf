739a3a794c3f24a621622271f166aaca5c80e17a
James Oates <james.oates@madetech.com>
2021-06-24 12:01:20 +0100

    Replaced - in terraform resource name with _

--- terraform/04-departments.tf
+++ terraform/04-departments.tf
@@ -1,4 +1,4 @@
-module "department_housing-repairs" {
+module "department_housing_repairs" {
   source                 = "../modules/department"
   tags                   = module.tags.values
   identifier_prefix      = local.short_identifier_prefix
