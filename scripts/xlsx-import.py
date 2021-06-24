d4a569090a91f73340b324fff86497ba7719142a
el <elena@madetech.com>
2021-06-24 10:04:49 +0100

    clean up

--- scripts/xlsx-import.py
+++ scripts/xlsx-import.py
@@ -22,35 +22,12 @@ def get_glue_env_var(key, default="none"):
     else:
         return default
 
-def get_secret(logger, secret_name, region_name):
-    session = boto3.session.Session()
-
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    get_secret_value_response = client.get_secret_value(
-        SecretId=secret_name
-    )
-
-    if 'SecretString' in get_secret_value_response:
-        return get_secret_value_response['SecretString']
-    else:
-        return get_secret_value_response['SecretBinary'].decode('ascii')
-
-
 ## @params: [JOB_NAME]
 s3_bucket_source = get_glue_env_var('s3_bucket_source', '')
 s3_bucket_target = get_glue_env_var('s3_bucket_target', '')
 header_row_number = get_glue_env_var('header_row_number', 0)
-googleSheetsDocumentKey = get_glue_env_var('document_key', '')
 worksheet_name = get_glue_env_var('worksheet_name', '')
 logger = glueContext.get_logger()
-
-awsSecret = get_secret(logger, get_glue_env_var('secret_id', ''), "eu-west-2")
-googleSheetsJsonCredentials = json.loads(awsSecret)
-
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
@@ -60,22 +37,8 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-
-spreadsheetId = "###"  # Please set the Spreadsheet ID.
-
-scopes = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-googleSheetsCredentials = service_account.Credentials.from_service_account_info(googleSheetsJsonCredentials)
-googleSheetsScopedCredentials = googleSheetsCredentials.with_scopes(scopes)
-googleSheetsClient = gspread.authorize(googleSheetsScopedCredentials)
-
-access_token = googleSheetsClient.auth.token
-url = "https://www.googleapis.com/drive/v3/files/" + spreadsheetId + "/export?mimeType=application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet"
-res = requests.get(url, headers={"Authorization": "Bearer " + access_token})
-# book = openpyxl.load_workbook(filename=BytesIO(res.content), data_only=False)
-# hd_sheet = book.active
-
 panada_df = pd.read_excel(
-    filename=BytesIO(res.content),
+    s3_bucket_source,
     engine='openpyxl',
     skiprows=range(0, int(header_row_number)),
     sheet_name=worksheet_name
