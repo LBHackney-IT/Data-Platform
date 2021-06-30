3f21c3c71262f958fe88c694292e5d84820554df
b-dalton <ben.dalton@madetech.com>
2021-06-30 15:20:40 +0100

    remove spirit animal output

--- terraform/99-outputs.tf
+++ terraform/99-outputs.tf
@@ -18,11 +18,6 @@ output "network_vpc_subnet_cider_blocks" {
   value = [for subnet in data.aws_subnet.network : subnet.cidr_block]
 }
 
-output "spirit_animal" {
-  description = "Your terraform assigned spirit animal"
-  value       = random_pet.name.id
-}
-
 output "ecr_repository_worker_endpoint" {
   value = module.liberator_to_parquet.ecr_repository_worker_endpoint
 }
