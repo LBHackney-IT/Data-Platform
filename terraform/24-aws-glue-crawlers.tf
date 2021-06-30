862ee85b5e283ae792993da70d9187eda29328a0
b-dalton <ben.dalton@madetech.com>
2021-06-30 12:25:50 +0100

    add crawler for repairs dlo with cleaned addresses

--- terraform/24-aws-glue-crawlers.tf
+++ terraform/24-aws-glue-crawlers.tf
@@ -129,4 +129,25 @@ resource "aws_glue_crawler" "refined_zone_housing_repairs_repairs_dlo_cleaned_cr
   })
 }
 
+resource "aws_glue_crawler" "refined_zone_housing_repairs_repairs_dlo_with_cleaned_addresses_crawler" {
+  tags = module.tags.values
+
+  database_name = module.department_housing_repairs.refined_zone_catalog_database_name
+  name          = "${local.identifier_prefix}-refined-zone-housing-repairs-repairs-dlo-with-cleaned-addresses"
+  role          = aws_iam_role.glue_role.arn
+  table_prefix  = "housing_repairs_repairs_dlo_with_cleaned_addresses"
+
+  s3_target {
+    path       = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-dlo/with-cleaned-addresses/"
+    exclusions = local.glue_crawler_excluded_blobs
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 4
+    }
+  })
+}
+
 
