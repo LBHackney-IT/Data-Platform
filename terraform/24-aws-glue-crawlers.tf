0eb0a135061867dab0f79e1e8aa3c4441552bf78
b-dalton <ben.dalton@madetech.com>
2021-07-02 13:53:34 +0100

    create database and crawler for addresses api data

--- terraform/24-aws-glue-crawlers.tf
+++ terraform/24-aws-glue-crawlers.tf
@@ -53,8 +53,29 @@ resource "aws_glue_trigger" "landing_zone_liberator_crawler_trigger" {
 }
 
 // ==== RAW ZONE ===========
-resource "aws_glue_catalog_database" "raw_zone_catalog_database" {
-  name = "${local.identifier_prefix}-raw-zone-database"
+resource "aws_glue_catalog_database" "raw_zone_unrestricted_address_api" {
+  name = "${local.identifier_prefix}-raw-zone-unrestricted-address-api"
+}
+
+resource "aws_glue_crawler" "raw_zone_unrestricted_address_api_crawler" {
+  tags = module.tags.values
+
+  database_name = aws_glue_catalog_database.raw_zone_unrestricted_address_api
+  name          = "${local.identifier_prefix}-raw-zone-unrestricted-address-api"
+  role          = aws_iam_role.glue_role.arn
+  table_prefix  = "unrestricted_address_api_"
+
+  s3_target {
+    path       = "s3://${module.raw_zone.bucket_id}/unrestricted/addresses_api/"
+    exclusions = local.glue_crawler_excluded_blobs
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 4
+    }
+  })
 }
 
 resource "aws_glue_catalog_database" "raw_zone_parking_manual_uploads" {
