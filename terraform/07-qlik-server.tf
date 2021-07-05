3a5f576f59090370f1458b6560ebe7b768b9ebec
joates-madetech <80255046+joates-madetech@users.noreply.github.com>
2021-07-05 16:40:56 +0100

    Added a module that deploys a Windows Server for Qlik Sense. (#176)

---
+++ terraform/07-qlik-server.tf
@@ -0,0 +1,10 @@
+module "qlik_server" {
+  count = local.is_live_environment ? 1 : 0
+
+  source            = "../modules/qlik-sense-server"
+  tags              = module.tags.values
+  vpc_id            = data.aws_vpc.network.id
+  vpc_subnet_ids    = local.subnet_ids_list
+  instance_type     = var.qlik_server_instance_type
+  identifier_prefix = local.identifier_prefix
+}
