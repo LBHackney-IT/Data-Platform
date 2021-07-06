2ea2cf2183e129f4648f03b9148cd48ea7da8db2
elcorbs <emma@madetech.com>
2021-07-06 10:16:28 +0100

    Move get_s3_folders to the helpers script module

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -53,3 +53,21 @@ def convert_pandas_df_to_spark_dynamic_df(sql_context, panadas_df):
     spark_df = add_import_time_columns(spark_df)
 
     return spark_df
+
+def get_s3_subfolders(s3_client, bucket_name, prefix):
+  there_are_more_objects_in_the_bucket_to_fetch = True
+  folders = []
+  continuation_token = {}
+  while there_are_more_objects_in_the_bucket_to_fetch:
+    list_objects_response = s3_client.list_objects_v2(
+      Bucket=bucket_name,
+      Delimiter='/',
+      Prefix=prefix,
+      **continuation_token
+    )
+
+    folders.extend(x['Prefix'] for x in list_objects_response.get('CommonPrefixes', []))
+    there_are_more_objects_in_the_bucket_to_fetch = list_objects_response['IsTruncated']
+    continuation_token['ContinuationToken'] = list_objects_response.get('NextContinuationToken')
+
+  return set(folders)
