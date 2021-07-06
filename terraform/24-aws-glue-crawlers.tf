ab2bac35c66a2896a55f1d2239e7272ad31b6287
el <elena@madetech.com>
2021-07-06 10:31:41 +0100

    Add dlo cleaning job triggers for live only
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    Co-authored-by: maysakanoni <maysa@madetech.com>

--- terraform/24-aws-glue-crawlers.tf
+++ terraform/24-aws-glue-crawlers.tf
@@ -192,30 +192,34 @@ resource "aws_glue_crawler" "refined_zone_housing_repairs_repairs_dlo_with_clean
   })
 }
 resource "aws_glue_trigger" "housing_repairs_repairs_dlo_cleaning_job" {
+  count = local.is_live_environment ? 1 : 0
+
   name          = "${local.identifier_prefix}-housing-repairs-repairs-dlo-cleaning-job-trigger"
   type          = "CONDITIONAL"
-  workflow_name = module.repairs_dlo.workflow_name
+  workflow_name = module.repairs_dlo[0].workflow_name
 
   predicate {
     conditions {
-      crawler_name = module.repairs_dlo.crawler_name
+      crawler_name = module.repairs_dlo[0].crawler_name
       crawl_state  = "SUCCEEDED"
     }
   }
 
   actions {
-    job_name = aws_glue_job.repairs_dlo_cleaning.*.name
+    job_name = aws_glue_job.repairs_dlo_cleaning[0].name
   }
 }
 
 resource "aws_glue_trigger" "housing_repairs_repairs_dlo_cleaning_crawler" {
+  count = local.is_live_environment ? 1 : 0
+
   name          = "${local.identifier_prefix}-housing-repairs-repairs-dlo-cleaning-crawler-trigger"
   type          = "CONDITIONAL"
-  workflow_name = module.repairs_dlo.workflow_name
+  workflow_name = module.repairs_dlo[0].workflow_name
 
   predicate {
     conditions {
-      job_name = aws_glue_job.repairs_dlo_cleaning.*.name
+      job_name = aws_glue_job.repairs_dlo_cleaning[0].name
       state    = "SUCCEEDED"
     }
   }
@@ -245,4 +249,3 @@ resource "aws_glue_crawler" "refined_zone_housing_repairs_repairs_dlo_with_match
     }
   })
 }
-
