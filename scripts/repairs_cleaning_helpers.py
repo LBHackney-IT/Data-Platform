765d709e56eddc5ac148a08c519b4afc85005bd2
Maysa Kanoni <46002877+maysakanoni@users.noreply.github.com>
2021-07-07 15:39:42 +0100

    Repairs purdy cleaning job (#193)
    * add repairs purdy script to notebook
    
    * add repairs cleaning helper file
    
    * add repairs purdy jobs,script, crawlers & workflow
    
    * update notebook script
    
    * update script
    
    * give method more descripttive name
    
    * add partition keys form helper function

---
+++ scripts/repairs_cleaning_helpers.py
@@ -0,0 +1,31 @@
+
+import pyspark.sql.functions as F
+from pyspark.sql.types import StringType
+import re
+
+
+def map_repair_priority(code):
+    if code == 'Immediate':
+        return 1
+    elif code == 'Emergency':
+        return 2
+    elif code == 'Urgent':
+        return 3
+    elif code == 'Normal':
+        return 4
+    else:
+        return None
+
+
+# # convert to a UDF Function by passing in the function and the return type of function (string in this case)
+udf_map_repair_priority = F.udf(map_repair_priority, StringType())
+
+
+def remove_multiple_and_trailing_underscores_and_lowercase(df):
+    # remove trialing underscores
+    df = df.select([F.col(col).alias(re.sub("_$", "", col))
+                   for col in df.columns])
+    # lowercase and remove double underscores
+    df2 = df.select([F.col(col).alias(
+        re.sub("[^0-9a-zA-Z$]+", "_", col.lower())) for col in df.columns])
+    return df2
