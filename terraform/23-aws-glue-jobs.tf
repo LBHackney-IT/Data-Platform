be746b5a289941bff649369c5714a47d55949ca8
el <elena@madetech.com>
2021-07-07 16:00:13 +0100

    move xlsx imports to seprate file

--- terraform/23-aws-glue-jobs.tf
+++ terraform/23-aws-glue-jobs.tf
@@ -1,37 +1,3 @@
-module "repairs_fire_alarm_aov" {
-  count = local.is_live_environment ? 1 : 0
-
-  source                         = "../modules/import-xlsx-file-from-g-drive"
-  glue_role_arn                  = aws_iam_role.glue_role.arn
-  glue_scripts_bucket_id         = module.glue_scripts.bucket_id
-  glue_catalog_database_name     = module.department_housing_repairs.raw_zone_catalog_database_name
-  glue_temp_storage_bucket_id    = module.glue_temp_storage.bucket_arn
-  helpers_script_key             = aws_s3_bucket_object.helpers.key
-  xlsx_import_script_key         = aws_s3_bucket_object.xlsx_import_script.key
-  identifier_prefix              = local.identifier_prefix
-  lambda_artefact_storage_bucket = module.lambda_artefact_storage.bucket_id
-  landing_zone_bucket_id         = module.landing_zone.bucket_id
-  landing_zone_kms_key_arn       = module.landing_zone.kms_key_arn
-  landing_zone_bucket_arn        = module.landing_zone.bucket_arn
-  tags                           = module.tags.values
-  google_sheets_document_id      = "1VlM80P6J8N0P3ZeU8VobBP9kMbpr1Lzq"
-  glue_job_name                  = "repairs spreadsheet"
-  department_folder_name         = "housing"
-  output_folder_name             = "repairs_spreadsheet"
-  raw_zone_bucket_id             = module.raw_zone.bucket_id
-  input_file_name                = "electrical_mechnical_fire_safety_temp_order_number_wc_12.10.20r1.xlsx"
-  worksheets = {
-    sheet1 : {
-      header_row_number = 1
-      worksheet_name    = "Fire AlarmAOV"
-    }
-    sheet2 : {
-      header_row_number = 1
-      worksheet_name    = "Door Entry"
-    }
-  }
-}
-
 resource "aws_glue_job" "address_matching_glue_job" {
   count = local.is_live_environment ? 1 : 0
 
