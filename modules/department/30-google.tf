071eeca257c0a3e951c600e52ed461f9ab84bb61
joates-madetech <james.oates@madetech.com>
2021-07-12 15:22:35 +0100

    Changed to short identifier prefix to resolve name length issue

--- modules/department/30-google.tf
+++ modules/department/30-google.tf
@@ -4,7 +4,7 @@ module "google_service_account" {
   source                     = "../google-service-account"
   is_live_environment        = var.is_live_environment
   department_name            = local.department_identifier
-  identifier_prefix          = var.identifier_prefix
+  identifier_prefix          = var.short_identifier_prefix
   application                = var.application
   google_project_id          = data.google_project.project.project_id
   secrets_manager_kms_key_id = var.secrets_manager_kms_key.key_id
