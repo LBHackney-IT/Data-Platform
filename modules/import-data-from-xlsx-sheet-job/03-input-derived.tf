1cb1db9f2fc2091f01c13af9e8b2dc8b8fc793ba
joates-madetech <james.oates@madetech.com>
2021-07-13 14:54:13 +0100

    Refactored a few names around xlsx file ingestion

---
+++ modules/import-data-from-xlsx-sheet-job/03-input-derived.tf
@@ -0,0 +1,5 @@
+locals {
+  worksheet_key = lower(replace(trimspace(var.worksheet_name), " ", "-")),
+  import_name = "${var.department_folder_name}-${local.worksheet_key}"
+  s3_output_path = "s3://${var.raw_zone_bucket_id}/${var.department_folder_name}/${var.output_folder_name}"
+}
--- modules/import-data-from-xlsx-sheet-job/03-interpolated-defaults.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  import_name = "${var.department_folder_name}-${lower(replace(var.worksheet_name, " ", "-"))}"
-}
