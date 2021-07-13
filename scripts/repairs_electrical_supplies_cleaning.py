764fb8d8c1d09143202000895376f650a88fcbe7
b-dalton <ben.dalton@madetech.com>
2021-07-13 16:10:15 +0100

    add repairs_electrical_supplies_cleaning glue job, crawler and triggers

---
+++ scripts/repairs_electrical_supplies_cleaning.py
@@ -0,0 +1,69 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+import pyspark.sql.functions as F
+from awsglue.dynamicframe import DynamicFrame
+import re
+from helpers import get_glue_env_var, get_latest_partitions, PARTITION_KEYS
+from repairs_cleaning_helpers import udf_map_repair_priority, clean_column_names
+
+source_catalog_database = get_glue_env_var('source_catalog_database', '')
+source_catalog_table = get_glue_env_var('source_catalog_table', '')
+cleaned_repairs_s3_bucket_target = get_glue_env_var(
+    'cleaned_repairs_s3_bucket_target', '')
+
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+logger = glueContext.get_logger()
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+source_data = glueContext.create_dynamic_frame.from_catalog(
+    name_space=source_catalog_database,
+    table_name=source_catalog_table,
+)
+
+df = source_data.toDF()
+
+df = get_latest_partitions(df)
+
+df2 = clean_column_names(df)
+
+# drop unnamed columns
+columns_to_drop = [i for i in df2.columns if i.startswith('unnamed')]
+df2 = df2.drop(*columns_to_drop)
+
+df2 = df2.withColumn('date', F.to_date('date', "dd/MM/yyyy"))
+
+df2 = df2.withColumn('data_source', F.lit('ElecMechFire - Electrical Supplies'))
+
+# rename column names to reflect harmonised column names
+df2 = df2.withColumnRenamed('date', 'datetime_raised') \
+    .withColumnRenamed('requested_by', 'operative') \
+    .withColumnRenamed('address', 'property_address') \
+    .withColumnRenamed('description', 'description_of_work') \
+    .withColumnRenamed('priority_code', 'work_priority_description') \
+    .withColumnRenamed('temp_order_number', 'temp_order_number_full') \
+    .withColumnRenamed('cost_code', 'budget_code')\
+    .withColumnRenamed('total_invoiced', 'order_value')\
+    .withColumnRenamed('works_status_comments', 'order_status')\
+    .withColumnRenamed('contractor_s_own_ref_no', 'contractor_ref')\
+
+
+# apply function
+df2 = df2.withColumn('work_priority_priority_code',
+                     udf_map_repair_priority('work_priority_description'))
+
+cleanedDataframe = DynamicFrame.fromDF(df2, glueContext, "cleanedDataframe")
+parquetData = glueContext.write_dynamic_frame.from_options(
+    frame=cleanedDataframe,
+    connection_type="s3",
+    format="parquet",
+    connection_options={"path": cleaned_repairs_s3_bucket_target,"partitionKeys": PARTITION_KEYS},
+    transformation_ctx="parquetData")
+job.commit()
---
+++ terraform/23-aws-glue-job-repairs-electrical-supplies.tf
@@ -0,0 +1,96 @@
+resource "aws_s3_bucket_object" "repairs_electrical_supplies_cleaning_script" {
+  tags = module.tags.values
+
+  bucket = module.glue_scripts.bucket_id
+  key    = "scripts/repairs_electrical_supplies.py"
+  acl    = "private"
+  source = "../scripts/repairs_electrical_supplies.py"
+  etag   = filemd5("../scripts/repairs_electrical_supplies.py")
+}
+
+resource "aws_glue_job" "housing_repairs_electrical_supplies_cleaning" {
+  count = local.is_live_environment ? 1 : 0
+
+  tags = module.tags.values
+
+  name              = "${local.short_identifier_prefix}Housing Repairs - Repairs ElecMechFire Electrical Supplies Cleaning"
+  number_of_workers = 10
+  worker_type       = "G.1X"
+  role_arn          = aws_iam_role.glue_role.arn
+  command {
+    python_version  = "3"
+    script_location = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.repairs_electrical_supplies_cleaning_script.key}"
+  }
+
+  glue_version = "2.0"
+
+  default_arguments = {
+    "--cleaned_repairs_s3_bucket_target" = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/electrical-supplies/cleaned"
+    "--source_catalog_database"          = module.department_housing_repairs.raw_zone_catalog_database_name
+    "--source_catalog_table"             = "housing_repairs_electrical_supplies"
+    "--TempDir"                          = module.glue_temp_storage.bucket_url
+    "--extra-py-files"                   = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.helpers.key},s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.repairs_cleaning_helpers.key}"
+  }
+}
+
+resource "aws_glue_crawler" "refined_zone_housing_repairs_electrical_supplies_cleaned_crawler" {
+  tags = module.tags.values
+
+  database_name = module.department_housing_repairs.refined_zone_catalog_database_name
+  name          = "${local.short_identifier_prefix}refined-zone-housing-repairs-electrical-supplies-cleaned"
+  role          = aws_iam_role.glue_role.arn
+  table_prefix  = "housing_repairs_electrical_supplies_"
+
+
+  s3_target {
+    path       = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/electrical-supplies/cleaned/"
+    exclusions = local.glue_crawler_excluded_blobs
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 5
+    }
+  })
+}
+
+resource "aws_glue_trigger" "housing_repairs_electrical_supplies_cleaning_job" {
+  count = local.is_live_environment ? 1 : 0
+
+  name          = "${local.identifier_prefix}-housing-repairs-electrical-supplies-cleaning-job-trigger"
+  type          = "CONDITIONAL"
+  workflow_name = "housing-repairs-electrical-supplies"
+  tags          = module.tags.values
+
+
+  predicate {
+    conditions {
+      crawler_name = "dataplatform-stg-raw-zone-housing-repairs-Electrical Supplies"
+      crawl_state  = "SUCCEEDED"
+    }
+  }
+
+  actions {
+    job_name = aws_glue_job.housing_repairs_electrical_supplies_cleaning[0].name
+  }
+}
+
+resource "aws_glue_trigger" "housing_repairs_electrical_supplies_cleaning_crawler" {
+  count = local.is_live_environment ? 1 : 0
+
+  name          = "${local.identifier_prefix}-housing-repairs-electrical-supplies-cleaning-crawler-trigger"
+  type          = "CONDITIONAL"
+  workflow_name = "housing-repairs-electrical-supplies"
+  tags          = module.tags.values
+
+  predicate {
+    conditions {
+      job_name = aws_glue_job.housing_repairs_electrical_supplies_cleaning[0].name
+      state    = "SUCCEEDED"
+    }
+  }
+  actions {
+    crawler_name = aws_glue_crawler.refined_zone_housing_repairs_electrical_supplies_cleaned_crawler.name
+  }
+}
