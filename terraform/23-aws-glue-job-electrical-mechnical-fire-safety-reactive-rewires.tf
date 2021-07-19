bb7a06e224ccc931fdc7ebd2633f6cac0e85f765
Ben Dalton <70905620+b-dalton@users.noreply.github.com>
2021-07-19 14:23:55 +0100

    remove "housing" prefix in refined zone folder name
    Co-authored-by: joates-madetech <80255046+joates-madetech@users.noreply.github.com>

--- terraform/23-aws-glue-job-electrical-mechnical-fire-safety-reactive-rewires.tf
+++ terraform/23-aws-glue-job-electrical-mechnical-fire-safety-reactive-rewires.tf
@@ -25,7 +25,7 @@ resource "aws_glue_job" "housing_elec_mech_fire_reactive_rewires_cleaning" {
   glue_version = "2.0"
 
   default_arguments = {
-    "--cleaned_repairs_s3_bucket_target" = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/housing-reactive-rewires/cleaned/"
+    "--cleaned_repairs_s3_bucket_target" = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/reactive-rewires/cleaned/"
     "--source_catalog_database"          = module.department_housing_repairs.raw_zone_catalog_database_name
     "--source_catalog_table"             = module.repairs_fire_alarm_aov[0].worksheet_resources["reactive-rewires"].catalog_table
     "--TempDir"                          = module.glue_temp_storage.bucket_url
