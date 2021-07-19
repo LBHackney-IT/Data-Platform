9ef78e7954cf1917c632f227a4448aaf7e57ff84
maysakanoni <maysa@madetech.com>
2021-07-19 16:33:20 +0100

    remove reference to housing in the output path
    this is not needed and does not add value

--- terraform/23-aws-glue-job-electrical-mechnical-fire-safety-communal-lighting.tf
+++ terraform/23-aws-glue-job-electrical-mechnical-fire-safety-communal-lighting.tf
@@ -25,7 +25,7 @@ resource "aws_glue_job" "housing_elec_mech_fire_communal_lighting" {
   glue_version = "2.0"
 
   default_arguments = {
-    "--cleaned_repairs_s3_bucket_target" = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/housing-communal-lighting/cleaned/"
+    "--cleaned_repairs_s3_bucket_target" = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/communal-lighting/cleaned/"
     "--source_catalog_database"          = module.department_housing_repairs.raw_zone_catalog_database_name
     "--source_catalog_table"             = module.repairs_fire_alarm_aov[0].worksheet_resources["communal-lighting"].catalog_table
     "--TempDir"                          = module.glue_temp_storage.bucket_url
@@ -42,7 +42,7 @@ resource "aws_glue_crawler" "refined_zone_housing_repairs_elec_mech_fire_communa
   table_prefix  = "housing_repairs_elec_mech_fire_communal_lighting_"
 
   s3_target {
-    path = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/housing-communal-lighting/cleaned/"
+    path = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/communal-lighting/cleaned/"
 
 
     exclusions = local.glue_crawler_excluded_blobs
