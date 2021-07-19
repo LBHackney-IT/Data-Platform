b9dd5f47283ec2da10b0f0a6acd5d4146689c047
maysakanoni <maysa@madetech.com>
2021-07-19 15:53:53 +0100

    fix typo
    Co-authored-by: el <elena@madetech.com>
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    Co-authored-by: joates <james.oates@madetech.com>

--- terraform/23-aws-glue-job-electrical-mechnical-fire-safety-lift-breakdown-ela copy.tf
+++ terraform/23-aws-glue-job-electrical-mechnical-fire-safety-lift-breakdown-ela copy.tf
@@ -27,7 +27,7 @@ resource "aws_glue_job" "housing_elec_mech_fire_lift_breakdown_ela_cleaning" {
   default_arguments = {
     "--cleaned_repairs_s3_bucket_target" = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-electrical-mechanical-fire/housing-lift-breakdown-ela/cleaned/"
     "--source_catalog_database"          = module.department_housing_repairs.raw_zone_catalog_database_name
-    "--source_catalog_table"             = module.repairs_fire_alarm_aov[0].worksheet_resources["lift-breakdown---elafv"].catalog_table
+    "--source_catalog_table"             = module.repairs_fire_alarm_aov[0].worksheet_resources["lift-breakdown---elaf"].catalog_table
     "--TempDir"                          = module.glue_temp_storage.bucket_url
     "--extra-py-files"                   = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.helpers.key},s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.repairs_cleaning_helpers.key}"
   }
