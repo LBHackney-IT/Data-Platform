a9edf6601cb83380d84de9392bf625216cd56032
b-dalton <ben.dalton@madetech.com>
2021-07-19 16:04:06 +0100

    map repairs priority without using a udf

--- scripts/elec_mech_fire_reactive_rewires_cleaning.py
+++ scripts/elec_mech_fire_reactive_rewires_cleaning.py
@@ -8,7 +8,7 @@ import pyspark.sql.functions as F
 from awsglue.dynamicframe import DynamicFrame
 import re
 from helpers import get_glue_env_var, get_latest_partitions, PARTITION_KEYS
-from repairs_cleaning_helpers import udf_map_repair_priority, clean_column_names
+from repairs_cleaning_helpers import clean_column_names
 
 source_catalog_database = get_glue_env_var('source_catalog_database', '')
 source_catalog_table = get_glue_env_var('source_catalog_table', '')
@@ -71,9 +71,12 @@ df2 = df2.withColumnRenamed('requested_by', 'operative') \
     .withColumnRenamed('cost_of_repairs_work', 'order_value')\
     .withColumnRenamed('contractor_s_own_ref_no', 'contractor_ref')
 
-# apply function
-df2 = df2.withColumn('work_priority_priority_code',
-                     udf_map_repair_priority('work_priority_description'))
+# map repairs priority
+df2 = df2.withColumn("work_priority_priority_code", when(df2['work_priority_description'] == "Immediate", 1)
+                                 .when(df2['work_priority_description'] == "Emergency", 2)
+                                 .when(df2['work_priority_description'] == "Urgent", 3)
+                                 .when(df2['work_priority_description'] == "Normal", 4)
+                                 .otherwise(None))
 
 cleanedDataframe = DynamicFrame.fromDF(df2, glueContext, "cleanedDataframe")
 parquetData = glueContext.write_dynamic_frame.from_options(
