8443b87d894a3a6711b8c822d4fac966b32e3a36
Adrian Clay <adrian@madetech.com>
2021-07-25 08:59:44 +0100

    Enable spark eventLog during tests
    This will mean event logs are spit out inside the spark_events
    folder each time the tests are run.
    
    Inspecting these logs using the history server, will allow us to
    debug the performance of the spark queries made during the tests.
    
    Also:
     * Add make command to boot up the history server
     * Add make command which boots up a bash shell within the glue
       docker image.
     * Shutdown the spark session cleanly after the tests have run
       otherwise the event logs aren't marked as complete
     * Expose Spark web UI port 4040 on the test command to monitor
       performance while tests are in progress
     * Use pytest --durations to print out wall time of each
       test
    
    See also:
    https://spark.apache.org/docs/latest/monitoring.html#viewing-after-the-fact

--- scripts/README.md
+++ scripts/README.md
@@ -5,3 +5,12 @@ Prerequisites
 
 Running the tests
   - `make test`
+
+## Debugging Spark portion of tests
+
+While `make test` is running, you can access the [Spark Web UI][spark_web_ui] on http://localhost:4040
+
+To inspect previous test runs, you can run the history server with `make history-server` and
+access it on http://localhost:18080
+
+[spark_web_ui]: https://spark.apache.org/docs/latest/monitoring.html#web-interfaces
--- scripts/test_spark_example.py
+++ scripts/test_spark_example.py
@@ -1,23 +1,31 @@
 
 from spark_example import only_hackney_addresses
 
-from unittest.case import TestCase
+import pytest
 from pyspark.sql import SparkSession, Row
 
-spark = SparkSession.builder.master("local").getOrCreate()
+import os
 
-class TestSparkExample(TestCase):
-  def test_filters_only_hackney_addresses(self):
-    self.assertEqual(
+@pytest.fixture(scope='session')
+def spark():
+  event_log_dir = 'file://' + os.path.dirname(__file__) + '/spark_events'
+  s = SparkSession.builder.config("spark.eventLog.dir", event_log_dir).config("spark.eventLog.enabled", True).master("local").getOrCreate()
+  yield s
+  s.stop()
+
+class TestSparkExample:
+  def test_filters_only_hackney_addresses(self, spark):
+    assert (
       [
         {'line1': '13', 'line2': 'Cheese Lane', 'postcode': 'E8 13HB', 'council': 'Hackney'}
-      ],
-      self.only_hackney_addresses([
+      ]
+      ==
+      self.only_hackney_addresses(spark, [
         {'line1': '13', 'line2': 'Cheese Lane', 'postcode': 'E8 13HB', 'council': 'Hackney'},
         {'line1': '13', 'line2': 'Pickle Lane', 'postcode': 'E15 13HB', 'council': 'Newham'},
       ])
     )
 
-  def only_hackney_addresses(self, addresses):
+  def only_hackney_addresses(self, spark, addresses):
     query_addresses = spark.createDataFrame(spark.sparkContext.parallelize([Row(**i) for i in addresses]))
     return [row.asDict() for row in only_hackney_addresses(query_addresses).rdd.collect()]
