ab0884bc86a4afae317960f98cf51f0971ee0219
joates-madetech <james.oates@madetech.com>
2021-07-30 12:04:41 +0100

    Fixed name length issue

--- modules/department/60-aws-sso.tf
+++ modules/department/60-aws-sso.tf
@@ -1,7 +1,9 @@
 resource "aws_ssoadmin_permission_set" "department" {
   provider = aws.aws_hackit_account
 
-  name             = "DataPlatCollab${local.department_pascalcase}-DoNotUse"
+  // Name must not exceed 32 characters
+  // name             = "DataPlatform${local.department_pascalcase}"
+  name             = "DataPlat${local.department_pascalcase}DoNotUse"
   description      = "This is a test permission set created by Terraform"
   instance_arn     = var.sso_instance_arn
   session_duration = "PT12H"
@@ -16,25 +18,25 @@ resource "aws_ssoadmin_permission_set_inline_policy" "department" {
   permission_set_arn = aws_ssoadmin_permission_set.department.arn
 }
 
-data "aws_identitystore_group" "department" {
-  provider = aws.aws_hackit_account
-
-  identity_store_id = var.identity_store_id
-
-  filter {
-    attribute_path  = "DisplayName"
-    attribute_value = "saml-aws-data-platform-collaborator-parking@hackney.gov.uk"
-  }
-}
-
-// Added this as I want to see if it fails when it can't be found
-data "aws_identitystore_group" "will_fail" {
-  provider = aws.aws_hackit_account
-
-  identity_store_id = var.identity_store_id
-
-  filter {
-    attribute_path  = "DisplayName"
-    attribute_value = "purposefully-does-not-exist@hackney.gov.uk"
-  }
-}
+//data "aws_identitystore_group" "department" {
+//  provider = aws.aws_hackit_account
+//
+//  identity_store_id = var.identity_store_id
+//
+//  filter {
+//    attribute_path  = "DisplayName"
+//    attribute_value = "saml-aws-data-platform-collaborator-parking@hackney.gov.uk"
+//  }
+//}
+//
+//// Added this as I want to see if it fails when it can't be found
+//data "aws_identitystore_group" "will_fail" {
+//  provider = aws.aws_hackit_account
+//
+//  identity_store_id = var.identity_store_id
+//
+//  filter {
+//    attribute_path  = "DisplayName"
+//    attribute_value = "purposefully-does-not-exist@hackney.gov.uk"
+//  }
+//}
