06628b59f803e137169e00af8575475581a7a22f
elcorbs <emma@madetech.com>
2021-08-02 14:12:08 +0100

    formatting changes

--- modules/department/00-init.tf
+++ modules/department/00-init.tf
@@ -7,8 +7,8 @@ terraform {
 
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = ">= 3.0"
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
     }
   }
 }
