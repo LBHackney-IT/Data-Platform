d6f383374e327d3ae5fae77003d3a74efa7f5cf6
elcorbs <emma@madetech.com>
2021-08-02 11:24:14 +0100

    Module to clean, address clean & (in the future) address match repairs sheets data
    Applied this module to herts heritage as a first step.

---
+++ modules/housing-repairs-google-sheets-cleaning/01-inputs-required.tf
@@ -0,0 +1,95 @@
+variable "tags" {
+  description = "AWS tags"
+  type        = map(string)
+}
+
+variable "identifier_prefix" {
+  description = "Project wide resource identifier prefix"
+  type        = string
+}
+
+variable "department_name" {
+  description = "Department folder name"
+  type        = string
+}
+
+variable "data_cleaning_script_key" {
+  description = "Key of the location of the data cleaning script"
+  type        = string
+}
+
+variable "short_identifier_prefix" {
+  description = "Short project wide resource identifier prefix"
+  type        = string
+}
+
+variable "glue_scripts_bucket_id" {
+  description = "Id of bucket where glue jobs scripts are stored"
+  type        = string
+}
+
+variable "glue_temp_storage_bucket_id" {
+  description = "Id of temp glue job storage"
+  type        = string
+}
+
+variable "refined_zone_bucket_id" {
+  description = "Refined zone bucket id"
+  type        = string
+}
+
+variable "helper_script_key" {
+  description = "Helpers script key"
+  type        = string
+}
+
+variable "cleaning_helper_script_key" {
+  description = "Cleaning helpers script key"
+  type        = string
+}
+
+variable "glue_crawler_excluded_blobs" {
+  description = "A list of blobs to ignore when crawling the job"
+  type        = list(string)
+  default     = []
+}
+
+variable "catalog_database" {
+  description = "Catalog data name"
+  type        = string
+}
+
+variable "source_catalog_table" {
+  description = "Name of the source table in the catalog"
+  type        = string
+}
+
+variable "trigger_crawler_name" {
+  description = "Name of the trigger which should trigger the data cleaning defined in this module"
+  type        = string
+}
+
+variable "workflow_name" {
+  description = "Name of the workflow to add all the triggers created here to"
+  type        = string
+}
+
+variable "refined_zone_catalog_database_name" {
+  description = "Refined zone catalog database name"
+  type        = string
+}
+
+variable "dataset_name" {
+  description = "Name of the data set"
+  type        = string
+}
+
+variable "address_cleaning_job_name" {
+  description = "Address cleaning job name"
+  type        = string
+}
+
+variable "glue_role_arn" {
+  default = "Glue Role ARN that the job will use to excecute"
+  type    = string
+}
