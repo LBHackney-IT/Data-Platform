193a4b3dffe0f6a8c826ed28a3499eddc8a2c70b
el <elena@madetech.com>
2021-08-04 11:51:04 +0100

    HOTFIX: change dlo address matching destination Co-authored-by: joates-madetech <james.oates@madetech.com> Co-authored-by: Emma <emma@madetech.com> Co-authored-by: maysakanoni <maysa@madetech.com>

--- terraform/23-aws-glue-job-repairs-dlo.tf
+++ terraform/23-aws-glue-job-repairs-dlo.tf
@@ -313,11 +313,8 @@ resource "aws_glue_job" "repairs_dlo_levenshtein_address_matching" {
     "--addresses_api_data_table"    = "unrestricted_address_api_dbo_hackney_address"
     "--source_catalog_database"     = "housing-repairs-refined-zone"
     "--source_catalog_table"        = "housing_repairs_repairs_dlo_with_cleaned_addresses_with_cleaned_addresses"
-    "--target_destination"          = "s3://${module.refined_zone.bucket_id}/housing-repairs/repairs-dlo/with-matched-addresses/"
+    "--target_destination"          = "s3://${module.trusted_zone.bucket_id}/housing-repairs/repairs/"
     "--TempDir"                     = module.glue_temp_storage.bucket_url
     "--extra-py-files"              = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.helpers.key}"
   }
 }
-
-
-
