27434f7525709e7114f02477d408b42d47db05c3
el <elena@madetech.com>
2021-08-16 10:24:58 +0100

    edit service area

--- terraform/63-api-ingestor.tf
+++ terraform/63-api-ingestor.tf
@@ -7,7 +7,7 @@ module "tascomi" {
   zone_bucket_arn                = module.landing_zone.bucket_arn
   zone_bucket_id                 = module.landing_zone.bucket_id
   resource_name                  = "users"
-  service_area                   = "housing"
+  service_area                   = "planning"
   glue_scripts_bucket_id         = module.glue_scripts.bucket_id
   glue_role_arn                  = aws_iam_role.glue_role.arn
   glue_catalog_database_name     = module.department_housing_repairs.landing_zone_catalog_database_name
