90e1c8fabc9bc24539581ea199e74c40112d3c40
Adrian Clay <adrian@madetech.com>
2021-08-24 12:09:03 +0100

    Run the housing repairs crawler on a schedule
    Previously this was configured to trigger after all
    repairs "Address Matching" jobs had complete.
    
    This wasn't working, and we believe this is due to this
    documentation:
    
    >  All jobs or crawlers in a dependency chain must be
    >  descendants of a single scheduled or on-demand trigger.
    
    In our case, we have mutliple schedules for each of the different
    repair sheets.
    
    We have decided to move to a scheduled based approach as it
    will avoid this problem.  Another approach we considered
    was to run the crawler at the end of each workflow.  We
    were concerned about that erroring if multiple workflows
    wanted to run the crawler at the same time.
    
    See also:
    https://docs.aws.amazon.com/glue/latest/dg/about-triggers.html
    
    Co-authored-by: b-dalton <ben.dalton@madetech.com>
    Co-authored-by: Emma Corbett <emma@madetech.com>

--- terraform/23-aws-glue-crawler-housing-repairs.tf
+++ terraform/23-aws-glue-crawler-housing-repairs.tf
@@ -1,6 +1,4 @@
 resource "aws_glue_crawler" "trusted_zone_housing_repairs_crawler" {
-  count = local.is_live_environment ? 1 : 0
-
   tags = module.tags.values
 
   database_name = module.department_housing_repairs.trusted_zone_catalog_database_name
@@ -21,90 +19,14 @@ resource "aws_glue_crawler" "trusted_zone_housing_repairs_crawler" {
 }
 
 resource "aws_glue_trigger" "housing_repairs_trusted_crawler" {
-  count = local.is_live_environment ? 1 : 0
-
   tags = module.tags.values
 
-  name = "${local.short_identifier_prefix}housing-repairs-repairs-trusted-crawler-trigger"
-  type = "CONDITIONAL"
+  name     = "${local.short_identifier_prefix}housing-repairs-repairs-trusted-crawler-trigger"
+  schedule = "cron(0 7,8,9,10 ? * MON-FRI *)"
+  type     = "SCHEDULED"
+  enabled  = local.is_live_environment
 
-  predicate {
-    logical = "AND"
-    conditions {
-      job_name = module.communal_lighting[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.door_entry[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.dpa[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.electrical_supplies[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.fire_alarmaov[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.lift_breakdown_el[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.lightning_protection[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.reactive_rewires[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.electric_heating[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.emergency_lighting_servicing[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.tv_aerials[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.housing_repairs_alphatrack[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.housing_repairs_avonline[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-
-    conditions {
-      job_name = module.housing_repairs_axis[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = aws_glue_job.repairs_dlo_levenshtein_address_matching[0].name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.housing_repairs_herts_heritage[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.housing_repairs_purdy[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-    conditions {
-      job_name = module.housing_repairs_stannah[0].address_matching_job_name
-      state    = "SUCCEEDED"
-    }
-  }
   actions {
-    crawler_name = aws_glue_crawler.trusted_zone_housing_repairs_crawler[0].name
+    crawler_name = aws_glue_crawler.trusted_zone_housing_repairs_crawler.name
   }
 }
