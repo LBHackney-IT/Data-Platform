2dcd4a6550fae2850d8127191caa9e284786a088
elcorbs <emma@madetech.com>
2021-08-25 10:29:08 +0100

    Add conftest.py containing spark setup for tests
    Co-authored-by: Adrian Clay <adrian.clay@hackney.gov.uk>
    Co-authored-by: b-dalton <ben.dalton@madetech.com>

---
+++ scripts/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from pyspark.sql import SparkSession
+import os
+
+@pytest.fixture(scope='session')
+def spark():
+  event_log_dir = 'file://' + os.path.dirname(__file__) + '/spark_events'
+  s = SparkSession.builder.config("spark.eventLog.dir", event_log_dir).config("spark.eventLog.enabled", True).master("local").getOrCreate()
+  yield s
+  s.stop()
