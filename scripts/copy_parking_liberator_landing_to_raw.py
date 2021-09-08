6a8bddd95a329cd35cf8abde99e0aa9228e679e6
Emma <emma@madetech.com>
2021-09-08 14:52:51 +0100

    Stop accessing Name value of table as table list is justa  list of names

--- scripts/copy_parking_liberator_landing_to_raw.py
+++ scripts/copy_parking_liberator_landing_to_raw.py
@@ -30,34 +30,34 @@ logger = glue_context.get_logger()
 filtering_pattern = re.compile(table_filter_expression)
 tables_to_move = [table for table in glue_context.tableNames(dbName=database_name_source) if filtering_pattern.match(table)]
 
-for table in tables_to_move:
-  logger.info(f"Starting copying table {database_name_source}.{table['Name']}")
+for table_name in tables_to_move:
+  logger.info(f"Starting copying table {database_name_source}.{table_name}")
   table_data_frame = glue_context.create_dynamic_frame.from_catalog(
       name_space = database_name_source,
-      table_name = table['Name'],
-      transformation_ctx = "data_source" + table['Name']
+      table_name = table_name,
+      transformation_ctx = "data_source" + table_name
   ).toDF()
 
   if(len(table_data_frame.columns) == 0):
-    logger.info(f"Aborting copying data for table {database_name_source}.{table['Name']}, as it has empty columns.")
+    logger.info(f"Aborting copying data for table {database_name_source}.{table_name}, as it has empty columns.")
     continue
 
   table_with_timestamp = add_timestamp_column(table_data_frame)
 
   data_sink = glue_context.getSink(
-    path = "s3://" + bucket_target + "/" + prefix + table['Name'] + "/",
+    path = "s3://" + bucket_target + "/" + prefix + table_name + "/",
     connection_type = "s3",
     updateBehavior = "UPDATE_IN_DATABASE",
     partitionKeys = PARTITION_KEYS,
     enableUpdateCatalog = True,
-    transformation_ctx = "data_sink" + table['Name']
+    transformation_ctx = "data_sink" + table_name
   )
   data_sink.setCatalogInfo(
     catalogDatabase = database_name_target,
-    catalogTableName = table['Name']
+    catalogTableName = table_name
   )
   data_sink.setFormat("glueparquet")
   data_sink.writeFrame(DynamicFrame.fromDF(table_with_timestamp, glue_context, "result_dataframe"))
-  logger.info(f"Finished copying table {database_name_source}.{table['Name']}")
+  logger.info(f"Finished copying table {database_name_source}.{table_name}")
 
 job.commit()
