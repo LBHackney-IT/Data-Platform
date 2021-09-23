f1247991a9f963aa785ca720d1cd3e1b5c54a199
Daro Clark <daro.clark_hackney@L1611459.ad.hackney.gov.uk>
2021-09-23 09:24:02 +0100

    test file for the elec mech fire dpa repairs dataset

--- scripts/test_elec_mech_fire_dpa.py
+++ scripts/test_elec_mech_fire_dpa.py
@@ -3,20 +3,19 @@ import datetime
 from pyspark.sql import Row
 from unittest.case import TestCase
 import pytest
+from unit_testing_helpers import assertDictionaryContains
 
 class TestMechFireDpaCleaning:
     def test_date_conversion(self, spark):
         response = self.clean_mech_fire_data(spark, [{'date': datetime.datetime(2002, 1, 10, 0, 0)}])
 
-        self.assertDictionaryContains({'date': datetime.datetime(2002, 1, 10, 0, 0)}, response[0])
+        assertDictionaryContains({'date': datetime.datetime(2002, 1, 10, 0, 0)}, response[0])
 
     def test_status_standard(self, spark):
         response = self.clean_mech_fire_data(spark, [{'order_status': 'Y'}])
 
-        self.assertDictionaryContains({'order_status': 'Completed'}, response[0])
+        assertDictionaryContains({'order_status': 'Completed'}, response[0])
 
-    def assertDictionaryContains(self, expected, actual):
-        TestCase().assertEqual(actual, {**actual,  **expected})
 
     def clean_mech_fire_data(self, spark, repairs):
         repairs_with_imports = [{'import_year': '2021', 'import_month': '08', 'import_day': '19','date':datetime.datetime(2002, 1, 10, 0, 0),'order_status':'Y', **i} for i in repairs]
