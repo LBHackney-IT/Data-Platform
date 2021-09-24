bc743afb3c50efc6aea24968d88a2e3134bfa50f
Emma <emma@madetech.com>
2021-09-24 16:09:34 +0100

    Fix cron schedult expression

--- terraform/24-aws-glue-tascomi-data.tf
+++ terraform/24-aws-glue-tascomi-data.tf
@@ -140,7 +140,7 @@ resource "aws_glue_trigger" "tascomi_tables_weekly_ingestion_triggers" {
 
   name     = "${local.short_identifier_prefix}Tascomi ${title(replace(each.value, "_", " "))} Ingestion Trigger"
   type     = "SCHEDULED"
-  schedule = "cron(0 22 * * SUN *)"
+  schedule = "cron(0 22 ? * SUN *)"
   enabled  = local.is_live_environment
 
   actions {
