c6d92de7165917d8d4c99f069f7e51a108d4c252
b-dalton <ben.dalton@madetech.com>
2021-09-28 16:08:49 +0100

    add department name to tags on glue jobs and resources

--- terraform/23-aws-glue-crawler-housing-repairs.tf
+++ terraform/23-aws-glue-crawler-housing-repairs.tf
@@ -1,5 +1,5 @@
 resource "aws_glue_crawler" "trusted_zone_housing_repairs_crawler" {
-  tags = module.tags.values
+  tags = local.tags_with_housing_repairs_department
 
   database_name = module.department_housing_repairs.trusted_zone_catalog_database_name
   name          = "${local.short_identifier_prefix}trusted-zone-housing-repairs"
@@ -19,7 +19,7 @@ resource "aws_glue_crawler" "trusted_zone_housing_repairs_crawler" {
 }
 
 resource "aws_glue_trigger" "housing_repairs_trusted_crawler" {
-  tags = module.tags.values
+  tags = local.tags_with_housing_repairs_department
 
   name     = "${local.short_identifier_prefix}housing-repairs-repairs-trusted-crawler-trigger"
   schedule = "cron(0 7,8,9,10 ? * MON-FRI *)"
