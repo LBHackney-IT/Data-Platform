0e71394434479b8dff2a21d9447a008868e1f260
Ben Reynolds-Carr <benreynolds-carr@192.168.1.120>
2021-10-04 15:47:55 +0100

    Created new Planning department based on existing template

--- terraform/05-departments.tf
+++ terraform/05-departments.tf
@@ -127,7 +127,33 @@ module "department_env_enforcement" {
   application                     = local.application_snake
   short_identifier_prefix         = local.short_identifier_prefix
   identifier_prefix               = local.identifier_prefix
-  name                            = "Env Enforcement"
+  name                            = "Env Enforcement`"
+  landing_zone_bucket             = module.landing_zone
+  raw_zone_bucket                 = module.raw_zone
+  refined_zone_bucket             = module.refined_zone
+  trusted_zone_bucket             = module.trusted_zone
+  athena_storage_bucket           = module.athena_storage
+  glue_scripts_bucket             = module.glue_scripts
+  glue_temp_storage_bucket        = module.glue_temp_storage
+  secrets_manager_kms_key         = aws_kms_key.secrets_manager_key
+  sso_instance_arn                = local.sso_instance_arn
+  identity_store_id               = local.identity_store_id
+  google_group_admin_display_name = local.google_group_admin_display_name
+}
+
+module "department_planning" {
+  providers = {
+    aws                    = aws
+    aws.aws_hackit_account = aws.aws_hackit_account
+  }
+
+  source                          = "../modules/department"
+  tags                            = module.tags.values
+  is_live_environment             = local.is_live_environment
+  application                     = local.application_snake
+  short_identifier_prefix         = local.short_identifier_prefix
+  identifier_prefix               = local.identifier_prefix
+  name                            = "Planning"
   landing_zone_bucket             = module.landing_zone
   raw_zone_bucket                 = module.raw_zone
   refined_zone_bucket             = module.refined_zone
