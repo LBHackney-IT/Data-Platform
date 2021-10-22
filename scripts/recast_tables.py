4c1adb67970dd04aa6061a3bdc8e76292061ddf8
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2021-10-22 10:16:36 +0200

    tests

---
+++ scripts/recast_tables.py
@@ -0,0 +1,74 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+from pyspark.sql.functions import col, max
+from pyspark.sql.types import StringType, IntegerType, BooleanType, DateType, FloatType, TimestampType, LongType, DoubleType, MapType
+from helpers import get_latest_partitions, get_glue_env_var, PARTITION_KEYS
+
+
+def castColumns(columnDict,tableName,df,typeName,dataType):
+    # check if this datatype is represented in the dictionary
+    if typeName in columnDict.columns:
+        # check if this table is represented for this data type in the dictionary
+        if tableName+":" in columnDict.select(typeName).schema.simpleString():
+            # iterate on columns of this type
+            for colName in columnDict.select(typeName+'.'+tableName).first()[0].split(','):
+                # recast
+                df = df.withColumn(colName ,col(colName).cast(dataType))
+    return df
+
+def castColumnsAllTypes(ColumnDict,tableName,df):
+    df = castColumns(columnDict=ColumnDict, tableName=tableName, df=df, typeName="timestamp", dataType=TimestampType())
+    df = castColumns(columnDict=ColumnDict, tableName=tableName, df=df, typeName="integer", dataType=IntegerType())
+    df = castColumns(columnDict=ColumnDict, tableName=tableName, df=df, typeName="boolean", dataType=BooleanType())
+    df = castColumns(columnDict=ColumnDict, tableName=tableName, df=df, typeName="float", dataType=DoubleType())
+    df = castColumns(columnDict=ColumnDict, tableName=tableName, df=df, typeName="long", dataType=LongType())
+    df = castColumns(columnDict=ColumnDict, tableName=tableName, df=df, typeName="double", dataType=DoubleType())
+    df = castColumns(columnDict=ColumnDict, tableName=tableName, df=df, typeName="date", dataType=DateType())
+    return df
+
+if __name__ == "__main__":
+    args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+    table_list_string = get_glue_env_var('table_list','')
+    source_catalog_database = get_glue_env_var('source_catalog_database', '')
+    s3_bucket_target = get_glue_env_var('s3_bucket_target', '')
+    column_dict_path = get_glue_env_var('column_dict_path', '')
+
+    conf = SparkConf().set("spark.sql.sources.partitionOverwriteMode","dynamic")
+    sc = SparkContext.getOrCreate(conf)
+    glueContext = GlueContext(sc)
+    spark = SparkSession(sc)
+    logger = glueContext.get_logger()
+    job = Job(glueContext)
+    job.init(args['JOB_NAME'], args)
+
+    #   load table list
+    table_list = table_list_string.split(',')
+    #   load columns dictionary
+    columnsDictionary = spark.read.option("multiline", "true").json(column_dict_path)
+
+
+    for nameOfTableToRecast in table_list:
+        #   load data
+        source_ddf = glueContext.create_dynamic_frame.from_catalog(
+            name_space=source_catalog_database,
+            table_name=nameOfTableToRecast
+        )
+        source_df = source_ddf.toDF()
+
+
+        #   recast
+        source_df = castColumnsAllTypes(columnDict=columnsDictionary, tableName=nameOfTableToRecast, df=source_df)
+
+        # WRITE TO S3
+        resultDataFrame = DynamicFrame.fromDF(source_df, glueContext, "resultDataFrame")
+        target_destination = s3_bucket_target + nameOfTableToRecast
+    
+        resultDataFrame.toDF().write.mode("overwrite").format("parquet").partitionBy(PARTITION_KEYS).save(target_destination)
+    
+    job.commit()
--- scripts/test_recast_tables.py
+++ scripts/test_recast_tables.py
@@ -1,24 +1,65 @@
-from recast_tables import castColumns
+from recast_tables import castColumns, castColumnsAllTypes
 from pyspark.sql import Row
-from pyspark.sql.types import TimestampType
-from datetime import datetime
+from pyspark.sql.types import TimestampType, IntegerType, BooleanType, FloatType, LongType, DoubleType, DateType
+from datetime import datetime, date
 
 class TestRecastTables:
   
-  def test_castColumns_timestamp(self, spark):
-    response = self.castColumns(spark, [{'submit_date': '2021-09-24 08:58:47'}], "timestamp", TimestampType())
-    expected = [{'submit_date': datetime(2021, 9, 24, 8, 58, 47)}]
-    assert (
-      response
-      ==
-      expected
-    )
+    def test_castColumns_timestamp(self, spark):
+        response = self.castColumns(spark, [{'submit_date': '2021-09-24 08:58:47'}], "timestamp", TimestampType())
+        expected = [{'submit_date': datetime(2021, 9, 24, 8, 58, 47)}]
+        assert (response == expected)
+  
+    def test_castColumns_integer(self, spark):
+        response = self.castColumns(spark, [{'id_number': '12'}], "integer", IntegerType())
+        expected = [{'id_number': 12}]
+        assert (response == expected)
 
-  def castColumns(self, spark, data, typeName, dataType):
-    # The column dictionary is saved in JSON format in a separate file in this directory.
-    # Then we are using the same code that is used in the script to access this file just with a relative path
-    # instead of an S3 path.
+    def test_castColumns_boolean(self, spark):
+        response = self.castColumns(spark, [{'flag': 'f'},{'flag': 't'}], "boolean", BooleanType())
+        expected = [{'flag': False},{'flag': True}]
+        assert (response == expected)
 
-    columnsDictionary = spark.read.option("multiline", "true").json("./recast_columns_mock_column_type.json")
-    query_data = spark.createDataFrame(spark.sparkContext.parallelize([Row(**i) for i in data]))
-    return [row.asDict() for row in castColumns(columnsDictionary, "MyTable", query_data, typeName, dataType).rdd.collect()]
+    # Comparison of floats not doable with assert, hence the small precision 91.0 in this test
+    def test_castColumns_float(self, spark):
+        response = self.castColumns(spark, [{'measurement': '91.0'}], "float", FloatType())
+        expected = [{'measurement': 91.0}]
+        assert (response == expected)
+
+    def test_castColumns_double(self, spark):
+        response = self.castColumns(spark, [{'large_measurement': '91.535554'}], "double", DoubleType())
+        expected = [{'large_measurement': 91.535554}]
+        assert (response == expected)
+
+
+    def test_castColumns_long(self, spark):
+        response = self.castColumns(spark, [{'long_id': '9157952949254835554'}], "long", LongType())
+        expected = [{'long_id': 9157952949254835554}]
+        assert (response == expected)
+
+    def test_castColumns_date(self, spark):
+        response = self.castColumns(spark, [{'start_date': '2021-09-24'}], "date", DateType())
+        expected = [{'start_date': date(2021, 9, 24)}]
+        assert (response == expected)
+
+    def test_castColumns_allTypes(self, spark):
+        response = self.castColumnsAllTypes(spark, [{'submit_date': '2021-09-24 08:58:47', 'id_number': '12', 'flag': 'f', 'measurement': '91.0', 'large_measurement': '91.535554', 'long_id': '9157952949254835554', 'start_date': '2021-09-24'}])
+        expected = [{'submit_date': datetime(2021, 9, 24, 8, 58, 47), 'id_number': 12, 'flag': False, 'measurement': 91.0, 'large_measurement': 91.535554, 'long_id': 9157952949254835554,'start_date': date(2021, 9, 24)}]
+        assert (response == expected)
+
+    def castColumns(self, spark, data, typeName, dataType):
+        # The column dictionary is saved in JSON format in a separate file in this directory.
+        # Then we are using the same code that is used in the script to access this file just with a relative path
+        # instead of an S3 path.
+
+        columnsDictionary = spark.read.option("multiline", "true").json("./stub_column_type_dictionary.json")
+        query_data = spark.createDataFrame(spark.sparkContext.parallelize([Row(**i) for i in data]))
+        return [row.asDict() for row in castColumns(columnsDictionary, "MyTable", query_data, typeName, dataType).rdd.collect()]
+
+    def castColumnsAllTypes(self, spark, data):
+        # The column dictionary is saved in JSON format in a separate file in this directory.
+        # Then we are using the same code that is used in the script to access this file just with a relative path
+        # instead of an S3 path.
+        columnsDictionary = spark.read.option("multiline", "true").json("./stub_column_type_dictionary.json")
+        query_data = spark.createDataFrame(spark.sparkContext.parallelize([Row(**i) for i in data]))
+        return [row.asDict() for row in castColumnsAllTypes(columnsDictionary, "MyTable", query_data).rdd.collect()]
