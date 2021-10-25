307fb97626011c1d9fabfd34eaba2b418c707be1
b-dalton <ben.dalton@madetech.com>
2021-10-25 10:43:48 +0100

    refactor housing repairs address cleaning job to use glue job module

--- modules/housing-repairs-google-sheets-cleaning/10-aws-glue-data-cleaning.tf
+++ modules/housing-repairs-google-sheets-cleaning/10-aws-glue-data-cleaning.tf
@@ -1,20 +1,16 @@
-locals {
-  glue_job_name = "${var.short_identifier_prefix}Housing Repairs - ${title(replace(var.dataset_name, "-", " "))} Cleaning"
-}
-
-module "housing_repairs_google_sheets_import" {
+module "housing_repairs_google_sheets_cleaning" {
   source = "../aws-glue-job"
 
   department = var.department
-  job_name   = local.glue_job_name
+  job_name   = "${local.glue_job_name} Cleaning"
   job_parameters = {
     "--source_catalog_database"          = var.catalog_database
     "--source_catalog_table"             = var.source_catalog_table
     "--cleaned_repairs_s3_bucket_target" = "s3://${var.refined_zone_bucket_id}/housing-repairs/${var.dataset_name}/cleaned/"
     "--TempDir"                          = var.glue_temp_storage_bucket_id
-    "--extra-py-files"                   = "s3://${var.glue_scripts_bucket_id}/${var.helper_script_key},s3://${var.glue_scripts_bucket_id}/${var.cleaning_helper_script_key}"
+    "--extra-py-files"                   = local.extra_py_files
   }
-  script_name            = var.address_cleaning_script_key
+  script_name            = var.data_cleaning_script_key
   workflow_name          = var.workflow_name
   triggered_by_crawler   = var.trigger_crawler_name
   glue_scripts_bucket_id = var.glue_scripts_bucket_id
