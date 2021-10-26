faeddd5aae257c266b0aa828b9d0b21e55f611e7
b-dalton <ben.dalton@madetech.com>
2021-10-26 14:24:34 +0100

    fix housing repairs address cleaning parameter

--- modules/housing-repairs-google-sheets-cleaning/11-aws-glue-address-cleaning.tf
+++ modules/housing-repairs-google-sheets-cleaning/11-aws-glue-address-cleaning.tf
@@ -8,10 +8,9 @@ module "housing_repairs_google_sheets_address_cleaning" {
     "--extra-py-files"                   = "s3://${var.glue_scripts_bucket_id}/${var.helper_script_key},s3://${var.glue_scripts_bucket_id}/${var.cleaning_helper_script_key}"
     "--source_catalog_database"          = var.refined_zone_catalog_database_name
     "--source_catalog_table"             = "housing_repairs_${replace(var.dataset_name, "-", "_")}_cleaned"
-    "--cleaned_repairs_s3_bucket_target" = "s3://${var.refined_zone_bucket_id}/housing-repairs/${var.dataset_name}/with-cleaned-addresses"
+    "--cleaned_addresses_s3_bucket_target" = "s3://${var.refined_zone_bucket_id}/housing-repairs/${var.dataset_name}/with-cleaned-addresses"
     "--source_address_column_header"     = "property_address"
     "--source_postcode_column_header"    = "None"
-
   }
   script_name            = var.address_cleaning_script_key
   workflow_name          = var.workflow_name
