04e9c839789c62aea092d5736b7f3f2e288644e2
Emma <emma@madetech.com>
2021-10-26 10:18:49 +0100

    Add table prefixes to housing repairs google sheets cleaning jobs

--- modules/housing-repairs-google-sheets-cleaning/10-aws-glue-data-cleaning.tf
+++ modules/housing-repairs-google-sheets-cleaning/10-aws-glue-data-cleaning.tf
@@ -15,6 +15,7 @@ module "housing_repairs_google_sheets_cleaning" {
   triggered_by_crawler   = var.trigger_crawler_name
   glue_scripts_bucket_id = var.glue_scripts_bucket_id
   crawler_details = {
+    table_prefix       = "housing_repairs_${replace(var.dataset_name, "-", "_")}_"
     database_name      = var.catalog_database
     s3_target_location = "s3://${var.refined_zone_bucket_id}/housing-repairs/${var.dataset_name}/cleaned/"
   }
