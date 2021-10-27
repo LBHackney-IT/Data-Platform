ebfcbcec62110354d7fa0304a42d9b297240d4cb
Emma <emma@madetech.com>
2021-10-27 10:44:40 +0100

    Give departments access to any secrets prefixed with their department identifier
    Rename tascomi secrets & housing service account key so be in that space so their glue role has access

--- modules/department/03-input-derived.tf
+++ modules/department/03-input-derived.tf
@@ -2,3 +2,5 @@ locals {
   department_identifier = replace(lower(var.name), "/[^a-zA-Z0-9]+/", "-")
   department_pascalcase = replace(title(replace(var.name, "/[^a-zA-Z0-9]+/", " ")), " ", "")
 }
+
+data "aws_caller_identity" "current" {}
--- terraform/50-aws-secrets-manager.tf
+++ terraform/50-aws-secrets-manager.tf
@@ -18,7 +18,7 @@ resource "aws_secretsmanager_secret" "sheets_credentials_housing" {
   // name_prefix is added here, in case you destroy the secret.
   // Secrets will linger for around 6-7 days in case recovery is required,
   // and you will be unable to create with the same name.
-  name_prefix = "${local.identifier_prefix}-sheets-credential-housing-"
+  name_prefix = "${local.identifier_prefix}/${module.department_housing_repairs.identifier}/sheets-credential-housing-"
 
   kms_key_id = aws_kms_key.secrets_manager_key.id
 }
@@ -30,46 +30,11 @@ resource "aws_secretsmanager_secret_version" "housing_json_credentials_secret_ve
   secret_binary = google_service_account_key.housing_json_credentials[0].private_key
 }
 
-data "aws_iam_policy_document" "housing_secrets_manager_read_only" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "secretsmanager:DescribeSecret",
-      "secretsmanager:GetSecretValue"
-    ]
-    resources = [
-      aws_secretsmanager_secret.sheets_credentials_housing.arn
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "kms:Decrypt",
-      "kms:GenerateDataKey"
-    ]
-    resources = [
-      aws_kms_key.secrets_manager_key.arn
-    ]
-  }
-}
-resource "aws_iam_policy" "housing_secrets_manager_read_only" {
-  tags = module.tags.values
-
-  name   = lower("${local.short_identifier_prefix}housing-secrets-manager-read-only")
-  policy = data.aws_iam_policy_document.housing_secrets_manager_read_only.json
-}
-
-resource "aws_iam_role_policy_attachment" "housing_glue_agent_secrets_manager_read_only" {
-  role       = module.department_housing_repairs.glue_role_arn
-  policy_arn = aws_iam_policy.glue_can_write_to_cloudwatch.arn
-}
-
 // Tascomi Key
 resource "aws_secretsmanager_secret" "tascomi_api_public_key" {
   tags = module.tags.values
 
-  name_prefix = "${local.short_identifier_prefix}tascomi-api-public-key"
+  name_prefix = "${local.identifier_prefix}/${module.department_planning.identifier}/tascomi-api-public-key"
 
   kms_key_id = aws_kms_key.secrets_manager_key.id
 }
@@ -77,43 +42,7 @@ resource "aws_secretsmanager_secret" "tascomi_api_public_key" {
 resource "aws_secretsmanager_secret" "tascomi_api_private_key" {
   tags = module.tags.values
 
-  name_prefix = "${local.short_identifier_prefix}tascomi-api-private-key"
+  name_prefix = "${local.identifier_prefix}/${module.department_planning.identifier}/tascomi-api-private-key"
 
   kms_key_id = aws_kms_key.secrets_manager_key.id
-}
-
-data "aws_iam_policy_document" "tascomi_secrets_manager_read_only" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "secretsmanager:DescribeSecret",
-      "secretsmanager:GetSecretValue"
-    ]
-    resources = [
-      aws_secretsmanager_secret.tascomi_api_public_key.arn,
-      aws_secretsmanager_secret.tascomi_api_public_key.arn
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "kms:Decrypt",
-      "kms:GenerateDataKey"
-    ]
-    resources = [
-      aws_kms_key.secrets_manager_key.arn
-    ]
-  }
-}
-resource "aws_iam_policy" "tascomi_secrets_manager_read_only" {
-  tags = module.tags.values
-
-  name   = lower("${local.short_identifier_prefix}tascomi-secrets-manager-read-only")
-  policy = data.aws_iam_policy_document.tascomi_secrets_manager_read_only.json
-}
-
-resource "aws_iam_role_policy_attachment" "planning_glue_agent_tascomi_secrets_manager_read_only" {
-  role       = module.department_housing_repairs.glue_role_arn
-  policy_arn = aws_iam_policy.glue_can_write_to_cloudwatch.arn
 }
