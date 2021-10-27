4a926c7912a1ff9fd6084fad7e7c5f132f4f9aec
Emma <emma@madetech.com>
2021-10-27 17:10:26 +0100

    Reenable tascomi triggers

--- terraform/24-aws-glue-tascomi-data.tf
+++ terraform/24-aws-glue-tascomi-data.tf
@@ -91,7 +91,7 @@ resource "aws_glue_trigger" "tascomi_tables_daily_ingestion_triggers" {
   name     = "${local.short_identifier_prefix}Tascomi ${title(replace(each.value, "_", " "))} Ingestion Trigger"
   type     = "SCHEDULED"
   schedule = "cron(0 3 * * ? *)"
-  enabled  = false
+  enabled  = true
 
   actions {
     job_name = module.ingest_tascomi_data.job_name
@@ -108,7 +108,7 @@ resource "aws_glue_trigger" "tascomi_tables_weekly_ingestion_triggers" {
   name     = "${local.short_identifier_prefix}Tascomi ${title(replace(each.value, "_", " "))} Ingestion Trigger"
   type     = "SCHEDULED"
   schedule = "cron(0 22 ? * SUN *)"
-  enabled  = false
+  enabled  = true
 
   actions {
     job_name = module.ingest_tascomi_data.job_name
