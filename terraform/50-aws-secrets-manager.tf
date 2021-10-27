f6e4d2e1306320efe49fb74fe7ad5ef58f48ff51
Emma <emma@madetech.com>
2021-10-27 09:41:18 +0100

    Grant permission to department glue jobs to access necessary permissions

--- terraform/50-aws-secrets-manager.tf
+++ terraform/50-aws-secrets-manager.tf
@@ -11,6 +11,7 @@ resource "aws_kms_alias" "key_alias" {
   target_key_id = aws_kms_key.secrets_manager_key.key_id
 }
 
+// Housing Service Account
 resource "aws_secretsmanager_secret" "sheets_credentials_housing" {
   tags = module.tags.values
 
@@ -29,7 +30,42 @@ resource "aws_secretsmanager_secret_version" "housing_json_credentials_secret_ve
   secret_binary = google_service_account_key.housing_json_credentials[0].private_key
 }
 
+data "aws_iam_policy_document" "housing_secrets_manager_read_only" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      aws_secretsmanager_secret.sheets_credentials_housing.arn
+    ]
+  }
 
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = [
+      aws_kms_key.secrets_manager_key.arn
+    ]
+  }
+}
+resource "aws_iam_policy" "housing_secrets_manager_read_only" {
+  tags = module.tags.values
+
+  name   = lower("${local.short_identifier_prefix}housing-secrets-manager-read-only")
+  policy = data.aws_iam_policy_document.housing_secrets_manager_read_only.json
+}
+
+resource "aws_iam_role_policy_attachment" "housing_glue_agent_secrets_manager_read_only" {
+  role       = module.department_housing_repairs.glue_role_arn
+  policy_arn = aws_iam_policy.glue_can_write_to_cloudwatch.arn
+}
+
+// Tascomi Key
 resource "aws_secretsmanager_secret" "tascomi_api_public_key" {
   tags = module.tags.values
 
@@ -44,4 +80,40 @@ resource "aws_secretsmanager_secret" "tascomi_api_private_key" {
   name_prefix = "${local.short_identifier_prefix}tascomi-api-private-key"
 
   kms_key_id = aws_kms_key.secrets_manager_key.id
+}
+
+data "aws_iam_policy_document" "tascomi_secrets_manager_read_only" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      aws_secretsmanager_secret.tascomi_api_public_key.arn,
+      aws_secretsmanager_secret.tascomi_api_public_key.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey"
+    ]
+    resources = [
+      aws_kms_key.secrets_manager_key.arn
+    ]
+  }
+}
+resource "aws_iam_policy" "tascomi_secrets_manager_read_only" {
+  tags = module.tags.values
+
+  name   = lower("${local.short_identifier_prefix}tascomi-secrets-manager-read-only")
+  policy = data.aws_iam_policy_document.tascomi_secrets_manager_read_only.json
+}
+
+resource "aws_iam_role_policy_attachment" "planning_glue_agent_tascomi_secrets_manager_read_only" {
+  role       = module.department_housing_repairs.glue_role_arn
+  policy_arn = aws_iam_policy.glue_can_write_to_cloudwatch.arn
 }
