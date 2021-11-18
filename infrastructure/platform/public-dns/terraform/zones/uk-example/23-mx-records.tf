43bb12b582148f84fac54b6b98bb109da4f221fd
joates-madetech <james.oates@madetech.com>
2021-11-18 16:22:52 +0000

    Changed how the tags module is imported

--- infrastructure/platform/public-dns/terraform/zones/uk-example/23-mx-records.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "example_uk_mx" {
-  zone_id = aws_route53_zone.example_uk.zone_id
-  name    = "example.uk"
-  type    = "MX"
-  ttl     = "3600"
-  records = ["10 mx.1.example.uk", "10 mx.2.example.uk"]
-}
