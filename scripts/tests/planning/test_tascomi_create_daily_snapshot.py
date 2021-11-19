df1a2c8deeadaeb14ad39d16e41ade7d7e75731c
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2021-11-19 11:44:15 +0100

    Tascomi - snapshot date in test script

--- scripts/tests/planning/test_tascomi_create_daily_snapshot.py
+++ scripts/tests/planning/test_tascomi_create_daily_snapshot.py
@@ -2,7 +2,13 @@ from jobs.planning.tascomi_create_daily_snapshot import prepare_increments, appl
 from pyspark.sql import Row
 from datetime import datetime, date
 
+
 class TestSnapshotCreation:
+
+    snapshotYear = str(datetime.now().year)
+    snapshotMonth = str(datetime.now().month).zfill(2)
+    snapshotDay = str(datetime.now().day).zfill(2)
+    snapshotDate = snapshotYear + snapshotMonth + snapshotDay
 
     def test_apply_changed_record(self, spark):
         input_snapshot = [{'id': 1, 'application_stage': 8}]
@@ -19,37 +25,20 @@ class TestSnapshotCreation:
         assert (response == expected)
 
     def test_prepare_several_days_increments(self, spark):
-        input_increment = [{'id': 1, 'application_stage': 5, 'last_updated': datetime(2021, 9, 24, 0, 0, 0)},{'id': 1, 'application_stage': 6, 'last_updated': datetime(2021, 9, 25, 0, 0, 0)}]
-        expected = [{'id': 1, 'application_stage': 6, 'last_updated': datetime(2021, 9, 25, 0, 0, 0)}]
+        '''when applying several days increments containing several changes of the sane application, we should prepare the increment only keeping the latest version'''
+        input_increment = [{'id': 1, 'application_stage': 5, 'last_updated': datetime(2021, 9, 24, 1, 0, 0)},{'id': 1, 'application_stage': 6, 'last_updated': datetime(2021, 9, 25, 0, 0, 0)}]
+        expected = [{'id': 1, 'application_stage': 6, 'last_updated': datetime(2021, 9, 25, 0, 0, 0), 'snapshot_year': self.snapshotYear, 'snapshot_month': self.snapshotMonth, 'snapshot_day': self.snapshotDay, 'snapshot_date': self.snapshotDate}]
         response = self.prepare_increments(spark, input_increment)
         assert (response == expected)
 
     def test_prepare_several_records_increments(self, spark):
+        '''when applying several days increments containing several applications, we should prepare the increment keeping one record per application'''
         input_increment = [{'id': 1, 'application_stage': 5, 'last_updated': datetime(2021, 9, 24, 0, 0, 0)},{'id': 2, 'application_stage': 5, 'last_updated': datetime(2021, 9, 25, 0, 0, 0)}]
-        expected = [{'id': 1, 'application_stage': 5, 'last_updated': datetime(2021, 9, 24, 0, 0, 0), 'snapshot_year': '2021', 'snapshot_month': '9', 'snapshot_day': '24', 'snapshot_date': '2021'},{'id': 2, 'application_stage': 5, 'last_updated': datetime(2021, 9, 25, 0, 0, 0)}]
+        expected = [{'id': 1, 'application_stage': 5, 'last_updated': datetime(2021, 9, 24, 0, 0, 0), 'snapshot_year': self.snapshotYear, 'snapshot_month': self.snapshotMonth, 'snapshot_day': self.snapshotDay, 'snapshot_date': self.snapshotDate},{'id': 2, 'application_stage': 5, 'last_updated': datetime(2021, 9, 25, 0, 0, 0), 'snapshot_year': self.snapshotYear, 'snapshot_month': self.snapshotMonth, 'snapshot_day': self.snapshotDay, 'snapshot_date': self.snapshotDate}]
         response = self.prepare_increments(spark, input_increment)
         assert (response == expected)
   
-    # def test_castColumns_integer(self, spark):
-    #     input_data = [{'id_number': '12'}]
-    #     expected = [{'id_number': 12}]
-    #     response = self.castColumns(spark, self.column_type_dictionary_path, "MyTable", input_data, "integer", IntegerType())
-    #     assert (response == expected)
-
-    # def test_castColumns_boolean(self, spark):
-    #     input_data = [{'flag': 'f'},{'flag': 't'}]
-    #     expected = [{'flag': False},{'flag': True}]
-    #     response = self.castColumns(spark,self.column_type_dictionary_path, "MyTable", input_data, "boolean", BooleanType())
-    #     assert (response == expected)
-
-    
-    # def test_castColumns_when_all_datatypes_not_represented_in_dictionary(self, spark):
-    #     input_data = [{'id_number': '12', 'flag': 'f', 'start_date': '2021-09-24'}]
-    #     expected = [{'id_number': 12, 'flag': False,'start_date': '2021-09-24'}]
-    #     response = self.castColumnsAllTypes(spark, self.column_type_dictionary_partial_path, "MyTable", input_data)
-    #     assert (response == expected)
-
-
+  
     def apply_increments(self, spark, input_snapshot, input_increment):
         query_data_snapshot = spark.createDataFrame(spark.sparkContext.parallelize([Row(**i) for i in input_snapshot]))
         query_data_increment = spark.createDataFrame(spark.sparkContext.parallelize([Row(**i) for i in input_increment]))
