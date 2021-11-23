463bf226571986a7f03979ddb0fbf337aabee725
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2021-11-23 10:52:37 +0100

    change terrform so the jobs don't write to temp

--- terraform/25-aws-glue-job-planning.tf
+++ terraform/25-aws-glue-job-planning.tf
@@ -38,29 +38,28 @@ locals {
 
 module "tascomi_parse_tables_increments" {
   source = "../modules/aws-glue-job"
-  
+
   department        = module.department_planning
   job_name          = "${local.short_identifier_prefix}tascomi_parse_tables_increments_planning"
   helper_module_key = aws_s3_bucket_object.helpers.key
   pydeequ_zip_key   = aws_s3_bucket_object.pydeequ.key
   job_parameters = {
     "--job-bookmark-option"     = "job-bookmark-enable"
-    "--s3_bucket_target"        = "s3://${module.raw_zone.bucket_id}/TEMP/increment/"
+    "--s3_bucket_target"        = "s3://${module.raw_zone.bucket_id}/planning/tascomi/parsed/"
     "--enable-glue-datacatalog" = "true"
     "--source_catalog_database" = aws_glue_catalog_database.raw_zone_tascomi.name
     "--table_list"              = local.table_list
   }
-  script_s3_object_key = aws_s3_bucket_object.tascomi_parse_tables_increments.key
+  script_name = "tascomi_parse_tables_increments"
   # triggered_by_crawler = module.ingest_tascomi_data.crawler_name
 
   crawler_details = {
     database_name      = aws_glue_catalog_database.raw_zone_tascomi.name
-    s3_target_location = "s3://${module.raw_zone.bucket_id}/TEMP/increment/"
-    table_prefix       = "increment_"
+    s3_target_location = "s3://${module.raw_zone.bucket_id}/planning/tascomi/parsed/"
     configuration = jsonencode({
       Version = 1.0
       Grouping = {
-        TableLevelConfiguration = 4
+        TableLevelConfiguration = 5
       }
     })
   }
@@ -76,22 +75,22 @@ module "tascomi_recast_tables_increments" {
   job_parameters = {
     "--job-bookmark-option"     = "job-bookmark-enable"
     "--column_dict_path"        = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.tascomi_column_type_dictionary.key}"
-    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/TEMP/increment/"
+    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/planning/tascomi/increment/"
     "--enable-glue-datacatalog" = "true"
     "--source_catalog_database" = aws_glue_catalog_database.refined_zone_tascomi.name
     "--table_list"              = local.table_list
   }
-  script_s3_object_key = aws_s3_bucket_object.tascomi_recast_tables_increments.key
+  script_name = "tascomi_recast_tables_increments"
   triggered_by_crawler = module.tascomi_parse_tables_increments.crawler_name
 
   crawler_details = {
     database_name      = aws_glue_catalog_database.refined_zone_tascomi.name
-    s3_target_location = "s3://${module.refined_zone.bucket_id}/TEMP/increment/"
+    s3_target_location = "s3://${module.refined_zone.bucket_id}/planning/tascomi/increment/"
     table_prefix       = "increment_"
     configuration = jsonencode({
       Version = 1.0
       Grouping = {
-        TableLevelConfiguration = 4
+        TableLevelConfiguration = 5
       }
     })
   }
@@ -105,22 +104,21 @@ module "tascomi_create_daily_snapshot" {
   helper_module_key = aws_s3_bucket_object.helpers.key
   pydeequ_zip_key   = aws_s3_bucket_object.pydeequ.key
   job_parameters = {
-    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/TEMP/snapshot/"
+    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/planning/tascomi/snapshot/"
     "--enable-glue-datacatalog" = "true"
     "--source_catalog_database" = aws_glue_catalog_database.refined_zone_tascomi.name
     "--table_list"              = local.table_list
   }
-  script_s3_object_key = aws_s3_bucket_object.tascomi_create_daily_snapshot.key
+  script_name = "tascomi_create_daily_snapshot"
   triggered_by_crawler = module.tascomi_recast_tables_increments.crawler_name
 
   crawler_details = {
     database_name      = aws_glue_catalog_database.refined_zone_tascomi.name
-    s3_target_location = "s3://${module.refined_zone.bucket_id}/TEMP/snapshot/"
-    table_prefix       = "snapshot_"
+    s3_target_location = "s3://${module.refined_zone.bucket_id}/planning/tascomi/snapshot/"
     configuration = jsonencode({
       Version = 1.0
       Grouping = {
-        TableLevelConfiguration = 4
+        TableLevelConfiguration = 5
       }
     })
   }
