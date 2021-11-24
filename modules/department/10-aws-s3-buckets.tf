3d0ab4fa9341abfc94426843a573d19a421fc2b9
b-dalton <ben.dalton@madetech.com>
2021-11-24 09:01:54 +0000

    add manual folder in each department directory in landing zone

--- modules/department/10-aws-s3-buckets.tf
+++ modules/department/10-aws-s3-buckets.tf
@@ -1,3 +1,9 @@
+resource "aws_s3_bucket_object" "landing_zone_manual_folder" {
+  bucket       = var.landing_zone_bucket.bucket_id
+  key          = "${local.department_identifier}/manual/"
+  content_type = "application/x-directory"
+}
+
 //resource "aws_s3_bucket_object" "landing_zone_department_folder" {
 //  bucket       = var.landing_zone_bucket_id
 //  acl          = "private"
