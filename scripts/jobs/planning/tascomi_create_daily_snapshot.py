a155b51dd66fd52545d75c7aaec71c59f5f7b3ce
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2021-11-24 15:05:02 +0100

    refactor snapshot script to work from the first iteration

--- scripts/jobs/planning/tascomi_create_daily_snapshot.py
+++ scripts/jobs/planning/tascomi_create_daily_snapshot.py
@@ -9,17 +9,17 @@ import pyspark.sql.functions as F
 from pyspark.sql.functions import col, max, date_format, date_sub, current_date
 from pyspark.sql import Window
 from datetime import datetime
-from helpers.helpers import get_glue_env_var, PARTITION_KEYS, table_exists_in_catalog
+from helpers.helpers import get_glue_env_var, table_exists_in_catalog, createPushDownPredicate
 
 
 
 
-def createPushDownPredicate(daysBuffer):
-    if daysBuffer > 0:
-        pushDownPredicate = f"snapshot_date>=date_format(date_sub(current_date, {daysBuffer}), 'yyyyMMdd')"
-    else:
-        pushDownPredicate = ''
-    return pushDownPredicate
+# def createPushDownPredicate(partitionDateColumn, daysBuffer):
+#     if daysBuffer > 0:
+#         pushDownPredicate = f"{partitionDateColumn}>=date_format(date_sub(current_date, {daysBuffer}), 'yyyyMMdd')"
+#     else:
+#         pushDownPredicate = ''
+#     return pushDownPredicate
     
 def get_latest_snapshot(dfa):
    dfa = dfa.where(col('snapshot_date') == dfa.select(max('snapshot_date')).first()[0])
@@ -43,14 +43,21 @@ def prepare_increments(increment_df):
     increment_df = increment_df.withColumn('latest', F.max('last_updated').over(w))\
     .where(F.col('last_updated') == F.col('latest'))\
     .drop('latest')
-    # Calculate new currency date
-    increment_df = add_snapshot_date_columns(increment_df)
     return increment_df
     
 def apply_increments(snapshot_df, increment_df):
     snapshot_df = snapshot_df.join(increment_df, 'id', 'left_anti')
     snapshot_df = snapshot_df.unionByName(increment_df)
     return snapshot_df
+    
+def loadIncrementsSinceDate(increment_table_name, name_space, date):
+    increment_ddf = glueContext.create_dynamic_frame.from_catalog(
+        name_space=name_space,
+        table_name=increment_table_name,
+        push_down_predicate = f"import_date>{last_snapshot_date}"
+    )
+    increment_df = increment_ddf.toDF()
+    return increment_df
 
 if __name__ == "__main__":
     args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -60,8 +67,8 @@ if __name__ == "__main__":
     s3_bucket_target = get_glue_env_var('s3_bucket_target', '')
 
 
-    conf = SparkConf().set("spark.sql.sources.partitionOverwriteMode","dynamic")
-    sc = SparkContext.getOrCreate(conf)
+    
+    sc = SparkContext.getOrCreate()
     glueContext = GlueContext(sc)
     spark = SparkSession(sc)
     logger = glueContext.get_logger()
@@ -70,49 +77,57 @@ if __name__ == "__main__":
 
     #   load table list
     table_list = table_list_string.split(',')
-    # Prepare pushdown predicate for loading
-    pushDownPredicate = createPushDownPredicate(daysBuffer=5)
-    
+
     for nameOfTableToRecast in table_list:
         snapshot_table_name = f"snapshot_{nameOfTableToRecast}"
         increment_table_name = f"increment_{nameOfTableToRecast}"
         
+        # Snapshot table not in glue catalogue
         if not table_exists_in_catalog(glueContext, snapshot_table_name, source_catalog_database):
-            logger.info(f"Couldn't find table {snapshot_table_name} in database {source_catalog_database}, moving onto next table.")
-            continue
+            logger.info(f"Couldn't find table {snapshot_table_name} in database {source_catalog_database}, creating a snapshot from all the increments, starting from 20210101")
+            # Increment table does not exist in glue catalogue
+            if not table_exists_in_catalog(glueContext, increment_table_name, source_catalog_database):
+                logger.info(f"No snapshot and no increment for {increment_table_name}, going to the next table")
+                continue
+            increment_df = loadIncrementsSinceDate(increment_table_name=increment_table_name, name_space=source_catalog_database, date="20210101")
+            if increment_df.rdd.isEmpty():
+                logger.info(f"No snapshot and no increment for {increment_table_name}, going to the next table")
+                continue
+            # create first snapshot
+            increment_df = prepare_increments(increment_df)
+            snapshot_df = increment_df
         
-        #   load latest snpashot
-        snapshot_ddf = glueContext.create_dynamic_frame.from_catalog(
-            name_space="dataplatform-stg-tascomi-refined-zone",
-            table_name=f"snapshot_{nameOfTableToRecast}",
-            push_down_predicate = pushDownPredicate
-        )
-        snapshot_df = snapshot_ddf.toDF()
-        snapshot_df = get_latest_snapshot(snapshot_df)
-        last_snapshot_date = snapshot_df.select(max('snapshot_date')).first()[0]
-        
-        # load increments later than the last snapshot date
-        if table_exists_in_catalog(glueContext, increment_table_name, source_catalog_database):
-            increment_ddf = glueContext.create_dynamic_frame.from_catalog(
-                name_space="dataplatform-stg-tascomi-refined-zone",
-                table_name=f"increment_{nameOfTableToRecast}",
-                push_down_predicate = f"import_date>{last_snapshot_date}"
-            )
-            increment_df = increment_ddf.toDF()
-
-            if increment_df.count() > 0 :
-                # prepare COU
-                increment_df = prepare_increments(increment_df)
-                # apply COU
-                snapshot_df = apply_increments(snapshot_df, increment_df)
-            else:
-                logger.info(f"No new increment in {increment_table_name}, saving same snapshot as yesterday")
+        # snapshot table in glue catalogue
         else:
-            logger.info(f"Couldn't find table {increment_table_name} in database {source_catalog_database}, saving same snapshot as yesterday")
-
+            pushDownPredicate = createPushDownPredicate(partitionDateColumn='snapshot_date',daysBuffer=10)
+            #   load latest snpashot
+            snapshot_ddf = glueContext.create_dynamic_frame.from_catalog(
+                name_space = source_catalog_database,
+                table_name = snapshot_table_name,
+                push_down_predicate = pushDownPredicate
+            )
+            snapshot_df = snapshot_ddf.toDF()
+            snapshot_df = get_latest_snapshot(snapshot_df)
+            last_snapshot_date = snapshot_df.select(max('snapshot_date')).first()[0]
+        
+            # load increments since the last snapshot date
+            if table_exists_in_catalog(glueContext, increment_table_name, source_catalog_database):
+                increment_df = loadIncrementsSinceDate(increment_table_name=increment_table_name, name_space=source_catalog_database, date=last_snapshot_date)
+                if increment_df.rdd.isEmpty():
+                    logger.info(f"No new increment in {increment_table_name}, saving same snapshot as yesterday")
+                else:
+                    # prepare COU
+                    increment_df = prepare_increments(increment_df)
+                    increment_df = add_snapshot_date_columns(increment_df)
+                    # apply COU
+                    logger.info(f"Applying increment {increment_table_name}")
+                    snapshot_df = apply_increments(snapshot_df, increment_df)
+            else:
+                logger.info(f"Couldn't find table {increment_table_name} in database {source_catalog_database}, saving same snapshot as yesterday")
+            
         # add currency date and set it a partition key
         snapshot_df = add_snapshot_date_columns(snapshot_df)
-        NEW_PARTITION_KEYS = ["snapshot_year", "snapshot_month", "snapshot_day", "snapshot_date"]
+        PARTITION_KEYS = ["snapshot_year", "snapshot_month", "snapshot_day", "snapshot_date"]
         
         # WRITE TO S3
         resultDataFrame = DynamicFrame.fromDF(snapshot_df, glueContext, "resultDataFrame")
@@ -121,7 +136,7 @@ if __name__ == "__main__":
             frame=resultDataFrame,
             connection_type="s3",
             format="parquet",
-            connection_options={"path": target_destination, "partitionKeys": NEW_PARTITION_KEYS},
+            connection_options={"path": target_destination, "partitionKeys": PARTITION_KEYS},
             transformation_ctx="parquetData"
         )
     
