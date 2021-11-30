abb6ef11c0a6dd3b282867532ec73521d922a1e6
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2021-11-30 11:23:10 +0100

    move tascomni ingestion to new increment/snapshot pipeline

--- terraform/25-aws-glue-job-planning.tf
+++ terraform/25-aws-glue-job-planning.tf
@@ -1,3 +1,75 @@
+locals {
+  number_of_workers   = 4
+  max_concurrent_runs = max(length(local.tascomi_table_names), length(local.tascomi_static_tables))
+  tascomi_table_names = [
+    "applications",
+    "contacts",
+    "emails",
+    "enforcements",
+    "fees",
+    "public_comments",
+    "communications",
+    "fee_payments",
+    "appeals",
+    "dtf_locations",
+    "documents",
+    "users"
+  ]
+
+  tascomi_static_tables = [
+    "appeal_decision",
+    "appeal_status",
+    "appeal_types",
+    "breach_types",
+    "committees",
+    "communication_templates",
+    "communication_types",
+    "contact_types",
+    "decision_levels",
+    "decision_types",
+    "document_types",
+    "fee_types",
+    "ps_development_codes",
+    "public_consultations"
+  ]
+
+  table_list = "appeals,applications,contacts,documents,dtf_locations,emails,enforcements,public_comments,communications,fee_payments,appeal_status,appeal_types,committees,communications,communication_types,contact_types,document_types,fee_types,public_consultations,users,ps_development_codes,decision_types,appeal_decision,fees"
+
+}
+
+module "ingest_tascomi_data" {
+  source = "../modules/aws-glue-job"
+
+  department                      = module.department_planning
+  number_of_workers_for_glue_job  = local.number_of_workers
+  max_concurrent_runs_of_glue_job = local.max_concurrent_runs
+  job_name                        = "${local.short_identifier_prefix}tascomi_api_ingestion_planning"
+  helper_module_key               = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                 = aws_s3_bucket_object.pydeequ.key
+  job_parameters = {
+    "--s3_bucket_target"        = module.raw_zone.bucket_id
+    "--s3_prefix"               = "planning/tascomi/api-responses/"
+    "--enable-glue-datacatalog" = "true"
+    "--public_key_secret_id"    = aws_secretsmanager_secret.tascomi_api_public_key.id
+    "--private_key_secret_id"   = aws_secretsmanager_secret.tascomi_api_private_key.id
+    "--number_of_workers"       = local.number_of_workers
+    "--target_database_name"    = aws_glue_catalog_database.raw_zone_tascomi.name
+  }
+  script_name = "tascomi_api_ingestion"
+
+  crawler_details = {
+    database_name      = aws_glue_catalog_database.raw_zone_tascomi.name
+    s3_target_location = "s3://${module.raw_zone.bucket_id}/planning/tascomi/api-responses/"
+    table_prefix       = "api_response_"
+    configuration = jsonencode({
+      Version = 1.0
+      Grouping = {
+        TableLevelConfiguration = 5
+      }
+    })
+  }
+}
+
 module "tascomi_parse_tables_increments" {
   source = "../modules/aws-glue-job"
 
@@ -13,7 +85,7 @@ module "tascomi_parse_tables_increments" {
     "--table_list"              = local.table_list
   }
   script_name = "tascomi_parse_tables_increments"
-  # triggered_by_crawler = module.ingest_tascomi_data.crawler_name
+  triggered_by_crawler = module.ingest_tascomi_data.crawler_name
 
   crawler_details = {
     database_name      = aws_glue_catalog_database.raw_zone_tascomi.name
