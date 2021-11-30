446d33dc21dba6d70c372d8920985d0185cfc8bd
Sandrine Balley <42965897+LBHSBALLEY@users.noreply.github.com>
2021-11-30 15:00:20 +0000

    Tascomi increment and snapshot (#431)
    * move tascomni ingestion to new increment/snapshot pipeline
    
    * add database resources to new tascomi terraform
    
    * add column dictionary to new tascomi terraform
    
    * rename Tascomi terraform to its original name
    
    * Re-add ingestion triggers to Tascomi pipeline

--- terraform/24-aws-glue-tascomi-data.tf
+++ terraform/24-aws-glue-tascomi-data.tf
@@ -1,4 +1,3 @@
-
 locals {
   number_of_workers   = 4
   max_concurrent_runs = max(length(local.tascomi_table_names), length(local.tascomi_static_tables))
@@ -38,24 +37,31 @@ locals {
 
 }
 
-## RAW ZONE
-
-## Glue job, database and crawler
-resource "aws_s3_bucket_object" "ingest_tascomi_data" {
-  bucket = module.glue_scripts.bucket_id
-  key    = "scripts/planning/tascomi_api_ingestion.py"
-  acl    = "private"
-  source = "../scripts/jobs/planning/tascomi_api_ingestion.py"
-  etag   = filemd5("../scripts/jobs/planning/tascomi_api_ingestion.py")
+# Database resources
+resource "aws_glue_catalog_database" "raw_zone_tascomi" {
+  name = "${local.identifier_prefix}-tascomi-raw-zone"
+}
+resource "aws_glue_catalog_database" "refined_zone_tascomi" {
+  name = "${local.identifier_prefix}-tascomi-refined-zone"
 }
 
+# Columns type dictionary resources
+resource "aws_s3_bucket_object" "tascomi_column_type_dictionary" {
+  bucket = module.glue_scripts.bucket_id
+  key    = "scripts/planning/tascomi-column-type-dictionary.json"
+  acl    = "private"
+  source = "../scripts/jobs/planning/tascomi-column-type-dictionary.json"
+  etag   = filemd5("../scripts/jobs/planning/tascomi-column-type-dictionary.json")
+}
+
+# Ingestion
 module "ingest_tascomi_data" {
   source = "../modules/aws-glue-job"
 
   department                      = module.department_planning
   number_of_workers_for_glue_job  = local.number_of_workers
   max_concurrent_runs_of_glue_job = local.max_concurrent_runs
-  job_name                        = "${local.short_identifier_prefix}Ingest tascomi data"
+  job_name                        = "${local.short_identifier_prefix}tascomi_api_ingestion_planning"
   helper_module_key               = aws_s3_bucket_object.helpers.key
   pydeequ_zip_key                 = aws_s3_bucket_object.pydeequ.key
   job_parameters = {
@@ -67,7 +73,7 @@ module "ingest_tascomi_data" {
     "--number_of_workers"       = local.number_of_workers
     "--target_database_name"    = aws_glue_catalog_database.raw_zone_tascomi.name
   }
-  script_s3_object_key = aws_s3_bucket_object.ingest_tascomi_data.key
+  script_name = "tascomi_api_ingestion"
 
   crawler_details = {
     database_name      = aws_glue_catalog_database.raw_zone_tascomi.name
@@ -82,10 +88,7 @@ module "ingest_tascomi_data" {
   }
 }
 
-resource "aws_glue_catalog_database" "raw_zone_tascomi" {
-  name = "${local.identifier_prefix}-tascomi-raw-zone"
-}
-
+# Triggers for ingestion
 resource "aws_glue_trigger" "tascomi_tables_daily_ingestion_triggers" {
   tags     = module.tags.values
   for_each = toset(local.tascomi_table_names)
@@ -121,28 +124,21 @@ resource "aws_glue_trigger" "tascomi_tables_weekly_ingestion_triggers" {
 }
 
 
-resource "aws_s3_bucket_object" "parse_tascomi_tables_script" {
-  bucket = module.glue_scripts.bucket_id
-  key    = "scripts/planning/tascomi_parse_tables.py"
-  acl    = "private"
-  source = "../scripts/jobs/planning/tascomi_parse_tables.py"
-  etag   = filemd5("../scripts/jobs/planning/tascomi_parse_tables.py")
-}
-
-module "parse_tascomi_tables" {
+module "tascomi_parse_tables_increments" {
   source = "../modules/aws-glue-job"
 
   department        = module.department_planning
-  job_name          = "${local.short_identifier_prefix}Parse tascomi tables"
+  job_name          = "${local.short_identifier_prefix}tascomi_parse_tables_increments_planning"
   helper_module_key = aws_s3_bucket_object.helpers.key
   pydeequ_zip_key   = aws_s3_bucket_object.pydeequ.key
   job_parameters = {
+    "--job-bookmark-option"     = "job-bookmark-enable"
     "--s3_bucket_target"        = "s3://${module.raw_zone.bucket_id}/planning/tascomi/parsed/"
     "--enable-glue-datacatalog" = "true"
     "--source_catalog_database" = aws_glue_catalog_database.raw_zone_tascomi.name
     "--table_list"              = local.table_list
   }
-  script_s3_object_key = aws_s3_bucket_object.parse_tascomi_tables_script.key
+  script_name = "tascomi_parse_tables_increments"
   triggered_by_crawler = module.ingest_tascomi_data.crawler_name
 
   crawler_details = {
@@ -157,56 +153,62 @@ module "parse_tascomi_tables" {
   }
 }
 
-## Refined zone
-
-## Glue job, database and crawler
-
-resource "aws_glue_catalog_database" "refined_zone_tascomi" {
-  name = "${local.identifier_prefix}-tascomi-refined-zone"
-}
-
-
-resource "aws_s3_bucket_object" "tascomi_column_type_dictionary" {
-  bucket = module.glue_scripts.bucket_id
-  key    = "scripts/planning/tascomi-column-type-dictionary.json"
-  acl    = "private"
-  source = "../scripts/jobs/planning/tascomi-column-type-dictionary.json"
-  etag   = filemd5("../scripts/jobs/planning/tascomi-column-type-dictionary.json")
-}
-
-resource "aws_s3_bucket_object" "recast_tables_script" {
-  bucket = module.glue_scripts.bucket_id
-  key    = "scripts/recast_tables.py"
-  acl    = "private"
-  source = "../scripts/jobs/recast_tables.py"
-  etag   = filemd5("../scripts/jobs/recast_tables.py")
-}
-
-module "recast_tascomi_tables" {
+module "tascomi_recast_tables_increments" {
   source = "../modules/aws-glue-job"
 
   department        = module.department_planning
-  job_name          = "${local.short_identifier_prefix}Recast tascomi tables"
+  job_name          = "${local.short_identifier_prefix}tascomi_recast_tables_increments_planning"
   helper_module_key = aws_s3_bucket_object.helpers.key
   pydeequ_zip_key   = aws_s3_bucket_object.pydeequ.key
   job_parameters = {
-    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/planning/tascomi/"
+    "--job-bookmark-option"     = "job-bookmark-enable"
     "--column_dict_path"        = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.tascomi_column_type_dictionary.key}"
+    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/planning/tascomi/increment/"
     "--enable-glue-datacatalog" = "true"
-    "--source_catalog_database" = aws_glue_catalog_database.raw_zone_tascomi.name
+    "--source_catalog_database" = aws_glue_catalog_database.refined_zone_tascomi.name
     "--table_list"              = local.table_list
   }
-  script_s3_object_key = aws_s3_bucket_object.recast_tables_script.key
-  triggered_by_crawler = module.parse_tascomi_tables.crawler_name
+  script_name = "tascomi_recast_tables_increments"
+  triggered_by_crawler = module.tascomi_parse_tables_increments.crawler_name
 
   crawler_details = {
     database_name      = aws_glue_catalog_database.refined_zone_tascomi.name
-    s3_target_location = "s3://${module.refined_zone.bucket_id}/planning/tascomi/"
+    s3_target_location = "s3://${module.refined_zone.bucket_id}/planning/tascomi/increment/"
+    table_prefix       = "increment_"
     configuration = jsonencode({
       Version = 1.0
       Grouping = {
-        TableLevelConfiguration = 4
+        TableLevelConfiguration = 5
       }
     })
   }
 }
+
+module "tascomi_create_daily_snapshot" {
+  source = "../modules/aws-glue-job"
+
+  department        = module.department_planning
+  job_name          = "${local.short_identifier_prefix}tascomi_create_daily_snapshot_planning"
+  helper_module_key = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key   = aws_s3_bucket_object.pydeequ.key
+  job_parameters = {
+    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/planning/tascomi/snapshot/"
+    "--enable-glue-datacatalog" = "true"
+    "--source_catalog_database" = aws_glue_catalog_database.refined_zone_tascomi.name
+    "--table_list"              = local.table_list
+  }
+  script_name = "tascomi_create_daily_snapshot"
+  triggered_by_crawler = module.tascomi_recast_tables_increments.crawler_name
+
+  crawler_details = {
+    database_name      = aws_glue_catalog_database.refined_zone_tascomi.name
+    s3_target_location = "s3://${module.refined_zone.bucket_id}/planning/tascomi/snapshot/"
+    configuration = jsonencode({
+      Version = 1.0
+      Grouping = {
+        TableLevelConfiguration = 5
+      }
+    })
+  }
+}
+
