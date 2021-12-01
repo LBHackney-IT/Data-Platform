269b3ee712342e7c3b95aa9734e52b0ffa993c2a
b-dalton <ben.dalton@madetech.com>
2021-12-01 16:44:52 +0000

    remove disabled vpc peering connection

--- terraform-networking/21-vpc-peering-connection.tf
+++ terraform-networking/21-vpc-peering-connection.tf
@@ -22,25 +22,6 @@ module "api_vpc_peering_cross_account" {
   accepter_allow_remote_vpc_dns_resolution = "false"
 }
 
-module "vpc_peering_cross_account" {
-  tags = module.tags.values
-
-  enabled = false
-
-  source = "git::https://github.com/cloudposse/terraform-aws-vpc-peering-multi-account.git?ref=tags/0.16.0"
-  name   = "${local.identifier_prefix}-vpc-peering-connection"
-
-  requester_aws_assume_role_arn             = local.requester_assume_role_arn
-  requester_region                          = var.aws_deploy_region
-  requester_vpc_id                          = module.core_vpc.vpc_id
-  requester_allow_remote_vpc_dns_resolution = "true"
-
-  accepter_aws_assume_role_arn             = local.aws_api_accepter_assume_role_arn
-  accepter_region                          = var.aws_deploy_region
-  accepter_vpc_id                          = var.aws_api_vpc_id
-  accepter_allow_remote_vpc_dns_resolution = "false"
-}
-
 module "housing_vpc_peering_cross_account" {
   tags = module.tags.values
 
