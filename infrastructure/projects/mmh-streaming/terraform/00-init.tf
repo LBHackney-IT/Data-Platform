cb506835afa7d19f0438fe426e4241e2b8164940
shauntillyard <2923115+untitled121@users.noreply.github.com>
2021-12-01 17:28:40 +0000

    Removed duplicate provider

--- infrastructure/projects/mmh-streaming/terraform/00-init.tf
+++ infrastructure/projects/mmh-streaming/terraform/00-init.tf
@@ -9,16 +9,6 @@ provider "aws" {
   }
 }
 
-provider "aws" {
-  alias  = "ireland"
-  region = "eu-west-1"
-
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_deploy_account}:role/${var.aws_deploy_iam_role_name}"
-    session_name = "Terraform"
-  }
-}
-
 # General
 terraform {
   backend "s3" {}
