2ccb654ca89fcb93755a6b531526b86865a3d7eb
shauntillyard <2923115+untitled121@users.noreply.github.com>
2021-12-01 17:28:12 +0000

    Fixed missing inputs

--- infrastructure/projects/mmh-streaming/terraform/01-inputs-required.tf
+++ infrastructure/projects/mmh-streaming/terraform/01-inputs-required.tf
@@ -1,6 +1,23 @@
 variable "aws_deploy_region" {
   description = "AWS region to deploy to"
   type        = string
+  default     = "eu-west-2"
+
+}
+
+variable "aws_deploy_account" {
+  description = "AWS account id to deploy to"
+  type        = string
+}
+
+variable "aws_deploy_iam_role_name" {
+  description = "AWS account id to deploy to"
+  type        = string
+}
+
+variable "core_region" {
+  description = "region to deploy to"
+  type        = string
 }
 
 variable "core_cidr" {
