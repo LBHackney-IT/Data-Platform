a4cc524ae9deadd0ae12af1b1984f80829a058aa
b-dalton <ben.dalton@madetech.com>
2021-12-01 13:01:21 +0000

    comment out unused outputs

--- terraform-networking/99-outputs.tf
+++ terraform-networking/99-outputs.tf
@@ -8,22 +8,22 @@ output "private_subnets_ids" {
   value       = module.core_vpc.private_subnets
 }
 
-output "api_vpc_peer_requester_connection_id" {
-  value       = module.api_vpc_peering_cross_account.requester_connection_id
-  description = "Requester VPC peering connection ID"
-}
-
-output "api_vpc_peer_requester_accept_status" {
-  value       = module.api_vpc_peering_cross_account.requester_accept_status
-  description = "Requester VPC peering connection request status"
-}
-
-output "api_vpc_peer_accepter_connection_id" {
-  value       = module.api_vpc_peering_cross_account.accepter_connection_id
-  description = "Accepter VPC peering connection ID"
-}
-
-output "api_vpc_peer_accepter_accept_status" {
-  value       = module.api_vpc_peering_cross_account.accepter_accept_status
-  description = "Accepter VPC peering connection request status"
-}
+#output "api_vpc_peer_requester_connection_id" {
+#  value       = module.vpc_peering_cross_account.requester_connection_id
+#  description = "Requester VPC peering connection ID"
+#}
+#
+#output "api_vpc_peer_requester_accept_status" {
+#  value       = module.vpc_peering_cross_account.requester_accept_status
+#  description = "Requester VPC peering connection request status"
+#}
+#
+#output "api_vpc_peer_accepter_connection_id" {
+#  value       = module.vpc_peering_cross_account.accepter_connection_id
+#  description = "Accepter VPC peering connection ID"
+#}
+#
+#output "api_vpc_peer_accepter_accept_status" {
+#  value       = module.vpc_peering_cross_account.accepter_accept_status
+#  description = "Accepter VPC peering connection request status"
+#}
