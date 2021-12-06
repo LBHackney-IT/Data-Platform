fbcba9bc42f9a3f0df4c2fa33a97e4d9819265f5
b-dalton <ben.dalton@madetech.com>
2021-12-06 15:25:55 +0000

    remove invalid instance profile

--- terraform/test-terraform-import.tf
+++ terraform/test-terraform-import.tf
@@ -3,5 +3,4 @@ resource "aws_instance" "test_terraform_import" {
   ami                  = "ami-0d37e07bd4ff37148"
   instance_type        = "t2.micro"
   subnet_id            = "subnet-09de42c945f5507df"
-  iam_instance_profile = "el-ec2"
 }
