e6d9b3b38e8deeacee1dfdf7b865d6a05be4d2ca
b-dalton <ben.dalton@madetech.com>
2021-12-06 14:57:49 +0000

    update instance resource to import

--- terraform/test-terraform-import.tf
+++ terraform/test-terraform-import.tf
@@ -1,5 +1,7 @@
 resource "aws_instance" "test_terraform_import" {
-  tags = module.tags.values
-  ami           = "ami-0d37e07bd4ff37148"
-  instance_type = "t2.micro"
+  tags                 = module.tags.values
+  ami                  = "ami-0d37e07bd4ff37148"
+  instance_type        = "t2.micro"
+  subnet_id            = "subnet-09de42c945f5507df"
+  iam_instance_profile = "el-ec2"
 }
