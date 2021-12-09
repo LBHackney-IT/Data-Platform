8db8f5a7e0900c7a281dc38a6bcce7ac3769c09d
joates-madetech <80255046+joates-madetech@users.noreply.github.com>
2021-12-09 10:30:00 +0000

    Remove unrequired Qlik equipment (#440)

--- modules/qlik-sense-server/03-input-derived.tf
+++ modules/qlik-sense-server/03-input-derived.tf
@@ -1,8 +1,10 @@
-resource "random_id" "random_subnet" {
-  byte_length = 2
-}
+data "aws_caller_identity" "current" {}
 
-locals {
-  subnet_ids_random_index = random_id.random_subnet.dec % length(var.vpc_subnet_ids)
-  instance_subnet_id      = var.vpc_subnet_ids[local.subnet_ids_random_index]
-}
+#resource "random_id" "random_subnet" {
+#  byte_length = 2
+#}
+#
+#locals {
+#  subnet_ids_random_index = random_id.random_subnet.dec % length(var.vpc_subnet_ids)
+#  instance_subnet_id      = var.vpc_subnet_ids[local.subnet_ids_random_index]
+#}
