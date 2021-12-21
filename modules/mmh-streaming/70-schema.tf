5fa1a4688d442e3d72338571b09bf6fb957a8fac
blu3pr1n7 <asdf>
2021-12-21 11:34:36 +0000

    Updated schema name mmh_schema -> mmh_tenure_schema

--- modules/mmh-streaming/70-schema.tf
+++ modules/mmh-streaming/70-schema.tf
@@ -3,7 +3,7 @@ resource "aws_glue_registry" "lbh_data_platform_schema_registry" {
   tags          = var.tags
 }
 
-resource "aws_glue_schema" "mmh_schema" {
+resource "aws_glue_schema" "mmh_tenure_schema" {
   schema_name       = "${var.identifier_prefix}mmh"
   registry_arn      = aws_glue_registry.lbh_data_platform_schema_registry.arn
   data_format       = "AVRO"
