c87b1b49e02a5cfeb30cbcd0a9fe83aac0cb12ba
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2022-01-04 22:59:09 +0100

    Levenshtein: refactor into 2 functions and remove match_type UDF

--- scripts/jobs/levenshtein_address_matching.py
+++ scripts/jobs/levenshtein_address_matching.py
@@ -11,47 +11,8 @@ from pyspark.sql.types import StringType
 
 from helpers.helpers import get_glue_env_var, get_latest_partitions, PARTITION_KEYS, create_pushdown_predicate
 
-# create a classification function
-def match_type(levenshtein):
-    if levenshtein == 0:
-        return 'perfect match'
-    elif levenshtein > 15:
-        return 'unmatched'
-    else:
-        return 'imperfect match'
 
-
-if __name__ == "__main__":
-    glueContext = GlueContext(SparkContext.getOrCreate())
-    logger = glueContext.get_logger()
-    job = Job(glueContext)
-
-
-    addresses_api_data_database = get_glue_env_var('addresses_api_data_database', '')
-    addresses_api_data_table = get_glue_env_var('addresses_api_data_table', '')
-    source_catalog_database = get_glue_env_var('source_catalog_database', '')
-    source_catalog_table = get_glue_env_var('source_catalog_table', '')
-    target_destination = get_glue_env_var('target_destination', '')
-    match_to_property_shell = get_glue_env_var('match_to_property_shell', '')
-
-    PARTITION_KEYS.append("data_source")
-
-    # convert the classification function to a UDF Function by passing in the function and the return type of function (string in this case)
-    udf_matchtype = F.udf(match_type, StringType())
-
-
-    # PREP QUERY DATA
-
-    query_addresses = glueContext.create_dynamic_frame.from_catalog(
-        name_space=source_catalog_database,
-        table_name=source_catalog_table,
-    )
-
-    query_addresses = query_addresses.toDF()
-    query_addresses = get_latest_partitions(query_addresses)
-    logger.info(f"Getting latest partitions query addresses from catalog: {query_addresses}")
-    logger.info(f"number of query addresses records: {query_addresses.count()}")
-
+def prep_source_data(query_addresses):
     query_addresses_sample = query_addresses.filter(
         "concatenated_string_to_match != ''")
     query_addresses_sample = query_addresses_sample.filter("uprn is null")
@@ -63,21 +24,11 @@ if __name__ == "__main__":
 
     query_concat = query_concat.select(
         "prinx", "query_address", "postcode").withColumnRenamed("postcode", "query_postcode")
+    
+    return query_concat
 
-
-    # PREP ADDRESSES API DATA
-
-    pushDownPredicate = create_pushdown_predicate(partitionDateColumn='import_date',daysBuffer=3)
-    target_addresses = glueContext.create_dynamic_frame.from_catalog(
-        name_space=addresses_api_data_database,
-        table_name=addresses_api_data_table,
-        push_down_predicate = pushDownPredicate
-    )
-
-    target_addresses = target_addresses.toDF()
-    target_addresses = get_latest_partitions(target_addresses)
-    logger.info(f"Getting addresses API data latest partitions: {target_addresses}, records: {target_addresses.count()}")
-
+def prep_addresses_api_data(target_addresses, match_to_property_shell):
+    
     # set property shell preferences
     if match_to_property_shell == 'force': 
         target_addresses = target_addresses.where("blpu_class LIKE 'P%'")
@@ -117,9 +68,11 @@ if __name__ == "__main__":
         F.concat_ws(" ", "line1", "line2", "postcode")
     )
 
-
     target_concat = target_concat.select("target_address", "target_address_short", "target_address_medium", 
                                         "postcode", "uprn", "blpu_class").withColumnRenamed("postcode", "target_postcode")
+    return target_concat
+
+def match_addresses(query_concat, target_concat, logger):
 
     # COMPARE QUERY AND TARGET
 
@@ -141,7 +94,7 @@ if __name__ == "__main__":
 
     # ROUND 0 - look for perfect
     perfectFull = cross_compare.filter("levenshtein = 0").dropDuplicates(['prinx'])
-    perfectFull = perfectFull.withColumn("match_type", udf_matchtype("levenshtein"))
+    perfectFull = perfectFull.withColumn("match_type", F.lit("perfect"))
     perfectFull = perfectFull.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), "match_type")
 
 
@@ -151,7 +104,7 @@ if __name__ == "__main__":
         .filter("levenshtein_short = 0")\
         .dropDuplicates(['prinx'])
 
-    perfectShort = perfectShort.withColumn("match_type", udf_matchtype("levenshtein"))
+    perfectShort = perfectShort.withColumn("match_type", F.lit("perfect on first line and postcode"))
     perfectShort = perfectShort.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), "match_type")
 
 
@@ -162,7 +115,7 @@ if __name__ == "__main__":
         .filter("levenshtein_medium = 0")\
         .dropDuplicates(['prinx'])
 
-    perfectMedium = perfectMedium.withColumn("match_type", udf_matchtype("levenshtein"))
+    perfectMedium = perfectMedium.withColumn("match_type", F.lit("perfect on first 2 lines and postcode"))
     perfectMedium = perfectMedium.select(F.col("prinx"), F.col("query_address"), 
     F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), "match_type")
 
@@ -173,7 +126,7 @@ if __name__ == "__main__":
         .join(perfectMedium, "prinx", "left_anti")\
         .filter("levenshtein_10char = 0").dropDuplicates(['prinx'])
 
-    perfect10char = perfect10char.withColumn("match_type", udf_matchtype("levenshtein"))
+    perfect10char = perfect10char.withColumn("match_type", F.lit("perfect on first 10 characters and postcode"))
     perfect10char = perfect10char.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), "match_type")
 
 
@@ -196,15 +149,14 @@ if __name__ == "__main__":
         .filter('rank = 1')\
         .dropDuplicates(['prinx'])
 
-    # apply function for match_type
-    bestMatch_round1 = bestMatch_round1.withColumn(
-        "match_type", udf_matchtype("levenshtein"))
+    bestMatch_round1 = bestMatch_round1.withColumn("match_type", F.lit("Levenshtein < 3 on full address, same postcode"))
     bestMatch_round1 = bestMatch_round1.withColumn("round", F.lit("round 1"))
 
     bestMatch_round1 = bestMatch_round1.select("prinx", "query_address", F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), "match_type", "round")
 
     logger.info(f"Round 1 best match: {bestMatch_round1}")
     logger.info(f"Round 1 best match record count: {bestMatch_round1.count()}")
+    
     # ROUND 2
 
     cross_compare = cross_compare.join(bestMatch_round1, "prinx", "left_anti")
@@ -215,8 +167,7 @@ if __name__ == "__main__":
         .filter('rank = 1')\
         .dropDuplicates(['prinx'])
 
-    bestMatch_round2 = bestMatch_round2.withColumn(
-        "match_type", udf_matchtype("levenshtein_short"))
+    bestMatch_round2 = bestMatch_round2.withColumn("match_type", F.lit("Levenshtein < 3 on first address line, same postcode"))
     bestMatch_round2 = bestMatch_round2.withColumn("round", F.lit("round 2"))
     bestMatch_round2 = bestMatch_round2.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), F.col("match_type"), "round")
 
@@ -234,8 +185,7 @@ if __name__ == "__main__":
         .filter('rank = 1')\
         .dropDuplicates(['prinx'])
 
-    bestMatch_round3 = bestMatch_round3.withColumn(
-        "match_type", udf_matchtype("levenshtein_medium"))
+    bestMatch_round3 = bestMatch_round3.withColumn("match_type", F.lit("Levenshtein < 3 on first 2 address lines, same postcode"))
     bestMatch_round3 = bestMatch_round3.withColumn("round", F.lit("round 3"))
     bestMatch_round3 = bestMatch_round3.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), F.col("match_type"), "round")
 
@@ -269,7 +219,7 @@ if __name__ == "__main__":
         .dropDuplicates(['prinx'])
 
     bestMatch_round4 = bestMatch_round4.withColumn(
-        "match_type", F.lit("imperfect match"))
+        "match_type", F.lit("Levenshtein < 5 on full address, postcode may differ"))
     bestMatch_round4 = bestMatch_round4.withColumn("round", F.lit("round 4"))
 
     bestMatch_round4 = bestMatch_round4.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), F.col("match_type"), "round")
@@ -289,7 +239,7 @@ if __name__ == "__main__":
         .dropDuplicates(['prinx'])
 
     bestMatch_round5 = bestMatch_round5.withColumn(
-        "match_type", F.lit("imperfect match"))
+        "match_type", F.lit("Levenshtein < 5 on first address line, postcode may differ"))
     bestMatch_round5 = bestMatch_round5.withColumn("round", F.lit("round 5"))
 
     bestMatch_round5 = bestMatch_round5.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), F.col("match_type"), "round")
@@ -310,13 +260,14 @@ if __name__ == "__main__":
         .dropDuplicates(['prinx'])
 
     bestMatch_round6 = bestMatch_round6.withColumn(
-        "match_type", F.lit("imperfect match"))
+        "match_type", F.lit("Levenshtein < 5 on first 2 address lines, postcode may differ"))
     bestMatch_round6 = bestMatch_round6.withColumn("round", F.lit("round 6"))
 
     bestMatch_round6 = bestMatch_round6.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), F.col("match_type"), "round")
 
     logger.info(f"Round 6 best match: {bestMatch_round6}")
     logger.info(f"Round 6 best match record count: {bestMatch_round6.count()}")
+    
     # ROUND 7 - last chance
 
     # take all the rest (including empty postcodes) and mark as unmatched
@@ -339,7 +290,7 @@ if __name__ == "__main__":
         .filter('rank = 1')\
         .dropDuplicates(['prinx'])
 
-    bestMatch_lastChance = bestMatch_lastChance.withColumn("match_type", udf_matchtype("levenshtein"))
+    bestMatch_lastChance = bestMatch_lastChance.withColumn("match_type", F.lit("Best match after all constrained rounds. Postcode may differ."))
     bestMatch_lastChance = bestMatch_lastChance.withColumn("round", F.lit("last chance"))
     bestMatch_lastChance = bestMatch_lastChance.select(F.col("prinx"), F.col("query_address"), F.col("uprn").alias("matched_uprn"), F.col("target_address").alias("matched_address"), F.col("blpu_class").alias("matched_blpu_class"), F.col("match_type"), "round")
 
@@ -353,12 +304,64 @@ if __name__ == "__main__":
 
     logger.info(f"all round results together: {all_best_match}")
     logger.info(f"all round results together: {all_best_match.count()}")
-    # JOIN MATCH RESULTS WITH INITIAL DATASET
+    
+    return all_best_match
+    
 
+if __name__ == "__main__":
+    glueContext = GlueContext(SparkContext.getOrCreate())
+    logger = glueContext.get_logger()
+    job = Job(glueContext)
+
+
+    addresses_api_data_database = get_glue_env_var('addresses_api_data_database', '')
+    addresses_api_data_table = get_glue_env_var('addresses_api_data_table', '')
+    source_catalog_database = get_glue_env_var('source_catalog_database', '')
+    source_catalog_table = get_glue_env_var('source_catalog_table', '')
+    target_destination = get_glue_env_var('target_destination', '')
+    match_to_property_shell = get_glue_env_var('match_to_property_shell', '')
+
+    PARTITION_KEYS.append("data_source")
+
+
+    # PREP QUERY DATA
+
+    query_addresses = glueContext.create_dynamic_frame.from_catalog(
+        name_space=source_catalog_database,
+        table_name=source_catalog_table,
+    )
+
+    query_addresses = query_addresses.toDF()
+    query_addresses = get_latest_partitions(query_addresses)
+    logger.info(f"Getting latest partitions query addresses from catalog: {query_addresses}")
+    logger.info(f"number of query addresses records: {query_addresses.count()}")
+
+    query_concat = prep_source_data(query_addresses)
+
+    # PREP ADDRESSES API DATA
+
+    pushDownPredicate = create_pushdown_predicate(partitionDateColumn='import_date',daysBuffer=3)
+    target_addresses = glueContext.create_dynamic_frame.from_catalog(
+        name_space=addresses_api_data_database,
+        table_name=addresses_api_data_table,
+        push_down_predicate = pushDownPredicate
+    )
+
+    target_addresses = target_addresses.toDF()
+    target_addresses = get_latest_partitions(target_addresses)
+    logger.info(f"Getting addresses API data latest partitions: {target_addresses}, records: {target_addresses.count()}")
+
+    target_concat = prep_addresses_api_data(target_addresses, match_to_property_shell)
+
+    # MATCH QUERY AND TARGET
+    all_best_match = match_addresses(query_concat, target_concat, logger)
+
+    # JOIN MATCH RESULTS WITH INITIAL DATASET
     matchingResults = query_addresses.join(all_best_match, "prinx", "left")
     logger.info(f"joined match results with initial dataset: {matchingResults}")
     logger.info(f"joined match results with initial dataset: {matchingResults.count()}")
 
+    # WRITE RESULTS
     resultDataFrame = DynamicFrame.fromDF(
         matchingResults, glueContext, "resultDataFrame")
 
