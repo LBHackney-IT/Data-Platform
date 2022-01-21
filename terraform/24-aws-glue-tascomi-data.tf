d749651f5ab8fb3a9133741d0f79af2ffde9ed39
Marta <marta.villalobos@hackney.gov.uk>
2022-01-21 11:45:25 +0000

    add committee_application_map table to terraform tascomi

--- terraform/24-aws-glue-tascomi-data.tf
+++ terraform/24-aws-glue-tascomi-data.tf
@@ -30,7 +30,8 @@ locals {
     "document_types",
     "fee_types",
     "ps_development_codes",
-    "public_consultations"
+    "public_consultations",
+    "committee_application_map"
   ]
 
   table_list = join(",", concat(local.tascomi_table_names, local.tascomi_static_tables))
