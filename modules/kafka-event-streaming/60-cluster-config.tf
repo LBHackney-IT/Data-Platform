36fc9f06afd27ba354ccfdb3425d59cc35886829
Emma <emma@madetech.com>
2022-01-25 12:18:11 +0000

    Link configuration to the cluster

--- modules/kafka-event-streaming/60-cluster-config.tf
+++ modules/kafka-event-streaming/60-cluster-config.tf
@@ -1,9 +1,21 @@
-#resource "aws_msk_configuration" "mmh_msk_config" {
-#  kafka_versions = ["2.1.0"]
-#  name           = "${var.identifier_prefix}mmh"
-#
-#  server_properties = <<PROPERTIES
-#auto.create.topics.enable = true
-#delete.topic.enable = true
-#PROPERTIES
-#}
+resource "aws_msk_configuration" "event_streaming" {
+  kafka_versions = ["2.1.0"]
+# TODO: Rename config name
+  name           = "${var.identifier_prefix}mmh"
+
+  server_properties = <<PROPERTIES
+auto.create.topics.enable=true
+default.replication.factor=3
+min.insync.replicas=2
+num.io.threads=8
+num.network.threads=5
+num.partitions=1
+num.replica.fetchers=2
+replica.lag.time.max.ms=30000
+socket.receive.buffer.bytes=102400
+socket.request.max.bytes=104857600
+socket.send.buffer.bytes=102400
+unclean.leader.election.enable=true
+zookeeper.session.timeout.ms=18000
+PROPERTIES
+}
