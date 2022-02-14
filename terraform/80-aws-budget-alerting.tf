fd692641437d690d3530fdbdab1e9124af7819d7
Ben Reynolds-Carr <ben.reynolds-carr@hackney.gov.uk>
2022-02-14 16:46:29 +0000

    WIP: Budget Alerting
    TODO: Add lambda python

--- terraform/80-aws-budget-alerting.tf
+++ terraform/80-aws-budget-alerting.tf
@@ -1,9 +1,45 @@
+resource "aws_sns_topic" "budget_notification" {
+  name = "budget_notification"
+}
+
+resource "aws_sns_topic_policy" "budget_notification_policy" {
+  arn = aws_sns_topic.budget_notification
+
+  policy = data.aws_iam_policy_document.budget_notification_policy.json
+}
+
+data "aws_iam_policy_document" "budget_notification_policy" {
+  statement {
+    actions = [
+      "SNS:Publish"
+    ]
+    effect = "Allow"
+
+    resources = [
+      aws_sns_topic.budget_notification.arn
+    ]
+
+    principals {
+      identifiers = [
+        "budgets.amazonaws.com"
+      ]
+      type = "Service"
+    }
+  }
+}
+
 resource "aws_budgets_budget" "all-cost-budget" {
   name         = "all-cost-budget"
   budget_type  = "COST"
-  limit_amount = "10"
+  limit_amount = "1000" # Initial value. Will be overwritten by the scheduled lambda function
   limit_unit   = "USD"
-  time_unit    = "DAILY"
+  time_unit    = "MONTHLY"
+
+  lifecycle {
+    ignore_changes = [
+      limit_amount,
+    ]
+  }
 
   notification {
     comparison_operator        = "GREATER_THAN"
@@ -17,13 +53,19 @@ resource "aws_budgets_budget" "all-cost-budget" {
 resource "aws_budgets_budget" "monthly_cost_forecast" {
   name         = "monthly-cost-forecast"
   budget_type  = "COST"
-  limit_amount = "10"
+  limit_amount = "1000"
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 
+  lifecycle {
+    ignore_changes = [
+      limit_amount,
+    ]
+  }
+
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = "10"
+    threshold                  = "100"
     threshold_type             = "PERCENTAGE"
     notification_type          = "FORECASTED"
     subscriber_email_addresses = ["ben.reynolds-carr@hackney.gov.uk"]
