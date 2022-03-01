39088b5df163bb931efc42ba44a82bce1f54c52c
joates-madetech <james.oates@madetech.com>
2022-03-01 16:49:58 +0000

    Applyed AWS provider version restriction due to breaking change

--- terraform/07-qlik-server.tf
+++ terraform/07-qlik-server.tf
@@ -1,5 +1,5 @@
 module "qlik_server" {
-  count = local.is_live_environment ? 1 : 0
+  //count = local.is_live_environment ? 1 : 0
 
   source                  = "../modules/qlik-sense-server"
   tags                    = module.tags.values
