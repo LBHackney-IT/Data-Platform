be8e935cd823b8f08423359b4fdaf6089bac6339
b-dalton <ben.dalton@madetech.com>
2022-03-02 13:28:53 +0000

    Refactor test set up

--- scripts/tests/test_update_table_ingestion_details.py
+++ scripts/tests/test_update_table_ingestion_details.py
@@ -3,10 +3,6 @@ from helpers.database_ingestion_helpers import update_table_ingestion_details
 class TestDatabaseIngestionHelpers:
   def test_update_table_ingestion_details_adds_one_table_ingestion_details(self):
     table = []
-    table_name = "testTable"
-    minutes_taken = 3
-    error = "False"
-    error_details = "None"
 
     expected_response = [
       {
@@ -17,7 +13,7 @@ class TestDatabaseIngestionHelpers:
       }
     ]
 
-    update_table_ingestion_details(table, table_name, minutes_taken, error, error_details)
+    update_table_ingestion_details(table, table_name="testTable", minutes_taken=3, error="False", error_details="None")
 
     assert table == expected_response
 
