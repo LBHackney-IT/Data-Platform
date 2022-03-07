885e903c67ad62e7867f0eb7af1c6d9d861f7510
daroclark <daro.clark@hackney.gov.uk>
2022-03-07 13:22:18 +0000

    Update 27-xlsx-imports-from-g-drive.tf
    Adding files for env enforcement

--- terraform/27-xlsx-imports-from-g-drive.tf
+++ terraform/27-xlsx-imports-from-g-drive.tf
@@ -72,3 +72,108 @@ module "repairs_fire_alarm_aov" {
     }
   }
 }
+    
+module "env_enforcement_estate_cleaning {
+  count = local.is_live_environment ? 1 : 0
+
+  source                         = "../modules/import-xlsx-file-from-g-drive"
+  department                     = module.department_env_enforcement
+  glue_scripts_bucket_id         = module.glue_scripts.bucket_id
+  glue_catalog_database_name     = module.department_env_enforcement.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_id    = module.glue_temp_storage.bucket_url
+  glue_role_arn                  = aws_iam_role.glue_role.arn
+  helper_module_key              = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = aws_s3_bucket_object.pydeequ.key
+  jars_key                       = aws_s3_bucket_object.jars.key
+  xlsx_import_script_key         = aws_s3_bucket_object.xlsx_import_script.key
+  identifier_prefix              = local.short_identifier_prefix
+  lambda_artefact_storage_bucket = module.lambda_artefact_storage.bucket_id
+  landing_zone_bucket_id         = module.landing_zone.bucket_id
+  landing_zone_kms_key_arn       = module.landing_zone.kms_key_arn
+  landing_zone_bucket_arn        = module.landing_zone.bucket_arn
+  google_sheets_document_id      = "1brqikq1CaMGqujqWTKa4S71NYoOpDP2p"
+  glue_job_name                  = "Estate Cleaning"
+  output_folder_name             = "estate-cleaning"
+  raw_zone_bucket_id             = module.raw_zone.bucket_id
+  input_file_name                = "c_r_cleaner_report__areas_1_-4_from_january_2022.xlsx"
+  worksheets = {
+    sheet1 : {
+      header_row_number = 1
+      worksheet_name    = "Estate cleaning Area 1"
+    }
+    sheet2 : {
+      header_row_number = 1
+      worksheet_name    = "Estate cleaning Area 2"
+    }
+    sheet3 : {
+      header_row_number = 1
+      worksheet_name    = "Estate cleaning Area 3"
+    }
+    sheet4 : {
+      header_row_number = 1
+      worksheet_name    = "Estate cleaning Area 4"
+    }
+  }
+}
+
+module "env_enforcement_fix_my_street_noise {
+  count = local.is_live_environment ? 1 : 0
+
+  source                         = "../modules/import-xlsx-file-from-g-drive"
+  department                     = module.department_env_enforcement
+  glue_scripts_bucket_id         = module.glue_scripts.bucket_id
+  glue_catalog_database_name     = module.department_env_enforcement.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_id    = module.glue_temp_storage.bucket_url
+  glue_role_arn                  = aws_iam_role.glue_role.arn
+  helper_module_key              = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = aws_s3_bucket_object.pydeequ.key
+  jars_key                       = aws_s3_bucket_object.jars.key
+  xlsx_import_script_key         = aws_s3_bucket_object.xlsx_import_script.key
+  identifier_prefix              = local.short_identifier_prefix
+  lambda_artefact_storage_bucket = module.lambda_artefact_storage.bucket_id
+  landing_zone_bucket_id         = module.landing_zone.bucket_id
+  landing_zone_kms_key_arn       = module.landing_zone.kms_key_arn
+  landing_zone_bucket_arn        = module.landing_zone.bucket_arn
+  google_sheets_document_id      = "1kUizzzxaD6T1qX2hMNIrxuowdWUCxvbP"
+  glue_job_name                  = Fix My Street Noise"
+  output_folder_name             = "fix-my-street-noise"
+  raw_zone_bucket_id             = module.raw_zone.bucket_id
+  input_file_name                = "noise_fms_01012021_to_date.xlsx"
+  worksheets = {
+    sheet1 : {
+      header_row_number = 1
+      worksheet_name    = "Sheet1"
+    }
+  }
+}
+
+module "env_enforcement_cc_tv {
+  count = local.is_live_environment ? 1 : 0
+
+  source                         = "../modules/import-xlsx-file-from-g-drive"
+  department                     = module.department_env_enforcement
+  glue_scripts_bucket_id         = module.glue_scripts.bucket_id
+  glue_catalog_database_name     = module.department_env_enforcement.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_id    = module.glue_temp_storage.bucket_url
+  glue_role_arn                  = aws_iam_role.glue_role.arn
+  helper_module_key              = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = aws_s3_bucket_object.pydeequ.key
+  jars_key                       = aws_s3_bucket_object.jars.key
+  xlsx_import_script_key         = aws_s3_bucket_object.xlsx_import_script.key
+  identifier_prefix              = local.short_identifier_prefix
+  lambda_artefact_storage_bucket = module.lambda_artefact_storage.bucket_id
+  landing_zone_bucket_id         = module.landing_zone.bucket_id
+  landing_zone_kms_key_arn       = module.landing_zone.kms_key_arn
+  landing_zone_bucket_arn        = module.landing_zone.bucket_arn
+  google_sheets_document_id      = "13uiSwGDj-EPTVTUJtJgqbz2UabyRuFmw"
+  glue_job_name                  = CCTV"
+  output_folder_name             = "cc-tv"
+  raw_zone_bucket_id             = module.raw_zone.bucket_id
+  input_file_name                = "cctv.xlsx"
+  worksheets = {
+    sheet1 : {
+      header_row_number = 1
+      worksheet_name    = "Sheet1"
+    }
+  }
+}
