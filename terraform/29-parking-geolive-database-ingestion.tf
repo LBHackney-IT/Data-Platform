2aa25ab319a1963c3c7afc03e68f8d63b797bfb2
b-dalton <ben.dalton@madetech.com>
2022-03-07 16:00:24 +0000

    Format file

--- terraform/29-parking-geolive-database-ingestion.tf
+++ terraform/29-parking-geolive-database-ingestion.tf
@@ -18,27 +18,27 @@ module "parking_geolive_database_ingestion" {
 module "parking_geolive_ingestion_job" {
   source = "../modules/aws-glue-job"
 
-  department  = module.department_parking
-  job_name    = "${local.short_identifier_prefix}geolive parking schema ingestion"
-  script_name = "ingest_database_tables_via_jdbc_connection"
-  helper_module_key = aws_s3_bucket_object.helpers.key
-  pydeequ_zip_key = aws_s3_bucket_object.pydeequ.key
-  jdbc_connections = [module.parking_geolive_database_ingestion[0].jdbc_connection_name]
+  department           = module.department_parking
+  job_name             = "${local.short_identifier_prefix}geolive parking schema ingestion"
+  script_name          = "ingest_database_tables_via_jdbc_connection"
+  helper_module_key    = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key      = aws_s3_bucket_object.pydeequ.key
+  jdbc_connections     = [module.parking_geolive_database_ingestion[0].jdbc_connection_name]
   triggered_by_crawler = module.parking_geolive_database_ingestion[0].crawler_name
-  workflow_name = module.parking_geolive_database_ingestion[0].workflow_name
+  workflow_name        = module.parking_geolive_database_ingestion[0].workflow_name
   job_parameters = {
-    "--s3_ingestion_bucket_target" = "s3://${module.raw_zone.bucket_id}/parking/geolive/"
+    "--s3_ingestion_bucket_target"  = "s3://${module.raw_zone.bucket_id}/parking/geolive/"
     "--s3_ingestion_details_target" = "s3://${module.raw_zone.bucket_id}/parking/geolive/ingestion-details/"
-    "--source_data_database" = module.parking_geolive_database_ingestion[0].ingestion_database_name
+    "--source_data_database"        = module.parking_geolive_database_ingestion[0].ingestion_database_name
   }
   crawler_details = {
     database_name      = module.parking_geolive_database_ingestion[0].ingestion_database_name
     s3_target_location = "s3://${module.raw_zone.bucket_id}/parking/geolive/"
     configuration = jsonencode({
-        Version = 1.0
-        Grouping = {
-            TableLevelConfiguration = 4
-        }
+      Version = 1.0
+      Grouping = {
+        TableLevelConfiguration = 4
+      }
     })
   }
 }
