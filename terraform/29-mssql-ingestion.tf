d78fb89a7878e608a787fd310f5890a45c2d7351
Ben Dalton <70905620+b-dalton@users.noreply.github.com>
2022-03-10 10:27:13 +0000

    Add glue jobs to copy benefits-housing-needs & revenues to raw zone (#530)
    * Add glue jobs to move housing-and-benefits & revenues to raw zone
    
    * Rename script and glue job resources
    
    * rename resources
    
    * Fix reference to department module
    
    * Update name of target s3 prefix housing benefits needs
    
    * Rename resources to reflect department names
    
    * Remove duplication in jdbc connection resource names

--- terraform/29-mssql-ingestion.tf
+++ terraform/29-mssql-ingestion.tf
@@ -4,7 +4,7 @@ module "academy_mssql_database_ingestion" {
 
   source = "../modules/database-ingestion-via-jdbc-connection"
 
-  name                        = "revenue-benefits-and-council-tax"
+  name                        = "academy-benefits-housing-needs-and-revenues"
   jdbc_connection_url         = "jdbc:sqlserver://10.120.23.22:1433;databaseName=LBHATestRBViews"
   jdbc_connection_description = "JDBC connection to Academy Production Insights LBHATestRBViews database"
   jdbc_connection_subnet_id   = local.subnet_ids_list[local.subnet_ids_random_index]
@@ -18,13 +18,13 @@ resource "aws_glue_catalog_database" "landing_zone_academy" {
   name = "${local.short_identifier_prefix}academy-landing-zone"
 }
 
-module "ingest_rev_bev_council_tax_to_landing_zone" {
+module "ingest_academy_revenues_and_benefits_housing_needs_to_landing_zone" {
   count = local.is_live_environment ? 1 : 0
   tags  = module.tags.values
 
   source = "../modules/aws-glue-job"
 
-  job_name               = "${local.short_identifier_prefix}Revenue & Benefits and Council Tax Database Ingestion"
+  job_name               = "${local.short_identifier_prefix}Academy Revenues & Benefits Housing Needs Database Ingestion"
   script_s3_object_key   = aws_s3_bucket_object.ingest_database_tables_via_jdbc_connection.key
   environment            = var.environment
   pydeequ_zip_key        = aws_s3_bucket_object.pydeequ.key
@@ -53,5 +53,77 @@ module "ingest_rev_bev_council_tax_to_landing_zone" {
         TableLevelConfiguration = 3
       }
     })
+  }
+}
+
+resource "aws_s3_bucket_object" "copy_academy_revenues_and_housing_benefits_landing_to_raw" {
+  bucket      = module.glue_scripts.bucket_id
+  key         = "scripts/copy_academy_revenues_and_benefits_housing_needs_landing_to_raw.py"
+  acl         = "private"
+  source      = "../scripts/jobs/copy_academy_revenues_and_benefits_housing_needs_landing_to_raw.py"
+  source_hash = filemd5("../scripts/jobs/copy_academy_revenues_and_benefits_housing_needs_landing_to_raw.py")
+}
+
+module "copy_academy_benefits_housing_needs_to_raw_zone" {
+  count = local.is_live_environment ? 1 : 0
+  tags  = module.tags.values
+
+  source = "../modules/aws-glue-job"
+
+  job_name               = "${local.short_identifier_prefix}Copy Academy Benefits Housing Needs to raw zone"
+  script_s3_object_key   = aws_s3_bucket_object.copy_academy_revenues_and_housing_benefits_landing_to_raw.key
+  environment            = var.environment
+  pydeequ_zip_key        = aws_s3_bucket_object.pydeequ.key
+  helper_module_key      = aws_s3_bucket_object.helpers.key
+  jdbc_connections       = [module.academy_mssql_database_ingestion[0].jdbc_connection_name]
+  glue_role_arn          = aws_iam_role.glue_role.arn
+  glue_temp_bucket_id    = module.glue_temp_storage.bucket_id
+  glue_scripts_bucket_id = module.glue_scripts.bucket_id
+  workflow_name          = module.academy_mssql_database_ingestion[0].workflow_name
+  triggered_by_crawler   = module.ingest_academy_revenues_and_benefits_housing_needs_to_landing_zone[0].crawler_name
+  job_parameters = {
+    "--s3_bucket_target"                 = module.raw_zone.bucket_id
+    "--s3_prefix"                        = "benefits-housing-needs/"
+    "--table_filter_expression"          = "^lbhatestrbviews_core_hb.*"
+    "--glue_database_name_source"        = aws_glue_catalog_database.landing_zone_academy.name
+    "--glue_database_name_target"        = module.department_benefits_and_housing_needs.raw_zone_catalog_database_name
+    "--TempDir"                          = "s3://${module.glue_temp_storage.bucket_id}/"
+    "--extra-py-files"                   = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.helpers.key}"
+    "--extra-jars"                       = "s3://${module.glue_scripts.bucket_id}/jars/deequ-1.0.3.jar"
+    "--enable-glue-datacatalog"          = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--job-bookmark-option"              = "job-bookmark-enable"
+  }
+}
+
+module "copy_academy_revenues_to_raw_zone" {
+  count = local.is_live_environment ? 1 : 0
+  tags  = module.tags.values
+
+  source = "../modules/aws-glue-job"
+
+  job_name               = "${local.short_identifier_prefix}Copy Academy Revenues to raw zone"
+  script_s3_object_key   = aws_s3_bucket_object.copy_academy_revenues_and_housing_benefits_landing_to_raw.key
+  environment            = var.environment
+  pydeequ_zip_key        = aws_s3_bucket_object.pydeequ.key
+  helper_module_key      = aws_s3_bucket_object.helpers.key
+  jdbc_connections       = [module.academy_mssql_database_ingestion[0].jdbc_connection_name]
+  glue_role_arn          = aws_iam_role.glue_role.arn
+  glue_temp_bucket_id    = module.glue_temp_storage.bucket_id
+  glue_scripts_bucket_id = module.glue_scripts.bucket_id
+  workflow_name          = module.academy_mssql_database_ingestion[0].workflow_name
+  triggered_by_crawler   = module.ingest_academy_revenues_and_benefits_housing_needs_to_landing_zone[0].crawler_name
+  job_parameters = {
+    "--s3_bucket_target"                 = module.raw_zone.bucket_id
+    "--s3_prefix"                        = "revenues/"
+    "--table_filter_expression"          = "(^lbhatestrbviews_core_(?!hb).*)|(^lbhatestrbviews_current_.*)|(^lbhatestrbviews_xdbvw_.*)"
+    "--glue_database_name_source"        = aws_glue_catalog_database.landing_zone_academy.name
+    "--glue_database_name_target"        = module.department_revenues.raw_zone_catalog_database_name
+    "--TempDir"                          = "s3://${module.glue_temp_storage.bucket_id}/"
+    "--extra-py-files"                   = "s3://${module.glue_scripts.bucket_id}/${aws_s3_bucket_object.helpers.key}"
+    "--extra-jars"                       = "s3://${module.glue_scripts.bucket_id}/jars/deequ-1.0.3.jar"
+    "--enable-glue-datacatalog"          = "true"
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--job-bookmark-option"              = "job-bookmark-enable"
   }
 }
