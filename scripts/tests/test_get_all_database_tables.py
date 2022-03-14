28cc7a506917124eea30332ad17504520fa38e18
b-dalton <ben.dalton@madetech.com>
2022-03-14 15:00:36 +0000

    Create tests helper function for setting up database tables stub response

--- scripts/tests/test_get_all_database_tables.py
+++ scripts/tests/test_get_all_database_tables.py
@@ -78,24 +78,8 @@ class GetDatabaseTablesTest(TestCase):
   def test_get_all_database_tables_calls_get_filtered_tables_with_filtered_tables_expression(self):
       table_filter_expression = 'my-test-table-1'
 
-      self.set_up_get_database_tables_stub(
-        {
-          'DatabaseName': 'test-db',
-        },
-        {
-          'TableList': [
-            {
-              'Name': 'my-test-table-1'
-            },
-            {
-              'Name': 'my-test-table-2'
-            },
-            {
-              'Name': 'my-test-table-3'
-            }
-          ]
-        }
-      )
+      self.set_up_test_database_tables_stub_response()
+
       filtered_tables_mock = Mock()
       self.get_all_database_tables('test-db', table_filter_expression)
       filtered_tables_mock(['my-test-table-1', 'my-test-table-2', 'my-test-table-3'], table_filter_expression)
@@ -105,48 +89,14 @@ class GetDatabaseTablesTest(TestCase):
   def test_get_all_database_tables_gets_filtered_tables_given_filtered_tables_expression(self):
       table_filter_expression = 'my-test-table-1'
 
-      self.set_up_get_database_tables_stub(
-        {
-          'DatabaseName': 'test-db',
-        },
-        {
-          'TableList': [
-            {
-              'Name': 'my-test-table-1'
-            },
-            {
-              'Name': 'my-test-table-2'
-            },
-            {
-              'Name': 'my-test-table-3'
-            }
-          ]
-        }
-      )
+      self.set_up_test_database_tables_stub_response()
 
       self.assertEqual(self.get_all_database_tables('test-db', table_filter_expression), ['my-test-table-1'])
 
   def test_get_all_database_tables_gets_different_filtered_tables_given_filtered_tables_expression(self):
       table_filter_expression = 'my-test-table-2'
 
-      self.set_up_get_database_tables_stub(
-        {
-          'DatabaseName': 'test-db',
-        },
-        {
-          'TableList': [
-            {
-              'Name': 'my-test-table-1'
-            },
-            {
-              'Name': 'my-test-table-2'
-            },
-            {
-              'Name': 'my-test-table-3'
-            }
-          ]
-        }
-      )
+      self.set_up_test_database_tables_stub_response()
 
       self.assertEqual(self.get_all_database_tables('test-db', table_filter_expression), ['my-test-table-2'])
 
@@ -170,4 +120,24 @@ class GetDatabaseTablesTest(TestCase):
   def get_all_database_tables(self, *args):
     self.stubber.activate()
     return get_all_database_tables(self.glue, *args)
+
+  def set_up_test_database_tables_stub_response(self):
+    return self.set_up_get_database_tables_stub(
+      {
+        'DatabaseName': 'test-db',
+      },
+      {
+        'TableList': [
+        {
+          'Name': 'my-test-table-1'
+        },
+        {
+          'Name': 'my-test-table-2'
+        },
+        {
+          'Name': 'my-test-table-3'
+        }
+      ]
+    }
+  )
 
