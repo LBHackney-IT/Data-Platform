fffeaaa250758a9b0ff33e35ef0d32c89ad563c7
Emma <emma@madetech.com>
2022-03-17 13:55:05 +0000

    Create array correctly, remove comments

--- docker/production-to-pre-production/sync-last-x-days.sh
+++ docker/production-to-pre-production/sync-last-x-days.sh
@@ -11,7 +11,7 @@ list_of_dates_to_retain=""
 
 echo $s3_sync_source
 echo $s3_sync_target
-include_opts=[]
+include_opts=()
 for i in $(seq 0 $((days_to_retain-1))); do
     date_to_import=$(date -v "-${i}d" +"%Y%m%d")
     include_opts+=( --include="*date=$date_to_import/*" )
@@ -19,24 +19,9 @@ for i in $(seq 0 $((days_to_retain-1))); do
     # list_of_dates_to_retain+="${d},"
 done
 
-echo $exclude_opts
-
 aws s3 sync $s3_sync_source $s3_sync_target \
     --exclude "*" "${include_opts[@]}" \
-    #  --exclude "*" --include="*date=20220317/* --include="*date=20220316/*" ....
     --acl "bucket-owner-full-control" \
     --dryrun
 
-# Remove trailing comma
-# list_of_dates_to_retain=${list_of_dates_to_retain%?}
-
-# echo $list_of_dates_to_retain
-# echo "*date=[$list_of_dates_to_retain]*"
-
-# aws s3 sync $s3_sync_source $s3_sync_target \
-    # --exclude "*" --include "*date=[$list_of_dates_to_retain]/*" \
-    # --acl "bucket-owner-full-control" \
-    # --dryrun
-
 # remove records older than 60 days
-# python3 remove_old_data.py
