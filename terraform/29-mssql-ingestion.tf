d13a9da01975266089eaf7aff59edff35fbbaaf1
b-dalton <ben.dalton@madetech.com>
2022-03-17 09:32:07 +0000

    Remove `create_starting_trigger` from glue module and remove from mssql ingestion

--- terraform/29-mssql-ingestion.tf
+++ terraform/29-mssql-ingestion.tf
@@ -78,7 +78,6 @@ module "ingest_academy_revenues_and_benefits_housing_needs_to_landing_zone" {
   glue_temp_bucket_id             = module.glue_temp_storage.bucket_id
   glue_scripts_bucket_id          = module.glue_scripts.bucket_id
   max_concurrent_runs_of_glue_job = local.academy_ingestion_max_concurrent_runs
-  create_starting_trigger         = false
   glue_job_timeout                = 300
   job_parameters = {
     "--source_data_database"             = module.academy_mssql_database_ingestion[0].ingestion_database_name
