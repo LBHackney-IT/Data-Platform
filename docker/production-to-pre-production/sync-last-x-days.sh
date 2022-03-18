655fe81549d5b2db2bf0fbe69a1bd5f2063e8f36
Emma <emma@madetech.com>
2022-03-18 11:09:54 +0000

    Remove old files in backfilling script & cleanup Dockerfile
    Co-authored-by: Ben Dalton <ben.dalton@madetech.com>

--- docker/production-to-pre-production/sync-last-x-days.sh
+++ docker/production-to-pre-production/sync-last-x-days.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# sync 60 days raw zone
-
-date=`date +"%Y%m%d"` # 20220316
+date=`date +"%Y%m%d"`
 
 s3_sync_target="s3://${S3_SYNC_TARGET}"
 s3_sync_source="s3://${S3_SYNC_SOURCE}"
 days_to_retain=${NUMBER_OF_DAYS_TO_RETAIN}
-list_of_dates_to_retain=""
 
-echo $s3_sync_source
-echo $s3_sync_target
-include_opts=()
+echo "S3 bucket source: $s3_sync_source"
+echo "S3 bucket target: $s3_sync_target"
+
 for i in $(seq 0 $((days_to_retain-1))); do
     date_to_import=$(date -v "-${i}d" +"%Y%m%d")
-    include_opts+=( --include="*date=$date_to_import/*" )
-    echo $date_to_import
-    # list_of_dates_to_retain+="${d},"
+
+    sync_include_opts+=( --include="*date=$date_to_import/*" )
+    rm_exclude_opts+=( --exclude="*date=$date_to_import/*" )
 done
 
-aws s3 sync $s3_sync_source $s3_sync_target \
-    --exclude "*" "${include_opts[@]}" \
-    --acl "bucket-owner-full-control" \
-    --dryrun
+echo "Include flags to be used with s3 sync cmd: ${sync_include_opts[*]}"
+echo "Exclude flags to be used with s3 rm cmd: ${rm_exclude_opts[*]}"
 
-# remove records older than 60 days
+echo "Syncing records....."
+aws s3 sync $s3_sync_source $s3_sync_target --acl "bucket-owner-full-control" --exclude "*" "${sync_include_opts[@]}"
+
+echo "Removing old records..."
+aws s3 rm $s3_sync_target --recursive --include "*" "${rm_exclude_opts[@]}"
