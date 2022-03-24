e2ba6aeb3898529bf0e08ebf4b313d3042266799
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-03-24 16:54:11 +0000

    Remove output lookup in favour of ssm parameter

--- terraform/01-inputs-required.tf
+++ terraform/01-inputs-required.tf
@@ -30,11 +30,6 @@ variable "aws_hackit_account_id" {
   type        = string
 }
 
-variable "aws_vpc_id" {
-  description = "The ID of the AWS VPC"
-  type        = string
-}
-
 variable "google_project_id" {
   description = "The ID of the google project used as the target for resource deployment"
   type        = string
