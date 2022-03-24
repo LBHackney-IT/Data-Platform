f2088581ffc955c3db70b0d7fe12eb8bff860bd3
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-03-24 15:26:32 +0000

    bit easier

--- terraform/03-input-derived.tf
+++ terraform/03-input-derived.tf
@@ -20,7 +20,7 @@ locals {
   team_snake                      = lower(replace(var.team, " ", "-"))
   environment                     = lower(replace(local.is_live_environment ? var.environment : terraform.workspace, " ", "-"))
   application_snake               = lower(replace(var.application, " ", "-"))
-  identifier_prefix               = lower("${local.application_snake}-${local.is_live_environment ? local.environment : "dev"}")
+  identifier_prefix               = lower("${local.application_snake}-${local.environment}")
   short_identifier_prefix         = lower(replace(local.is_live_environment ? "" : "${terraform.workspace}-", " ", "-"))
   google_group_admin_display_name = local.is_live_environment ? "saml-aws-data-platform-super-admins@hackney.gov.uk" : var.email_to_notify
 }
@@ -41,5 +41,5 @@ locals {
 }
 
 data "aws_ssm_parameter" "aws_vpc_id" {
-  name = "/${local.application_snake}-${local.is_live_environment ? var.environment : "dev"}/vpc/vpc_id"
+  name = "/${local.application_snake}/vpc/vpc_id"
 }
