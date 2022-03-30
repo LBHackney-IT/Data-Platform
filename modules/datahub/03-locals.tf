251d3774b79d1c1d88bf38258c6526edef2d2da6
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-03-30 13:41:29 +0100

    Four services/containers

--- modules/datahub/03-locals.tf
+++ modules/datahub/03-locals.tf
@@ -1,4 +1,18 @@
 locals {
+  broker_properties = ({
+    container_name         = "broker"
+    port                   = 9092
+    cpu                    = 256
+    memory                 = 2048
+    load_balancer_required = false
+  })
+  datahub_actions = ({
+    container_name         = "datahub-actions"
+    port                   = 80
+    cpu                    = 256
+    memory                 = 2048
+    load_balancer_required = false
+  })
   datahub_frontend_react_properties = ({
     container_name         = "datahub-frontend-react"
     port                   = 9002
@@ -6,9 +20,9 @@ locals {
     memory                 = 2048
     load_balancer_required = true
   })
-  broker_properties = ({
-    container_name         = "broker"
-    port                   = 9092
+  datahub_gms = ({
+    container_name         = "datahub-gms"
+    port                   = 8080
     cpu                    = 256
     memory                 = 2048
     load_balancer_required = false
--- modules/datahub/94-ecs-services.tf
+++ modules/datahub/94-ecs-services.tf
@@ -1,4 +1,42 @@
-module "datahub" {
+module "broker" {
+  source = "../aws-ecs-fargate-service"
+
+  tags                    = var.tags
+  operation_name          = var.short_identifier_prefix
+  environment_variables   = var.environment_variables
+  ecs_cluster_arn         = aws_ecs_cluster.datahub.arn
+  environment             = var.environment
+  identifier_prefix       = var.identifier_prefix
+  short_identifier_prefix = var.short_identifier_prefix
+  alb_id                  = aws_alb.datahub.id
+  alb_target_group_arn    = aws_alb_target_group.datahub.arn
+  alb_security_group_id   = aws_security_group.datahub_alb.id
+  aws_subnet_ids          = var.aws_subnet_ids
+  vpc_id                  = var.vpc_id
+  ecr_repository_url      = aws_ecr_repository.datahub.repository_url
+  container_properties    = local.broker_properties
+}
+
+module "datahub-actions" {
+  source = "../aws-ecs-fargate-service"
+
+  tags                    = var.tags
+  operation_name          = var.short_identifier_prefix
+  environment_variables   = var.environment_variables
+  ecs_cluster_arn         = aws_ecs_cluster.datahub.arn
+  environment             = var.environment
+  identifier_prefix       = var.identifier_prefix
+  short_identifier_prefix = var.short_identifier_prefix
+  alb_id                  = aws_alb.datahub.id
+  alb_target_group_arn    = aws_alb_target_group.datahub.arn
+  alb_security_group_id   = aws_security_group.datahub_alb.id
+  aws_subnet_ids          = var.aws_subnet_ids
+  vpc_id                  = var.vpc_id
+  ecr_repository_url      = aws_ecr_repository.datahub.repository_url
+  container_properties    = local.datahub_actions
+}
+
+module "datahub-frontend-react" {
   source = "../aws-ecs-fargate-service"
 
   tags                    = var.tags
@@ -15,4 +53,23 @@ module "datahub" {
   vpc_id                  = var.vpc_id
   ecr_repository_url      = aws_ecr_repository.datahub.repository_url
   container_properties    = local.datahub_frontend_react_properties
+}
+
+module "datahub-gms" {
+  source = "../aws-ecs-fargate-service"
+
+  tags                    = var.tags
+  operation_name          = var.short_identifier_prefix
+  environment_variables   = var.environment_variables
+  ecs_cluster_arn         = aws_ecs_cluster.datahub.arn
+  environment             = var.environment
+  identifier_prefix       = var.identifier_prefix
+  short_identifier_prefix = var.short_identifier_prefix
+  alb_id                  = aws_alb.datahub.id
+  alb_target_group_arn    = aws_alb_target_group.datahub.arn
+  alb_security_group_id   = aws_security_group.datahub_alb.id
+  aws_subnet_ids          = var.aws_subnet_ids
+  vpc_id                  = var.vpc_id
+  ecr_repository_url      = aws_ecr_repository.datahub.repository_url
+  container_properties    = local.datahub_gms
 }
