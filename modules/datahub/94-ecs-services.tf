e576ccdb46421983cae5711cf5f6e0425498fdd8
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-03-31 13:30:02 +0100

    Add the rest of then containers to connect to our services

--- modules/datahub/94-ecs-services.tf
+++ modules/datahub/94-ecs-services.tf
@@ -1,5 +1,5 @@
 module "broker" {
-  source = "../aws-ecs-fargate-service"
+  source = "../aws-ecs-docker-service"
 
   tags                      = var.tags
   operation_name            = var.short_identifier_prefix
@@ -17,7 +17,7 @@ module "broker" {
 }
 
 module "datahub_actions" {
-  source = "../aws-ecs-fargate-service"
+  source = "../aws-ecs-docker-service"
 
   tags                      = var.tags
   operation_name            = var.short_identifier_prefix
@@ -35,7 +35,7 @@ module "datahub_actions" {
 }
 
 module "datahub_frontend_react" {
-  source = "../aws-ecs-fargate-service"
+  source = "../aws-ecs-docker-service"
 
   tags                      = var.tags
   operation_name            = var.short_identifier_prefix
@@ -53,7 +53,7 @@ module "datahub_frontend_react" {
 }
 
 module "datahub_gms" {
-  source = "../aws-ecs-fargate-service"
+  source = "../aws-ecs-docker-service"
 
   tags                      = var.tags
   operation_name            = var.short_identifier_prefix
@@ -71,7 +71,7 @@ module "datahub_gms" {
 }
 
 module "zookeeper" {
-  source = "../aws-ecs-fargate-service"
+  source = "../aws-ecs-docker-service"
 
   tags                      = var.tags
   operation_name            = var.short_identifier_prefix
@@ -86,4 +86,76 @@ module "zookeeper" {
   vpc_id                    = var.vpc_id
   cloudwatch_log_group_name = aws_cloudwatch_log_group.datahub.name
   container_properties      = local.zookeeper
+}
+
+module "mysql_setup" {
+  source = "../aws-ecs-docker-service"
+
+  tags                      = var.tags
+  operation_name            = var.short_identifier_prefix
+  ecs_cluster_arn           = aws_ecs_cluster.datahub.arn
+  environment               = var.environment
+  identifier_prefix         = var.identifier_prefix
+  short_identifier_prefix   = var.short_identifier_prefix
+  alb_id                    = aws_alb.datahub.id
+  alb_target_group_arn      = aws_alb_target_group.datahub.arn
+  alb_security_group_id     = aws_security_group.datahub_alb.id
+  aws_subnet_ids            = var.aws_subnet_ids
+  vpc_id                    = var.vpc_id
+  cloudwatch_log_group_name = aws_cloudwatch_log_group.datahub.name
+  container_properties      = local.mysql_setup
+}
+
+module "elasticsearch_setup" {
+  source = "../aws-ecs-docker-service"
+
+  tags                      = var.tags
+  operation_name            = var.short_identifier_prefix
+  ecs_cluster_arn           = aws_ecs_cluster.datahub.arn
+  environment               = var.environment
+  identifier_prefix         = var.identifier_prefix
+  short_identifier_prefix   = var.short_identifier_prefix
+  alb_id                    = aws_alb.datahub.id
+  alb_target_group_arn      = aws_alb_target_group.datahub.arn
+  alb_security_group_id     = aws_security_group.datahub_alb.id
+  aws_subnet_ids            = var.aws_subnet_ids
+  vpc_id                    = var.vpc_id
+  cloudwatch_log_group_name = aws_cloudwatch_log_group.datahub.name
+  container_properties      = local.elasticsearch_setup
+}
+
+module "kafka_setup" {
+  source = "../aws-ecs-docker-service"
+
+  tags                      = var.tags
+  operation_name            = var.short_identifier_prefix
+  ecs_cluster_arn           = aws_ecs_cluster.datahub.arn
+  environment               = var.environment
+  identifier_prefix         = var.identifier_prefix
+  short_identifier_prefix   = var.short_identifier_prefix
+  alb_id                    = aws_alb.datahub.id
+  alb_target_group_arn      = aws_alb_target_group.datahub.arn
+  alb_security_group_id     = aws_security_group.datahub_alb.id
+  aws_subnet_ids            = var.aws_subnet_ids
+  vpc_id                    = var.vpc_id
+  cloudwatch_log_group_name = aws_cloudwatch_log_group.datahub.name
+  container_properties      = local.kafka_setup
+}
+
+module "schema_registry" {
+  source = "../aws-ecs-docker-service"
+
+  tags                      = var.tags
+  operation_name            = var.short_identifier_prefix
+  ecs_cluster_arn           = aws_ecs_cluster.datahub.arn
+  environment               = var.environment
+  identifier_prefix         = var.identifier_prefix
+  short_identifier_prefix   = var.short_identifier_prefix
+  alb_id                    = aws_alb.datahub.id
+  alb_target_group_arn      = aws_alb_target_group.datahub.arn
+  alb_security_group_id     = aws_security_group.datahub_alb.id
+  aws_subnet_ids            = var.aws_subnet_ids
+  vpc_id                    = var.vpc_id
+  cloudwatch_log_group_name = aws_cloudwatch_log_group.datahub.name
+  container_properties      = local.schema_registry
 }
