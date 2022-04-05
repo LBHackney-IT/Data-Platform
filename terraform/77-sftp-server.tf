d2546cbcef2b4f31bb14ae823d67053ae0ad6707
Emma <emma@madetech.com>
2022-04-05 16:01:27 +0100

    AWS transfer server with ringo user

---
+++ terraform/77-sftp-server.tf
@@ -0,0 +1,123 @@
+
+data "aws_iam_policy_document" "file_transfer" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      identifiers = [
+        "transfer.amazonaws.com"
+      ]
+      type = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "ringo" {
+  tags = module.tags.values
+
+  name               = "${local.short_identifier_prefix}ringo-file-transfer"
+  assume_role_policy = data.aws_iam_policy_document.file_transfer.json
+}
+
+data "aws_iam_policy_document" "ringo_can_write_to_landing_zone" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:DeleteObjectVersion",
+      "s3:GetObjectVersion",
+      "s3:GetObjectACL",
+      "s3:PutObjectACL"
+    ]
+    resources = [
+      "${module.landing_zone.bucket_arn}/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      "${module.landing_zone.bucket_arn}"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:*"
+    ]
+    resources = [
+      module.landing_zone.kms_key_arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ringo_can_write_to_landing_zone" {
+  tags = module.tags.values
+
+  name   = "${local.short_identifier_prefix}ringo-can-write-to-landing-zone"
+  policy = data.aws_iam_policy_document.ringo_can_write_to_landing_zone.json
+}
+
+resource "aws_iam_role_policy_attachment" "ringo_can_write_to_landing_zone" {
+  role       = aws_iam_role.ringo.name
+  policy_arn = aws_iam_policy.ringo_can_write_to_landing_zone.arn
+}
+
+resource "aws_transfer_user" "ringo" {
+  tags = module.tags.values
+
+  server_id = aws_transfer_server.sftp.id
+  user_name = "ringo"
+  role      = aws_iam_role.ringo.arn
+
+  home_directory_type = "PATH"
+  home_directory      = "/${module.landing_zone.bucket_id}/ringo"
+}
+
+resource "aws_transfer_server" "sftp" {
+  tags = merge(module.tags.values, {
+    Name = "${local.short_identifier_prefix}dataplatform-sftp"
+  })
+  domain       = "S3"
+  protocols    = ["SFTP"]
+  logging_role = aws_iam_role.file_transfer.arn
+}
+
+resource "aws_iam_role" "file_transfer" {
+  tags = module.tags.values
+
+  name               = "${local.short_identifier_prefix}file-transfer"
+  assume_role_policy = data.aws_iam_policy_document.file_transfer.json
+}
+
+data "aws_iam_policy_document" "file_transfer_can_write_logs" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "file_transfer_can_write_logs" {
+  tags = module.tags.values
+
+  name   = "${local.short_identifier_prefix}file-transfer-can-write-logs"
+  policy = data.aws_iam_policy_document.file_transfer_can_write_logs.json
+}
+
+resource "aws_iam_role_policy_attachment" "file_transfer_can_write_logs" {
+  role       = aws_iam_role.file_transfer.name
+  policy_arn = aws_iam_policy.file_transfer_can_write_logs.arn
+}
+
