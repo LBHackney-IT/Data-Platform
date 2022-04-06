da3811b696f5b92957c979ce7897ea91ee74163e
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-04-06 11:02:47 +0100

    Slow and steady wins the race -_-

--- modules/datahub/10-rds.tf
+++ modules/datahub/10-rds.tf
@@ -1,17 +1,41 @@
 resource "aws_db_instance" "datahub" {
-  allocated_storage    = 15
-  engine               = "mysql"
-  engine_version       = "8.0"
-  instance_class       = "db.t3.micro"
-  username             = "datahub"
-  identifier           = "${var.short_identifier_prefix}datahub"
-  password             = random_password.datahub_secret.result
-  db_subnet_group_name = aws_db_subnet_group.default.name
-  skip_final_snapshot  = true
+  allocated_storage      = 15
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  username               = "datahub"
+  identifier             = "${var.short_identifier_prefix}datahub"
+  password               = random_password.datahub_secret.result
+  db_subnet_group_name   = aws_db_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.datahub.id]
+  skip_final_snapshot    = true
 }
 
 resource "aws_db_subnet_group" "default" {
   tags       = var.tags
   name       = "${var.short_identifier_prefix}datahub"
   subnet_ids = data.aws_subnet_ids.subnet_ids.ids
+}
+
+resource "aws_security_group" "datahub" {
+  name   = "${var.short_identifier_prefix}datahub"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    cidr_blocks = [
+      data.aws_vpc.vpc.cidr_block,
+    ]
+  }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
