9d2fb74a41190ee10ed4aa81e008c8d2acc663b9
Sandrine Balley <42965897+LBHSBALLEY@users.noreply.github.com>
2022-04-19 14:13:08 +0200

    Create 30-better-conversations-tables-ingestion.tf
    Test the DP Dynamo ingestion process with Beter Conversations tables.

---
+++ terraform/30-better-conversations-tables-ingestion.tf
@@ -0,0 +1,37 @@
+data "aws_ssm_parameter" "role_arn_to_access_better_conversations_tables" {
+  name = "/better-conversations/prod/role-arn-to-access-dynamodb-table"
+}
+
+module "ingest_better_conversations_tables" {
+  source        = "../modules/aws-glue-job"
+  environment   = var.environment
+  tags          = module.tags.values
+  glue_role_arn = aws_iam_role.glue_role.arn
+
+  job_name                       = "${local.short_identifier_prefix}Ingest Better Conversations tables"
+  job_description                = "Ingest a snapshot of 2 Better Conversations tables from the API Prod Dynamo DB instance"
+  script_s3_object_key           = aws_s3_bucket_object.dynamodb_tables_ingest.key
+  helper_module_key              = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = aws_s3_bucket_object.pydeequ.key
+  number_of_workers_for_glue_job = 5
+  glue_scripts_bucket_id         = module.glue_scripts.bucket_id
+  glue_temp_bucket_id            = module.glue_temp_storage.bucket_id
+  job_parameters = {
+    "--table_names"       = "cf-snapshot-production-conversations,cf-snapshot-production-referrals",
+    "--role_arn"          = data.aws_ssm_parameter.role_arn_to_access_better_conversations_tables
+    "--s3_target"         = "s3://${module.landing_zone.bucket_id}/better-conversations/"
+    "--number_of_workers" = 5
+  }
+
+  crawler_details = {
+    database_name      = aws_glue_catalog_database.landing_zone_catalog_database.name
+    s3_target_location = "s3://${module.landing_zone.bucket_id}/better-conversations/"
+    table_prefix       = "better_conversations_"
+    configuration = jsonencode({
+      Version = 1.0
+      Grouping = {
+        TableLevelConfiguration = 3
+      }
+    })
+  }
+}
