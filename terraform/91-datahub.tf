02782178e35ba712a6c5eccc41e0d6dae8e4fb64
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-04-20 16:16:23 +0100

    Clean up unused variables

--- terraform/91-datahub.tf
+++ terraform/91-datahub.tf
@@ -1,8 +1,6 @@
 module "datahub" {
   source                  = "../modules/datahub"
   tags                    = module.tags.values
-  environment             = var.environment
-  identifier_prefix       = local.identifier_prefix
   short_identifier_prefix = local.short_identifier_prefix
   vpc_id                  = data.aws_vpc.network.id
   kafka_properties = {
