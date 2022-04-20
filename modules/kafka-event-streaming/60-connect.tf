9605dd82536f459e5b0f92f9d93a339449696197
joates-madetech <80255046+joates-madetech@users.noreply.github.com>
2022-04-20 12:14:42 +0100

    Added tests to the the kafka connect installer script (#589)
    * Added tests to the the kafka connect installer script
    
    * Removed some characters that snuck in
    
    * Run kafka connector tests in the  pipeline
    
    Co-authored-by: Emma <emma@madetech.com>

--- modules/kafka-event-streaming/60-connect.tf
+++ modules/kafka-event-streaming/60-connect.tf
@@ -1,6 +1,7 @@
 resource "aws_mskconnect_custom_plugin" "tenure_api" {
   content_type = "ZIP"
   name         = "${var.short_identifier_prefix}confluentinc-kafka-connect-s3-10-0-5-merged"
+  description  = "A bundle consisting of the Confluentinc S3 Connect and Avro Deserializer"
   location {
     s3 {
       bucket_arn = module.kafka_dependency_storage.bucket_arn
--- modules/kafka-event-streaming/scripts/install_kafka_connectors.py
+++ modules/kafka-event-streaming/scripts/install_kafka_connectors.py
@@ -1,11 +1,11 @@
 import argparse
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Union
+from botocore.client import BaseClient
 import logging
 import time
 
 import boto3
-import botocore
 import json
 
 logging.basicConfig()
@@ -19,18 +19,19 @@ def generate_arg_parser():
 
 
 @dataclass
-class PluginResponse:
+class PluginDetails:
     name: str
     state: str
     arn: str
-    revision: str
+    revision: int
 
 
 @dataclass
 class PluginConfig:
-   bucket_arn: str
-   file_key: str
-   name: str
+    bucket_arn: str
+    file_key: str
+    name: str
+
 
 @dataclass
 class ConnectorConfiguration:
@@ -52,7 +53,7 @@ class LogDeliveryConfiguration:
     firehose_delivery_stream: str or None = None
     firehose_delivery_stream_enabled: bool or None = None
     s3_bucket: str or None = None
-    s3_bucket_enabled: bool or None = None 
+    s3_bucket_enabled: bool or None = None
     s3_prefix: str or None = None
 
 
@@ -62,10 +63,11 @@ class ClusterConfig:
     vpc_security_groups: List[str]
     vpc_subnets: List[str]
 
+
 @dataclass
 class ConnectorCapacity:
-    auto_scaling: Dict[str, str]
-    provisioned_capacity: Dict[str, str]
+    auto_scaling: Dict[str, Union[int, str]]
+    provisioned_capacity: Dict[str, Union[int, str]]
 
 
 @dataclass
@@ -73,6 +75,7 @@ class ConnectorResponse:
     name: str
     arn: str
     state: str
+
 
 @dataclass
 class ConnectorConfiguration:
@@ -83,108 +86,114 @@ class ConnectorConfiguration:
     kafka_connect_version: str
     log_delivery: LogDeliveryConfiguration
     service_execution_role_arn: str
-    plugin: PluginResponse
+    plugin: PluginDetails
     capacity: ConnectorCapacity
     worker_configuration: WorkerConfiguration or None
 
-def check_connector_exists_with_config(connector_config: ConnectorConfiguration):
-    client = boto3.client("kafkaconnect")
-    response = client.list_connectors(
-        connectorNamePrefix=connector_config.name,
-        maxResults=100,
-    )
-    print(response)
-    connectors = response['connectors']
-    if len(connectors) == 0:
-        print("Needs Creating")
-        return {"action": "CREATE"}
 
-    connectorArn = connectors[0]["connectorArn"]
-    description = client.describe_connector(
-        connectorArn=connectorArn
-    )
-
-# these checks dont work
-    if description["connectorDescription"] == connector_config.description:
-        print("Needs Recreation because of description")
-        return {"action": "RECREATE", "arn": description["connectorArn"], "version": description["currentVersion"]}
-    if description["kafkaConnectVersion"] == connector_config.kafka_connect_version:
-        print("Needs Recreation because of version")
-        return {"action": "RECREATE", "arn": description["connectorArn"], "version": description["currentVersion"]}
-    if description["connectorConfiguration"] == connector_config.configuration:
-        print("Needs Recreation because of config")
-        return {"action": "RECREATE", "arn": description["connectorArn"], "version": description["currentVersion"]}
+def get_s3_client() -> BaseClient:
+    return boto3.client("kafkaconnect")
 
 
-    if  description["capacity"]["autoScaling"] == connector_config.capacity.auto_scaling \
-    or description["capacity"]["provisionedCapacity"] == connector_config.capacity.provisioned_capacity:
-        print("Needs Updating")
-        return {"action": "UPDATE", "arn": description["connectorArn"], "version": description["currentVersion"] }
-
-    return {"action": "None"}
-def update_connector(connectorArn:str, currentVersion:str, capacityConfig:ConnectorCapacity):
-    client = boto3.client("kafkaconnect")
-    response = client.update_connector(
-        capacity=get_capacity_request(capacityConfig),
-        connectorArn=connectorArn,
-        currentVersion=currentVersion
-    )
-    logger.info(f"update_connector response {response}")
-
-def delete_connector(connectorArn:str, currentVersion:str):
-    client = boto3.client("kafkaconnect")
-    response = client.delete_connector(
-        connectorArn=connectorArn,
-        currentVersion=currentVersion
-    )
-    state = "DELETING"
-    logger.info(f"delete_connector response {response}")
-    while state == "DELETING":
-        time.sleep(5)
-        state = "DELETED" if len(client.list_connectors(
-            connectorNamePrefix=connector_config.name,
-            maxResults=100,
-        )['connectors']) == 0 else "DELETING"
-
-
-
-
-
-def get_plugin_if_exists(name: str):
-    client = boto3.client("kafkaconnect")
+# TODO: Add pagination encase there are more than 100 connectors
+def get_plugin_if_exists(client: BaseClient, name: str):
     response = client.list_custom_plugins(
         maxResults=100,
     )
     plugins = response['customPlugins']
-    return next((PluginResponse(
+    return next((PluginDetails(
         name=plugin["name"],
         state=plugin["customPluginState"],
         arn=plugin["customPluginArn"],
         revision=plugin["latestRevision"]["revision"]
     ) for plugin in plugins if plugin["name"] == name), None)
 
-def get_capacity_request(connector_config: ConnectorCapacity):
-    capacity = {}
-    if connector_config.capacity.auto_scaling:
-        capacity['autoScaling'] = connector_config.capacity.auto_scaling
-    if connector_config.capacity.provisioned_capacity:
-        capacity['provisionedCapacity'] = connector_config.capacity.provisioned_capacity
-    return capacity
 
-def create_connector(connector_config: ConnectorConfiguration):
-    client = boto3.client("kafkaconnect")
+def connector_config_from_input_config(input_config, plugin: PluginDetails) -> ConnectorConfiguration:
+    default_s3_plugin_config = input_config["default_s3_plugin_configuration"]["value"]
 
+    cluster_config_tf = input_config["cluster_config"]["value"]
+    cluster_config = ClusterConfig(
+        bootstrap_servers=cluster_config_tf["bootstrap_brokers_tls"],
+        vpc_security_groups=cluster_config_tf["vpc_security_groups"],
+        vpc_subnets=cluster_config_tf["vpc_subnets"]
+    )
+
+    log_delivery_tf = default_s3_plugin_config["connector_log_delivery_config"]
+    log_delivery = LogDeliveryConfiguration(
+        log_group=log_delivery_tf.get("log_group"),
+        log_group_enabled=log_delivery_tf.get("log_group_enabled")
+    )
+
+    connector_capacity_tf = default_s3_plugin_config["capacity"]
+    connector_capacity = ConnectorCapacity(
+        auto_scaling=connector_capacity_tf.get("auto_scaling"),
+        provisioned_capacity=connector_capacity_tf.get("provisioned_capacity")
+    )
+
+    return ConnectorConfiguration(
+        name=input_config["tenure_connector_name"]["value"],
+        description="Kafka connector to write tenure API changes to S3",
+        configuration=default_s3_plugin_config["connector_configuration"],
+        cluster_config=cluster_config,
+        kafka_connect_version=default_s3_plugin_config["connect_version"],
+        log_delivery=log_delivery,
+        service_execution_role_arn=default_s3_plugin_config["service_execution_role_arn"],
+        plugin=plugin,
+        capacity=connector_capacity,
+        worker_configuration=input_config.get("worker_configuration", {}).get("value", None)
+    )
+
+
+def get_connector(client: BaseClient, connector_name: str):
+    response = client.list_connectors(
+        connectorNamePrefix=connector_name,
+        maxResults=100,
+    )
+    connectors = response['connectors']
+    if len(connectors) == 0:
+        print("Connector Not Found By Name")
+        return False
+
+    connector_arn = connectors[0]['connectorArn']
+
+    connector = client.describe_connector(
+        connectorArn=connector_arn
+    )
+    return connector
+
+
+def check_connector_exists_with_config(client: BaseClient, connector_configuration: ConnectorConfiguration):
+    description = get_connector(client, connector_configuration.name)
+
+    if False == description:
+        return {"action": "CREATE"}
+
+    # these checks don't work
+    if description["connectorDescription"] != connector_configuration.description \
+            or description["kafkaConnectVersion"] != connector_configuration.kafka_connect_version \
+            or description["connectorConfiguration"] != connector_configuration.configuration:
+        print("Connection Needs Recreating")
+        return {"action": "RECREATE", "arn": description["connectorArn"], "version": description["currentVersion"]}
+    if description["capacity"]["autoScaling"] != connector_configuration.capacity.auto_scaling:
+        print("Needs Updating")
+        return {"action": "UPDATE", "arn": description["connectorArn"], "version": description["currentVersion"]}
+
+    return {"action": "NONE"}
+
+
+def create_connector(client: BaseClient, connector_configuration: ConnectorConfiguration):
     response = client.create_connector(
-        capacity=get_capacity_request(connector_config),
-        connectorConfiguration=connector_config.configuration,
-        connectorName=connector_config.name,
-        connectorDescription=connector_config.description,
+        capacity=get_capacity_request(connector_configuration),
+        connectorConfiguration=connector_configuration.configuration,
+        connectorName=connector_configuration.name,
+        connectorDescription=connector_configuration.description,
         kafkaCluster={
             'apacheKafkaCluster': {
-                'bootstrapServers': connector_config.cluster_config.bootstrap_servers,
+                'bootstrapServers': connector_configuration.cluster_config.bootstrap_servers,
                 'vpc': {
-                    'securityGroups': connector_config.cluster_config.vpc_security_groups,
-                    'subnets': connector_config.cluster_config.vpc_subnets
+                    'securityGroups': connector_configuration.cluster_config.vpc_security_groups,
+                    'subnets': connector_configuration.cluster_config.vpc_subnets
                 }
             }
         },
@@ -194,24 +203,24 @@ def create_connector(connector_config: ConnectorConfiguration):
         kafkaClusterEncryptionInTransit={
             'encryptionType': 'TLS'
         },
-        kafkaConnectVersion=connector_config.kafka_connect_version,
+        kafkaConnectVersion=connector_configuration.kafka_connect_version,
         logDelivery={
             'workerLogDelivery': {
                 'cloudWatchLogs': {
-                    'enabled': connector_config.log_delivery.log_group_enabled,
-                    'logGroup': connector_config.log_delivery.log_group
+                    'enabled': connector_configuration.log_delivery.log_group_enabled,
+                    'logGroup': connector_configuration.log_delivery.log_group
                 }
             }
         },
         plugins=[
             {
                 'customPlugin': {
-                    'customPluginArn': plugin_response.arn,
-                    'revision': plugin_response.revision
+                    'customPluginArn': connector_configuration.plugin.arn,
+                    'revision': connector_configuration.plugin.revision
                 }
             },
         ],
-        serviceExecutionRoleArn=connector_config.service_execution_role_arn
+        serviceExecutionRoleArn=connector_configuration.service_execution_role_arn
     )
     return ConnectorResponse(
         name=response["connectorName"],
@@ -219,58 +228,79 @@ def create_connector(connector_config: ConnectorConfiguration):
         state=response["connectorState"]
     )
 
+
+def update_connector(client: BaseClient, connector_arn: str, current_version: str,
+                     capacity_config: ConnectorConfiguration):
+    response = client.update_connector(
+        capacity=get_capacity_request(capacity_config),
+        connectorArn=connector_arn,
+        currentVersion=current_version
+    )
+    logger.info(f"update_connector response {response}")
+    return response
+
+
+def delete_connector(client: BaseClient, connector_arn: str, current_version: str):
+    response = client.delete_connector(
+        connectorArn=connector_arn,
+        currentVersion=current_version
+    )
+    state = "DELETING"
+    logger.info(f"delete_connector response {response}")
+    while state == "DELETING":
+        time.sleep(5)
+        state_details = client.describe_connector(
+            connectorArn=connector_arn
+        )
+        state = state_details["connectorState"]
+
+
+def get_capacity_request(connector_configuration: ConnectorConfiguration):
+    capacity = {}
+    if connector_configuration.capacity.auto_scaling:
+        capacity['autoScaling'] = connector_configuration.capacity.auto_scaling
+    if connector_configuration.capacity.provisioned_capacity:
+        capacity['provisionedCapacity'] = connector_configuration.capacity.provisioned_capacity
+    return capacity
+
+
+def entry_point(raw_config):
+    s3_client = get_s3_client()
+    raw_plugin_config = raw_config["default_s3_plugin_configuration"]["value"]["connector_s3_plugin"]
+    plugin_config = PluginConfig(
+        bucket_arn=raw_plugin_config["bucket_arn"],
+        file_key=raw_plugin_config["file_key"],
+        name=raw_plugin_config["name"]
+    )
+
+    plugin_details = get_plugin_if_exists(s3_client, plugin_config.name)
+
+    if not plugin_details:
+        logger.info("Cannot Find Plugin")
+        return
+
+    logger.info(f"custom_plugin_response {plugin_details}")
+
+    connector_config = connector_config_from_input_config(raw_config, plugin_details)
+    connector_action = check_connector_exists_with_config(s3_client, connector_config)
+
+    if connector_action["action"] == "CREATE":
+        connector_response = create_connector(s3_client, connector_config)
+        logger.info(f"connector_response {connector_response}")
+    elif connector_action["action"] == "UPDATE":
+        connector_response = update_connector(s3_client, connector_action["arn"], connector_action["version"], connector_config)
+        logger.info(f"connector_response {connector_response}")
+    elif connector_action["action"] == "RECREATE":
+        delete_connector(s3_client, connector_action["arn"], connector_action["version"])
+        connector_response = create_connector(s3_client, connector_config)
+        logger.info(f"connector_response {connector_response}")
+    else:
+        logger.info(f"connector {connector_config.name} already exists")
+
+
 if __name__ == "__main__":
     parser = generate_arg_parser()
     args = vars(parser.parse_args())
     config = json.loads(args["input"])
     logger.debug(f"given config {config}")
-
-    plugin_config:PluginConfig = config["default_s3_plugin_configuration"]["value"]["connector_s3_plugin"]
-
-    plugin_response = get_plugin_if_exists(plugin_config["name"])
-    logger.info(f"custom_plugin_response {plugin_response}")
-
-    cluster_config_tf = config["cluster_config"]["value"]
-    cluster_config = ClusterConfig(
-        bootstrap_servers=cluster_config_tf["bootstrap_brokers_tls"],
-        vpc_security_groups=cluster_config_tf["vpc_security_groups"],
-        vpc_subnets=cluster_config_tf["vpc_subnets"]
-    )
-    log_delivery_tf = config["default_s3_plugin_configuration"]["value"]["connector_log_delivery_config"]
-    log_delivery = LogDeliveryConfiguration(
-        log_group=log_delivery_tf.get("log_group"),
-        log_group_enabled=log_delivery_tf.get("log_group_enabled")
-    )
-
-    connector_capacity_tf = config["default_s3_plugin_configuration"]["value"]["capacity"]
-    connector_capacity = ConnectorCapacity(
-        auto_scaling=connector_capacity_tf.get("auto_scaling"),
-        provisioned_capacity=connector_capacity_tf.get("provisioned_capacity")
-    )
-
-    connector_config = ConnectorConfiguration(
-        name=config["tenure_connector_name"]["value"],
-        description="Kafka connector to write tenure changes to S3",
-        configuration=config["default_s3_plugin_configuration"]["value"]["connector_configuration"],
-        cluster_config=cluster_config,
-        kafka_connect_version=config["default_s3_plugin_configuration"]["value"]["connect_version"],
-        log_delivery=log_delivery,
-        service_execution_role_arn=config["default_s3_plugin_configuration"]["value"]["service_execution_role_arn"],
-        plugin=plugin_response,
-        capacity=connector_capacity,
-        worker_configuration=config.get("worker_configuration", {}).get("value", None)
-    )
-
-    connector_action = check_connector_exists_with_config(connector_config)
-
-    if connector_action["action"] == "CREATE":
-        connector_response = create_connector(connector_config)
-        logger.info(f"connector_response {connector_response}")
-    elif connector_action["action"] == "UPDATE":
-        update_connector(connector_action["arn"], connector_action["version"], connector_config.capacity)
-    elif connector_action["action"] == "RECREATE":
-        delete_connector(connector_action["arn"], connector_action["version"])
-        connector_response = create_connector(connector_config)
-        logger.info(f"connector_response {connector_response}")
-    else:
-        logger.info(f"connector {connector_config.name} already exists")
+    entry_point(config)
