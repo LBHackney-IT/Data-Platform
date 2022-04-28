4bf14ce49a36ff443d0b5aec101224a1df6e4653
Emma Corbett <38878719+elcorbs@users.noreply.github.com>
2022-04-28 15:37:01 +0100

    Kafka connector missing security group rules & s3 permissions (#617)
    * Fix up security group for connector
    
    * Fix up s3 permissions for kafka connect
    
    * Rename terraform output

--- modules/kafka-event-streaming/20-security-groups.tf
+++ modules/kafka-event-streaming/20-security-groups.tf
@@ -14,3 +14,34 @@ resource "aws_security_group" "kafka" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
+
+resource "aws_security_group_rule" "allow_outbound_traffic_within_sg" {
+  description              = "Allow all outbound traffic within the security group"
+  security_group_id        = aws_security_group.kafka.id
+  protocol                 = "-1"
+  from_port                = 0
+  to_port                  = 0
+  type                     = "egress"
+  source_security_group_id = aws_security_group.kafka.id
+}
+
+resource "aws_security_group_rule" "allow_outbound_traffic_to_s3" {
+  description       = "Allow outbound traffic to port 443 to allow writing to S3"
+  security_group_id = aws_security_group.kafka.id
+  protocol          = "TCP"
+  from_port         = 443
+  to_port           = 443
+  type              = "egress"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+}
+
+resource "aws_security_group_rule" "allow_outbound_traffic_to_schema_registry" {
+  description              = "Allow outbound traffic to schema registry load balancer "
+  security_group_id        = aws_security_group.kafka.id
+  protocol                 = "TCP"
+  from_port                = 8081
+  to_port                  = 8081
+  type                     = "egress"
+  source_security_group_id = module.schema_registry.load_balancer_security_group_id
+}
--- modules/kafka-schema-registry/99-outpus.tf
+++ modules/kafka-schema-registry/99-outpus.tf
@@ -1,3 +1,7 @@
 output "load_balancer_dns_name" {
   value = aws_alb.schema_registry.dns_name
 }
+
+output "load_balancer_security_group_id" {
+  value = aws_security_group.schema_registry_alb.id
+}
