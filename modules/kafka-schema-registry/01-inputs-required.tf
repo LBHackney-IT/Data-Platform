a71da7b33d5a6b702a4c274289a407bedf3c760c
Emma Corbett <38878719+elcorbs@users.noreply.github.com>
2022-05-04 13:23:12 +0100

    Post new schemas to the kafka schema registry (#625)
    * Remove glue schema registry
    
    * Script to post schema to the registry running in terraform local exec block

--- modules/kafka-schema-registry/01-inputs-required.tf
+++ modules/kafka-schema-registry/01-inputs-required.tf
@@ -29,3 +29,23 @@ variable "bootstrap_servers" {
   description = "One or more DNS names (or IP addresses) and port pairs."
   type        = string
 }
+
+variable "bastion_private_key_ssm_parameter_name" {
+  description = "SSM paramater name where the bastion private key is stored"
+  type        = string
+}
+
+variable "bastion_instance_id" {
+  description = "Instance ID of the bastion"
+  type        = string
+}
+
+variable "topics" {
+  description = "List of kafka topics to manage schemas for"
+  type        = list(string)
+}
+
+variable "is_live_environment" {
+  description = "A flag indicting if we are running in a live environment for setting up automation"
+  type        = bool
+}
