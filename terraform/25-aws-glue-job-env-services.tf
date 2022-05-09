76c248b69f60d5311b693ed4174a4f9b7c16587b
timburke-hackit <tim.burke@hackney.gov.uk>
2022-05-09 16:23:01 +0100

    wip

---
+++ terraform/25-aws-glue-job-env-services.tf
@@ -0,0 +1,65 @@
+resource "aws_s3_bucket_object" "aqs" {
+  for_each    = fileset(path.module, "../jobs/env_services/aqs/*.json")
+  bucket      = module.glue_scripts.bucket_id
+  key         = "../jobs/env_services/aqs/${each.value}"
+  acl         = "private"
+  source      = "../jobs/env_services/aqs/${each.value}"
+  source_hash = filemd5("../jobs/env_services/aqs/${each.value}")
+}
+
+resource "aws_glue_trigger" "alloy_daily_table_ingestion" {
+  tags     = module.tags.values
+  for_each = fileset(path.module, "../jobs/env_services/aqs/*.json")
+  name     = "${local.short_identifier_prefix}Alloy ${title(replace(each.value, "_", " "))} Ingestion Trigger"
+  type     = "SCHEDULED"
+  schedule = "cron(0 23 ? * MON-FRI *)"
+  enabled  = local.is_live_environment
+
+  actions {
+    job_name = module.alloy_api_ingestion_raw_env_services.job_name
+    arguments = {
+      "--aqs"      = file(each.value)
+      "--filename" = "trimsuffix(${each.value},\".json\")/trimsuffix(${each.value}, \".json\").csv"
+      "--resource" = "trimsuffix(${each.value},\".json\")"
+    }
+  }
+}
+
+module "alloy_api_ingestion_raw_env_services" {
+  job_description            = "This job queries the Alloy API for data and converts the exported csv to Parquet saved to S3"
+  source                     = "../modules/aws-glue-job"
+  department                 = module.department_environmental_services
+  job_name                   = "${local.short_identifier_prefix}alloy_api_ingestion_env_services"
+  helper_module_key          = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key            = aws_s3_bucket_object.pydeequ.key
+  spark_ui_output_storage_id = module.spark_ui_output_storage.bucket_id
+  script_name                = "alloy_api_ingestion"
+  job_parameters = {
+    "--job-bookmark-option"     = "job-bookmark-enable"
+    "--enable-glue-datacatalog" = "true"
+    "--s3_bucket_target"        = module.raw_zone.bucket_id
+    "--s3_prefix"               = "env-services/alloy/api-responses/"
+    "--secret_name"             = "${local.identifier_prefix}/env-services/alloy-api-key"
+    "--database"                = module.department_environmental_services.raw_zone_catalog_database_name
+  }
+  crawler_details = {
+    database_name      = module.department_environmental_services.raw_zone_catalog_database_name
+    s3_target_location = "s3://${module.raw_zone.bucket_id}/env/services/alloy/api_response"
+    table_prefix       = "alloy_"
+  }
+}
+
+/*
+previous approach to read json from s3 bucket:
+data "aws_s3_objects" "alloy_aqs_objects" {
+  bucket = ""
+}
+
+data "aws_s3_object" "aqs_query_bodies" {
+  count  = length(data.aws_s3_objects.alloy_aqs_objects.keys)
+  key    = element(data.aws_s3_objects.alloy_aqs_objects.keys, count.index)
+  body   = element(data.aws_s3_objects.alloy_aqs_objects.body, count.index)
+  bucket = data.aws_s3_objects.alloy_aqs_objects.bucket
+}
+*/
+
