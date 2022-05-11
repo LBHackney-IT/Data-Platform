1ce0787bc68c3bd11226358a1d3543798e741231
Emma Corbett <38878719+elcorbs@users.noreply.github.com>
2022-05-11 16:23:52 +0100

    Increase glue job worker types & timeout on academy ingestions (#646)

--- terraform/29-mssql-ingestion.tf
+++ terraform/29-mssql-ingestion.tf
@@ -77,7 +77,8 @@ module "ingest_academy_revenues_and_benefits_housing_needs_to_landing_zone" {
   glue_scripts_bucket_id          = module.glue_scripts.bucket_id
   spark_ui_output_storage_id      = module.spark_ui_output_storage.bucket_id
   max_concurrent_runs_of_glue_job = local.academy_ingestion_max_concurrent_runs
-  glue_job_timeout                = 300
+  glue_job_timeout                = 420
+  glue_job_worker_type            = "G.1X"
   job_parameters = {
     "--source_data_database"        = module.academy_mssql_database_ingestion[0].ingestion_database_name
     "--s3_ingestion_bucket_target"  = "s3://${module.landing_zone.bucket_id}/academy/"
