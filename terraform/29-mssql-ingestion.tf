c33c11a99ecf14b789d0c7461d3707dc83125585
b-dalton <ben.dalton@madetech.com>
2022-05-11 16:11:42 +0100

    Add later run to copy academy to raw

--- terraform/29-mssql-ingestion.tf
+++ terraform/29-mssql-ingestion.tf
@@ -108,7 +108,7 @@ resource "aws_glue_trigger" "academy_revenues_and_benefits_housing_needs_landing
 
   name     = "${local.short_identifier_prefix}academy-revenues-benefits-housing-needs-database-ingestion-crawler-trigger"
   type     = "SCHEDULED"
-  schedule = "cron(15 8 ? * MON,TUE,WED,THU,FRI *)"
+  schedule = "cron(15 8,12 ? * MON,TUE,WED,THU,FRI *)"
   enabled  = local.is_live_environment
 
   actions {
