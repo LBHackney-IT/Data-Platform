db8d729413033ee2df843d6a6099f647aa67b323
b-dalton <ben.dalton@madetech.com>
2022-05-13 13:23:39 +0100

    Refactor alloy ingestion
    Create triggers outside of glue module
    This is so we can dynamically trigger multiple instances of the glue job
    with different input arguments
    Also pulled crawler and trigger out into own resources

--- terraform/25-aws-glue-job-env-services.tf
+++ terraform/25-aws-glue-job-env-services.tf
@@ -1,32 +1,41 @@
+locals {
+  alloy_queries                     = fileset(path.module, "../scripts/jobs/env_services/aqs/*json")
+  alloy_queries_max_concurrent_runs = length(local.alloy_queries)
+}
+
 resource "aws_glue_trigger" "alloy_daily_table_ingestion" {
   tags     = module.tags.values
-  name     = "${local.short_identifier_prefix}Alloy Ingestion Trigger"
+  enabled  = local.is_live_environment
+  for_each = toset(local.alloy_queries)
+
+  name     = "${local.short_identifier_prefix}${each.value} Alloy Ingestion Trigger"
   type     = "SCHEDULED"
   schedule = "cron(0 23 ? * MON-FRI *)"
-  enabled  = local.is_live_environment
 
-  dynamic "actions" {
-    for_each = fileset(path.module, "../scripts/jobs/env_services/aqs/*json")
-    content {
-      job_name = module.alloy_api_ingestion_raw_env_services.job_name
-      arguments = {
-        "--aqs"      = file(actions.value)
-        "--filename" = "trimsuffix(${actions.value},\".json\")/trimsuffix(${actions.value}, \".json\").csv"
-        "--resource" = "trimsuffix(${actions.value},\".json\")"
-      }
+  actions {
+    job_name = module.alloy_api_ingestion_raw_env_services[0].job_name
+    arguments = {
+      "--aqs"      = file(each.value)
+      "--filename" = "trimsuffix(${each.value},\".json\")/trimsuffix(${each.value}, \".json\").csv"
+      "--resource" = "trimsuffix(${each.value},\".json\")"
     }
   }
 }
 
 module "alloy_api_ingestion_raw_env_services" {
-  job_description            = "This job queries the Alloy API for data and converts the exported csv to Parquet saved to S3"
-  source                     = "../modules/aws-glue-job"
-  department                 = module.department_environmental_services
-  job_name                   = "${local.short_identifier_prefix}alloy_api_ingestion_env_services"
-  helper_module_key          = aws_s3_bucket_object.helpers.key
-  pydeequ_zip_key            = aws_s3_bucket_object.pydeequ.key
-  spark_ui_output_storage_id = module.spark_ui_output_storage.bucket_id
-  script_name                = "alloy_api_ingestion"
+  count = local.is_live_environment ? 1 : 0
+  tags  = module.tags.values
+
+  job_description                 = "This job queries the Alloy API for data and converts the exported csv to Parquet saved to S3"
+  source                          = "../modules/aws-glue-job"
+  department                      = module.department_environmental_services
+  job_name                        = "${local.short_identifier_prefix}alloy_api_ingestion_env_services"
+  helper_module_key               = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                 = aws_s3_bucket_object.pydeequ.key
+  spark_ui_output_storage_id      = module.spark_ui_output_storage.bucket_id
+  trigger_enabled                 = false
+  max_concurrent_runs_of_glue_job = local.alloy_queries_max_concurrent_runs
+  script_name                     = "alloy_api_ingestion"
   job_parameters = {
     "--job-bookmark-option"     = "job-bookmark-enable"
     "--enable-glue-datacatalog" = "true"
@@ -35,9 +44,42 @@ module "alloy_api_ingestion_raw_env_services" {
     "--secret_name"             = "${local.identifier_prefix}/env-services/alloy-api-key"
     "--database"                = module.department_environmental_services.raw_zone_catalog_database_name
   }
-  crawler_details = {
-    database_name      = module.department_environmental_services.raw_zone_catalog_database_name
-    s3_target_location = "s3://${module.raw_zone.bucket_id}/env-services/alloy/api-responses/"
-    table_prefix       = "alloy_"
+}
+
+resource "aws_glue_trigger" "alloy_daily_table_ingestion_crawler_trigger" {
+  tags    = module.tags.values
+  name    = "${local.short_identifier_prefix}Alloy Ingestion Crawler"
+  type    = "CONDITIONAL"
+  enabled = local.is_live_environment
+
+  actions {
+    crawler_name = aws_glue_crawler.alloy_daily_table_ingestion.name
   }
+
+  predicate {
+    conditions {
+      job_name = module.alloy_api_ingestion_raw_env_services[0].job_name
+      state    = "SUCCEEDED"
+    }
+  }
+}
+
+resource "aws_glue_crawler" "alloy_daily_table_ingestion" {
+  tags = module.tags.values
+
+  database_name = module.department_environmental_services.raw_zone_catalog_database_name
+  name          = "${local.short_identifier_prefix}Alloy Ingestion"
+  role          = module.department_environmental_services.glue_role_arn
+
+  s3_target {
+    path = "s3://${module.raw_zone.bucket_id}/env-services/alloy/api-responses/"
+  }
+  table_prefix = "alloy_"
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 5
+    }
+  })
 }
