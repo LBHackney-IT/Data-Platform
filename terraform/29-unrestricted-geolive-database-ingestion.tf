d1176808f171e0dbce5809c9891fc53720c9d66a
LBHMVillalobos <marta.villalobos@hackney.gov.uk>
2022-05-13 09:34:16 +0100

    remove variables that are no longer required

--- terraform/29-unrestricted-geolive-database-ingestion.tf
+++ terraform/29-unrestricted-geolive-database-ingestion.tf
@@ -7,9 +7,7 @@ module "boundaries_geolive_database_ingestion" {
   name                        = "geolive-boundaries-schema"
   jdbc_connection_url         = "jdbc:postgresql://10.120.8.145:5432/geolive"
   jdbc_connection_description = "JDBC connection to Geolive PostgreSQL database, to access the boundaries schema only"
-  jdbc_connection_subnet_id      = local.subnet_ids_list[local.subnet_ids_random_index]
-  database_availability_zone  = "eu-west-2a"
-  vpc_id                      = data.aws_vpc.network.id
+  jdbc_connection_subnet     = data.aws_subnet.network[local.instance_subnet_id]
   identifier_prefix           = local.short_identifier_prefix
   database_secret_name        = "database-credentials/geolive-boundaries"
   schema_name                 = "boundaries"
