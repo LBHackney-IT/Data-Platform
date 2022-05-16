c51398d4faf952c7b62f87835917fdab33049ce9
b-dalton <ben.dalton@madetech.com>
2022-05-16 08:57:39 +0100

    Correct conditional

--- modules/database-ingestion-via-jdbc-connection/03-inputs-derived.tf
+++ modules/database-ingestion-via-jdbc-connection/03-inputs-derived.tf
@@ -1,3 +1,3 @@
 locals {
-  database_and_schema_name_lowercase = var.schema_name ? lower("${local.database_name}-${var.schema_name}") : lower(local.database_name)
+  database_and_schema_name_lowercase = var.schema_name == null ? lower(local.database_name) : lower("${local.database_name}-${var.schema_name}")
 }
