741a0f870314e540e2bb16faf1332d6a140faea8
timburke-hackit <tim.burke@hackney.gov.uk>
2022-05-23 21:32:22 +0100

    refactor alloy snapshot job

--- scripts/jobs/env_services/alloy_create_snapshot.py
+++ scripts/jobs/env_services/alloy_create_snapshot.py
@@ -12,26 +12,35 @@ from datetime import datetime
 from helpers.helpers import get_glue_env_var, table_exists_in_catalog, create_pushdown_predicate
 
 
-def get_latest_snapshot(dfa):
-    dfa = dfa.where(col('snapshot_date') == dfa.select(
-        max('snapshot_date')).first()[0])
+def get_latest_snapshot(dfa, snapshot_date_col):
+    """
+    get the latest snapshot table based on the snapshot_date column
+    """
+    dfa = dfa.where(col(snapshot_date_col) == dfa.select(
+        max(snapshot_date_col)).first()[0])
     return dfa
 
 
 def add_snapshot_date_columns(data_frame):
+    """
+    create columns for snapshot date
+    """
     now = datetime.now()
-    snapshotYear = str(now.year)
-    snapshotMonth = str(now.month).zfill(2)
-    snapshotDay = str(now.day).zfill(2)
-    snapshotDate = snapshotYear + snapshotMonth + snapshotDay
-    data_frame = data_frame.withColumn('snapshot_year', F.lit(snapshotYear))
-    data_frame = data_frame.withColumn('snapshot_month', F.lit(snapshotMonth))
-    data_frame = data_frame.withColumn('snapshot_day', F.lit(snapshotDay))
-    data_frame = data_frame.withColumn('snapshot_date', F.lit(snapshotDate))
+    snapshot_year = str(now.year)
+    snapshot_month = str(now.month).zfill(2)
+    snapshot_day = str(now.day).zfill(2)
+    snapshot_date = snapshot_year + snapshot_month + snapshot_day
+    data_frame = data_frame.withColumn('snapshot_year', F.lit(snapshot_year))
+    data_frame = data_frame.withColumn('snapshot_month', F.lit(snapshot_month))
+    data_frame = data_frame.withColumn('snapshot_day', F.lit(snapshot_day))
+    data_frame = data_frame.withColumn('snapshot_date', F.lit(snapshot_date))
     return data_frame
 
 
 def prepare_increments(increment_df, id_col, increment_date_col):
+    """
+    create increment df with latest row for each item
+    """
     id_partition = Window.partitionBy(id_col)
     increment_df = increment_df.withColumn('latest', F.max(increment_date_col).over(
         id_partition)).where(F.col(increment_date_col) == F.col('latest')).drop('latest')
@@ -39,12 +48,18 @@ def prepare_increments(increment_df, id_col, increment_date_col):
 
 
 def apply_increments(snapshot_df, increment_df, id_col):
+    """
+    update previous snapshot with increment df
+    """
     snapshot_df = snapshot_df.join(increment_df, id_col, 'left_anti')
     snapshot_df = snapshot_df.unionByName(increment_df)
     return snapshot_df
 
 
-def loadIncrementsSinceDate(increment_table_name, name_space, date):
+def load_increments_since_date(increment_table_name, name_space, date):
+    """
+    load all of the increments since the provided date
+    """
     increment_ddf = glueContext.create_dynamic_frame.from_catalog(
         name_space=name_space,
         table_name=increment_table_name,
@@ -55,27 +70,18 @@ def loadIncrementsSinceDate(increment_table_name, name_space, date):
     return increment_df
 
 
-def set_increment_df(glueContext, source_catalog_database, table_name, increment_table_name, last_snapshot_date=None):
-    if not (table_exists_in_catalog(glueContext, table_name, source_catalog_database) or table_exists_in_catalog(glueContext, increment_table_name, source_catalog_database)):
-        logger.info(f'No snapshot or increments found for {table_name}.')
-        return None
-
-    if not table_exists_in_catalog(glueContext, table_name, source_catalog_database):
-        logger.info(
-            f'no snapshot found for {table_name}, returning all increments')
-        increment_df = loadIncrementsSinceDate(
-            f'increment_{table_name}', source_catalog_database, "20210101")
-        increment_df = prepare_increments(increment_df)
-        increment_df = add_snapshot_date_columns(increment_df)
-        return increment_df
-
-    logger.info(
-        f'snapshot for {table_name} found, returning subsequent increments')
-    increment_df = loadIncrementsSinceDate(
+def set_increment_df(source_catalog_database, increment_table_name,
+                     increment_date_col, id_col, last_snapshot_date='1970-01-01'
+                     ):
+    """
+    set increment to be applied to previous snapshot
+    """
+    df = load_increments_since_date(
         increment_table_name, source_catalog_database, last_snapshot_date)
-    increment_df = prepare_increments(increment_df)
-    increment_df = add_snapshot_date_columns(increment_df)
-    return increment_df
+    df = prepare_increments(
+        df, id_col, increment_date_col)
+    df = add_snapshot_date_columns(df)
+    return df
 
 
 if __name__ == "__main__":
@@ -87,46 +93,63 @@ if __name__ == "__main__":
     job = Job(glueContext)
     job.init(args['JOB_NAME'], args)
 
+    snapshot_catalog_database = get_glue_env_var(
+        'snapshot_catalog_database', '')
+    increment_catalog_database = get_glue_env_var(
+        'increment_catalog_database', '')
     table_name = get_glue_env_var('table_name', '')
+    increment_table_prefix = get_glue_env_var('increment_prefix', '')
+    snapshot_table_prefix = get_glue_env_var('snapshot_prefix', '')
     id_col = get_glue_env_var('id_col', '')
-    increment_date_col = get_glue_env_var('increment_date_col',)
-    source_catalog_database = get_glue_env_var('source_catalog_database', '')
+    increment_date_col = get_glue_env_var('increment_date_col', '')
+    snapshot_date_col = get_glue_env_var('snapshot_date_col', '')
     s3_bucket_target = get_glue_env_var('s3_bucket_target', '')
 
-    increment_table_name = f'increment_{table_name}'
+    table_name = table.replace(' ', '_').lower()
 
-    if not (table_exists_in_catalog(glueContext, table_name, source_catalog_database) or table_exists_in_catalog(glueContext, increment_table_name, source_catalog_database)):
+    increment_table_name = f'{increment_table_prefix}{table_name}'
+    snapshot_table_name = f'{snapshot_table_prefix}{table_name}'
+
+    if not (
+        table_exists_in_catalog(glueContext, snapshot_table_name,
+                                snapshot_catalog_database)
+        or table_exists_in_catalog(glueContext, increment_table_name,
+                                   increment_catalog_database)
+    ):
         logger.info(f'No snapshot or increments found for {table_name}.')
         job.commit()
-        os._exit()
+        raise Exception(
+            f'no snapshot or increments found for {snapshot_table_name} in {snapshot_catalog_database} or {increment_table_name} in {increment_catalog_database}')
 
-    if not table_exists_in_catalog(glueContext, table_name, source_catalog_database):
-        increment_df = set_increment_df(
-            glueContext, source_catalog_database, table_name, increment_table_name)
+    if not table_exists_in_catalog(glueContext, snapshot_table_name, snapshot_catalog_database):
+        increment_df = set_increment_df(increment_catalog_database, increment_table_name,
+                                        increment_date_col, id_col)
         snapshot_df = increment_df
 
     else:
         pushDownPredicate = create_pushdown_predicate(
-            partitionDateColumn='snapshot_date', daysBuffer=14)
+            partitionDateColumn=snapshot_date_col, daysBuffer=14)
         snapshot_ddf = glueContext.create_dynamic_frame.from_catalog(
-            name_space=source_catalog_database,
-            table_name=table_name,
+            name_space=snapshot_catalog_database,
+            table_name=snapshot_table_name,
             push_down_predicate=pushDownPredicate
         )
         snapshot_df = snapshot_ddf.toDF()
-        snapshot_df = get_latest_snapshot(snapshot_df)
+        snapshot_df = get_latest_snapshot(snapshot_df, snapshot_date_col)
         last_snapshot_date = snapshot_df.select(
-            max('snapshot_date')).first()[0]
+            max(snapshot_date_col)).first()[0]
         increment_df = set_increment_df(
-            glueContext, source_catalog_database, table_name, increment_table_name, last_snapshot_date)
-        snapshot_df = apply_increments(snapshot_df, increment_df)
+            glueContext, increment_catalog_database, increment_table_name,
+            last_snapshot_date
+        )
+        snapshot_df = apply_increments(snapshot_df, increment_df, 'item_id')
 
     PARTITION_KEYS = ["snapshot_year", "snapshot_month",
                       "snapshot_day", "snapshot_date"]
 
     resultDataFrame = DynamicFrame.fromDF(
         snapshot_df, glueContext, "resultDataFrame")
-    target_destination = s3_bucket_target + table_name
+    target_destination = s3_bucket_target + snapshot_table_name
     parquetData = glueContext.write_dynamic_frame.from_options(
         frame=resultDataFrame,
         connection_type="s3",
