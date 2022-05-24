10192961ff381064cb8e18704d72d4b2378755b5
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2022-05-24 15:20:11 +0100

    Training module 1 tuomo (#681)
    * add modules for training module 1
    
    * format

--- terraform/26-google-sheets-imports.tf
+++ terraform/26-google-sheets-imports.tf
@@ -802,3 +802,45 @@ module "sandbox_lindseycoulson_covid_locations" {
   enable_glue_trigger             = false
   spark_ui_output_storage_id      = module.spark_ui_output_storage.bucket_id
 }
+
+module "sandbox_tuomo_covid_vaccinations" {
+  count                           = local.is_live_environment ? 1 : 0
+  source                          = "../modules/google-sheets-glue-job"
+  identifier_prefix               = local.short_identifier_prefix
+  is_live_environment             = local.is_live_environment
+  glue_scripts_bucket_id          = module.glue_scripts.bucket_id
+  helper_module_key               = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                 = aws_s3_bucket_object.pydeequ.key
+  glue_catalog_database_name      = module.department_sandbox.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_url    = module.glue_temp_storage.bucket_url
+  glue_crawler_excluded_blobs     = local.glue_crawler_excluded_blobs
+  google_sheets_import_script_key = aws_s3_bucket_object.google_sheets_import_script.key
+  bucket_id                       = module.raw_zone.bucket_id
+  google_sheets_document_id       = "1A4HkdA94G7jXA6zDnHmI2lA6Ttmy4Rj4R0OQPsb4vZ0"
+  google_sheets_worksheet_name    = "vaccinations"
+  department                      = module.department_sandbox
+  dataset_name                    = "tuomo_covid_vaccinations"
+  enable_glue_trigger             = false
+  spark_ui_output_storage_id      = module.spark_ui_output_storage.bucket_id
+}
+
+module "sandbox_tuomo_covid_locations" {
+  count                           = local.is_live_environment ? 1 : 0
+  source                          = "../modules/google-sheets-glue-job"
+  identifier_prefix               = local.short_identifier_prefix
+  is_live_environment             = local.is_live_environment
+  glue_scripts_bucket_id          = module.glue_scripts.bucket_id
+  helper_module_key               = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                 = aws_s3_bucket_object.pydeequ.key
+  glue_catalog_database_name      = module.department_sandbox.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_url    = module.glue_temp_storage.bucket_url
+  glue_crawler_excluded_blobs     = local.glue_crawler_excluded_blobs
+  google_sheets_import_script_key = aws_s3_bucket_object.google_sheets_import_script.key
+  bucket_id                       = module.raw_zone.bucket_id
+  google_sheets_document_id       = "1A4HkdA94G7jXA6zDnHmI2lA6Ttmy4Rj4R0OQPsb4vZ0"
+  google_sheets_worksheet_name    = "locations"
+  department                      = module.department_sandbox
+  dataset_name                    = "tuomo_covid_locations"
+  enable_glue_trigger             = false
+  spark_ui_output_storage_id      = module.spark_ui_output_storage.bucket_id
+}
