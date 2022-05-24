a166180ac49c1fe64d5147947d5680b19d210133
b-dalton <ben.dalton@madetech.com>
2022-05-24 14:51:48 +0100

    Remove duplicate scope

--- lambdas/g_drive_to_s3/main.py
+++ lambdas/g_drive_to_s3/main.py
@@ -67,7 +67,6 @@ def lambda_handler(event, lambda_context):
     scopes = [
         'https://www.googleapis.com/auth/drive.file',
         'https://www.googleapis.com/auth/drive',
-        'https://www.googleapis.com/auth/drive.file',
         'https://www.googleapis.com/auth/drive.metadata'
     ]
 
