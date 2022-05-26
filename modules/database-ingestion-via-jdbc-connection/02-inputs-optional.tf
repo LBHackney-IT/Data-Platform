9ac644efe97abadc355542286447d0fa24b18988
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-05-26 14:33:54 +0100

    DPP-134 - Bugfix - Iam roles not unique

--- modules/database-ingestion-via-jdbc-connection/02-inputs-optional.tf
+++ modules/database-ingestion-via-jdbc-connection/02-inputs-optional.tf
@@ -9,3 +9,9 @@ variable "create_workflow" {
   type        = bool
   default     = true
 }
+
+variable "table_filter" {
+  description = "Used to create unique resources we recreating JDBC connection for multiple tables based on list of filters"
+  type        = string
+  default     = null
+}
