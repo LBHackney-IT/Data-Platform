a60a1dd6e2746c87ffbca1261763ed3e7fe41c4e
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-05-26 14:40:39 +0100

    Typo

--- modules/database-ingestion-via-jdbc-connection/03-inputs-derived.tf
+++ modules/database-ingestion-via-jdbc-connection/03-inputs-derived.tf
@@ -1,4 +1,4 @@
 locals {
   database_and_schema_name_lowercase = var.schema_name == null ? lower(local.database_name) : lower("${local.database_name}-${var.schema_name}")
-  iam_role_name                      = var.table_filter == null ? database_and_schema_name_lowercase : lower(var.table_filter)
+  iam_role_name                      = var.table_filter == null ? local.database_and_schema_name_lowercase : lower(var.table_filter)
 }
