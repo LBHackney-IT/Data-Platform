ab523ffd6cfd8ca57b42fb174fba1e85d1c473b2
daroclark <daro.clark@hackney.gov.uk>
2022-05-30 15:30:26 +0100

    Update scripts/jobs/planning/tascomi_officers_trusted.py
    Co-authored-by: annajgibson <annajgibson@users.noreply.github.com>

--- scripts/jobs/planning/tascomi_officers_trusted.py
+++ scripts/jobs/planning/tascomi_officers_trusted.py
@@ -1,5 +1,4 @@
  # Before running your job, go to the Job Details tab and customise:
-    # - the temporary storage path (same as above)
     # - the job parameters (replace the template values coming from the Terraform with real values)
 
 import sys
