e3ffcb1e7dc810c5ffabc8492ae44fb21c6b4ff8
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2022-05-31 16:35:25 +0200

    restructure dict in spatial enrichment job

--- scripts/jobs/geospatial/spatial_enrichment.py
+++ scripts/jobs/geospatial/spatial_enrichment.py
@@ -94,39 +94,50 @@ def deal_with_nan_colums(df,boundary_tables_dict):
     This function replaces nan value with an empty string for specific columns passed in a dictionary
     """
     columns_with_potential_nan_values = []
-    for key in boundary_tables_dict:
-        for colKey in boundary_tables_dict[key]:
-            columns_with_potential_nan_values.append(boundary_tables_dict[key][colKey])
+    for boundary_table_key in boundary_tables_dict:
+        for column_key in boundary_tables_dict[boundary_table_key]["columns_to_append"]:
+            columns_with_potential_nan_values.append(boundary_tables_dict[boundary_table_key]["columns_to_append"][column_key])
     for column_name in columns_with_potential_nan_values:
         df[column_name] = df[column_name].fillna('')
     return df
-        
+
+
+def convert_coordinate_columns_to_double(spark_point_df):
+    """
+    This function converts all coordinate columns of a spark dataframe to double
+    """
+    spark_point_df = spark_point_df.withColumn("latitude",spark_point_df.latitude.cast(DoubleType()))
+    spark_point_df = spark_point_df.withColumn("longitude",spark_point_df.longitude.cast(DoubleType()))
+    spark_point_df = spark_point_df.withColumn("eastings",spark_point_df.eastings.cast(DoubleType()))
+    spark_point_df = spark_point_df.withColumn("northings",spark_point_df.northings.cast(DoubleType()))
 
 # Dictionary of the geography tables we're using for the enrichment     
 #  format: {
-    # "glue_database_1"."glue_table_1":{"column_1_to_append_to_enriched_table":"alias_for_column_1_in_enriched_table","column_2_to_append_to_enriched_table":"alias_for_column_2_in_enriched_table"},
-    # "glue_database_2"."glue_table_2":{"column_1_to_append_to_enriched_table":"alias_for_column_1_in_enriched_table","column_2_to_append_to_enriched_table":"alias_for_column_2_in_enriched_table"}
+    # "key1": {"database_name":"", "table_name":"", "columns_to_append":{"column_1_to_append_to_enriched_table":"alias_for_column_1_in_enriched_table","column_2_to_append_to_enriched_table":"alias_for_column_2_in_enriched_table"}},
+    # "key2": {"database_name":"", "table_name":"", "columns_to_append":{"column_1_to_append_to_enriched_table":"alias_for_column_1_in_enriched_table","column_2_to_append_to_enriched_table":"alias_for_column_2_in_enriched_table"}}
     # }                
 boundary_tables_dict = {
-    "unrestricted-raw-zone.geolive_boundaries_hackney_ward": {"name":"ward_name", "census_code":"ward_ons_code"},
-    "unrestricted-raw-zone.geolive_boundaries_hackney_lsoa_2011": {"code":"lsoa_ons_code", "lsoa_name":"lsoa_name"},
-    "unrestricted-raw-zone.geolive_boundaries_hackney_msoa_2011": {"msoa11cd":"msoa_ons_code", "msoa11nm":"msoa_name"}
+    "ward": {"database_name":"unrestricted-raw-zone", "table_name":"geolive_boundaries_hackney_ward","columns_to_append":{"name":"ward_name", "census_code":"ward_ons_code"}},
+    "lsoa": {"database_name":"unrestricted-raw-zone", "table_name":"geolive_boundaries_hackney_lsoa_2011","columns_to_append":{"code":"lsoa_ons_code", "lsoa_name":"lsoa_name"}},
+    "msoa": {"database_name":"unrestricted-raw-zone", "table_name":"geolive_boundaries_hackney_msoa_2011","columns_to_append":{"msoa11cd":"msoa_ons_code", "msoa11nm":"msoa_name"}}
 }
 
 if __name__ == "__main__":
 
+    # get args and job parameters
     args = getResolvedOptions(sys.argv, ['TempDir','JOB_NAME'])
+
+    table_list_string = get_glue_env_var('table_list','')
+    source_catalog_database = get_glue_env_var('source_catalog_database', '')
+    s3_bucket_target = get_glue_env_var('s3_bucket_target', '')
     
+    # start session
     sc = SparkContext()
     glueContext = GlueContext(sc)
     spark = glueContext.spark_session
     job = Job(glueContext)
     job.init(args['JOB_NAME'], args)
     logger = glueContext.get_logger()
-
-    table_list_string = get_glue_env_var('table_list','')
-    source_catalog_database = get_glue_env_var('source_catalog_database', '')
-    s3_bucket_target = get_glue_env_var('s3_bucket_target', '')
     
     # wipe out the target folder
     logger.info(f'clearing target bucket')
@@ -135,25 +146,23 @@ if __name__ == "__main__":
     # load all boundary tables, prepare them and keep them in a list of geodataframes for later
     list_boundary_df = []
 
-    for key in boundary_tables_dict:
+    for boundary_table_key in boundary_tables_dict:
         #load the table from S3
-        database_name = key.split('.')[0]
-        table_name = key.split('.')[1]
         boundary_data_source = glueContext.create_dynamic_frame.from_catalog(
-            name_space=database_name,
-            table_name=table_name
+            name_space = boundary_tables_dict[boundary_table_key]["database_name"],
+            table_name = boundary_tables_dict[boundary_table_key]["table_name"]
         )
         boundary_df = boundary_data_source.toDF()
         boundary_df = get_latest_partitions(boundary_df)
-        logger.info(f'list of columns to keep in addition to geom: {boundary_tables_dict[key].keys()}')
+        logger.info(f'list of columns to keep in addition to geom: {boundary_tables_dict[boundary_table_key]["columns_to_append"].keys()}')
         #select these columns + geom
-        columns_to_keep = boundary_tables_dict[key].keys()
+        columns_to_keep = boundary_tables_dict[boundary_table_key]["columns_to_append"].keys()
         boundary_df = boundary_df.select('geom',*columns_to_keep)
 
-        for colKey in boundary_tables_dict[key]:
-            logger.info(f'{colKey} -> {boundary_tables_dict[key][colKey]}')
+        for column_key in boundary_tables_dict[boundary_table_key]["columns_to_append"]:
+            logger.info(f'{column_key} -> {boundary_tables_dict[boundary_table_key]["columns_to_append"][column_key]}')
             # rename these columns
-            boundary_df = boundary_df.withColumnRenamed(colKey, boundary_tables_dict[key][colKey])
+            boundary_df = boundary_df.withColumnRenamed(column_key, boundary_tables_dict[boundary_table_key]["columns_to_append"][column_key])
         boundary_df = boundary_df.toPandas()
         boundary_geometry = boundary_df['geom'].apply(shapely.wkb.loads, args=(True,)) 
         boundary_df = boundary_df.drop(columns=['geom'])
@@ -204,11 +213,8 @@ if __name__ == "__main__":
         # back from pandas to spark
         spark_point_df = spark.createDataFrame(point_pd_df)
         
-        # Convert coordinates columns to double (Pandas make them decimal when calculating them)
-        spark_point_df = spark_point_df.withColumn("latitude",spark_point_df.latitude.cast(DoubleType()))
-        spark_point_df = spark_point_df.withColumn("longitude",spark_point_df.longitude.cast(DoubleType()))
-        spark_point_df = spark_point_df.withColumn("eastings",spark_point_df.eastings.cast(DoubleType()))
-        spark_point_df = spark_point_df.withColumn("northings",spark_point_df.northings.cast(DoubleType()))
+        # Convert coordinates columns to double (Pandas made them decimal when calculating them)
+        spark_point_df = convert_coordinate_columns_to_double(spark_point_df)
         
         # Convert data frame to dynamic frame 
         dynamic_frame = DynamicFrame.fromDF(spark_point_df, glueContext, "target_data_to_write")
