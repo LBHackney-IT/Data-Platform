014427c9b60ead8ee7268b55929b21b94d201eb2
b-dalton <ben.dalton@madetech.com>
2022-06-06 11:54:41 +0100

    Create Qlik Service account outside of department, remove department set up as not needed

--- terraform/41-google-service-accounts.tf
+++ terraform/41-google-service-accounts.tf
@@ -25,3 +25,33 @@ resource "google_service_account_key" "housing_json_credentials" {
     rotation_time = time_rotating.key_rotation_housing[0].rotation_rfc3339
   }
 }
+
+/* ==== SERVICE ACCOUNT - QLIK =================================================================================== */
+resource "google_service_account" "service_account_qlik" {
+  count = local.is_live_environment ? 1 : 0
+
+  account_id   = lower("${local.identifier_prefix} qlik data and insight")
+  display_name = "${var.application} - Qlik-Data-and-Insight"
+
+  project = var.google_project_id
+}
+
+resource "time_rotating" "key_rotation_qlik" {
+  count = local.is_live_environment ? 1 : 0
+
+  rotation_days = 30
+}
+
+resource "google_service_account_key" "qlik_json_credentials" {
+  count = local.is_live_environment ? 1 : 0
+
+  service_account_id = google_service_account.service_account_qlik[0].name
+  public_key_type    = "TYPE_X509_PEM_FILE"
+
+  keepers = {
+    # Arbitrary map of values that, when changed, will trigger a new key to be generated
+    secret_id     = aws_secretsmanager_secret.sheets_credentials_qlik.id
+    rotation_time = time_rotating.key_rotation_qlik[0].rotation_rfc3339
+  }
+}
+
