67dd50f6d1b695b8ecbb0ff6df14222d34d03753
Emma <emma@madetech.com>
2022-06-06 08:39:01 +0100

    Only download files from gdrive if they have been updated

--- terraform/25-aws-glue-job-sandbox.tf
+++ terraform/25-aws-glue-job-sandbox.tf
@@ -172,10 +172,10 @@ module "covid_vaccinations_arda_sandbox" {
   spark_ui_output_storage_id = module.spark_ui_output_storage.bucket_id
   helper_module_key          = aws_s3_bucket_object.helpers.key
   job_parameters = {
-    "--s3_bucket_target"         = "s3://${module.refined_zone.bucket_id}/sandbox/covid_locations_arda/"
-    "--source_catalog_database"  = module.department_sandbox.raw_zone_catalog_database_name
-    "--source_catalog_table"     = "sandbox_arda_covid_vaccination_loc"
-    "--source_catalog_table_vaccinations"    = "sandbox_arda_covid_vaccination_vac"
+    "--s3_bucket_target"                  = "s3://${module.refined_zone.bucket_id}/sandbox/covid_locations_arda/"
+    "--source_catalog_database"           = module.department_sandbox.raw_zone_catalog_database_name
+    "--source_catalog_table"              = "sandbox_arda_covid_vaccination_loc"
+    "--source_catalog_table_vaccinations" = "sandbox_arda_covid_vaccination_vac"
   }
   crawler_details = {
     database_name      = module.department_sandbox.refined_zone_catalog_database_name
