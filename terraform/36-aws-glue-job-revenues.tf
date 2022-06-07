b2b21c4c7f07bdfe03986e56bbd4172e2b20f469
b-dalton <ben.dalton@madetech.com>
2022-06-07 13:34:51 +0100

    Formatting

--- terraform/36-aws-glue-job-revenues.tf
+++ terraform/36-aws-glue-job-revenues.tf
@@ -1,18 +1,18 @@
 module "etl_ctax_live_properties" {
-  source                     = "../modules/aws-glue-job"
-  department                 = module.department_revenues
-  job_name                   = "${local.short_identifier_prefix}etl_ctax_live_properties"
-  helper_module_key          = aws_s3_bucket_object.helpers.key
-  pydeequ_zip_key            = aws_s3_bucket_object.pydeequ.key
-  script_name                = "etl_ctax_live_properties"
-  triggered_by_job           = module.etl_ctax_live_properties.job_name
-  job_description            = "Created with AWS Glue Studio: Revenues ETL CTax_Live_Properties_Automation"
-  trigger_enabled            = false
-  schedule                   = "cron(0 55 9 ? * MON-FRI *)"
-  glue_job_worker_type       = "G.2X"
-  number_of_workers_for_glue_job  = 10
-  glue_job_timeout                = 1440
-  spark_ui_output_storage_id  = module.spark_ui_output_storage.bucket_id
+  source                         = "../modules/aws-glue-job"
+  department                     = module.department_revenues
+  job_name                       = "${local.short_identifier_prefix}etl_ctax_live_properties"
+  helper_module_key              = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = aws_s3_bucket_object.pydeequ.key
+  script_name                    = "etl_ctax_live_properties"
+  triggered_by_job               = module.etl_ctax_live_properties.job_name
+  job_description                = "Created with AWS Glue Studio: Revenues ETL CTax_Live_Properties_Automation"
+  trigger_enabled                = false
+  schedule                       = "cron(0 55 9 ? * MON-FRI *)"
+  glue_job_worker_type           = "G.2X"
+  number_of_workers_for_glue_job = 10
+  glue_job_timeout               = 1440
+  spark_ui_output_storage_id     = module.spark_ui_output_storage.bucket_id
   job_parameters = {
     "--job-bookmark-option" = "job-bookmark-disable"
     "--environment"         = var.environment
@@ -20,19 +20,19 @@ module "etl_ctax_live_properties" {
 }
 
 module "etl_zerobase_ctax_live_properties" {
-  source                     = "../modules/aws-glue-job"
-  department                 = module.department_revenues
-  job_name                   = "${local.short_identifier_prefix}etl_zerobase_ctax_live_properties"
-  helper_module_key          = aws_s3_bucket_object.helpers.key
-  pydeequ_zip_key            = aws_s3_bucket_object.pydeequ.key
-  script_name                = "etl_zerobase_ctax_live_properties"
-  triggered_by_job           = module.etl_zerobase_ctax_live_properties.job_name
-  job_description            = "Created with AWS Glue Studio: Revenues ETL CTax_Live_Properties_Initialization"
-  trigger_enabled            = false
-  glue_job_worker_type       = "G.2X"
-  number_of_workers_for_glue_job  = 10
-  glue_job_timeout                = 1440
-  spark_ui_output_storage_id  = module.spark_ui_output_storage.bucket_id
+  source                         = "../modules/aws-glue-job"
+  department                     = module.department_revenues
+  job_name                       = "${local.short_identifier_prefix}etl_zerobase_ctax_live_properties"
+  helper_module_key              = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = aws_s3_bucket_object.pydeequ.key
+  script_name                    = "etl_zerobase_ctax_live_properties"
+  triggered_by_job               = module.etl_zerobase_ctax_live_properties.job_name
+  job_description                = "Created with AWS Glue Studio: Revenues ETL CTax_Live_Properties_Initialization"
+  trigger_enabled                = false
+  glue_job_worker_type           = "G.2X"
+  number_of_workers_for_glue_job = 10
+  glue_job_timeout               = 1440
+  spark_ui_output_storage_id     = module.spark_ui_output_storage.bucket_id
   job_parameters = {
     "--job-bookmark-option" = "job-bookmark-disable"
     "--environment"         = var.environment
