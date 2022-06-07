2aa7073111441e246b3ecdd23d0a5de2b4ddb4b5
Ben Dalton <70905620+b-dalton@users.noreply.github.com>
2022-06-07 14:19:24 +0100

    Disable geolive sql ingestion to unrestricted raw zone (#689)

--- terraform/60-db-snapshot-to-s3.tf
+++ terraform/60-db-snapshot-to-s3.tf
@@ -12,20 +12,20 @@ module "lambda_artefact_storage_for_api_account" {
   }
 }
 
-module "db_snapshot_to_s3" {
-  source                         = "../modules/db-snapshot-to-s3"
-  tags                           = module.tags.values
-  project                        = var.project
-  environment                    = var.environment
-  identifier_prefix              = local.identifier_prefix
-  lambda_artefact_storage_bucket = module.lambda_artefact_storage_for_api_account.bucket_id
-  zone_kms_key_arn               = module.raw_zone.kms_key_arn
-  zone_bucket_arn                = module.raw_zone.bucket_arn
-  zone_bucket_id                 = module.raw_zone.bucket_id
-  service_area                   = "unrestricted"
-  rds_instance_ids               = var.rds_instance_ids
-
-  providers = {
-    aws = aws.aws_api_account
-  }
-}
+//module "db_snapshot_to_s3" {
+//  source                         = "../modules/db-snapshot-to-s3"
+//  tags                           = module.tags.values
+//  project                        = var.project
+//  environment                    = var.environment
+//  identifier_prefix              = local.identifier_prefix
+//  lambda_artefact_storage_bucket = module.lambda_artefact_storage_for_api_account.bucket_id
+//  zone_kms_key_arn               = module.raw_zone.kms_key_arn
+//  zone_bucket_arn                = module.raw_zone.bucket_arn
+//  zone_bucket_id                 = module.raw_zone.bucket_id
+//  service_area                   = "unrestricted"
+//  rds_instance_ids               = var.rds_instance_ids
+//
+//  providers = {
+//    aws = aws.aws_api_account
+//  }
+//}
