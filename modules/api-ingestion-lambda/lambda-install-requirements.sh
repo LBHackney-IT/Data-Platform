fe59043f0bf3b1f68dd33d7f609f4f00fc4cdaf4
b-dalton <ben.dalton@madetech.com>
2022-06-15 12:59:47 +0100

    Update bash command to reference makefile correctly

--- modules/api-ingestion-lambda/lambda-install-requirements.sh
+++ modules/api-ingestion-lambda/lambda-install-requirements.sh
@@ -4,4 +4,4 @@ set -ex
 
 lambda_name=$1
 
-(cd ~/lambdas/"$lambda_name"/ && make -f Makefile)
+cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd && cd "../../lambdas/api_ingestion_lambdas/${lambda_name}/" && make -f Makefile
