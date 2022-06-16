2ac2d3fc04a58472f35a5d9aaa947d5c0d16c515
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-16 09:41:05 +0100

    Fix tags path

--- terraform/etl/03-input-derived.tf
+++ terraform/etl/03-input-derived.tf
@@ -1,6 +1,6 @@
 # Any internal local variables should be declared here. We also import the tag module for convenience
 module "tags" {
-  source = "git@github.com:LBHackney-IT/infrastructure.git//modules/data-sources/aws-tags-lbh/module?ref=master"
+  source = "git@github.com:LBHackney-IT/infrastructure.git//modules/aws-tags-lbh/module?ref=master"
 
   application          = var.application
   automation_build_url = var.automation_build_url
