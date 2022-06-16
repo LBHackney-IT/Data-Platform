4a5ac06306a681cdf230f2d370ab5a9a565f1971
b-dalton <ben.dalton@madetech.com>
2022-06-16 13:08:28 +0100

    Remove null resource triggers so that it runs install lambda requirements

--- modules/api-ingestion-lambda/10-lambda.tf
+++ modules/api-ingestion-lambda/10-lambda.tf
@@ -146,11 +146,6 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_api_ingestion_lambda"
 }
 
 resource "null_resource" "run_make_install_requirements" {
-
-  triggers = {
-    dir_sha1 = sha1(join("", [for f in fileset("${path.module}/../lambdas/api_ingestion_lambdas/${local.lambda_name_underscore}_api_ingestion", "*") : filesha1(f)]))
-  }
-
   provisioner "local-exec" {
     interpreter = ["bash", "-c"]
     command     = "make install-requirements"
