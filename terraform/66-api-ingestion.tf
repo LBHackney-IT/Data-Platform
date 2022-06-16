24f519f3a4a1342452382dbae90d48432447b0aa
b-dalton <ben.dalton@madetech.com>
2022-06-16 14:29:07 +0100

    Move icaseworks lambda to root of lambdas directory, make naming more generic

--- terraform/66-api-ingestion.tf
+++ terraform/66-api-ingestion.tf
@@ -9,7 +9,7 @@ module "icaseworks_api_ingestion" {
 
   identifier_prefix              = local.short_identifier_prefix
   lambda_artefact_storage_bucket = module.lambda_artefact_storage.bucket_id
-  lambda_name                    = "icaseworks"
+  lambda_name                    = "icaseworks-api-ingestion"
   secrets_manager_kms_key        = aws_kms_key.secrets_manager_key
   s3_target_bucket_arn           = module.landing_zone.bucket_arn
   s3_target_bucket_name          = local.s3_target_bucket_name
