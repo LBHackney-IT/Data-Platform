7a32753510e493facc63095228e09d23f49993c8
joates-madetech <james.oates@madetech.com>
2022-06-16 14:38:44 +0100

    Fix for the schema naming anomalies

--- terraform/70-redshift.tf
+++ terraform/70-redshift.tf
@@ -27,54 +27,54 @@ locals {
   ]
 
   redshift_schemas = {
-    dataplatform_stg_tascomi_refined_zone = aws_glue_catalog_database.refined_zone_tascomi.name,
+    replace(aws_glue_catalog_database.refined_zone_tascomi.name, "-", "_") = aws_glue_catalog_database.refined_zone_tascomi.name,
 
-    housing_repairs_raw_zone     = module.department_housing_repairs.raw_zone_catalog_database_name,
-    housing_repairs_refined_zone = module.department_housing_repairs.refined_zone_catalog_database_name,
-    housing_repairs_trusted_zone = module.department_housing_repairs.trusted_zone_catalog_database_name,
+    replace(module.department_housing_repairs.raw_zone_catalog_database_name, "-", "_") = module.department_housing_repairs.raw_zone_catalog_database_name,
+    replace(module.department_housing_repairs.raw_zone_catalog_database_name, "-", "_") = module.department_housing_repairs.refined_zone_catalog_database_name,
+    replace(module.department_housing_repairs.raw_zone_catalog_database_name, "-", "_") = module.department_housing_repairs.trusted_zone_catalog_database_name,
 
     parking_raw_zone_liberator     = aws_glue_catalog_database.raw_zone_liberator.name,
     parking_refined_zone_liberator = aws_glue_catalog_database.refined_zone_liberator.name,
 
-    parking_raw_zone     = module.department_parking.raw_zone_catalog_database_name,
-    parking_refined_zone = module.department_parking.refined_zone_catalog_database_name,
-    parking_trusted_zone = module.department_parking.trusted_zone_catalog_database_name,
+    replace(module.department_parking.raw_zone_catalog_database_name, "-", "_") = module.department_parking.raw_zone_catalog_database_name,
+    replace(module.department_parking.raw_zone_catalog_database_name, "-", "_") = module.department_parking.refined_zone_catalog_database_name,
+    replace(module.department_parking.raw_zone_catalog_database_name, "-", "_") = module.department_parking.trusted_zone_catalog_database_name,
 
-    finance_raw_zone     = module.department_finance.raw_zone_catalog_database_name,
-    finance_refined_zone = module.department_finance.refined_zone_catalog_database_name,
-    finance_trusted_zone = module.department_finance.trusted_zone_catalog_database_name,
+    replace(module.department_finance.raw_zone_catalog_database_name, "-", "_") = module.department_finance.raw_zone_catalog_database_name,
+    replace(module.department_finance.raw_zone_catalog_database_name, "-", "_") = module.department_finance.refined_zone_catalog_database_name,
+    replace(module.department_finance.raw_zone_catalog_database_name, "-", "_") = module.department_finance.trusted_zone_catalog_database_name,
 
-    data_and_insight_raw_zone     = module.department_data_and_insight.raw_zone_catalog_database_name,
-    data_and_insight_refined_zone = module.department_data_and_insight.refined_zone_catalog_database_name,
-    data_and_insight_trusted_zone = module.department_data_and_insight.trusted_zone_catalog_database_name,
+    replace(module.department_data_and_insight.raw_zone_catalog_database_name, "-", "_") = module.department_data_and_insight.raw_zone_catalog_database_name,
+    replace(module.department_data_and_insight.raw_zone_catalog_database_name, "-", "_") = module.department_data_and_insight.refined_zone_catalog_database_name,
+    replace(module.department_data_and_insight.raw_zone_catalog_database_name, "-", "_") = module.department_data_and_insight.trusted_zone_catalog_database_name,
 
-    env_enforcement_raw_zone     = module.department_env_enforcement.raw_zone_catalog_database_name,
-    env_enforcement_refined_zone = module.department_env_enforcement.refined_zone_catalog_database_name,
-    env_enforcement_trusted_zone = module.department_env_enforcement.trusted_zone_catalog_database_name,
+    replace(module.department_env_enforcement.raw_zone_catalog_database_name, "-", "_") = module.department_env_enforcement.raw_zone_catalog_database_name,
+    replace(module.department_env_enforcement.raw_zone_catalog_database_name, "-", "_") = module.department_env_enforcement.refined_zone_catalog_database_name,
+    replace(module.department_env_enforcement.raw_zone_catalog_database_name, "-", "_") = module.department_env_enforcement.trusted_zone_catalog_database_name,
 
-    planning_raw_zone     = module.department_planning.raw_zone_catalog_database_name,
-    planning_refined_zone = module.department_planning.refined_zone_catalog_database_name,
-    planning_trusted_zone = module.department_planning.trusted_zone_catalog_database_name,
+    replace(module.department_planning.raw_zone_catalog_database_name, "-", "_") = module.department_planning.raw_zone_catalog_database_name,
+    replace(module.department_planning.raw_zone_catalog_database_name, "-", "_") = module.department_planning.refined_zone_catalog_database_name,
+    replace(module.department_planning.raw_zone_catalog_database_name, "-", "_") = module.department_planning.trusted_zone_catalog_database_name,
 
-    sandbox_trusted_zone = module.department_sandbox.trusted_zone_catalog_database_name,
-    sandbox_refined_zone = module.department_sandbox.refined_zone_catalog_database_name,
-    sandbox_raw_zone     = module.department_sandbox.raw_zone_catalog_database_name,
+    replace(module.department_sandbox.raw_zone_catalog_database_name, "-", "_") = module.department_sandbox.trusted_zone_catalog_database_name,
+    replace(module.department_sandbox.raw_zone_catalog_database_name, "-", "_") = module.department_sandbox.refined_zone_catalog_database_name,
+    replace(module.department_sandbox.raw_zone_catalog_database_name, "-", "_") = module.department_sandbox.raw_zone_catalog_database_name,
 
-    benefits_and_housing_needs_trusted_zone = module.department_benefits_and_housing_needs.trusted_zone_catalog_database_name,
-    benefits_and_housing_needs_refined_zone = module.department_benefits_and_housing_needs.refined_zone_catalog_database_name,
-    benefits_and_housing_needs_raw_zone     = module.department_benefits_and_housing_needs.raw_zone_catalog_database_name,
+    replace(module.department_benefits_and_housing_needs.raw_zone_catalog_database_name, "-", "_") = module.department_benefits_and_housing_needs.trusted_zone_catalog_database_name,
+    replace(module.department_benefits_and_housing_needs.raw_zone_catalog_database_name, "-", "_") = module.department_benefits_and_housing_needs.refined_zone_catalog_database_name,
+    replace(module.department_benefits_and_housing_needs.raw_zone_catalog_database_name, "-", "_") = module.department_benefits_and_housing_needs.raw_zone_catalog_database_name,
 
-    revenues_trusted_zone = module.department_revenues.trusted_zone_catalog_database_name,
-    revenues_refined_zone = module.department_revenues.refined_zone_catalog_database_name,
-    revenues_raw_zone     = module.department_revenues.raw_zone_catalog_database_name,
+    replace(module.department_revenues.raw_zone_catalog_database_name, "-", "_") = module.department_revenues.trusted_zone_catalog_database_name,
+    replace(module.department_revenues.raw_zone_catalog_database_name, "-", "_") = module.department_revenues.refined_zone_catalog_database_name,
+    replace(module.department_revenues.raw_zone_catalog_database_name, "-", "_") = module.department_revenues.raw_zone_catalog_database_name,
 
-    environmental_services_trusted_zone = module.department_environmental_services.trusted_zone_catalog_database_name,
-    environmental_services_refined_zone = module.department_environmental_services.refined_zone_catalog_database_name,
-    environmental_services_raw_zone     = module.department_environmental_services.raw_zone_catalog_database_name,
+    replace(module.department_environmental_services.raw_zone_catalog_database_name, "-", "_") = module.department_environmental_services.trusted_zone_catalog_database_name,
+    replace(module.department_environmental_services.raw_zone_catalog_database_name, "-", "_") = module.department_environmental_services.refined_zone_catalog_database_name,
+    replace(module.department_environmental_services.raw_zone_catalog_database_name, "-", "_") = module.department_environmental_services.raw_zone_catalog_database_name,
 
-    housing_trusted_zone = module.department_housing.trusted_zone_catalog_database_name,
-    housing_refined_zone = module.department_housing.refined_zone_catalog_database_name,
-    housing_raw_zone     = module.department_housing.raw_zone_catalog_database_name,
+    replace(module.department_housing.raw_zone_catalog_database_name, "-", "_") = module.department_housing.trusted_zone_catalog_database_name,
+    replace(module.department_housing.raw_zone_catalog_database_name, "-", "_") = module.department_housing.refined_zone_catalog_database_name,
+    replace(module.department_housing.raw_zone_catalog_database_name, "-", "_") = module.department_housing.raw_zone_catalog_database_name,
   }
 
   redshift_users = [
@@ -117,8 +117,8 @@ locals {
         replace(module.department_housing_repairs.refined_zone_catalog_database_name, "-", "_"),
         replace(module.department_housing_repairs.trusted_zone_catalog_database_name, "-", "_"),
 
-        replace(aws_glue_catalog_database.raw_zone_liberator.name, "-", "_"),
-        replace(aws_glue_catalog_database.refined_zone_liberator.name, "-", "_"),
+        "parking_raw_zone_liberator",
+        "parking_refined_zone_liberator",
 
         replace(module.department_parking.raw_zone_catalog_database_name, "-", "_"),
         replace(module.department_parking.refined_zone_catalog_database_name, "-", "_"),
