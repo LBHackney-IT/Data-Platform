6f35d7d406208d248c1edfaee3cb1b7998f5c8c1
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-21 16:52:01 +0100

    -dry-run optional

--- terraform/core/state-migration.sh
+++ terraform/core/state-migration.sh
@@ -1,13 +1,13 @@
-#./state-migration.sh development 2>&1 | tee state-migration-output
+#./state-migration.sh development -dry-run 2>&1 | tee state-migration-output
 rm -f state-migration-output
 FILES="../etl/*.tf"
 for f in $FILES
 do
     grep -Eo -e '^module \".*\"' -e '^data \".*\"' "$f" | tr -s " \"" "." | sed 's/\.$//' | \
-    while read -r resource_address; do aws-vault exec hackney-dataplatform-"${1}" -- terraform state mv -dry-run -state-out=./tfstate-etl.tfstate "$resource_address" "$resource_address"; done
+    while read -r resource_address; do aws-vault exec hackney-dataplatform-"${1}" -- terraform state mv $2 -state-out=./tfstate-etl.tfstate "$resource_address" "$resource_address"; done
 
     grep -Eo '^resource \".*\"' "$f" | tr -s " \"" "." | sed 's/\.$//' | sed 's/^resource\.//' | \
-    while read -r resource_address; do aws-vault exec hackney-dataplatform-"${1}" -- terraform state mv -dry-run -state-out=./tfstate-etl.tfstate "$resource_address" "$resource_address"; done;
+    while read -r resource_address; do aws-vault exec hackney-dataplatform-"${1}" -- terraform state mv $2 -state-out=./tfstate-etl.tfstate "$resource_address" "$resource_address"; done;
 done
 
 #cp ./tfstate-etl.tfstate ../etl/tfstate-etl.tfstate
