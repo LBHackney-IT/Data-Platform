be87d9a4620de27a15ddaec9acaa75c89c185d84
daroclark <daro.clark@hackney.gov.uk>
2022-06-23 09:25:45 +0100

    Create 24-aws-glue-housing.tf
    Terraform job for housing. including the load of mtfh reshape files

---
+++ terraform/24-aws-glue-housing.tf
@@ -0,0 +1,25 @@
+module "mtfh_reshape_to_refined" {
+  source = "../modules/aws-glue-job"
+
+  department                 = module.department_housing
+  job_name                   = "${local.short_identifier_prefix}noisework_complaints_to_refined"
+  glue_job_worker_type       = "G.1X"
+  helper_module_key          = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key            = aws_s3_bucket_object.pydeequ.key
+  spark_ui_output_storage_id = module.spark_ui_output_storage.bucket_id
+  job_parameters = {
+    "--job-bookmark-option"     = "job-bookmark-enable"
+    "--s3_bucket_target"        = "s3://${module.refined_zone.bucket_id}/housing/mtfh"
+    "--enable-glue-datacatalog" = "true"
+    "--source_catalog_database" = module.department_housing.raw_zone_catalog_database_name
+    "--source_catalog_table"    = "housing-raw-zone"
+  }
+  script_name          = "housing_mtfh_reshape_to_refined"
+  triggered_by_crawler = module.copy_mtfh_dynamo_db_tables_for_rentsense_to_housing_department_raw_zone.crawler_name
+
+  crawler_details = {
+    database_name      = module.department_housing.refined_zone_catalog_database_name
+    s3_target_location = "s3://${module.refined_zone.bucket_id}/housing/mtfh""
+  }
+
+}
