397365231a3a313d71877a4c8acb262b9a68b2fc
joates-madetech <james.oates@madetech.com>
2022-06-23 10:34:12 +0100

    typescript build

--- modules/copy-from-s3-to-s3/lambda/src/local.ts
+++ modules/copy-from-s3-to-s3/lambda/src/local.ts
@@ -1,17 +1,16 @@
-process.on('unhandledRejection', error => {
-  // Will print "unhandledRejection err is not defined"
-  console.log('[UnhandledRejection]', error.message);
-  console.log(error)
-});
+process.on('unhandledRejection', (error: Error) => {
+    // Will print "unhandledRejection err is not defined"
+    console.log('[UnhandledRejection]', error.message)
+    console.log(error)
+})
 
-process.env.ORIGIN_BUCKET_ID = "dataplatform-joates-landing-zone";
-process.env.ORIGIN_PATH = "/parking";
-process.env.TARGET_BUCKET_ID = "dataplatform-joates-landing-zone";
-process.env.TARGET_PATH = "/potato";
-const handler = require("lambdas/lambda/index");
+process.env.ORIGIN_BUCKET_ID = 'dataplatform-joates-landing-zone'
+process.env.ORIGIN_PATH = '/parking'
+process.env.TARGET_BUCKET_ID = 'dataplatform-joates-landing-zone'
+process.env.TARGET_PATH = '/potato'
 
-handler.handler({});
+const handler = require('./index')
 
+handler.handler({})
 
-// /housing/databases   / name of the database / name of the table /
-// import_year{todays year} / import_month={todays month} / import_day={todays day} / parquet filename
+export {}
