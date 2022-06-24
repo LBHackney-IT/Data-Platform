a8d7eac6c406cb97d0f67c55376cb61a282999dd
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-24 13:59:13 +0100

    Enable encryption at rest for elastic search - sonar fixes

--- terraform/modules/datahub/11-elastic-search.tf
+++ terraform/modules/datahub/11-elastic-search.tf
@@ -17,6 +17,10 @@ resource "aws_elasticsearch_domain" "es" {
     tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
+  encrypt_at_rest {
+    enabled = true
+  }
+
   ebs_options {
     ebs_enabled = true
     volume_size = 10
