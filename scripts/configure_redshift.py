813c9d2f8109c85627595471451971cdcb536dc5
joates-madetech <james.oates@madetech.com>
2022-06-24 10:30:32 +0100

    Change how bulk queries are run

--- scripts/configure_redshift.py
+++ scripts/configure_redshift.py
@@ -2,11 +2,21 @@ import boto3
 import sys
 import json
 from time import sleep
+from itertools import islice
 
-secrets_manager = boto3.client('secretsmanager')
-terraform_outputs_json=sys.argv[1]
+from botocore.client import BaseClient
 
-terraform_outputs = json.loads(terraform_outputs_json)
+
+def chunk_list(lst, size):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), size):
+        yield lst[i:i + size]
+
+
+def chunk_list_2(lst, size):
+    lst = iter(lst)
+    return iter(lambda: list(islice(lst, size)), ())
+
 
 class Redshift:
     def __init__(self, redshift_client, cluster_id, redshift_role, admin_user, database_name):
@@ -15,9 +25,9 @@ class Redshift:
         self.role = redshift_role
         self.admin_user = admin_user
         self.database_name = database_name
-    
+
     def execute_query(self, sql):
-        response = redshift_client.execute_statement(
+        response = self.redshift_client.execute_statement(
             ClusterIdentifier=self.cluster_id,
             Database=self.database_name,
             DbUser=self.admin_user,
@@ -27,27 +37,33 @@ class Redshift:
         while status != "FINISHED":
             query = self.describe_query(response['Id'])
             status = query['Status']
-    
+
             if query['Status'] == "FAILED":
                 print(f"query failed with error {query['Error']}")
                 raise Exception(f"query failed with error {query['Error']}")
             print(query)
             sleep(0.5)
         return response['Id']
-    
+
     def execute_batch_queries(self, sqls: list):
-        response = redshift_client.batch_execute_statement(
+        chunks = list(chunk_list(sqls, 10))
+        for chunk in chunks:
+            self.execute_batch_queries_sub(chunk)
+
+    def execute_batch_queries_sub(self, sqls: list):
+        response = self.redshift_client.batch_execute_statement(
             ClusterIdentifier=self.cluster_id,
             Database=self.database_name,
             DbUser=self.admin_user,
             Sqls=sqls
         )
         finished = False
-        while (not finished):
+        while not finished:
             query = self.describe_query(response['Id'])
             finished = all([sub_statement['Status'] == "FINISHED" for sub_statement in query['SubStatements']])
-            failed_queries = [sub_statement for sub_statement in query['SubStatements'] if sub_statement['Status'] == "FAILED" or sub_statement['Status'] == "ABORTED"]
- 
+            failed_queries = [sub_statement for sub_statement in query['SubStatements'] if
+                              sub_statement['Status'] == "FAILED" or sub_statement['Status'] == "ABORTED"]
+
             if len(failed_queries) > 0:
                 print(f"{len(failed_queries)} queries failed with errors:")
                 for query in failed_queries:
@@ -58,43 +74,43 @@ class Redshift:
         return response['Id']
 
     def get_results(self, statement_id):
-        result=redshift_client.get_statement_result(Id=statement_id)
+        result = self.redshift_client.get_statement_result(Id=statement_id)
         records = result['Records']
         next_token = result.get('NextToken')
-        while(next_token):
-            result = redshift_client.get_statement_result(Id=statement_id, NextToken=next_token)
+        while next_token:
+            result = self.redshift_client.get_statement_result(Id=statement_id, NextToken=next_token)
             records = records + result['Records']
             next_token = result.get('NextToken')
-        
+
         return records
-    
+
     def describe_query(self, statement_id):
-        response = redshift_client.describe_statement(
+        response = self.redshift_client.describe_statement(
             Id=statement_id
         )
         return response
 
 
 def create_schemas(redshift, schemas: dict) -> None:
-    create_schema_queries = [ f"CREATE EXTERNAL SCHEMA IF NOT EXISTS {schema}\
-            FROM DATA CATALOG\
-            DATABASE '{database}'\
-            IAM_ROLE '{redshift.role}'\
-            CREATE EXTERNAL DATABASE IF NOT EXISTS;" for schema, database in schemas.items()]
+    create_schema_queries = [f"CREATE EXTERNAL SCHEMA IF NOT EXISTS {schema}" +
+                             " FROM DATA CATALOG" +
+                             " DATABASE '{database}'" +
+                             " IAM_ROLE '{redshift.role}'" +
+                             " CREATE EXTERNAL DATABASE IF NOT EXISTS;" for schema, database in schemas.items()]
 
     redshift.execute_batch_queries(create_schema_queries)
     print(f"Created schemas")
 
 
 def get_users(redshift) -> list:
-    get_users="select usename from pg_user_info"
-    
-    get_users_query_id = redshift.execute_query(get_users)
+    get_users_query = "select usename from pg_user_info"
+
+    get_users_query_id = redshift.execute_query(get_users_query)
 
     return [record[0]['stringValue'] for record in redshift.get_results(get_users_query_id)]
 
 
-def get_password(secret_arn: str) -> str:
+def get_password(secrets_manager: BaseClient, secret_arn: str) -> str:
     response = secrets_manager.get_secret_value(
         SecretId=secret_arn,
     )
@@ -108,7 +124,7 @@ def get_password(secret_arn: str) -> str:
 def create_users(redshift, logins: list) -> None:
     if len(logins) == 0:
         print("No users to create")
-        return 
+        return
     create_user_queries = [f"CREATE USER {login['user']} WITH PASSWORD '{login['password']}'" for login in logins]
 
     redshift.execute_batch_queries(create_user_queries)
@@ -117,14 +133,14 @@ def create_users(redshift, logins: list) -> None:
 def update_passwords(redshift, logins: list) -> None:
     if len(logins) == 0:
         print("No users to update")
-        return 
+        return
     alter_password_queries = [f"ALTER USER {login['user']} WITH PASSWORD '{login['password']}'" for login in logins]
 
     redshift.execute_batch_queries(alter_password_queries)
 
 
-def configure_users(redshift, users: list) -> None:
-    logins = [{'user': user['user_name'], 'password': get_password(user['secret_arn'])} for user in users]
+def configure_users(redshift, secrets_manager: BaseClient, users: list) -> None:
+    logins = [{'user': user['user_name'], 'password': get_password(secrets_manager, user['secret_arn'])} for user in users]
     existing_usernames = get_users(redshift)
 
     existing_users_with_passwords = [login for login in logins if login["user"] in existing_usernames]
@@ -140,24 +156,39 @@ def grant_permissions_to_users(redshift, users: list) -> None:
         grant_temp_permissions = f"GRANT temp ON DATABASE {redshift.database_name} TO {username};"
         redshift.execute_query(grant_temp_permissions)
 
-        
-        grant_schema_permissions = [f"GRANT USAGE ON SCHEMA {schema} TO {username};" for schema in user["schemas_to_grant_access_to"]]
-        grant_table_permissions = [f"GRANT SELECT ON ALL TABLES IN SCHEMA {schema} TO {username};" for schema in user["schemas_to_grant_access_to"]]
+        grant_schema_permissions = [f"GRANT USAGE ON SCHEMA {schema} TO {username};" for schema in
+                                    user["schemas_to_grant_access_to"]]
+        grant_table_permissions = [f"GRANT SELECT ON ALL TABLES IN SCHEMA {schema} TO {username};" for schema in
+                                   user["schemas_to_grant_access_to"]]
 
         redshift.execute_batch_queries(grant_schema_permissions)
         redshift.execute_batch_queries(grant_table_permissions)
         print(f"Add permissions for user {username}")
-    return 
-
-cluster_id = terraform_outputs['redshift_cluster_id']['value']
-redshift_iam_role = terraform_outputs['redshift_iam_role_arn']['value']
-
-redshift_client = boto3.client('redshift-data')
-
-redshift = Redshift(cluster_id=cluster_id, redshift_client=redshift_client, redshift_role=redshift_iam_role, admin_user="data_engineers", database_name="data_platform")
+    return
 
 
-create_schemas(redshift, terraform_outputs['redshift_schemas']['value'])
-configure_users(redshift, terraform_outputs['redshift_users']['value'])
-grant_permissions_to_users(redshift, terraform_outputs['redshift_users']['value'])
+def main() -> None:
+    secrets_manager: BaseClient = boto3.client('secretsmanager')
+    terraform_outputs_json = sys.argv[1]
 
+    terraform_outputs = json.loads(terraform_outputs_json)
+
+    cluster_id = terraform_outputs['redshift_cluster_id']['value']
+    redshift_iam_role = terraform_outputs['redshift_iam_role_arn']['value']
+
+    redshift_client = boto3.client('redshift-data')
+
+    redshift = Redshift(
+        cluster_id=cluster_id,
+        redshift_client=redshift_client,
+        redshift_role=redshift_iam_role,
+        admin_user="data_engineers",
+        database_name="data_platform"
+    )
+
+    create_schemas(redshift, terraform_outputs['redshift_schemas']['value'])
+    configure_users(redshift, secrets_manager, terraform_outputs['redshift_users']['value'])
+    grant_permissions_to_users(redshift, terraform_outputs['redshift_users']['value'])
+
+
+main()
