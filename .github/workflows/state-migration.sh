fdfbf3590cbe7849d66d43b7a81b73eba5e14c92
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-28 22:26:36 +0100

    Comment out for now so i can get the production state list

--- .github/workflows/state-migration.sh
+++ .github/workflows/state-migration.sh
@@ -10,10 +10,11 @@ aws configure set aws_access_key_id "${aws_access_key_id}"
 aws configure set aws_secret_access_key "${aws_secret_access_key}"
 
 terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform.tfstate"
+terraform state list
 
-while read -r resource_address; do
-    terraform state mv -dry-run -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
-done < ../../.github/workflows/resources-to-move-"${environment}".txt
+#while read -r resource_address; do
+#    terraform state mv -dry-run -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
+#done < ../../.github/workflows/resources-to-move-"${environment}".txt
 
 #echo "New Core State"
 #terraform state list
