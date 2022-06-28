6a66251500e2853feea8d3d5cc67e8ec44a31698
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-28 14:58:23 +0100

    Add some more debug points

--- .github/workflows/state-migration.sh
+++ .github/workflows/state-migration.sh
@@ -12,6 +12,9 @@ aws configure set aws_access_key_id "${aws_access_key_id}"
 aws configure set aws_secret_access_key "${aws_secret_access_key}"
 terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform.tfstate"
 
+echo "Current State"
+terraform state list
+
 FILES="../etl/*.tf"
 for f in $FILES
 do
@@ -22,8 +25,11 @@ do
     while read -r resource_address; do terraform state mv -dry-run -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address"; done;
 done
 
-#
-cp ./"${1}"-terraform-etl.tfstate ../etl/"${1}"-terraform-etl.tfstate
+echo "New State"
+terraform state list
+
+ls
+cp ./"${environment}"-terraform-etl.tfstate ../etl/"${environment}"-terraform-etl.tfstate
 cd ../etl
 ls
-#terraform state push -force ./"${1}"-terraform-etl.tfstate
+#terraform state push -force ./"${environment}"-terraform-etl.tfstate
