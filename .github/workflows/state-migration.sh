79f2a852f66ff2ce75e40bb453e9d907e4be8c95
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-29 10:28:54 +0100

    Confused, going to list the state again

--- .github/workflows/state-migration.sh
+++ .github/workflows/state-migration.sh
@@ -10,16 +10,16 @@ aws configure set aws_access_key_id "${aws_access_key_id}"
 aws configure set aws_secret_access_key "${aws_secret_access_key}"
 
 terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform.tfstate"
-
-while read -r resource_address; do
-    terraform state mv -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
-done < ../../.github/workflows/resources-to-move.txt
+#
+#while read -r resource_address; do
+#    terraform state mv -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
+#done < ../../.github/workflows/resources-to-move.txt
 
 echo "New Core State"
 terraform state list
-
-ls
-cp ./"${environment}"-terraform-etl.tfstate ../etl/"${environment}"-terraform-etl.tfstate
-cd ../etl
-ls
-terraform state push -force ./"${environment}"-terraform-etl.tfstate
+#
+#ls
+#cp ./"${environment}"-terraform-etl.tfstate ../etl/"${environment}"-terraform-etl.tfstate
+#cd ../etl
+#ls
+#terraform state push -force ./"${environment}"-terraform-etl.tfstate
