1963f1cea89e442efa60b515fb81ff8b20387aa3
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-29 20:27:08 +0100

    Typo city

--- terraform/modules/data-sources/google-service-account/53-secrets-manager.tf
+++ terraform/modules/data-sources/google-service-account/53-secrets-manager.tf
@@ -1,5 +1,5 @@
 data "aws_secretsmanager_secret" "sheets_credentials" {
-  name_ = data.aws_ssm_parameter.sheets_credentials_name.value
+  name = data.aws_ssm_parameter.sheets_credentials_name.value
 }
 
 data "aws_ssm_parameter" "sheets_credentials_name" {
