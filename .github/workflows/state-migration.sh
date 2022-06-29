b5de0de222be79006799a6427f83f5e1ec734422
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-29 14:04:01 +0100

    Much better, just need to find the resource IDs

--- .github/workflows/state-migration.sh
+++ .github/workflows/state-migration.sh
@@ -9,17 +9,23 @@ aws configure set default.region eu-west-2
 aws configure set aws_access_key_id "${aws_access_key_id}"
 aws configure set aws_secret_access_key "${aws_secret_access_key}"
 
-terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform.tfstate"
 
+cd ../etl
+terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform-etl.tfstate"
+while read -r resource; do
+    terraform import -lock=false "$resource";
+done < ../../.github/workflows/resources-to-import.txt
+aws s3 cp s3://lbhackney-terraform-state/"${terraform_state_s3_key_prefix}"/"${environment}"-terraform-etl.tfstate .
+
+cd ../core
+terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform.tfstate"
 while read -r resource_address; do
-    terraform state mv -lock=false -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
+    terraform state mv -lock=false -state-out=../etl/"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
 done < ../../.github/workflows/resources-to-move.txt
 
 echo "New Core State"
 terraform state list
 
-ls
-cp ./"${environment}"-terraform-etl.tfstate ../etl/"${environment}"-terraform-etl.tfstate
 cd ../etl
 ls
 terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform-etl.tfstate"
