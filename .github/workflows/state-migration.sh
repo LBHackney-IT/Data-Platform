9a2ccb1c070ef4c603aec78518076a6d323e9cf7
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-06-29 13:44:44 +0100

    FFS

--- .github/workflows/state-migration.sh
+++ .github/workflows/state-migration.sh
@@ -12,7 +12,7 @@ aws configure set aws_secret_access_key "${aws_secret_access_key}"
 terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform.tfstate"
 
 while read -r resource_address; do
-    terraform state mv -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
+    terraform state mv -lock=false -state-out=./"${environment}"-terraform-etl.tfstate "$resource_address" "$resource_address";
 done < ../../.github/workflows/resources-to-move.txt
 
 echo "New Core State"
@@ -22,4 +22,5 @@ ls
 cp ./"${environment}"-terraform-etl.tfstate ../etl/"${environment}"-terraform-etl.tfstate
 cd ../etl
 ls
+terraform init -backend-config="region=eu-west-2" -backend-config="dynamodb_table=lbhackney-terraform-state-lock" -backend-config="encrypt=true" -backend-config="workspace_key_prefix=${terraform_state_s3_key_prefix}" -backend-config="bucket=lbhackney-terraform-state" -backend-config="key=${terraform_state_s3_key_prefix}/${environment}-terraform-etl.tfstate"
 terraform state push -force ./"${environment}"-terraform-etl.tfstate
