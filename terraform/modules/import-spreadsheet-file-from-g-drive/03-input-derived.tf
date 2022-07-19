d15a5a45617d6671bef57ed8ca0936295622e348
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-07-19 18:43:47 +0100

    Typo

--- terraform/modules/import-spreadsheet-file-from-g-drive/03-input-derived.tf
+++ terraform/modules/import-spreadsheet-file-from-g-drive/03-input-derived.tf
@@ -1,5 +1,5 @@
 locals {
   tags_with_department = merge(var.tags, { "PlatformDepartment" = var.department.identifier })
   file_name_list       = split(".", lower(var.worksheets[0].worksheet_name))
-  is_csv               = file_name_list[length(file_name_list) - 1] == "csv" ? true : false
+  is_csv               = local.file_name_list[length(local.file_name_list) - 1] == "csv" ? true : false
 }
