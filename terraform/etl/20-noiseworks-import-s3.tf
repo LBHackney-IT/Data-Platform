27c061ca7999683201435b77b084bf6ecfbea313
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-07-20 16:44:27 +0100

    Disable ingestion schedule on noisework in pre-production

--- terraform/etl/20-noiseworks-import-s3.tf
+++ terraform/etl/20-noiseworks-import-s3.tf
@@ -14,7 +14,7 @@ module "noiseworks_to_raw_zone" {
     "--deequ_metrics_location" = "s3://${module.raw_zone_data_source.bucket_id}/quality-metrics/department=env-enforcement/deequ-metrics.json"
   }
   script_name = "noiseworks_copy_csv_to_raw"
-  schedule    = "cron(0 2 * * ? *)"
+  schedule    = local.is_production_environment
 
   crawler_details = {
     database_name      = module.department_env_enforcement_data_source.raw_zone_catalog_database_name
