d71b2dae1d3374c8a63110bde12b72ff8731b6df
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-07-27 17:18:18 +0100

    Needs this at the start as these are all sub folder searches

--- docker/production-to-pre-production/sync-last-x-days.sh
+++ docker/production-to-pre-production/sync-last-x-days.sh
@@ -19,18 +19,18 @@ for i in $(seq 0 $((days_to_retain-1))); do
     # Include for sync from source to target: *data=2022-07-27/*
     sync_include_opts+=( --include="*date=$date_to_import_hyphen_separated/*" )
     # Include for sync from source to target: import_year=2022/import_month=07/import_day=27/*
-    sync_include_opts+=( --include="import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%-m")/import_day=$(date -d "$date_to_import" "+%-d")/*" )
+    sync_include_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%-m")/import_day=$(date -d "$date_to_import" "+%-d")/*" )
     # Include for sync from source to target: import_year=2022/import_month=7/import_day=27/*
-    sync_include_opts+=( --include="import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%m")/import_day=$(date -d "$date_to_import" "+%d")/*" )
+    sync_include_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%m")/import_day=$(date -d "$date_to_import" "+%d")/*" )
 
     # Exclude from deletion on target: *data=20220727/*
     rm_exclude_opts+=( --exclude="*date=$date_to_import/*" )
     # Exclude from deletion on target: *data=2022-07-27/*
     rm_exclude_opts+=( --exclude="*date=$date_to_import_hyphen_separated/*" )
     # Exclude from deletion on target: import_year=2022/import_month=07/import_day=27/*
-    rm_exclude_opts+=( --include="import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%-m")/import_day=$(date -d "$date_to_import" "+%-d")/*" )
+    rm_exclude_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%-m")/import_day=$(date -d "$date_to_import" "+%-d")/*" )
     # Exclude from deletion on target: import_year=2022/import_month=7/import_day=27/*
-    rm_exclude_opts+=( --include="import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%m")/import_day=$(date -d "$date_to_import" "+%d")/*" )
+    rm_exclude_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%m")/import_day=$(date -d "$date_to_import" "+%d")/*" )
 done
 
 echo "Include flags to be used with s3 sync cmd: ${sync_include_opts[*]}"
