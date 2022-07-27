adc5ed53968ccbf4b92c0be8723605dee4cff7da
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-07-27 19:02:27 +0100

    Only delete the data from the days_to_retain plus 1 instead of clearing the bucket of all data not in the time window

--- docker/production-to-pre-production/sync-last-x-days.sh
+++ docker/production-to-pre-production/sync-last-x-days.sh
@@ -22,22 +22,24 @@ for i in $(seq 0 $((days_to_retain-1))); do
     sync_include_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%-m")/import_day=$(date -d "$date_to_import" "+%-d")/*" )
     # Include for sync from source to target: *import_year=2022/import_month=07/import_day=27/*
     sync_include_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%m")/import_day=$(date -d "$date_to_import" "+%d")/*" )
-
-    # Exclude from deletion on target: *date=20220727/*
-    rm_exclude_opts+=( --exclude="*date=$date_to_import/*" )
-    # Exclude from deletion on target: *date=2022-07-27/*
-    rm_exclude_opts+=( --exclude="*date=$date_to_import_hyphen_separated/*" )
-    # Exclude from deletion on target: *import_year=2022/import_month=7/import_day=27/*
-    rm_exclude_opts+=( --exclude="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%-m")/import_day=$(date -d "$date_to_import" "+%-d")/*" )
-    # Exclude from deletion on target: *import_year=2022/import_month=07/import_day=27/*
-    rm_exclude_opts+=( --exclude="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%m")/import_day=$(date -d "$date_to_import" "+%d")/*" )
 done
 
+date_to_delete=$(date +"%Y%m%d" -d "${date} -${days_to_retain} day")
+date_to_delete_hyphen_separated=$(date +"%Y-%m-%d" -d "${date} -${days_to_retain} day")
+# Exclude from deletion on target: *date=20220727/*
+rm_include_opts+=( --include="*date=$date_to_delete/*" )
+# Exclude from deletion on target: *date=2022-07-27/*
+rm_include_opts+=( --include="*date=$date_to_delete_hyphen_separated/*" )
+# Exclude from deletion on target: *import_year=2022/import_month=7/import_day=27/*
+rm_include_opts+=( --include="*import_year=$(date -d "$date_to_delete" "+%Y")/import_month=$(date -d "$date_to_delete" "+%-m")/import_day=$(date -d "$date_to_delete" "+%-d")/*" )
+# Exclude from deletion on target: *import_year=2022/import_month=07/import_day=27/*
+rm_include_opts+=( --include="*import_year=$(date -d "$date_to_delete" "+%Y")/import_month=$(date -d "$date_to_delete" "+%m")/import_day=$(date -d "$date_to_delete" "+%d")/*" )
+
 echo "Include flags to be used with s3 sync cmd: ${sync_include_opts[*]}"
-echo "Exclude flags to be used with s3 rm cmd: ${rm_exclude_opts[*]}"
+echo "Include flags to be used with s3 rm cmd: ${rm_include_opts[*]}"
 
 echo "Syncing records....."
 aws s3 sync $s3_sync_source $s3_sync_target --acl "bucket-owner-full-control" --exclude "*" "${sync_include_opts[@]}"
 
 echo "Removing old records..."
-aws s3 rm $s3_sync_target --recursive --include "*" "${rm_exclude_opts[@]}"
+aws s3 rm $s3_sync_target --recursive --exclude "*" "${rm_exclude_opts[@]}"
