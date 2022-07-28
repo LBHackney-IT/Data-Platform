41004a3b76f9e059bf2f158c873fd7bfbecc2c2e
Ryan Bratten <81559982+ryanbratten@users.noreply.github.com>
2022-07-28 16:29:44 +0100

    Allow AWS to manage the sync and we will clean up any old data via the ECS task (#839)
    * Allow AWS to manage the sync and we will clean up any old data via the ECS task
    
    * Oops
    
    * Comment update
    
    * Fix arn and source bucket id
    
    * Update permissions
    
    * Update permissions
    
    * Change deletion to exlude instead of include a specific date and run the ECS task once a week

--- docker/production-to-pre-production/sync-last-x-days.sh
+++ docker/production-to-pre-production/sync-last-x-days.sh
@@ -10,36 +10,30 @@ days_to_retain=${NUMBER_OF_DAYS_TO_RETAIN}
 echo "S3 bucket source: $s3_sync_source"
 echo "S3 bucket target: $s3_sync_target"
 
-for i in $(seq 0 $((days_to_retain-1))); do
-    date_to_import=$(date +"%Y%m%d" -d "${date} -${i} day")
-    date_to_import_hyphen_separated=$(date +"%Y-%m-%d" -d "${date} -${i} day")
-    unset -v sync_include_opts
-    # Include for sync from source to target: *date=20220727/*
-    sync_include_opts+=( --include="*date=$date_to_import/*" )
-    # Include for sync from source to target: *date=2022-07-27/*
-    sync_include_opts+=( --include="*date=$date_to_import_hyphen_separated/*" )
-    # Include for sync from source to target: *import_year=2022/import_month=7/import_day=27/*
-    sync_include_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%-m")/import_day=$(date -d "$date_to_import" "+%-d")/*" )
-    # Include for sync from source to target: *import_year=2022/import_month=07/import_day=27/*
-    sync_include_opts+=( --include="*import_year=$(date -d "$date_to_import" "+%Y")/import_month=$(date -d "$date_to_import" "+%m")/import_day=$(date -d "$date_to_import" "+%d")/*" )
+#*****
+#*****
+#This script no longer copies the data from production to pre-production
+#The data copy is now handled by S3 replication configuration. Please see: terraform/core/35-sync-production-to-pre-production.tf
+#This script now only cleans up the data in pre-production that is older than the period defined by the NUMBER_OF_DAYS_TO_RETAIN variable
+#*****
+#*****
 
-    echo "Include flags to be used with s3 sync cmd: ${sync_include_opts[*]}"
-    echo "Syncing records from $date_to_import"
-    aws s3 sync $s3_sync_source $s3_sync_target --acl "bucket-owner-full-control" --exclude "*" "${sync_include_opts[@]}"
+for i in $(seq 0 $((days_to_retain-1))); do
+  date_to_retain=$(date +"%Y%m%d" -d "${date} -${i} day")
+  date_to_retain_hyphen_separated=$(date +"%Y-%m-%d" -d "${date} -${i} day")
+
+  # Exclude from deletion on target: *date=20220727/*
+  rm_exclude_opts+=( --exclude="*date=$date_to_retain/*" )
+  # Exclude from deletion on target: *date=2022-07-27/*
+  rm_exclude_opts+=( --exclude="*date=$date_to_retain_hyphen_separated/*" )
+  # Exclude from deletion on target: *import_year=2022/import_month=7/import_day=27/*
+  rm_exclude_opts+=( --exclude="*import_year=$(date -d "$date_to_retain" "+%Y")/import_month=$(date -d "$date_to_retain" "+%-m")/import_day=$(date -d "$date_to_retain" "+%-d")/*" )
+  # Exclude from deletion on target: *import_year=2022/import_month=07/import_day=27/*
+  rm_exclude_opts+=( --exclude="*import_year=$(date -d "$date_to_retain" "+%Y")/import_month=$(date -d "$date_to_retain" "+%m")/import_day=$(date -d "$date_to_retain" "+%d")/*" )
 done
 
-date_to_delete=$(date +"%Y%m%d" -d "${date} -${days_to_retain} day")
-date_to_delete_hyphen_separated=$(date +"%Y-%m-%d" -d "${date} -${days_to_retain} day")
-# Exclude from deletion on target: *date=20220727/*
-rm_include_opts+=( --include="*date=$date_to_delete/*" )
-# Exclude from deletion on target: *date=2022-07-27/*
-rm_include_opts+=( --include="*date=$date_to_delete_hyphen_separated/*" )
-# Exclude from deletion on target: *import_year=2022/import_month=7/import_day=27/*
-rm_include_opts+=( --include="*import_year=$(date -d "$date_to_delete" "+%Y")/import_month=$(date -d "$date_to_delete" "+%-m")/import_day=$(date -d "$date_to_delete" "+%-d")/*" )
-# Exclude from deletion on target: *import_year=2022/import_month=07/import_day=27/*
-rm_include_opts+=( --include="*import_year=$(date -d "$date_to_delete" "+%Y")/import_month=$(date -d "$date_to_delete" "+%m")/import_day=$(date -d "$date_to_delete" "+%d")/*" )
 
-echo "Include flags to be used with s3 rm cmd: ${rm_include_opts[*]}"
+echo "Exclude flags to be used with s3 rm cmd: ${rm_exclude_opts[*]}"
 
 echo "Removing old records..."
-aws s3 rm $s3_sync_target --recursive --exclude "*" "${rm_include_opts[@]}"
+aws s3 rm $s3_sync_target --recursive --include "*" "${rm_exclude_opts[@]}"
