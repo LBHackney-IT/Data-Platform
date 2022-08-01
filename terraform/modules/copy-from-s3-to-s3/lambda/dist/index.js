6c9ab09c22ae9a31cecc2af926d706b770c26bf4
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-08-01 14:35:00 +0100

    Format file

--- terraform/modules/copy-from-s3-to-s3/lambda/dist/index.js
+++ terraform/modules/copy-from-s3-to-s3/lambda/dist/index.js
@@ -1,15 +1,14 @@
 "use strict";
-let __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : {"default": mod};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", {value: true});
+Object.defineProperty(exports, "__esModule", { value: true });
 const aws_sdk_1 = __importDefault(require("aws-sdk"));
 const AWS_REGION = 'eu-west-1';
 const originBucketId = process.env.ORIGIN_BUCKET_ID || '';
 const originPath = process.env.ORIGIN_PATH || '';
 const targetBucketId = process.env.TARGET_BUCKET_ID || '';
 const targetPath = process.env.TARGET_PATH || '';
-
 async function listObjects(s3Client, bucket, prefix) {
     const params = {
         Bucket: bucket,
@@ -42,7 +41,6 @@ async function listObjects(s3Client, bucket, prefix) {
     }
     return objectList;
 }
-
 function mapKeys(objectList, originPath, targetPath) {
     let keyMap = {};
     for (const [, v] of Object.entries(objectList)) {
@@ -51,14 +49,12 @@ function mapKeys(objectList, originPath, targetPath) {
     }
     return keyMap;
 }
-
 async function copyObjects(s3Client, originBucket, objectList, targetBucket, keyMap) {
     for (const [, v] of Object.entries(objectList)) {
         let targetKey = keyMap[v.Key];
         await copyObject(s3Client, originBucket, v.Key, targetBucket, targetKey);
     }
 }
-
 async function copyObject(s3Client, originBucket, originKey, targetBucket, targetKey) {
     let params = {
         ServerSideEncryption: 'AES256',
@@ -76,12 +72,11 @@ async function copyObject(s3Client, originBucket, originKey, targetBucket, targe
     }));
     return s3Client.copyObject(params).promise();
 }
-
 exports.handler = async () => {
-    let s3Client = new aws_sdk_1.default.S3({region: AWS_REGION});
+    let s3Client = new aws_sdk_1.default.S3({ region: AWS_REGION });
     if (process.env.ASSUME_ROLE_ARN && process.env.ASSUME_ROLE_ARN.includes('role')) {
         console.log(`Assuming Role: ${process.env.ASSUME_ROLE_ARN}`);
-        let stsClient = new aws_sdk_1.default.STS({region: AWS_REGION});
+        let stsClient = new aws_sdk_1.default.STS({ region: AWS_REGION });
         const assumeRoleRequest = {
             RoleArn: process.env.ASSUME_ROLE_ARN,
             RoleSessionName: 'FileCopier',
@@ -95,8 +90,8 @@ exports.handler = async () => {
             secretAccessKey: result.Credentials.SecretAccessKey,
             sessionToken: result.Credentials.SessionToken
         };
-        s3Client = new aws_sdk_1.default.S3({region: AWS_REGION, credentials: credentials});
-        stsClient = new aws_sdk_1.default.STS({region: AWS_REGION, credentials: credentials});
+        s3Client = new aws_sdk_1.default.S3({ region: AWS_REGION, credentials: credentials });
+        stsClient = new aws_sdk_1.default.STS({ region: AWS_REGION, credentials: credentials });
         const getCallerIdentityResponse = await stsClient.getCallerIdentity().promise();
         console.log(JSON.stringify(getCallerIdentityResponse));
         console.log('Assume Role Complete');
