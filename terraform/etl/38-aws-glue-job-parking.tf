2a8ea679346976b639dcbf3bda0e27ad797125de
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-08-02 10:05:46 +0100

    Job failing to run in production, match to previously working pre-production job

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -97,6 +97,7 @@ module "parking_gds_permit_change_comparison" {
   workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
   number_of_workers_for_glue_job = 10
   glue_job_worker_type           = "G.1X"
+  glue_version                   = "3.0"
   job_parameters = {
     "--job-bookmark-option" = "job-bookmark-disable"
     "--environment"         = var.environment
