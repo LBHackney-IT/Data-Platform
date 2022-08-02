130d12a3a7d7f13203d87150e6e67d475f3ad958
Ben Reynolds-Carr <ben.reynolds-carr@hackney.gov.uk>
2022-08-02 10:54:32 +0100

    added ingress ports for kafka security group

--- terraform/modules/kafka-event-streaming/20-security-groups.tf
+++ terraform/modules/kafka-event-streaming/20-security-groups.tf
@@ -4,11 +4,19 @@ resource "aws_security_group" "kafka" {
   vpc_id      = var.vpc_id
   description = "Specifies rules for traffic to the kafka cluster"
 
-  # TODO: Allow only traffic from port 80 & 443
   ingress {
     description      = "Allows all inbound traffic"
-    from_port        = 0
-    to_port          = 0
+    from_port        = 80
+    to_port          = 80
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Allows all inbound traffic"
+    from_port        = 443
+    to_port          = 443
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
