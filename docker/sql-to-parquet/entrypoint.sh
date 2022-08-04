e87284ff1743cd286e0ba754a8cfb9e7aebe98cc
ryanbratten <ryan.bratten@hackney.gov.uk>
2022-08-04 15:46:46 +0100

    More stable solution with only one variable to pass in

--- docker/sql-to-parquet/entrypoint.sh
+++ docker/sql-to-parquet/entrypoint.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 set -eu -o pipefail
 
-DATE=${IMPORT_DATE:-$(date +"%y%m%d")}
+# If the import date is not passed in then used today's date - normal process
+if [ -z "${IMPORT_DATE_OVERRIDE+x}" ]; then
+  DATE=$(date +"%y%m%d")
+  SNAPSHOT_DATE=$(date +"%y-%m-%d-%H%M%S")
+# Else if the import date is passed in then use that - back dated ingestion
+else
+  DATE="${IMPORT_DATE_OVERRIDE/-/}"
+  SNAPSHOT_DATE="${IMPORT_DATE_OVERRIDE}-000000"
+fi
 
 echo "Date used for import - $DATE"
-
+SNAPSHOT_ID="sql-to-parquet-${SNAPSHOT_DATE}"
+echo "Snapshot Id - $SNAPSHOT_ID"
 FILENAME="liberator_dump_${DATE}"
 DBNAME="liberator"
 
-DATE=${SNAPSHOT_DATE:-$(date +"%y-%m-%d-%H%M%S")}
-SNAPSHOT_ID="sql-to-parquet-${DATE}"
-echo "Snapshot Id - $SNAPSHOT_ID"
-MYSQL_CONN_PARAMS="--user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST}"
-
-python3 delete_db_snapshots_in_db.py
-
-mkdir flatfile
-cd flatfile
-
-SQL_OBJECT_KEY="parking/${FILENAME}.zip"
-aws s3 cp s3://${BUCKET_NAME}/${SQL_OBJECT_KEY} .
-
-# This sleep was added because of an apparent race condition between
-# the download and unzipping, where unzip would give an error reading
-# from disk
-sleep 5
-
-unzip ${FILENAME}.zip
-
-echo "DROP DATABASE IF EXISTS ${DBNAME}" | mysql ${MYSQL_CONN_PARAMS}
-echo "CREATE DATABASE IF NOT EXISTS ${DBNAME}" | mysql ${MYSQL_CONN_PARAMS}
-
-mysql ${MYSQL_CONN_PARAMS} --database=${DBNAME} < *.sql
-aws rds create-db-snapshot --db-instance-identifier ${RDS_INSTANCE_ID} --db-snapshot-identifier ${SNAPSHOT_ID}
+#MYSQL_CONN_PARAMS="--user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST}"
+#
+#python3 delete_db_snapshots_in_db.py
+#
+#mkdir flatfile
+#cd flatfile
+#
+#SQL_OBJECT_KEY="parking/${FILENAME}.zip"
+#aws s3 cp s3://${BUCKET_NAME}/${SQL_OBJECT_KEY} .
+#
+## This sleep was added because of an apparent race condition between
+## the download and unzipping, where unzip would give an error reading
+## from disk
+#sleep 5
+#
+#unzip ${FILENAME}.zip
+#
+#echo "DROP DATABASE IF EXISTS ${DBNAME}" | mysql ${MYSQL_CONN_PARAMS}
+#echo "CREATE DATABASE IF NOT EXISTS ${DBNAME}" | mysql ${MYSQL_CONN_PARAMS}
+#
+#mysql ${MYSQL_CONN_PARAMS} --database=${DBNAME} < *.sql
+#aws rds create-db-snapshot --db-instance-identifier ${RDS_INSTANCE_ID} --db-snapshot-identifier ${SNAPSHOT_ID}
