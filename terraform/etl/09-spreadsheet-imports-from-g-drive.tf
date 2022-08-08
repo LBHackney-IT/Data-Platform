999d41d82d68738b525f82e1b79e7a8ac549c7c7
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2022-08-08 13:15:11 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf (#854)

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -515,3 +515,38 @@ module "parking_permits_consultation_survey_20220801" {
     }
   }
 }
+module "Permit_Diesel_Electric_Forecast" {
+  count                          = local.is_live_environment ? 1 : 0
+  source                         = "../modules/import-spreadsheet-file-from-g-drive"
+  is_production_environment      = local.is_production_environment
+  is_live_environment            = local.is_live_environment
+  department                     = module.department_parking_data_source
+  glue_scripts_bucket_id         = module.glue_scripts_data_source.bucket_id
+  glue_catalog_database_name     = module.department_parking_data_source.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_id    = module.glue_temp_storage_data_source.bucket_url
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  secrets_manager_kms_key        = data.aws_kms_key.secrets_manager_key
+  glue_role_arn                  = data.aws_iam_role.glue_role.arn
+  helper_module_key              = data.aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_bucket_object.pydeequ.key
+  jars_key                       = data.aws_s3_bucket_object.jars.key
+  spreadsheet_import_script_key  = aws_s3_bucket_object.spreadsheet_import_script.key
+  identifier_prefix              = local.short_identifier_prefix
+  lambda_artefact_storage_bucket = module.lambda_artefact_storage_data_source.bucket_id
+  landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
+  landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
+  landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
+  google_drive_document_id       = "1b6hcWaUFTV3_n8wewjHPkR3ewA_MJSK-"
+  glue_job_name                  = "Voucher Import"
+  output_folder_name             = "g-drive"
+  raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
+  input_file_name                = "Permit_Diesel_Electric_Forecast/permit_diesel_electric_forecast_july.csv"
+  ingestion_schedule             = "cron(0 21 * * ? *)"
+  enable_bookmarking             = true
+  worksheets = {
+    sheet1 : {
+      header_row_number = 0
+      worksheet_name    = "permit_diesel_electric_forecast"
+    }
+  }
+}
