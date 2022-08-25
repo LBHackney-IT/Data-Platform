e120513872d6b2d291dcb9d11d92720bfa158b69
Ryan Bratten <81559982+ryanbratten@users.noreply.github.com>
2022-08-25 15:24:50 +0100

    DPP-262 - Secure datahub database (#880)
    * DPP-262 - Secure database database
    
    * Automated backups
    
    * Datahub backups
    
    * Update 10-rds.tf

--- terraform/modules/datahub/10-rds.tf
+++ terraform/modules/datahub/10-rds.tf
@@ -1,15 +1,18 @@
 resource "aws_db_instance" "datahub" {
-  allocated_storage      = 15
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t3.micro"
-  username               = "datahub"
-  identifier             = replace("${var.short_identifier_prefix}datahub", "-", "")
-  password               = random_password.datahub_secret.result
-  db_subnet_group_name   = aws_db_subnet_group.datahub.name
-  vpc_security_group_ids = [aws_security_group.datahub.id]
-  skip_final_snapshot    = true
-  //  storage_encrypted      = true
+  allocated_storage       = 15
+  engine                  = "mysql"
+  engine_version          = "5.7"
+  instance_class          = "db.t3.micro"
+  username                = "datahub"
+  identifier              = replace("${var.short_identifier_prefix}datahub", "-", "")
+  password                = random_password.datahub_secret.result
+  db_subnet_group_name    = aws_db_subnet_group.datahub.name
+  vpc_security_group_ids  = [aws_security_group.datahub.id]
+  skip_final_snapshot     = false
+  deletion_protection     = var.is_live_environment
+  backup_retention_period = 14
+  backup_window           = "23:00-23:31"
+  tags                    = var.tags
 }
 
 resource "aws_db_subnet_group" "datahub" {
