dffc851f6d04f721e98c868d9e70af6fae506cc0
Sandrine Balley <42965897+LBHSBALLEY@users.noreply.github.com>
2022-09-05 11:17:49 +0200

    Update 24-aws-glue-tascomi-data.tf
    Terraform back to pre-execution_context state

--- terraform/etl/24-aws-glue-tascomi-data.tf
+++ terraform/etl/24-aws-glue-tascomi-data.tf
@@ -272,12 +272,12 @@ module "tascomi_applications_to_trusted" {
   spark_ui_output_storage_id = module.spark_ui_output_storage_data_source.bucket_id
   job_parameters = {
     "--job-bookmark-option"                    = "job-bookmark-enable"
-    "--target_destination"                     = "s3://${module.trusted_zone_data_source.bucket_id}/planning/tascomi/applications"
+    "--s3_bucket_target"                       = "s3://${module.trusted_zone_data_source.bucket_id}/planning/tascomi/applications"
     "--enable-glue-datacatalog"                = "true"
     "--source_catalog_database"                = aws_glue_catalog_database.refined_zone_tascomi.name
-    "--source_catalog_table_applications"      = "applications"
-    "--source_catalog_table_application_types" = "application_types"
-    "--source_catalog_table_ps_codes"          = "ps_development_codes"
+    "--source_catalog_table"                   = "applications"
+    "--source_catalog_table2"                  = "application_types"
+    "--source_catalog_table3"                  = "ps_development_codes"
   }
   script_name          = "tascomi_applications_trusted"
   triggered_by_crawler = module.tascomi_create_daily_snapshot.crawler_name
