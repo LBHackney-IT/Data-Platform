eb5de6a93fffbf028235ac8a4a45ae184c5a3924
LBHTKarki <tuomo.karki@hackney.gov.uk>
2022-10-04 16:42:53 +0100

    use existing script to avoid duplication

--- terraform/core/40-ringgo-sftp-to-s3-ingestion.tf
+++ terraform/core/40-ringgo-sftp-to-s3-ingestion.tf
@@ -65,7 +65,7 @@ module "ringgo_sftp_data_to_raw" {
   glue_scripts_bucket_id     = module.glue_scripts.bucket_id
   glue_temp_bucket_id        = module.glue_temp_storage.bucket_id
   glue_role_arn              = aws_iam_role.glue_role.arn
-  script_s3_object_key = aws_s3_bucket_object.copy_ringgo_sftp_csv_data_to_raw.key
+  script_s3_object_key       = aws_s3_bucket_object.copy_manually_uploaded_csv_data_to_raw.key
 
   job_parameters = {
     "--job-bookmark-option" = "job-bookmark-enable"
