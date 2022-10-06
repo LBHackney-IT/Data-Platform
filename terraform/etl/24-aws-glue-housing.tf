ffca250097bef383e2a2126967899ccc0878567e
daroclark <daro.clark@hackney.gov.uk>
2022-10-06 16:52:44 +0100

    Update 24-aws-glue-housing.tf

--- terraform/etl/24-aws-glue-housing.tf
+++ terraform/etl/24-aws-glue-housing.tf
@@ -64,10 +64,10 @@ module "rentsense_output_test" {
   is_live_environment       = local.is_live_environment
   is_production_environment = local.is_production_environment
 
-  glue_scripts_bucket_id     = module.glue_scripts.bucket_id
-  glue_temp_bucket_id        = module.glue_temp_storage.bucket_id
-  glue_role_arn              = aws_iam_role.glue_role.arn
-  job_name                   = "${local.short_identifier_prefix}Rentsense outputs"
+  glue_scripts_bucket_id     = module.glue_scripts_data_source.bucket_id
+  glue_temp_bucket_id        = module.glue_temp_storage_data_source.bucket_id
+  glue_role_arn              = data.aws_iam_role.glue_role.arn
+  job_name                   = "${local.short_identifier_prefix}Rentsense outputs test"
   glue_job_worker_type       = "G.1X"
   number_of_workers_for_glue_job  = 4
   helper_module_key          = data.aws_s3_bucket_object.helpers.key
@@ -77,6 +77,7 @@ module "rentsense_output_test" {
     "--job-bookmark-option"     = "job-bookmark-enable"
     "--s3_bucket"               = module.refined_zone_data_source.bucket_id
     "--s3_bucket_target"        = "s3://${module.refined_zone_data_source.bucket_id}/housing/rentsense"  
+    "--s3_landing_bucket_target"  = "s3://$module.landing_zone_data_source.bucket_id}/housing_export"  
     "--s3_landing"        = "s3://${module.landing_zone_data_source.bucket_id}" 
     "--enable-glue-datacatalog" = "true"
     "--source_raw_database" = module.department_housing_data_source.raw_zone_catalog_database_name
