32f918a8a3a31526ca9ac1f93fdb0a714596bdb9
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2022-10-10 13:08:03 +0100

    add new job (#949)

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -1005,3 +1005,28 @@ module "parking_shop_front_licence_totals" {
     "--environment"         = var.environment
   }
 }    
+
+module "parking_foi_pcn_gds_daily_summary_records" {
+  source                         = "../modules/aws-glue-job"
+  is_live_environment            = local.is_live_environment
+  is_production_environment      = local.is_production_environment
+  department                     = module.department_parking_data_source
+  job_name                       = "${local.short_identifier_prefix}parking_foi_pcn_gds_daily_summary_records"
+  helper_module_key              = data.aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_bucket_object.pydeequ.key
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  script_name                    = "parking_foi_pcn_gds_daily_summary_records"
+  glue_version                   = "2.0"
+  triggered_by_job               = module.parking_pcn_denormalisation.job_name
+  job_description                = "Records of daily summarising data from the FOI Google Data Studio dashboard as need to be under 100,000"
+  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
+  trigger_enabled                = local.is_production_environment
+  glue_job_timeout               = 240
+  number_of_workers_for_glue_job = 10
+  glue_job_worker_type           = "G.1X"
+  job_parameters = {
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--environment"         = var.environment
+  }
+}
+
