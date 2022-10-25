812d77d1895f6c038ea2db938657875b993dcac9
Sandrine Balley <42965897+LBHSBALLEY@users.noreply.github.com>
2022-10-25 17:30:43 +0200

    Create 41-env-services-geolive-database-ingestion.tf

---
+++ terraform/core/41-env-services-geolive-database-ingestion.tf
@@ -0,0 +1,14 @@
+module "recycling_geolive_database_ingestion" {
+  count = local.is_live_environment ? 1 : 0
+  tags  = module.tags.values
+
+  source = "../modules/database-ingestion-via-jdbc-connection"
+
+  name                        = "geolive-recycling-schema"
+  jdbc_connection_url         = "jdbc:postgresql://geolive-db-prod.cjgyygrtgrhl.eu-west-2.rds.amazonaws.com:5432/geolive"
+  jdbc_connection_description = "JDBC connection to Geolive PostgreSQL database, to access boundaries layers from the recycling schema"
+  jdbc_connection_subnet      = data.aws_subnet.network[local.instance_subnet_id]
+  identifier_prefix           = local.short_identifier_prefix
+  database_secret_name        = "database-credentials/geolive-boundaries"
+  schema_name                 = "recycling"
+}
