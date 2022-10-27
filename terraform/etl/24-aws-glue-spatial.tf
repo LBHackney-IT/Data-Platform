6e2496bd1249dcc26dcc791eb7b17364fc60abed
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2022-10-27 12:17:29 +0200

    spatial enrichment script

--- terraform/etl/24-aws-glue-spatial.tf
+++ terraform/etl/24-aws-glue-spatial.tf
@@ -26,3 +26,27 @@ module "llpg_raw_to_trusted" {
   }
 
 }
+
+module "env_services_geospatial_enrichment" {
+  source                    = "../modules/aws-glue-job"
+  is_live_environment       = local.is_live_environment
+  is_production_environment = local.is_production_environment
+
+  department                 = module.department_environmental_services_data_source
+  job_name                   = "${local.short_identifier_prefix}env_services_geospatial_enrichment"
+  glue_job_worker_type       = "G.1X"
+  helper_module_key          = data.aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key            = data.aws_s3_bucket_object.pydeequ.key
+  spark_ui_output_storage_id = module.spark_ui_output_storage_data_source.bucket_id
+  job_parameters = {
+    "--job-bookmark-option"     = "job-bookmark-enable"
+    "--additional-python-modules"        = "rtree,geopandas"
+    "--tables_to_enrich_dict" = "{'gully_cleanse': {'database_name':'env-services-raw-zone','table_name':'alloy_api_response_gully_cleanse','geom_column':'root_attributes_tasksassignabletasks_designs_gullies_attributes_itemsgeometry','geom_format': 'wkt','source_crs': 'epsg:4326','enrich_with':['ward', 'lsoa'],'target_location': 's3://${module.refined_zone_data_source.bucket_id}/env-services/spatially_enriched/'},'fly_tip_job': {'database_name':'env-services-raw-zone','table_name':'alloy_api_response_flytipjobs','geom_column':'attributes_itemsgeometry','geom_format': 'wkt','source_crs': 'epsg:4326','enrich_with':['ward', 'lsoa'],'target_location': 's3://${module.refined_zone_data_source.bucket_id}/env-services/spatially_enriched/'}}"
+  }
+  script_name          = "spatial_enrichment"
+
+  crawler_details = {
+    database_name      = module.department_environmental_services_data_source.refined_zone_catalog_database_name
+    s3_target_location = "s3://${module.refined_zone_data_source.bucket_id}/env-services/spatially_enriched"
+  }
+}
