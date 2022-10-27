091f63c55e7e7dceb6051c31eba03678409ffd9b
Sandrine Balley <42965897+LBHSBALLEY@users.noreply.github.com>
2022-10-27 18:24:49 +0200

    Update 15-unrestricted-geolive-database-ingestion.tf
    Create ingestion jobs modules after connection modules

--- terraform/core/15-unrestricted-geolive-database-ingestion.tf
+++ terraform/core/15-unrestricted-geolive-database-ingestion.tf
@@ -59,8 +59,40 @@ module "recycling_geolive_database_ingestion" {
   database_secret_name        = "database-credentials/geolive-boundaries"
   schema_name                 = "recycling"
 }
+
+module "recycling_boundaries_geolive_ingestion_job" {
+  count                     = local.is_live_environment ? 1 : 0
+  source                    = "../modules/aws-glue-job"
+  is_live_environment       = local.is_live_environment
+  is_production_environment = local.is_production_environment
+
+  department                 = module.department_unrestricted
+  job_name                   = "${local.short_identifier_prefix}geolive boundaries in recycling schema ingestion"
+  script_s3_object_key       = aws_s3_bucket_object.ingest_database_tables_via_jdbc_connection.key
+  spark_ui_output_storage_id = module.spark_ui_output_storage.bucket_id
+  helper_module_key          = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key            = aws_s3_bucket_object.pydeequ.key
+  jdbc_connections           = [module.recycling_geolive_database_ingestion[0].jdbc_connection_name]
+  triggered_by_crawler       = module.recycling_geolive_database_ingestion[0].crawler_name
+  workflow_name              = module.recycling_geolive_database_ingestion[0].workflow_name
+  job_parameters = {
+    "--s3_ingestion_bucket_target"  = "s3://${module.raw_zone.bucket_id}/unrestricted/geolive/boundaries/"
+    "--s3_ingestion_details_target" = "s3://${module.raw_zone.bucket_id}/unrestricted/geolive/boundaries/ingestion-details/"
+    "--source_data_database"        = module.recycling_geolive_database_ingestion[0].ingestion_database_name
+  }
+  crawler_details = {
+    database_name      = module.department_unrestricted.raw_zone_catalog_database_name
+    s3_target_location = "s3://${module.raw_zone.bucket_id}/unrestricted/geolive/boundaries/"
+    configuration = jsonencode({
+      Version = 1.0
+      Grouping = {
+        TableLevelConfiguration = 5
+      }
+    })
+  }
+}  
   
-  module "health_geolive_database_ingestion" {
+module "health_geolive_database_ingestion" {
   count = local.is_live_environment ? 1 : 0
   tags  = module.tags.values
 
@@ -74,3 +106,35 @@ module "recycling_geolive_database_ingestion" {
   database_secret_name        = "database-credentials/geolive-boundaries"
   schema_name                 = "health"
 }
+  
+module "health_boundaries_geolive_ingestion_job" {
+  count                     = local.is_live_environment ? 1 : 0
+  source                    = "../modules/aws-glue-job"
+  is_live_environment       = local.is_live_environment
+  is_production_environment = local.is_production_environment
+
+  department                 = module.department_unrestricted
+  job_name                   = "${local.short_identifier_prefix}geolive boundaries in health schema ingestion"
+  script_s3_object_key       = aws_s3_bucket_object.ingest_database_tables_via_jdbc_connection.key
+  spark_ui_output_storage_id = module.spark_ui_output_storage.bucket_id
+  helper_module_key          = aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key            = aws_s3_bucket_object.pydeequ.key
+  jdbc_connections           = [module.health_geolive_database_ingestion[0].jdbc_connection_name]
+  triggered_by_crawler       = module.health_geolive_database_ingestion[0].crawler_name
+  workflow_name              = module.health_geolive_database_ingestion[0].workflow_name
+  job_parameters = {
+    "--s3_ingestion_bucket_target"  = "s3://${module.raw_zone.bucket_id}/unrestricted/geolive/boundaries/"
+    "--s3_ingestion_details_target" = "s3://${module.raw_zone.bucket_id}/unrestricted/geolive/boundaries/ingestion-details/"
+    "--source_data_database"        = module.health_geolive_database_ingestion[0].ingestion_database_name
+  }
+  crawler_details = {
+    database_name      = module.department_unrestricted.raw_zone_catalog_database_name
+    s3_target_location = "s3://${module.raw_zone.bucket_id}/unrestricted/geolive/boundaries/"
+    configuration = jsonencode({
+      Version = 1.0
+      Grouping = {
+        TableLevelConfiguration = 5
+      }
+    })
+  }
+}  
