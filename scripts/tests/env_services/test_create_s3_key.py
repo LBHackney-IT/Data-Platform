d9a0b983ab17e42c9639c00260d3d875f8ec687e
timburke-hackit <tim.burke@hackney.gov.uk>
2022-11-08 10:18:07 +0000

    define test_prefix and test_file variables

--- scripts/tests/env_services/test_create_s3_key.py
+++ scripts/tests/env_services/test_create_s3_key.py
@@ -4,25 +4,28 @@ from freezegun import freeze_time
 
 from scripts.jobs.env_services.alloy_api_export import create_s3_key
 
+test_prefix = "my/s3/prefix/"
+test_file = "I_Hate_horses.csv"
+
 
 @freeze_time("2022-11-07")
 class TestCreateS3Key:
     def test_no_prefix_no_import_date(self):
-        output = create_s3_key(s3_prefix="my/s3/prefix/", file="I_Hate_horses.csv")
+        output = create_s3_key(s3_prefix=test_prefix, file=test_file)
         assert output == "my/s3/prefix/i_hate_horses/"
 
     def test_no_import_date(self):
         output = create_s3_key(
-            s3_prefix="my/s3/prefix/",
-            file="I_Hate_horses.csv",
+            s3_prefix=test_prefix,
+            file=test_file,
             prefix_to_remove="I_Hate_",
         )
         assert output == "my/s3/prefix/horses/"
 
     def test_no_prefix(self):
         output = create_s3_key(
-            s3_prefix="my/s3/prefix/",
-            file="I_Hate_horses.csv",
+            s3_prefix=test_prefix,
+            file=test_file,
             import_date=date.today(),
         )
         assert (
@@ -32,8 +35,8 @@ class TestCreateS3Key:
 
     def test_import_date_and_prefix(self):
         output = create_s3_key(
-            s3_prefix="my/s3/prefix/",
-            file="I_Hate_horses.csv",
+            s3_prefix=test_prefix,
+            file=test_file,
             prefix_to_remove="I_Hate_",
             import_date=date.today(),
         )
@@ -44,8 +47,8 @@ class TestCreateS3Key:
 
     def test_prefix_arg_not_in_filename(self):
         output = create_s3_key(
-            s3_prefix="my/s3/prefix/",
-            file="I_Hate_horses.csv",
+            s3_prefix=test_prefix,
+            file=test_file,
             prefix_to_remove="I_Love_",
             import_date=date.today(),
         )
@@ -56,8 +59,8 @@ class TestCreateS3Key:
 
     def test_prefix_case_insensitive(self):
         output = create_s3_key(
-            s3_prefix="my/s3/prefix/",
-            file="I_Hate_horses.csv",
+            s3_prefix=test_prefix,
+            file=test_file,
             prefix_to_remove="i_HaTe_",
             import_date=date.today(),
         )
@@ -68,8 +71,8 @@ class TestCreateS3Key:
 
     def test_include_file_name_in_key(self):
         output = create_s3_key(
-            s3_prefix="my/s3/prefix/",
-            file="I_Hate_horses.csv",
+            s3_prefix=test_prefix,
+            file=test_file,
             prefix_to_remove="i_HaTe_",
             import_date=date.today(),
             include_file_name=True,
