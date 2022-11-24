d934813a8d266793615c5410b819322dd958b456
Davina Clarke <87067339+daclarkelbh@users.noreply.github.com>
2022-11-24 12:49:45 +0000

    Update 09-spreadsheet-imports-from-g-drive.tf
    Remove module which loads into table which has an incorrect name @ line 483

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -480,41 +480,6 @@ module "Voucher_Import" {
     }
   }
 }
-module "parking_permits_consultation_survey_20220801" {
-  count                          = local.is_live_environment ? 1 : 0
-  source                         = "../modules/import-spreadsheet-file-from-g-drive"
-  is_production_environment      = local.is_production_environment
-  is_live_environment            = local.is_live_environment
-  department                     = module.department_parking_data_source
-  glue_scripts_bucket_id         = module.glue_scripts_data_source.bucket_id
-  glue_catalog_database_name     = module.department_parking_data_source.raw_zone_catalog_database_name
-  glue_temp_storage_bucket_id    = module.glue_temp_storage_data_source.bucket_url
-  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
-  secrets_manager_kms_key        = data.aws_kms_key.secrets_manager_key
-  glue_role_arn                  = data.aws_iam_role.glue_role.arn
-  helper_module_key              = data.aws_s3_bucket_object.helpers.key
-  pydeequ_zip_key                = data.aws_s3_bucket_object.pydeequ.key
-  jars_key                       = data.aws_s3_bucket_object.jars.key
-  spreadsheet_import_script_key  = aws_s3_bucket_object.spreadsheet_import_script.key
-  identifier_prefix              = local.short_identifier_prefix
-  lambda_artefact_storage_bucket = module.lambda_artefact_storage_data_source.bucket_id
-  landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
-  landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
-  landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1FJUrwqxR_BWJdXIn0u6jYMixXIczCyQF"
-  glue_job_name                  = "Parking Permit Survey Consultation Hub - 20220801"
-  output_folder_name             = "g-drive"
-  raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "Parking Permit Survey Consultation Hub/Permits Consultation Survey - export-2022-08-01-11-04-48 UTF-8.csv"
-  ingestion_schedule             = "cron(0 21 * * ? *)"
-  enable_bookmarking             = false
-  worksheets = {
-    sheet1 : {
-      header_row_number = 0
-      worksheet_name    = "Parking Permit Survey Consultation Hub"
-    }
-  }
-}
 module "Permit_Diesel_Electric_Forecast" {
   count                          = local.is_live_environment ? 1 : 0
   source                         = "../modules/import-spreadsheet-file-from-g-drive"
