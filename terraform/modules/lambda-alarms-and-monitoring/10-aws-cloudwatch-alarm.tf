bac9a14f260e62df24653d611dcd1de39c52b5ec
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2022-12-13 12:53:40 +0000

    use correct log group (#1061)

--- terraform/modules/lambda-alarms-and-monitoring/10-aws-cloudwatch-alarm.tf
+++ terraform/modules/lambda-alarms-and-monitoring/10-aws-cloudwatch-alarm.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_log_metric_filter" "metric_filter" {
   name           = "${var.lambda_name}-lambda-errors"
   pattern        = "ERROR"
-  log_group_name = "/aws/lambda/${var.alarms_handler_lambda_name}"
+  log_group_name = "/aws/lambda/${var.lambda_name}"
 
   metric_transformation {
     name          = "${var.lambda_name}-lambda-errors"
