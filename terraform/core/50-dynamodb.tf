bd15de8ac74e1ff8bd7c26cff408cae9fdcbbf28
timburke-hackit <tim.burke@hackney.gov.uk>
2023-01-04 13:39:41 +0000

    enable point in time recovery

--- terraform/core/50-dynamodb.tf
+++ terraform/core/50-dynamodb.tf
@@ -2,12 +2,13 @@ module "dynamodb_table" {
   source = "../modules/dynamodb"
   count  = local.is_live_environment ? 1 : 0
 
-  name         = "glue-watermarks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "jobName"
-  range_key    = "runId"
-  table_class  = "STANDARD"
-  tags         = module.tags.values
+  name                           = "glue-watermarks"
+  billing_mode                   = "PAY_PER_REQUEST"
+  hash_key                       = "jobName"
+  range_key                      = "runId"
+  table_class                    = "STANDARD"
+  point_in_time_recovery_enabled = true
+  tags                           = module.tags.values
 
   attributes = [
     {
