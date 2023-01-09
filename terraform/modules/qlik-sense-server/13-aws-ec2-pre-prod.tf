ba08ec19c3526e52b5f5b033def905423ec4ed83
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-01-09 16:10:47 +0000

    share the latest qlik backup ami with pre-prod account

---
+++ terraform/modules/qlik-sense-server/13-aws-ec2-pre-prod.tf
@@ -0,0 +1,19 @@
+locals {
+    backup_ami_id = "ami-050960cc7acbe15d9"
+}
+
+data "aws_secretsmanager_secret" "pre_production_account_id" {
+  count         = var.is_production_environment ? 1 : 0
+  name          = "manual/pre-production-account-id"
+}
+
+data "aws_secretsmanager_secret_version" "pre_production_account_id" {
+  count         = var.is_production_environment ? 1 : 0
+  secret_id     = data.aws_secretsmanager_secret.pre_production_account_id.id
+}
+
+resource "aws_ami_launch_permission" "ami_permissions_for_pre_prod" {
+  count         = var.is_production_environment ? 1 : 0
+  image_id      = local.backup_ami_id
+  account_id    = data.aws_secretsmanager_secret_version.pre_production_account_id.secret_string
+}
