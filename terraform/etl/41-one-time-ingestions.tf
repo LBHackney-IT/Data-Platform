9059f45ed1fc92b96b851e27f889558cc8394f10
HuuHieuDo <77067292+HuuHieuDo@users.noreply.github.com>
2023-01-10 16:43:39 +0000

    Making Glue for First ingestion

---
+++ terraform/etl/41-one-time-ingestions.tf
@@ -0,0 +1,23 @@
+# Ingestion
+module "ingest_vonage_data" {
+  source                    = "../modules/aws-glue-job"
+  is_live_environment       = local.is_live_environment
+  is_production_environment = local.is_production_environment
+
+  department                      = module.department_planning_data_source
+  number_of_workers_for_glue_job  = 2
+  max_concurrent_runs_of_glue_job = 1
+  job_name                        = "${local.short_identifier_prefix}vonage_api_ingestion_planning"
+  helper_module_key               = data.aws_s3_bucket_object.helpers.key
+  pydeequ_zip_key                 = data.aws_s3_bucket_object.pydeequ.key
+  job_parameters = {
+    "--s3_bucket"                 = 'dataplatform-stg-landing-zone'
+    "--output_folder_name"        = 'vonage'
+    "--secret_name"               = "vonage-key"
+    "--api_to_call"               = "stats"
+    "--table_to_call"             = "interactions"
+    "--number_of_workers"         = 2
+  }
+  script_name                = "vonage_api_ingestion"
+  spark_ui_output_storage_id = module.spark_ui_output_storage_data_source.bucket_id
+}
