5751f01a567d1d11b4cb2ca3a92c8b8a8e28fcc1
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-01-11 14:54:51 +0000

    only include secret manager resources for now

--- terraform/modules/qlik-sense-server/13-aws-ec2-pre-prod.tf
+++ terraform/modules/qlik-sense-server/13-aws-ec2-pre-prod.tf
@@ -32,36 +32,3 @@ resource "aws_secretsmanager_secret_version" "production_account_qlik_ec2_ebs_en
   secret_id     = aws_secretsmanager_secret.production_account_qlik_ec2_ebs_encryption_key_arn[0].id
   secret_string = "" #value managed manually
 }
-
-data "aws_iam_policy_document" "qlik_sense_shared_prod_key_policy" {
-    count = !var.is_production_environment && var.is_live_environment ? 1 : 0
-    
-    statement {
-      sid     = "AllowQlikEC2RoleAccessToTheSharedProdKey"
-      effect  = "Allow"
-      
-      actions = [
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:DescribeKey"
-      ]
-
-      resources = [aws_secretsmanager_secret_version.production_account_qlik_ec2_ebs_encryption_key_arn[0].secret_string]
-    }
-}
-
-resource "aws_iam_policy" "qlik_sense_preprod_can_access_shared_prod_key" {
-  count     = !var.is_production_environment && var.is_live_environment ? 1 : 0
-  tags      = var.tags
-
-  name      = "${var.identifier_prefix}-qlik-sense-preprod-can-access-shared-prod-key"
-  policy    = data.aws_iam_policy_document.qlik_sense_shared_prod_key_policy[0].json
-}
-
-resource "aws_iam_role_policy_attachment" "qlik_sense_prod_key_policy" {
-  count         = !var.is_production_environment && var.is_live_environment ? 1 : 0
-  role          = aws_iam_role.qlik_sense.id
-  policy_arn    = aws_iam_policy.qlik_sense_preprod_can_access_shared_prod_key[0].arn
-}
