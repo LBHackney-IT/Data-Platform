572e67b0dd7198540fe43d72b6ecd97509186ff4
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-01-13 10:53:24 +0000

    add ec2 instance

--- terraform/modules/qlik-sense-server/13-aws-ec2-pre-prod.tf
+++ terraform/modules/qlik-sense-server/13-aws-ec2-pre-prod.tf
@@ -1,5 +1,9 @@
 locals {
     backup_ami_id = "ami-050960cc7acbe15d9"
+    ec2_tags = {
+        BackupPolicy  = title(var.environment)
+        Name          = "${var.identifier_prefix}-qlik-sense-restore"
+    }
 }
 
 data "aws_secretsmanager_secret" "pre_production_account_id" {
@@ -82,4 +86,46 @@ data "aws_secretsmanager_secret" "qlik_sense_ec2_role_arn_on_pre_prod_account" {
 data "aws_secretsmanager_secret_version" "qlik_sense_ec2_role_arn_on_pre_prod_account" {
   count     = var.is_production_environment ? 1 : 0
   secret_id = data.aws_secretsmanager_secret.qlik_sense_ec2_role_arn_on_pre_prod_account[0].id
+}
+
+#manually added/managed value
+data "aws_secretsmanager_secret" "subnet_value_for_qlik_sense_pre_prod_instance" {
+  count = !var.is_production_environment && var.is_live_environment ? 1 : 0
+  name  = "${var.identifier_prefix}-manually-managed-value-subnet-value-for-qlik-sense-restore-pre-prod-instance"
+}
+
+data "aws_secretsmanager_secret_version" "subnet_value_for_qlik_sense_pre_prod_instance" {
+  count     = !var.is_production_environment && var.is_live_environment ? 1 : 0
+  secret_id = data.aws_secretsmanager_secret.subnet_value_for_qlik_sense_pre_prod_instance[0].id
+}
+
+resource "aws_instance" "qlik_sense_pre_prod_instance" {
+  count                     = !var.is_production_environment && var.is_live_environment ? 1 : 0
+  ami                       = local.backup_ami_id
+  instance_type             = "c5.4xlarge"
+  subnet_id                 = data.aws_secretsmanager_secret_version.subnet_value_for_qlik_sense_pre_prod_instance[0].secret_string
+  vpc_security_group_ids    = [aws_security_group.qlik_sense.id]
+  
+  private_dns_name_options {
+    enable_resource_name_dns_a_record = true
+  }
+
+  iam_instance_profile        = aws_iam_instance_profile.qlik_sense.id
+  disable_api_termination     = true
+  key_name                    = aws_key_pair.qlik_sense_server_key.key_name
+  tags                        = merge(var.tags, local.ec2_tags)
+  associate_public_ip_address = false
+
+  root_block_device {
+    encrypted               = true
+    delete_on_termination   = false
+    kms_key_id              = data.aws_secretsmanager_secret_version.production_account_qlik_ec2_ebs_encryption_key_arn[0].secret_string
+    tags                    = merge(var.tags, local.ec2_tags)
+    volume_size             = 1000
+    volume_type             = "gp3"
+  }
+  
+  lifecycle {
+    ignore_changes = [ami, subnet_id]
+  }
 }
