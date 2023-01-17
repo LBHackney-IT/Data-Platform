c3b5af57831296b20a6ab81429ea254a55cb3313
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-01-17 11:27:28 +0000

    add-ec2-config

--- terraform/modules/qlik-sense-server/13-aws-ec2-pre-prod.tf
+++ terraform/modules/qlik-sense-server/13-aws-ec2-pre-prod.tf
@@ -98,3 +98,32 @@ data "aws_secretsmanager_secret_version" "subnet_value_for_qlik_sense_pre_prod_i
   count     = !var.is_production_environment && var.is_live_environment ? 1 : 0
   secret_id = data.aws_secretsmanager_secret.subnet_value_for_qlik_sense_pre_prod_instance[0].id
 }
+
+resource "aws_instance" "qlik_sense_pre_prod_instance" {
+  count                     = !var.is_production_environment && var.is_live_environment ? 1 : 0
+  ami                       = local.backup_ami_id
+  instance_type             = "c5.4xlarge"
+  subnet_id                 = data.aws_secretsmanager_secret_version.subnet_value_for_qlik_sense_pre_prod_instance[0].secret_string
+  vpc_security_group_ids    = [aws_security_group.qlik_sense.id]
+
+  private_dns_name_options {
+    enable_resource_name_dns_a_record = true
+  }
+
+  iam_instance_profile        = aws_iam_instance_profile.qlik_sense.id
+  disable_api_termination     = true
+  key_name                    = aws_key_pair.qlik_sense_server_key.key_name
+  tags                        = merge(var.tags, local.ec2_tags)
+  associate_public_ip_address = false
+
+  root_block_device {
+    encrypted               = true
+    delete_on_termination   = false
+    kms_key_id              = aws_kms_key.key
+    tags                    = merge(var.tags, local.ec2_tags)
+  }
+
+  lifecycle {
+    ignore_changes = [ami, subnet_id]
+  }
+}
