d57f5d2c42b520e3e62606a7ec64e6ca7a9bc511
timburke-hackit <tim.burke@hackney.gov.uk>
2023-01-18 11:42:56 +0000

    change handler

--- terraform/modules/glue-failure-alert-notifications/10-main.tf
+++ terraform/modules/glue-failure-alert-notifications/10-main.tf
@@ -79,7 +79,7 @@ resource "aws_s3_bucket_object" "lambda" {
 resource "aws_lambda_function" "lambda" {
   function_name     = lower("${var.identifier_prefix}${var.lambda_name}")
   role              = aws_iam_role.lambda.arn
-  handler           = "main.handler"
+  handler           = "main.lambda_handler"
   runtime           = "python3.8"
   source_code_hash  = filebase64sha256(data.archive_file.lambda.output_path)
   s3_bucket         = var.lambda_artefact_storage_bucket
