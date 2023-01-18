f82f4dad0e0de59ab3f75d3cd5994784ffd78300
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-01-18 13:47:35 +0000

    ignore key change during testing

--- terraform/modules/qlik-sense-server/14-aws-ec2-prod-win-test.tf
+++ terraform/modules/qlik-sense-server/14-aws-ec2-prod-win-test.tf
@@ -39,4 +39,10 @@ resource "aws_instance" "test_windows_prod_instance" {
     kms_key_id              = aws_kms_key.key.id
     tags                    = merge(var.tags, local.win_prod_test_ec2_tags)
   }
+
+  lifecycle {
+      ignore_changes = [
+        root_block_device[0].kms_key_id,
+      ]
+  }
 }
