a180b94ae9bf50fb35435628c9f83a77dbd55c49
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2023-02-02 13:36:10 +0000

    DPP-95 Update filter for fetching running EC2 instance (#1147)
    * update filter for tg attachment
    
    * formatting

--- terraform/modules/qlik-sense-server/03-input-derived.tf
+++ terraform/modules/qlik-sense-server/03-input-derived.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_instance" "qlik-sense-aws-instance" {
   filter {
     name   = "tag:Name"
-    values = ["Qlik Migration ${upper(var.environment)}"]
+    values =  var.is_production_environment ? ["Qlik Migration ${upper(var.environment)}"] : ["dataplatform-stg-qlik-sense-restore"]
   }
 }
 
