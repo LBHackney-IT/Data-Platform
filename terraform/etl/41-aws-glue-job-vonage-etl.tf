097ac1ea90f7d4344a8610c709cba218eab620b9
HuuHieuDo <77067292+HuuHieuDo@users.noreply.github.com>
2023-02-06 14:03:16 +0000

    Change workers and worker type

--- terraform/etl/41-aws-glue-job-vonage-etl.tf
+++ terraform/etl/41-aws-glue-job-vonage-etl.tf
@@ -7,12 +7,12 @@ module "ingest_vonage_data" {
   count                     = local.is_live_environment ? 1 : 0
 
   department                      = module.department_customer_services_data_source
-  number_of_workers_for_glue_job  = 8
+  number_of_workers_for_glue_job  = 12
   max_concurrent_runs_of_glue_job = 1
   job_name                        = "${local.short_identifier_prefix}vonage_one_time_ingestion_customer_services"
   helper_module_key               = data.aws_s3_bucket_object.helpers.key
   pydeequ_zip_key                 = data.aws_s3_bucket_object.pydeequ.key
-  glue_job_worker_type           = "G.1X"
+  glue_job_worker_type           = "G.2X"
   glue_version                   = "4.0"
   glue_job_timeout               = 360
   job_parameters = {
