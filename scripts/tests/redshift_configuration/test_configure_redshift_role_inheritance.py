188beb8802d841035b16c8905e06741961441682
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-02-08 14:59:49 +0000

    refactor redshift script test setups

--- scripts/tests/redshift_configuration/test_configure_redshift_role_inheritance.py
+++ scripts/tests/redshift_configuration/test_configure_redshift_role_inheritance.py
@@ -1,36 +1,13 @@
 import json
+from unittest import mock
 import pytest
-from scripts.configure_redshift import main, Redshift, configure_role_inheritance
+from scripts.configure_redshift import Redshift, configure_role_inheritance
 
 class TestConfigureRoleInheritance():
 
-    @pytest.fixture(scope="function", autouse=True)
-    def boto3_mock(self, mocker):
-        return mocker.patch('scripts.configure_redshift.boto3')
-    
-    @pytest.fixture(scope="function", autouse=True)
-    def create_schemas_mock(self, mocker):
-        return mocker.patch('scripts.configure_redshift.create_schemas')
-    
-    @pytest.fixture(scope="function", autouse=True)
-    def configure_users(self, mocker):
-        return mocker.patch('scripts.configure_redshift.configure_users')
-    
-    @pytest.fixture(scope="function", autouse=True)
-    def grant_permissions_to_users(self, mocker):
-        return mocker.patch('scripts.configure_redshift.grant_permissions_to_users')
-
-    @pytest.fixture(scope="function", autouse=True)
-    def grant_permissions_to_users(self, mocker):
-        return mocker.patch('scripts.configure_redshift.get_roles_to_be_added')
-
     @pytest.fixture(scope="function")
-    def configure_role_inheritance_mock(self, mocker):
-        return mocker.patch('scripts.configure_redshift.configure_role_inheritance')
-    
-    @pytest.fixture(scope="function")
-    def redshift_mock(self, mocker):
-        return mocker.Mock(autospec=Redshift)
+    def redshift_mock(self):
+        return mock.create_autospec(Redshift)
 
     @pytest.fixture(scope="session")
     def terraform_output_json(self, terraform_output):
@@ -40,35 +17,19 @@ class TestConfigureRoleInheritance():
     def terraform_output_with_one_role_config_json(self, terraform_output_with_one_role):
         return json.loads(terraform_output_with_one_role)['redshift_roles']['value'] 
     
-    @pytest.fixture(scope="function", autouse=True)
-    def revoke_role_grants(self, mocker):
-        return mocker.patch('scripts.configure_redshift.revoke_role_grants')
-
-    @pytest.mark.main
-    def test_main_calls_configure_role_inheritance_when_role_configuration_is_present(self, redshift_mock, terraform_output, configure_role_inheritance_mock):
-        main(terraform_output, redshift_mock)
-        assert configure_role_inheritance_mock.call_count == 1
-
     @pytest.mark.configure_role_inheritance
-    def test_main_does_not_call_configure_role_inhertance_when_role_configuration_is_not_present(self, redshift_mock, terraform_output_without_roles_config, configure_role_inheritance_mock):
-        main(terraform_output_without_roles_config, redshift_mock)
-        assert configure_role_inheritance_mock.call_count == 0
-    
-    @pytest.mark.configure_role_inheritance
-    def test_configure_role_inheritance_calls_execute_batch_queries_on_redshift_to_grant_a_role_access_to_other_role(self, mocker, redshift_mock, terraform_output_json):
-        spy = mocker.spy(redshift_mock, "execute_batch_queries")
+    def test_configure_role_inheritance_calls_execute_batch_queries_on_redshift_to_grant_a_role_access_to_other_role(self, redshift_mock, terraform_output_json):
         expected_query = ['grant role role_one to role role_two;']
 
         configure_role_inheritance(redshift_mock, terraform_output_json)
 
-        spy.assert_called_once_with(expected_query)
+        redshift_mock.execute_batch_queries.assert_called_once_with(expected_query)
 
     @pytest.mark.configure_role_inheritance
-    def test_configure_role_inheritance_ignores_roles_that_are_missing_inheritance_configuration(self, mocker, redshift_mock, terraform_output_with_one_role_config_json):
-        spy = mocker.spy(redshift_mock, "execute_batch_queries")
+    def test_configure_role_inheritance_ignores_roles_that_are_missing_inheritance_configuration(self, redshift_mock, terraform_output_with_one_role_config_json):
         configure_role_inheritance(redshift_mock, terraform_output_with_one_role_config_json)
 
-        spy.assert_not_called()
+        redshift_mock.execute_batch_queries.assert_not_called()
     
     @pytest.mark.configure_role_inheritance
     def test_configure_role_inheritance_outputs_message_when_role_inheritance_is_added(self, redshift_mock, terraform_output_json, capfd):
