4092708a51538be6f81b616d9be9f933e4547346
daroclark <daro.clark@hackney.gov.uk>
2023-02-09 10:16:36 +0000

    Update 44-aws-glue-job-bens-housing-needs.tf

--- terraform/etl/44-aws-glue-job-bens-housing-needs.tf
+++ terraform/etl/44-aws-glue-job-bens-housing-needs.tf
@@ -22,7 +22,7 @@ module "housing_register_to_refined_and_trusted" {
     "--s3_target_refined"        = "s3://${module.refined_zone_data_source.bucket_id}/bens-housing-needs/housing-register"
     "--s3_target_trusted"        = "s3://${module.trusted_zone_data_source.bucket_id}/bens-housing-needs/housing-register"
     "--enable-glue-datacatalog"  = "true"
-    "--source_catalog_database"  = module.department_bens_housing_needs_data_source.refined_zone_catalog_database_name
+    "--source_catalog_database"  = module.department_housing_data_source.raw_zone_catalog_database_name
     "--source_catalog_database2" = module.department_unrestricted_data_source.refined_zone_catalog_database_name
   }
   script_name          = "housing_register_to_refined_and_trusted"
