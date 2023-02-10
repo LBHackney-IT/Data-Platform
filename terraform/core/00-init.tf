3cca487244c466be78caf8e7ad7de614c5366c53
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2023-02-10 11:13:26 +0000

    stop assuming role when deploying to dev (#1168)

--- terraform/core/00-init.tf
+++ terraform/core/00-init.tf
@@ -1,27 +1,42 @@
 # Core Infrastructure
 provider "aws" {
   region = var.aws_deploy_region
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_deploy_account_id}:role/${var.aws_deploy_iam_role_name}"
-    session_name = "Terraform"
+  
+  dynamic assume_role {
+    for_each = local.is_live_environment ? [1] : []
+
+    content {
+      role_arn     = "arn:aws:iam::${var.aws_deploy_account_id}:role/${var.aws_deploy_iam_role_name}"
+      session_name = "Terraform"
+    }
   }
 }
 
 provider "aws" {
   alias  = "aws_api_account"
   region = var.aws_deploy_region
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_api_account_id}:role/${var.aws_deploy_iam_role_name}"
-    session_name = "Terraform"
+  
+  dynamic assume_role {
+    for_each = local.is_live_environment ? [1] : []
+
+    content {
+      role_arn     = "arn:aws:iam::${var.aws_api_account_id}:role/${var.aws_deploy_iam_role_name}"
+      session_name = "Terraform"
+    }
   }
 }
 
 provider "aws" {
   alias  = "aws_hackit_account"
   region = "eu-west-1"
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.aws_hackit_account_id}:role/${var.aws_deploy_iam_role_name}"
-    session_name = "DataPlatform"
+  
+  dynamic assume_role {
+    for_each = local.is_live_environment ? [1] : []
+    
+    content {
+      role_arn     = "arn:aws:iam::${var.aws_hackit_account_id}:role/${var.aws_deploy_iam_role_name}"
+      session_name = "DataPlatform"
+    }
   }
 }
 
