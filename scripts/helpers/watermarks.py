a70d0e1dc68e37e726df666d7184738ee8c373cf
timburke-hackit <tim.burke@hackney.gov.uk>
2023-02-15 21:16:00 +0000

    wip - initial commit

---
+++ scripts/helpers/watermarks.py
@@ -0,0 +1,101 @@
+import boto3
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
+from botocore.client import ClientError
+
+
+class Watermarks:
+    """
+    Encapsulates an Amazon DynamoDB table that stores watermarks or state checkpoints for AWS Glue jobs.
+    """
+
+    def __init__(self, table_name: str, dynamodb_client=None) -> None:
+        """Initializes the Watermarks object.
+
+        :param table_name: The name of the DynamoDB table.
+        :param dynamodb_client: The DynamoDB client to use. If not provided, a new client will be created.
+        """
+        self.table_name = table_name
+        self.dynamodb_client = dynamodb_client or boto3.client("dynamodb")
+        self.deserializer = TypeDeserializer()
+        self.serializer = TypeSerializer()
+
+    def exists(self, table_name) -> bool:
+        """Checks if the DynamoDB table exists."""
+        try:
+            self.dynamodb_client.describe_table(TableName=table_name)
+            return True
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "ResourceNotFoundException":
+                return False
+            raise e
+
+    def create_watermark_item(
+        self, job_id: str, run_id: str, key: str = "watermarks", **kwargs
+    ) -> dict:
+        """Creates a watermark item for the given job and run id."""
+        item = {"jobName": job_id, "runId": run_id, key: kwargs}
+        return item
+
+    def add_watermark(self, watermark_item: dict) -> None:
+        """Adds a watermark item to the DynamoDB table."""
+        try:
+            watermark_item_serialized = self.serializer.serialize(watermark_item)
+            self.dynamodb_client.put_item(
+                TableName=self.table_name,
+                Item=watermark_item_serialized,
+            )
+        except ClientError as e:
+            logger.error(
+                "Could not add watermark item to DynamoDB table %s. Error code: %s: %s",
+                self.table_name,
+                e.response["Error"]["Code"],
+                e.response["Error"]["Message"],
+            )
+            raise e
+
+    def get_most_recent_run_id(self, job_id: str) -> str:
+        """Gets the most recent run id for the given job id."""
+        try:
+            response = self.dynamodb_client.query(
+                TableName=self.table_name,
+                KeyConditionExpression="jobName = :jobName",
+                ExpressionAttributeValues={":jobName": {"S": job_id}},
+                ScanIndexForward=False,
+                Limit=1,
+            )
+        except ClientError as e:
+            logger.error(
+                "Could not get most recent run id from DynamoDB table %s. Error code:"
+                " %s: %s",
+                self.table_name,
+                e.response["Error"]["Code"],
+                e.response["Error"]["Message"],
+            )
+            raise e
+        return response["Items"][0]["runId"]["S"]
+
+    def get_watermark(self, job_id: str, run_id: str = None) -> dict:
+        """Gets the watermark item for the given job and run id."""
+        if run_id is None:
+            run_id = self.get_most_recent_run_id(job_id)
+        try:
+            response = self.dynamodb_client.get_item(
+                TableName=self.table_name,
+                Key={"jobName": job_id, "runId": run_id},
+            )
+            response = self.deserializer.deserialize(response)
+        except ClientError as e:
+            logger.error(
+                "Could not get watermark item from DynamoDB table %s. Error code:"
+                " %s: %s",
+                self.table_name,
+                e.response["Error"]["Code"],
+                e.response["Error"]["Message"],
+            )
+            raise e
+        return response["Item"]
+
+    def get_watermark_value(self, job_id: str, key: str, run_id: str = None) -> str:
+        """Gets the value of a watermark for the given job and run id."""
+        watermark = self.get_watermark(job_id, run_id)
+        return watermark[key]["S"]
