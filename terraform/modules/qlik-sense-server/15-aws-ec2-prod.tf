a33b204aacc899b549215168cb12fe137958c36f
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-03-14 22:45:04 +0000

    use correct local values

--- terraform/modules/qlik-sense-server/15-aws-ec2-prod.tf
+++ terraform/modules/qlik-sense-server/15-aws-ec2-prod.tf
@@ -19,7 +19,7 @@ data "aws_secretsmanager_secret_version" "subnet_value_for_qlik_sense_prod_insta
 
 resource "aws_instance" "qlik_sense_prod_instance" {
   count                     = var.is_production_environment ? 1 : 0
-  ami                       = local.backup_ami_id
+  ami                       = local.backup_ami_id_prod
   instance_type             = "c5.4xlarge"
   subnet_id                 = data.aws_secretsmanager_secret_version.subnet_value_for_qlik_sense_prod_instance[0].secret_string
   vpc_security_group_ids    = [aws_security_group.qlik_sense.id]
@@ -31,14 +31,14 @@ resource "aws_instance" "qlik_sense_prod_instance" {
   iam_instance_profile        = aws_iam_instance_profile.qlik_sense.id
   disable_api_termination     = true
   key_name                    = aws_key_pair.qlik_sense_server_key.key_name
-  tags                        = merge(var.tags, local.ec2_tags)
+  tags                        = merge(var.tags, local.ec2_tags_prod)
   associate_public_ip_address = false
 
   root_block_device {
     encrypted               = true
     delete_on_termination   = false
     kms_key_id              = aws_kms_key.key.arn
-    tags                    = merge(var.tags, local.ec2_tags)
+    tags                    = merge(var.tags, local.ec2_tags_prod)
     volume_size             = 1000
     volume_type             = "gp3"
   }
