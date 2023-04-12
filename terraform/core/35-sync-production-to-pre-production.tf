547714bc53a0a6ba59f5526c72751066b82b3c4f
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-04-12 11:33:31 +0100

    remove redundant cleanup jobs

--- terraform/core/35-sync-production-to-pre-production.tf
+++ terraform/core/35-sync-production-to-pre-production.tf
@@ -100,53 +100,6 @@ data "aws_iam_policy_document" "task_role" {
   }
 }
 
-module "sync_production_to_pre_production" {
-  source = "../modules/aws-ecs-fargate-task"
-  count  = local.is_production_environment ? 1 : 0
-
-  tags                          = module.tags.values
-  operation_name                = "${local.short_identifier_prefix}sync-production-to-pre-production"
-  ecs_task_role_policy_document = data.aws_iam_policy_document.task_role.json
-  aws_subnet_ids                = data.aws_subnet_ids.network.ids
-  ecs_cluster_arn               = aws_ecs_cluster.workers.arn
-  tasks = [
-    {
-      task_prefix = "raw-zone-"
-      task_cpu    = 2048
-      task_memory = 4096
-      environment_variables = [
-        { name = "NUMBER_OF_DAYS_TO_RETAIN", value = "90" },
-        { name = "S3_SYNC_SOURCE", value = module.raw_zone.bucket_id },
-        { name = "S3_SYNC_TARGET", value = "dataplatform-stg-raw-zone" }
-      ]
-      cloudwatch_rule_schedule_expression = "cron(0 0 1 1 ? 2050)"
-    },
-    {
-      task_prefix = "refined-zone-"
-      task_cpu    = 256
-      task_memory = 512
-      environment_variables = [
-        { name = "NUMBER_OF_DAYS_TO_RETAIN", value = "90" },
-        { name = "S3_SYNC_SOURCE", value = module.refined_zone.bucket_id },
-        { name = "S3_SYNC_TARGET", value = "dataplatform-stg-refined-zone" }
-      ]
-      cloudwatch_rule_schedule_expression = "cron(0 0 1 1 ? 2050)"
-    },
-    {
-      task_prefix = "trusted-zone-"
-      task_cpu    = 256
-      task_memory = 512
-      environment_variables = [
-        { name = "NUMBER_OF_DAYS_TO_RETAIN", value = "90" },
-        { name = "S3_SYNC_SOURCE", value = module.trusted_zone.bucket_id },
-        { name = "S3_SYNC_TARGET", value = "dataplatform-stg-trusted-zone" }
-      ]
-      cloudwatch_rule_schedule_expression = "cron(0 0 1 1 ? 2050)"
-    }
-  ]
-  security_groups = [aws_security_group.pre_production_clean_up[0].id]
-}
-
 resource "aws_s3_bucket_replication_configuration" "raw_zone" {
   count  = local.is_production_environment ? 1 : 0
   role   = var.sync_production_to_pre_production_task_role
