9508656c85fd031e6f334fc6a3079c8112a779d4
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-04-21 16:52:39 +0100

    give d and i read access to all zones

---
+++ terraform/core/86-departments-extended-role.tf
@@ -0,0 +1,33 @@
+data "aws_iam_policy_document" "department_data_and_insight_all_zones_read_access_policy" {
+  statement {
+    sid    = "AllowReadAccessToAllZones"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject*",
+      "s3:ListBucket"
+    ]
+    resources = [
+      module.landing_zone.bucket_arn,
+      "${module.landing_zone.bucket_arn}/*",
+      module.raw_zone.bucket_arn,
+      "${module.raw_zone.bucket_arn}/*",
+      module.refined_zone.bucket_arn,
+      "${module.refined_zone.bucket_arn}/*",
+      module.trusted_zone.bucket_arn,
+      "${module.trusted_zone.bucket_arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "department_data_and_insight_all_zones_read_access_policy" {
+  tags = module.tags.values
+
+  name   = "${local.identifier_prefix}-department-data-and-insight-all-zones-read-access"
+  policy = data.aws_iam_policy_document.department_data_and_insight_all_zones_read_access_policy.json
+}
+
+resource "aws_iam_policy_attachment" "department_data_and_insight_all_zones_read_access_policy" {
+  name       = "${local.identifier_prefix}-department-data-and-insight-all-zones-read-access-policy"
+  roles      = [module.department_data_and_insight.glue_role_name]
+  policy_arn = aws_iam_policy.department_data_and_insight_all_zones_read_access_policy.arn
+}
