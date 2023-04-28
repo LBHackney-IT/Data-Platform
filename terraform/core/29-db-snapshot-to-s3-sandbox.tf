cbf1b2c4a54eafe9b51fa9fd6ac71ec6dfe1c288
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-04-28 14:57:13 +0100

    add rds snapshot to s3 dev setup

---
+++ terraform/core/29-db-snapshot-to-s3-sandbox.tf
@@ -0,0 +1,116 @@
+#These reources are for development use only to setup RDS snapshot to S3 process cross account between sandbox and DP dev accounts
+
+# 1. Deploy this first to get the database and bastion host in place
+module "db_snapshot_to_s3_sandbox_resources" {
+  count                   = 0
+  source                  = "../modules/db-snapshot-to-s3-sandbox-resources"
+  tags                    = module.tags.values
+  identifier_prefix       = local.identifier_prefix
+  aws_sandbox_subnet_ids  = var.aws_sandbox_subnet_ids
+  aws_sandbox_account_id  = var.aws_sandbox_account_id
+  aws_sandbox_vpc_id      = var.aws_sandbox_vpc_id
+
+  providers = {
+    aws                     = aws
+    aws.aws_sandbox_account = aws.aws_sandbox_account
+  }
+}
+
+# 2. Copy the deployed sandbox database name to rds_instance_ids variable in the env.tfvars file. This is required for step 3.
+
+# 3. lambda_artefact_storage_for_sandbox_account and db_snapshot_to_s3_sandbox can be deployed at the same time
+module "lambda_artefact_storage_for_sandbox_account" {
+  count = 0
+  source            = "../modules/s3-bucket"
+  tags              = module.tags.values
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = local.identifier_prefix
+  bucket_name       = "Lambda Artefact Storage"
+  bucket_identifier = "sandbox-lambda-artefact-storage"
+
+  providers = {
+    aws = aws.aws_sandbox_account
+  }
+}
+
+module "db_snapshot_to_s3_sandbox" {
+  count = 0
+  source                         = "../modules/db-snapshot-to-s3"
+  tags                           = module.tags.values
+  project                        = var.project
+  environment                    = var.environment
+  identifier_prefix              = local.identifier_prefix
+  lambda_artefact_storage_bucket = module.lambda_artefact_storage_for_sandbox_account[0].bucket_id
+  zone_kms_key_arn               = module.raw_zone.kms_key_arn
+  zone_bucket_arn                = module.raw_zone.bucket_arn
+  zone_bucket_id                 = module.raw_zone.bucket_id
+  service_area                   = "unrestricted"
+  rds_instance_ids               = var.rds_instance_ids
+  aws_account_suffix             = "-sandbox"
+
+  providers = {
+    aws = aws.aws_sandbox_account
+  }
+}
+
+#4. Update the raw zone bucket on DP dev account in your workspace with the following bucket and bucket key statements
+#   Use these as inputs for bucket_policy_statements and bucket_key_policy_statements in the raw zone bucket module
+
+# sandbox_s3_to_s3_copier_write_access_to_raw_zone_statement = {
+#     sid = "AllowSandboxS3toS3CopierWriteAccessToRawZoneUnrestrictedLocation"
+#     effect = "Allow"
+   
+#     actions = [
+#       "s3:ListBucket",
+#       "s3:PutObject",
+#       "s3:PutObjectAcl"
+#     ]
+#     principals = {
+#       type = "AWS"
+#       identifiers = [
+#         "arn:aws:iam::${var.aws_sandbox_account_id}:root",
+#         "arn:aws:iam::${var.aws_sandbox_account_id}:role/${local.identifier_prefix}-s3-to-s3-copier-lambda"
+#       ]
+#     }
+
+#     resources = [
+#       "${module.raw_zone.bucket_arn}",
+#       "${module.raw_zone.bucket_arn}/unrestricted/${replace(lower(local.identifier_prefix), "/[^a-zA-Z0-9]+/", "_")}_sandbox/*" 
+#     ]
+#   }
+
+#   sandbox_s3_to_s3_copier_raw_zone_key_statement = {
+#     sid = "SandboxS3ToS3CopierAccessToRawZoneKey"
+#     effect = "Allow"
+#     actions = [
+#       "kms:Encrypt",
+#       "kms:GenerateDataKey*"
+#     ]
+
+#     principals = {
+#       type = "AWS"
+#       identifiers = [
+#         "arn:aws:iam::${var.aws_sandbox_account_id}:root"
+#         "arn:aws:iam::${var.aws_sandbox_account_id}:role/${local.identifier_prefix}-s3-to-s3-copier-lambda"
+#       ]
+#     }
+  
+#   }
+
+#5. Uncomment the statement in the sandbox database key policy to allow the rds snapshot to s3 lambda role access to the key. This must be done after all other resources have been deployed.
+
+#6. Connect to the database from the bastion host using AWS Session Manager and import some dummy data for testing 
+# you can import some test data by following these steps:
+#  sudo yum update -y
+#  sudo amazon-linux-extras enable postgresql14
+#  sudo yum install postgresql-server -y
+#  psql --host=<YOUR_DATABASE_HOST> --port=5432 --username=sandbox_db_admin --password --dbname=<YOUR_DATABASE_NAME>
+# enter password from secrets manager 
+# you can use the following resources for quick setup
+# schema: https://github.com/LBHackney-IT/social-care-case-viewer-api/blob/master/database/schema.sql
+# seeds: https://github.com/LBHackney-IT/social-care-case-viewer-api/blob/master/database/seeds.sql
+
+#7. Test the setup by taking a manual snapshot of the sandbox database. Once the process is complete you should have data in your DP dev raw zone in the unrestrcited area
+
+#8 To remove the resources and revert the changes follow the steps in reverse order
---
+++ terraform/modules/db-snapshot-to-s3-sandbox-resources/11-bastion.tf
@@ -0,0 +1,130 @@
+data "aws_ami" "latest_amazon_linux_2" {
+  provider    = aws.aws_sandbox_account
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  # Use Amazon Linux 2 AMI (HVM) SSD Volume Type
+  name_regex = "^amzn2-ami-hvm-.*x86_64-gp2"
+  # Owner: Amazon
+  owners = ["137112412989"]
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  provider = aws.aws_sandbox_account
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+resource "aws_iam_role" "bastion" {
+  provider = aws.aws_sandbox_account
+  tags     = var.tags
+
+  name               = "${var.identifier_prefix}-sandbox-bastion"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+data "aws_iam_policy" "amazon_ssm_managed_instance_core" {
+  provider = aws.aws_sandbox_account
+  name     = "AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  provider   = aws.aws_sandbox_account
+  policy_arn = data.aws_iam_policy.amazon_ssm_managed_instance_core.arn
+  role       = aws_iam_role.bastion.id
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  provider = aws.aws_sandbox_account
+  tags     = var.tags
+
+  name = "${var.identifier_prefix}-sandbox-bastion-profile"
+  role = aws_iam_role.bastion.id
+}
+
+resource "aws_security_group" "bastion" {
+  provider    = aws.aws_sandbox_account
+  name        = "${var.identifier_prefix}-sandbox-bastion"
+  description = "Restrict traffic to bastion host"
+  vpc_id      = var.aws_sandbox_vpc_id
+  tags        = var.tags
+  revoke_rules_on_delete = true
+}
+
+resource "aws_security_group_rule" "bastion_egress" {
+  provider                 = aws.aws_sandbox_account
+  description              = "Allow outbound to postgres"
+  type                     = "egress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "TCP"
+  security_group_id        = aws_security_group.bastion.id
+  source_security_group_id = aws_security_group.rds_snapshot_to_s3.id
+}
+
+resource "aws_security_group_rule" "bastion_egress_to_systems_manager" {
+  provider                 = aws.aws_sandbox_account
+  description              = "Allow outbound to Systems Manager"
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "TCP"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.bastion.id
+}
+
+resource "aws_instance" "bastion" {
+  provider = aws.aws_sandbox_account
+  tags = merge(var.tags, {
+    "Name" : "${var.identifier_prefix}-sandbox-bastion",
+    "OOOShutdown" : "true"
+  })
+
+  ami           = data.aws_ami.latest_amazon_linux_2.id
+  instance_type = "t3.micro"
+
+  key_name             = aws_key_pair.generated_key.key_name
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+  subnet_id                   = var.aws_sandbox_subnet_ids[0]
+  vpc_security_group_ids      = [aws_security_group.bastion.id]
+  associate_public_ip_address = false
+
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
+}
+
+resource "tls_private_key" "bastion_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_ssm_parameter" "bastion_key" {
+  provider = aws.aws_sandbox_account
+  tags     = var.tags
+
+  name        = "/${var.identifier_prefix}/ec2/sandbox_bastion_key"
+  type        = "SecureString"
+  description = "The private key for the EC2 sandbox bastion instance"
+  value       = tls_private_key.bastion_key.private_key_pem
+}
+
+resource "aws_key_pair" "generated_key" {
+  provider = aws.aws_sandbox_account
+  tags     = var.tags
+
+  key_name   = "${var.identifier_prefix}-sandbox-bastion"
+  public_key = tls_private_key.bastion_key.public_key_openssh
+}
