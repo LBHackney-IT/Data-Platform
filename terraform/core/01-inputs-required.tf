9cd25e87f89963dda3f41c93e0f151f7a8966d1a
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-05-04 15:28:32 +0100

    add sandbox account id variables

--- terraform/core/01-inputs-required.tf
+++ terraform/core/01-inputs-required.tf
@@ -30,6 +30,11 @@ variable "aws_hackit_account_id" {
   type        = string
 }
 
+variable "aws_sandbox_account_id" {
+  description = "AWS sandbox account id"
+  type        = string
+}
+
 variable "google_project_id" {
   description = "The ID of the google project used as the target for resource deployment"
   type        = string
