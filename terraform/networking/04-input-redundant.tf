e1f6e535538a61e7396f66d224e1e3f05f23e6c0
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2023-05-11 11:09:48 +0100

    DPP-490 Add variable to supress warnings (#1309)
    * add variable to supress warnings
    
    * add missing variable
    
    * add datahub_url

--- terraform/networking/04-input-redundant.tf
+++ terraform/networking/04-input-redundant.tf
@@ -33,3 +33,19 @@ variable "copy_liberator_to_pre_prod_lambda_execution_role" {
 variable "pre_production_liberator_data_storage_kms_key_arn" {
   default = false
 }
+
+variable "emails_to_notify_with_budget_alerts" {
+  default = false
+}
+
+variable "qlik_ssl_certificate_domain" {
+  default = false
+}
+
+variable "rentsense_target_path" {
+  default = false
+}
+
+variable "datahub_url" {
+  default = false
+}
