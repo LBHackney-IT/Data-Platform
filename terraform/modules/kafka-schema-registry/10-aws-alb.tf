e0f9f5dc4ff1bb0239238a371fc2823484254938
LBHTKarki <tuomo.karki@hackney.gov.uk>
2023-05-12 09:36:44 +0100

    update subnet ids handling

--- terraform/modules/kafka-schema-registry/10-aws-alb.tf
+++ terraform/modules/kafka-schema-registry/10-aws-alb.tf
@@ -1,10 +1,13 @@
-data "aws_subnet_ids" "subnet_ids" {
-  vpc_id = var.vpc_id
+data "aws_subnets" "subnet_ids" {
+  filter {
+    name    = "vpc-id"
+    values  = [var.vpc_id]
+  }
 }
 
 data "aws_subnet" "subnets" {
-  count = length(data.aws_subnet_ids.subnet_ids.ids)
-  id    = tolist(data.aws_subnet_ids.subnet_ids.ids)[count.index]
+  count = length(data.aws_subnets.subnet_ids.ids)
+  id    = tolist(data.aws_subnets.subnet_ids.ids)[count.index]
 }
 
 resource "aws_security_group" "schema_registry_alb" {
