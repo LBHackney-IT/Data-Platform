eb117c04ab81f084567d092f9cdc447415b734ca
timburke-hackit <tim.burke@hackney.gov.uk>
2023-05-15 19:37:45 +0100

    change lambda source

--- terraform/etl/50-aws-lambda-export-dynamodb-pitr.tf
+++ terraform/etl/50-aws-lambda-export-dynamodb-pitr.tf
@@ -1,12 +1,12 @@
 module "export-mtfh-pitr" {
   source                         = "../modules/aws-lambda"
-  lambda_name                    = "dynamodb-pitr-export"
-  lambda_source_dir              = "../lambdas/dynamodb-pitr-export"
-  lambda_output_path             = "../lambdas/dynamodb_pitr_export.zip"
+  lambda_name                    = "mtfh-export-lambda"
+  lambda_source_dir              = "../../lambdas/mtfh_export_lambda"
+  lambda_output_path             = "../../lambdas/mtfh_export_lambda.zip"
   handler                        = "main.lambda_handler"
   identifier_prefix              = local.short_identifier_prefix
   lambda_artefact_storage_bucket = module.lambda_artefact_storage_data_source.bucket_id
-  s3_key                         = "dynamodb-pitr-export.zip"
+  s3_key                         = "mtfh_export_lambda.zip"
 
   lambda_environment_variables = {
     SECRET_NAME = "${local.short_identifier_prefix}manual-mtfh-step-functions-mtfh-test"
