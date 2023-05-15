f4826ea0c046b7308779f647e30330d523ebc49e
timburke-hackit <tim.burke@hackney.gov.uk>
2023-05-15 22:15:00 +0100

    change role from name to arn

--- terraform/modules/aws-lambda/20-iam.tf
+++ terraform/modules/aws-lambda/20-iam.tf
@@ -56,6 +56,6 @@ resource "aws_iam_policy" "lambda_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_role" {
-  role       = aws_iam_role.lambda_role.arn
+  role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.lambda_role.arn
 }
