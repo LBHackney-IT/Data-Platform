40aa37a02a32741e1defd7e71bd217368a05e568
timburke-hackit <tim.burke@hackney.gov.uk>
2023-05-25 17:21:14 +0100

    add lambda name to outputs

--- terraform/modules/aws-lambda/99-outputs.tf
+++ terraform/modules/aws-lambda/99-outputs.tf
@@ -3,13 +3,8 @@ output "lambda_function_arn" {
   value       = aws_lambda_function.lambda.arn
 }
 
-#output "lambda_function_arn_static" {
-#  description = "The static arn of the lambda function. Use this to avoid cycle errors between resources (e.g. Step Functions)"
-#  
-#}
-
-#output "lambda_function_name" {
-#  description = "value of the lambda function name"
-#  value       = aws_lambda_function.lambda.function_name
-#}
+output "lambda_iam_role" {
+  description = "name of the lambda function iam role"
+  value       = aws_iam_role.lambda_role.name
+}
 
