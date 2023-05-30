e733f61fb05ef3b6573421876d1d3903cd11ccbf
LBHTKarki <58432863+LBHTKarki@users.noreply.github.com>
2023-05-30 09:07:28 +0100

    DPP-513 Resolve dev terraform config issues (#1350)
    * resolve dev config issues
    
    * allow notebook destroy
    
    * readme update

--- terraform/core/83-development-deploy-role.tf
+++ terraform/core/83-development-deploy-role.tf
@@ -1,19 +1,25 @@
+# This should be one-off deployment per development account since the role is shared between workspaces
+# Only enable this if setting a development account from scratch
+locals {
+  deploy_development_deploy_role = false
+}
+
 data "aws_organizations_organization" "org" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
 }
 
 data "aws_secretsmanager_secret" "admin_sso_role" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
   name = "manual-developer-admin-sso-role-arn"
 }
 
 data "aws_secretsmanager_secret_version" "admin_sso_role" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
   secret_id = data.aws_secretsmanager_secret.admin_sso_role[0].id
 }
 
 data "aws_iam_policy_document" "development_deploy_assume_role" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
   
   statement {
     sid = "AllowDataPlatformDevelopersDeploymentAdminAccess"
@@ -45,13 +51,13 @@ data "aws_iam_policy_document" "development_deploy_assume_role" {
 }
 
 resource "aws_iam_role" "development_deploy_role" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
   name               = "development_deploy"
   assume_role_policy = data.aws_iam_policy_document.development_deploy_assume_role[0].json
 }
 
 data "aws_iam_policy_document" "development_deploy_policy_document" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
 
   statement {
     actions   = ["*"]
@@ -61,14 +67,14 @@ data "aws_iam_policy_document" "development_deploy_policy_document" {
 }
 
 resource "aws_iam_policy" "development_deploy_policy" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
 
   name     = "development_deploy"
   policy   = data.aws_iam_policy_document.development_deploy_policy_document[0].json
 }
 
 resource "aws_iam_role_policy_attachment" "development_deploy_role_policy_attachment" {
-  count = local.is_live_environment ? 0 : 1
+  count = !local.is_live_environment && local.deploy_development_deploy_role ? 1 : 0
 
   role       = aws_iam_role.development_deploy_role[0].name
   policy_arn = aws_iam_policy.development_deploy_policy[0].arn
