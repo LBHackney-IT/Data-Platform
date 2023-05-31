c03d701589fa0ea63560de1ff8cdf689a2ddb6fc
timburke-hackit <tim.burke@hackney.gov.uk>
2023-05-31 12:01:15 +0100

    use lambda_name variable for policy name

--- terraform/modules/aws-lambda/20-iam.tf
+++ terraform/modules/aws-lambda/20-iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.identifier_prefix}dynamodb-export-lambda-role"
+  name = "${var.identifier_prefix}${var.lambda_name}-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
