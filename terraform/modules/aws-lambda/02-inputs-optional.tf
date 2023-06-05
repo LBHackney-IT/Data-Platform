5430eb35cdf84bdd072e7aa605a6e1e04be156ba
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2023-06-05 14:31:35 +0100

    AWS lambda module (#1325)
    * export pitr lambda script
    
    * initial com
    
    * change lambda source
    
    * update ephemeral storage default
    
    * change role from name to arn
    
    * make environment variables dynamic
    
    * remove duplicate variable
    
    * add dependency handling
    
    * change source_code_hash
    
    * add output_file_mode variable
    
    * create lambda in non-production environment
    
    * add lambda name to outputs
    
    * correct region argument
    
    * add validation for python runtime
    
    * lambda timeout validation
    
    * add lambda memory validation
    
    * use lambda_name variable for policy name
    
    * regex for valid python runtimes
    
    * remove kms and secrets statements
    
    * missing brace
    
    * ephemeral storage validation
    
    * use aws_s3_object resource
    
    * remove include_requirements local
    
    * amend regex
    
    * add make file
    
    * add install_requirements var
    
    * index reference for null_resource

---
+++ terraform/modules/aws-lambda/02-inputs-optional.tf
@@ -0,0 +1,82 @@
+variable "identifier_prefix" {
+  type        = string
+  description = "Environment identifier prefix"
+  default     = ""
+}
+
+variable "secret_name" {
+  type        = string
+  description = "Name of the secret stored in Secrets Manager to grant access to"
+  default     = ""
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all resources"
+  default     = {}
+}
+
+variable "runtime" {
+  type        = string
+  description = "Runtime to use for the Lambda Function"
+  default     = "python3.8"
+  validation {
+    condition     = can(regex("python3[.]([7-9]|10)", var.runtime))
+    error_message = "Runtime must be a valid Python runtime"
+  }
+
+}
+
+variable "lambda_timeout" {
+  type        = number
+  description = "Amount of time the Lambda Function has to run in seconds"
+  default     = 60
+  validation {
+    condition     = var.lambda_timeout >= 1 && var.lambda_timeout <= 900
+    error_message = "Lambda Timeout must be between 1 and 900 seconds"
+  }
+}
+
+variable "lambda_memory_size" {
+  type        = number
+  description = "Memory Size for the Lambda Function"
+  default     = 128
+  validation {
+    condition     = var.lambda_memory_size >= 128 && var.lambda_memory_size <= 10240
+    error_message = "Lambda Memory Size must be between 128 and 10240 MB"
+  }
+}
+
+variable "ephemeral_storage" {
+  type        = number
+  description = "Additional Ephemeral Storage for the Lambda Function beyond the default 512MB"
+  default     = 512
+  validation {
+    condition     = var.ephemeral_storage >= 512 && var.ephemeral_storage <= 10240
+    error_message = "Ephemeral Storage must be between 512 and 10240 MB"
+  }
+}
+
+variable "lambda_output_path" {
+  type        = string
+  description = "Path to the Lambda artefact zip file"
+  default     = "../../../lambdas/lambda-archives/"
+}
+
+variable "environment_variables" {
+  type        = map(string)
+  description = "Environment Variables to pass to the Lambda Function"
+  default     = null
+}
+
+variable "output_file_mode" {
+  type        = number
+  description = "File Mode for the Lambda artefact zip file"
+  default     = 0666
+}
+
+variable "install_requirements" {
+  type        = bool
+  description = "Whether to create and install requirements.txt for the Lambda Function"
+  default     = true
+}
