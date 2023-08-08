7ac2b688507c898406b5b514f8f7b945239f85ab
timburke-hackit <tim.burke@hackney.gov.uk>
2023-08-08 12:43:47 +0100

    formatting

--- terraform/modules/sql-to-rds-snapshot/30-rds.tf
+++ terraform/modules/sql-to-rds-snapshot/30-rds.tf
@@ -5,14 +5,13 @@ resource "aws_db_subnet_group" "default" {
 }
 
 resource "aws_db_instance" "ingestion_db" {
-  allocated_storage     = 15
-  max_allocated_storage = 30
-  engine                = "mysql"
-  engine_version        = "8.0"
-  instance_class        = "db.t3.micro"
-  identifier            = var.instance_name
-  db_subnet_group_name  = aws_db_subnet_group.default.name
-
+  allocated_storage      = 15
+  max_allocated_storage  = 30
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  identifier             = var.instance_name
+  db_subnet_group_name   = aws_db_subnet_group.default.name
   username               = "dataplatform"
   password               = random_password.rds_password.result
   skip_final_snapshot    = true
