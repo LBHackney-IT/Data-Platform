11cad8b279cda6979081c3e0eafd170384663f0e
timburke-hackit <tim.burke@hackney.gov.uk>
2023-10-12 11:14:02 +0100

    remove event rule name output

--- terraform/modules/rds-snapshot-to-s3/99-outputs.tf
+++ terraform/modules/rds-snapshot-to-s3/99-outputs.tf
@@ -1,4 +1,1 @@
-output "cloudwatch_event_rule_names" {
-  description = "The names of the CloudWatch Event Rules"
-  value       = [for rule in aws_cloudwatch_event_rule.rds_event_rule : rule.name]
-}
+
