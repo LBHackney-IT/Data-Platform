2b07696ca0816c857f9ee80135e27c951dd6ccdb
timburke-hackit <tim.burke@hackney.gov.uk>
2023-10-12 09:25:59 +0100

    add lambdas

--- terraform/modules/rds-snapshot-to-s3/lambda.tf
+++ terraform/modules/rds-snapshot-to-s3/lambda.tf
@@ -1,12 +1,25 @@
-module "rds-to-s3-copier" {
+module "trigger_rds_snapshot_export" {
   source                         = "../aws-lambda"
   lambda_name                    = "rds-to-s3-copier"
-  runtime                        = "python3.8"
+  runtime                        = "python3.10"
   handler                        = "lambda_function.lambda_handler"
   lambda_artefact_storage_bucket = var.lambda_artefact_storage_bucket
-  lambda_source_dir              = "../../lambdas/s3-to-s3-export-copier-python"
-  lambda_output_path             = "../../lambdas/rds-to-s3-copier.zip"
-  s3_key                         = "rds-to-s3-copier.zip"
+  lambda_source_dir              = "../../lambdas/export_rds_snapshot_to_s3"
+  lambda_output_path             = "../../lambdas/export-rds-snapshot-to-s3.zip"
+  s3_key                         = "export-rds-snapshot-to-s3.zip"
+  identifier_prefix              = var.identifier_prefix
+  tags                           = var.tags
+}
+
+module "rds_snapshot_s3_to_s3_copier" {
+  source                         = "../aws-lambda"
+  lambda_name                    = "rds-export-s3-to-s3-copier"
+  runtime                        = "python3.10"
+  handler                        = "lambda_function.lambda_handler"
+  lambda_artefact_storage_bucket = var.lambda_artefact_storage_bucket
+  lambda_source_dir              = "../../lambdas/rds_export_s3_to_s3_copier"
+  lambda_output_path             = "../../lambdas/rds-export-s3-to-s3-copier.zip"
+  s3_key                         = "rds-export-s3-to-s3-copier.zip"
   identifier_prefix              = var.identifier_prefix
   tags                           = var.tags
 }
