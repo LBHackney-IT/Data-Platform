dbab932c499c9e09011e0132e958c9f69dc48291
timburke-hackit <tim.burke@hackney.gov.uk>
2023-10-16 12:12:12 +0100

    output lambda role

--- terraform/modules/rds-snapshot-to-s3/99-outputs.tf
+++ terraform/modules/rds-snapshot-to-s3/99-outputs.tf
@@ -1,1 +1,4 @@
-
+output "rds_snapshot_s3_to_s3_copier_lambda_role_arn" {
+  description = "ARN for the s3_to_s3_copier_lambda_role"
+  value       = aws_iam_role.rds_snapshot_s3_to_s3_copier_lambda_role.arn
+}
