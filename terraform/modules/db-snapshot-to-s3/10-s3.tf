3cd824d58b36da35e330ffcbfcd472a2060fbafd
timburke-hackit <tim.burke@hackney.gov.uk>
2023-10-16 14:18:43 +0100

    re-add existing rds s3 storage bucket

--- terraform/modules/db-snapshot-to-s3/10-s3.tf
+++ terraform/modules/db-snapshot-to-s3/10-s3.tf
@@ -1,1 +1,13 @@
+module "rds_export_storage" {
+  source = "../s3-bucket"
 
+  tags              = var.tags
+  project           = var.project
+  environment       = var.environment
+  identifier_prefix = var.identifier_prefix
+  bucket_name       = "RDS Export Storage"
+  bucket_identifier = "rds-export-storage${var.aws_account_suffix}"
+  role_arns_to_share_access_with = [
+    aws_iam_role.rds_snapshot_to_s3_lambda.arn
+  ]
+}
