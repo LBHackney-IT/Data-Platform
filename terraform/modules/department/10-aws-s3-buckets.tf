36495e74afc7bfc294450c4eee29664645e2f652
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2023-10-24 17:23:13 +0100

    add dept s3 paths (#1463)

--- terraform/modules/department/10-aws-s3-buckets.tf
+++ terraform/modules/department/10-aws-s3-buckets.tf
@@ -1,27 +1,34 @@
-//resource "aws_s3_object" "landing_zone_department_folder" {
-//  bucket       = var.landing_zone_bucket_id
-//  acl          = "private"
-//  key          = "${local.identifier}/"
-//  content_type = "application/x-directory"
-//}
-//
-//resource "aws_s3_object" "raw_zone_department_folder" {
-//  bucket       = var.raw_zone_bucket_id
-//  acl          = "private"
-//  key          = "${local.identifier}/"
-//  content_type = "application/x-directory"
-//}
-//
-//resource "aws_s3_object" "refined_zone_department_folder" {
-//  bucket       = var.refined_zone_bucket_id
-//  acl          = "private"
-//  key          = "${local.identifier}/"
-//  content_type = "application/x-directory"
-//}
-//
-//resource "aws_s3_object" "trusted_zone_department_folder" {
-//  bucket       = var.trusted_zone_bucket_id
-//  acl          = "private"
-//  key          = "${local.identifier}/"
-//  content_type = "application/x-directory"
-//}
+resource "aws_s3_object" "landing_zone_department_folder" {
+  bucket       = var.landing_zone_bucket.bucket_id
+  acl          = "private"
+  key          = "${local.department_identifier}/"
+  content_type = "application/x-directory"
+}
+
+resource "aws_s3_object" "landing_zone_department_folder_manual" {
+  bucket       = var.landing_zone_bucket.bucket_id
+  acl          = "private"
+  key          = "${local.department_identifier}/manual/"
+  content_type = "application/x-directory"
+}
+
+resource "aws_s3_object" "raw_zone_department_folder" {
+  bucket       = var.raw_zone_bucket.bucket_id
+  acl          = "private"
+  key          = "${local.department_identifier}/"
+  content_type = "application/x-directory"
+}
+
+resource "aws_s3_object" "refined_zone_department_folder" {
+  bucket       = var.refined_zone_bucket.bucket_id
+  acl          = "private"
+  key          = "${local.department_identifier}/"
+  content_type = "application/x-directory"
+}
+
+resource "aws_s3_object" "trusted_zone_department_folder" {
+  bucket       = var.trusted_zone_bucket.bucket_id
+  acl          = "private"
+  key          = "${local.department_identifier}/"
+  content_type = "application/x-directory"
+}
