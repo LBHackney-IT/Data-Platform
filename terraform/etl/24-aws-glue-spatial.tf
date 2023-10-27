604a7f3b1bf25a437da0a01e0789494da1464b82
Sandrine Balley <42965897+LBHSBALLEY@users.noreply.github.com>
2023-10-27 16:33:32 +0100

    Update 24-aws-glue-spatial.tf
    Add module for addressbase file loader and 2 lookup files for the future national_address module

--- terraform/etl/24-aws-glue-spatial.tf
+++ terraform/etl/24-aws-glue-spatial.tf
@@ -131,4 +131,44 @@ module "parking_geospatial_enrichment" {
   }
 }
 
+# 2 jobs for loading AddressBasePremium into unrestricted raw/refined 
 
+# Ward look up resource for AddressBasePremium loader
+resource "aws_s3_object" "ons_ward_lookup" {
+  bucket      = module.glue_scripts_data_source.bucket_id
+  key         = "scripts/unrestricted/ons_ward_lookup.csv"
+  acl         = "private"
+  source      = "../../scripts/jobs/unrestricted/ons_ward_lookup.csv"
+  source_hash = filemd5("../../scripts/jobs/unrestricted/ons_ward_lookup.csv")
+}
+
+# BLPU classification look up resource for AddressBasePremium loader
+resource "aws_s3_object" "blpu_class_lookup" {
+  bucket      = module.glue_scripts_data_source.bucket_id
+  key         = "scripts/unrestricted/blpu_class_lookup.csv"
+  acl         = "private"
+  source      = "../../scripts/jobs/unrestricted/blpu_class_lookup.csv"
+  source_hash = filemd5("../../scripts/jobs/unrestricted/blpu_class_lookup.csv")
+}
+
+# Job pre-processing csv files provided by OS 
+module "addressbasepremium_load_files" {
+  source                    = "../modules/aws-glue-job"
+  is_live_environment       = local.is_live_environment
+  is_production_environment = local.is_production_environment
+
+  department                 = module.department_unrestricted_data_source
+  job_name                   = "${local.short_identifier_prefix}addressbasepremium_load_files"
+  glue_version               = "4.0"
+  glue_job_worker_type       = "G.1X"
+  helper_module_key          = data.aws_s3_object.helpers.key
+  pydeequ_zip_key            = data.aws_s3_object.pydeequ.key
+  spark_ui_output_storage_id = module.spark_ui_output_storage_data_source.bucket_id
+  job_parameters = {
+    "--job-bookmark-option"  = "job-bookmark-disable"    
+    "--raw_bucket"           = "${module.raw_zone_data_source.bucket_id}"
+    "--raw_prefix"           = "unrestricted/os_addressbase_premium/full_supply/epoch_102/raw/"
+    "--processed_data_path"  = "s3://${module.raw_zone_data_source.bucket_id}/unrestricted/os_addressbase_premium/full_supply/epoch_102/processed/"
+  }
+  script_name                = "addressbasepremium_load_files"
+}
