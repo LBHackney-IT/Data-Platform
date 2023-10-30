3d92d4d8c5d27844a2c79e6e6516d9d1350926d3
Wayne Greeff <61699419+LBH-wgreeff@users.noreply.github.com>
2023-10-30 16:46:46 -0700

    Removing Mosaic VPC cross account access (#1468)

--- terraform/networking/21-vpc-peering-connection.tf
+++ terraform/networking/21-vpc-peering-connection.tf
@@ -23,24 +23,6 @@ module "api_vpc_peering_cross_account" {
   accepter_allow_remote_vpc_dns_resolution = "false"
 }
 
-module "mosaic_vpc_peering_cross_account" {
-  tags    = module.tags.values
-  enabled = true
-
-  source = "git::https://github.com/cloudposse/terraform-aws-vpc-peering-multi-account.git?ref=tags/0.19.1"
-  name   = "${local.identifier_prefix}-mosaic-vpc-peering-connection"
-
-  requester_aws_assume_role_arn             = local.requester_assume_role_arn
-  requester_region                          = var.aws_deploy_region
-  requester_vpc_id                          = module.core_vpc.vpc_id
-  requester_allow_remote_vpc_dns_resolution = "true"
-
-  accepter_aws_assume_role_arn             = local.aws_mosaic_accepter_assume_role_arn
-  accepter_region                          = var.aws_deploy_region
-  accepter_vpc_id                          = var.aws_mosaic_vpc_id
-  accepter_allow_remote_vpc_dns_resolution = "false"
-}
-
 module "dp_stg_prod_vpc_peering_cross_account" {
   tags    = module.tags.values
   enabled = false
