d18bc5df36a20579d62ade4234995fddf81a02f6
timburke-hackit <tim.burke@hackney.gov.uk>
2023-10-31 14:08:43 +0000

    update lambda handler

--- terraform/modules/rds-snapshot-to-s3/lambda.tf
+++ terraform/modules/rds-snapshot-to-s3/lambda.tf
@@ -2,7 +2,7 @@ module "trigger_rds_snapshot_export" {
   source                         = "../aws-lambda"
   lambda_name                    = "export-rds-snapshot-to-s3"
   runtime                        = "python3.9"
-  handler                        = "lambda_function.lambda_handler"
+  handler                        = "main.lambda_handler"
   lambda_artefact_storage_bucket = var.lambda_artefact_storage_bucket
   lambda_source_dir              = "../../lambdas/export_rds_snapshot_to_s3"
   lambda_output_path             = "../../lambdas/export-rds-snapshot-to-s3.zip"
@@ -20,7 +20,7 @@ module "rds_snapshot_s3_to_s3_copier" {
   source                         = "../aws-lambda"
   lambda_name                    = "rds-export-s3-to-s3-copier"
   runtime                        = "python3.9"
-  handler                        = "lambda_function.lambda_handler"
+  handler                        = "main.lambda_handler"
   lambda_artefact_storage_bucket = var.lambda_artefact_storage_bucket
   lambda_source_dir              = "../../lambdas/rds_snapshot_export_s3_to_s3_copier"
   lambda_output_path             = "../../lambdas/rds_snapshot_export_s3_to_s3_copier.zip"
