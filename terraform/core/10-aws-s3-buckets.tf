077689b0fb79740fb8f5d5a461b5e73100396289
timburke-hackit <tim.burke@hackney.gov.uk>
2023-11-08 15:11:42 +0000

    add sse config

--- terraform/core/10-aws-s3-buckets.tf
+++ terraform/core/10-aws-s3-buckets.tf
@@ -457,3 +457,14 @@ module "deprecated_rds_export_storage" {
   bucket_name       = "RDS Export Storage"
   bucket_identifier = "rds-export-storage"
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "deprecated_rds_export_storage_encryption" {
+  bucket = module.deprecated_rds_export_storage.bucket_id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+    bucket_key_enabled = true
+  }
+}
