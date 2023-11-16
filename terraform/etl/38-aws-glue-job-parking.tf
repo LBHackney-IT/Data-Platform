1ce99d45172793211d108401f0b67bd2884329bd
Davina Clarke <87067339+daclarkelbh@users.noreply.github.com>
2023-11-16 10:41:37 +0000

    Update 38-aws-glue-job-parking.tf (#1502)
    added job parking_correspondence_performance_records_with_pcn_downtime_gds to schedule.  Forgot to in last github change.

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -1326,3 +1326,26 @@ module "parking_cycle_hangar_allocation" {
     "--environment"         = var.environment
   }
 }
+module "parking_correspondence_performance_records_with_pcn_downtime_gds" {
+  source                         = "../modules/aws-glue-job"
+  is_live_environment            = local.is_live_environment
+  is_production_environment      = local.is_production_environment
+  department                     = module.department_parking_data_source
+  job_name                       = "${local.short_identifier_prefix}parking_correspondence_performance_records_with_pcn_downtime_gds"
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  script_name                    = "parking_correspondence_performance_records_with_pcn_downtime_gds"
+  triggered_by_job               = module.parking_pcn_denormalisation.job_name
+  job_description                = "correspondence performance records with pcn FOI records Team details and Downtime data for Google Studio - gds"
+  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
+  trigger_enabled                = local.is_production_environment
+  number_of_workers_for_glue_job = 10
+  glue_job_worker_type           = "G.1X"
+  glue_version                   = "4.0"
+  job_parameters = {
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--environment"         = var.environment
+    "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
+  }
+}
