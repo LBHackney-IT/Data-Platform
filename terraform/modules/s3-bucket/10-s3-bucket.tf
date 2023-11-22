aa33ce9d54da252ad987c4df79d3dfa0c63f6b48
timburke-hackit <tim.burke@hackney.gov.uk>
2023-11-22 15:04:16 +0000

    add kms_master_key_id argument

--- terraform/modules/s3-bucket/10-s3-bucket.tf
+++ terraform/modules/s3-bucket/10-s3-bucket.tf
@@ -114,7 +114,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      kms_master_key_id = aws_kms_key.key.arn
+      sse_algorithm     = "aws:kms"
     }
     bucket_key_enabled = true
   }
