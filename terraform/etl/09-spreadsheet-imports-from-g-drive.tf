2eaf281c82d7f56419235882cfef5d1d4bdd7a26
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2023-11-24 14:18:05 +0000

    Update 09-spreadsheet-imports-from-g-drive.tf
    Bailiff update Oct 2023

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -726,3 +726,73 @@ module "calendar" {
     }
   }
 }
+module "bailiff_return" {
+  count                          = local.is_live_environment ? 1 : 0
+  source                         = "../modules/import-spreadsheet-file-from-g-drive"
+  is_production_environment      = local.is_production_environment
+  is_live_environment            = local.is_live_environment
+  department                     = module.department_parking_data_source
+  glue_scripts_bucket_id         = module.glue_scripts_data_source.bucket_id
+  glue_catalog_database_name     = module.department_parking_data_source.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_id    = module.glue_temp_storage_data_source.bucket_url
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  secrets_manager_kms_key        = data.aws_kms_key.secrets_manager_key
+  glue_role_arn                  = data.aws_iam_role.glue_role.arn
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  jars_key                       = data.aws_s3_object.jars.key
+  spreadsheet_import_script_key  = aws_s3_object.spreadsheet_import_script.key
+  identifier_prefix              = local.short_identifier_prefix
+  lambda_artefact_storage_bucket = module.lambda_artefact_storage_data_source.bucket_id
+  landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
+  landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
+  landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
+  google_drive_document_id       = "1njGYbwRmS34o_QXXG01fUlQh1ubJnXWR"
+  glue_job_name                  = "bailiff_return"
+  output_folder_name             = "g-drive"
+  raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
+  input_file_name                = "bailiff_return/bailiff_return_30_10_2023.csv"
+  ingestion_schedule             = "cron(0 21 * * ? *)"
+  enable_bookmarking             = true
+  worksheets = {
+    sheet1 : {
+      header_row_number = 0
+      worksheet_name    = "bailiff_return"
+    }
+  }
+}
+module "bailiff_allocation" {
+  count                          = local.is_live_environment ? 1 : 0
+  source                         = "../modules/import-spreadsheet-file-from-g-drive"
+  is_production_environment      = local.is_production_environment
+  is_live_environment            = local.is_live_environment
+  department                     = module.department_parking_data_source
+  glue_scripts_bucket_id         = module.glue_scripts_data_source.bucket_id
+  glue_catalog_database_name     = module.department_parking_data_source.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_id    = module.glue_temp_storage_data_source.bucket_url
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  secrets_manager_kms_key        = data.aws_kms_key.secrets_manager_key
+  glue_role_arn                  = data.aws_iam_role.glue_role.arn
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  jars_key                       = data.aws_s3_object.jars.key
+  spreadsheet_import_script_key  = aws_s3_object.spreadsheet_import_script.key
+  identifier_prefix              = local.short_identifier_prefix
+  lambda_artefact_storage_bucket = module.lambda_artefact_storage_data_source.bucket_id
+  landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
+  landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
+  landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
+  google_drive_document_id       = "13utJrDe9SQBxrzvfGOIZS1jYUmOX4DfM"
+  glue_job_name                  = "bailiff_allocation"
+  output_folder_name             = "g-drive"
+  raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
+  input_file_name                = "bailiff_allocation/bailiff_allocation_30_10_2023.csv"
+  ingestion_schedule             = "cron(0 21 * * ? *)"
+  enable_bookmarking             = true
+  worksheets = {
+    sheet1 : {
+      header_row_number = 0
+      worksheet_name    = "bailiff_allocation"
+    }
+  }
+}
