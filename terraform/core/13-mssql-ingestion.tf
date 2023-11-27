d5762193c09a524bb647effdd60d8b4036f41ae4
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2023-11-27 12:13:11 +0000

    add missing comma to Academy step function definition (#1518)
    * add missing comma
    
    * trying to prod the Code scanning results / API check into life

--- terraform/core/13-mssql-ingestion.tf
+++ terraform/core/13-mssql-ingestion.tf
@@ -111,7 +111,7 @@ module "copy_academy_landing_to_raw" {
 
 }
 
-## Academy State Machine
+## Academy State Machine.
 
 locals {
   academy_state_machine_count = local.is_live_environment ? 1 : 0
@@ -250,7 +250,7 @@ module "academy_state_machine" {
             "Parameters": {
               "JobName": "${module.copy_academy_landing_to_raw[0].job_name}}",
               "Arguments": {
-                "--table_filter_expression.$": "$.landingToRaw.FilterString"
+                "--table_filter_expression.$": "$.landingToRaw.FilterString",
                 "--s3_prefix.$": "$.landingToRaw.S3Prefix"
               }
             },
