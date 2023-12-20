4f2b02f74f6e123255e066b7f419e07a14d7102d
timburke-hackit <tim.burke@hackney.gov.uk>
2023-12-20 15:48:47 +0000

    module for s3 bucket notifications

---
+++ terraform/modules/s3-bucket-notification-emails/01-inputs-required.tf
@@ -0,0 +1,20 @@
+variable "name" {
+  description = "The name of the SNS topic"
+}
+
+variable "bucket_id" {
+  description = "The ID of the S3 bucket to subscribe to"
+}
+
+variable "bucket_arn" {
+  description = "The ARN of the s3 bucket to subscribe to"
+}
+
+variable "email_list" {
+  description = "A comma separated list of email addresses to subscribe to the SNS topic"
+  default = ""
+}
+
+variable "lambda_artefact_storage_bucket" {
+  description = "S3 Bucket to store the Lambda artefact in"
+}
