bb399791d9fd0a564534e81f63c6757401b1c35c
LBH-wgreeff <wayne.greeff@hackney.gov.uk>
2024-01-17 13:31:30 +0000

    update to make sure file exists

--- terraform/modules/aws-lambda-layers/10-lambda-layer.tf
+++ terraform/modules/aws-lambda-layers/10-lambda-layer.tf
@@ -18,8 +18,8 @@ resource "null_resource" "run_install_requirements" {
 }
 
 resource "aws_lambda_layer_version" "lambda_layer" {
-  filename            = var.layer_zip_path
-  source_code_hash    = filebase64sha256(var.layer_zip_path)
+  filename            = fileexists(var.layer_zip_path)
+  source_code_hash    = fileexists((filebase64sha256(var.layer_zip_path)))
   layer_name          = var.layer_name
   compatible_runtimes = var.compatible_runtimes
 }
