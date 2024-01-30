0d22d785adb3c07d06fa8d637c13579e7d69ecca
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2024-01-30 15:49:34 +0000

    Update 38-aws-glue-job-parking.tf (#1576)
    * Update 38-aws-glue-job-parking.tf
    
    Create module for cycle hangar job
    
    * fix conf parameter
    
    ---------
    
    Co-authored-by: timburke-hackit <tim.burke@hackney.gov.uk>

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -1441,3 +1441,27 @@ module "parking_cycle_hangars_denormalised_inservice_live_gds" {
     "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
   }
 }
+# MRB 30-01-2024 job created
+module "parking_cycle_hangar_met_fail_monthly_format" {
+  source                         = "../modules/aws-glue-job"
+  is_live_environment            = local.is_live_environment
+  is_production_environment      = local.is_production_environment
+  department                     = module.department_parking_data_source
+  job_name                       = "${local.short_identifier_prefix}parking_cycle_hangar_met_fail_monthly_format"
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  script_name                    = "parking_cycle_hangar_met_fail_monthly_format"
+  triggered_by_job               = "${local.short_identifier_prefix}Copy parking Liberator landing zone to raw"
+  job_description                = "format cycle hangar maintenance data for qlik reporting"
+  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
+  trigger_enabled                = local.is_production_environment
+  number_of_workers_for_glue_job = 10
+  glue_job_worker_type           = "G.1X"
+  glue_version                   = "4.0"
+  job_parameters = {
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--environment"         = var.environment
+    "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
+  }
+}
