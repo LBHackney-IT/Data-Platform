ab2b429ff7f5be7f82f39e5217b23d23ee8b0e3c
Davina Clarke <87067339+daclarkelbh@users.noreply.github.com>
2024-02-16 08:54:40 +0000

    Create parking_open_pcns_vrms_linked_cancelled_ringer job (#1600)
    * Create parking_open_pcns_vrms_linked_cancelled_ringer.py
    
    Created job for Open PCNs linked to cancelled ringer pcns
    
    * Update 38-aws-glue-job-parking.tf
    
    Created module at line 1468 module "parking_open_pcns_vrms_linked_cancelled_ringer"

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -1465,3 +1465,26 @@ module "parking_cycle_hangar_met_fail_monthly_format" {
     "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
   }
 }
+module "parking_open_pcns_vrms_linked_cancelled_ringer" {
+  source                         = "../modules/aws-glue-job"
+  is_live_environment            = local.is_live_environment
+  is_production_environment      = local.is_production_environment
+  department                     = module.department_parking_data_source
+  job_name                       = "${local.short_identifier_prefix}parking_open_pcns_vrms_linked_cancelled_ringer"
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  script_name                    = "parking_open_pcns_vrms_linked_cancelled_ringer"
+  triggered_by_job               = module.parking_pcn_denormalisation.job_name
+  job_description                = "Parking Open PCNs linked to VRMs cancelled due to being a Ringer or Clone"
+  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
+  trigger_enabled                = local.is_production_environment
+  number_of_workers_for_glue_job = 10
+  glue_job_worker_type           = "G.1X"
+  glue_version                   = "4.0"
+  job_parameters = {
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--environment"         = var.environment
+    "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
+  }
+}
