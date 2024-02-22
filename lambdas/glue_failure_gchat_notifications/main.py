2267650d830a7d2b1369987ad20c79068db0f5c3
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2024-02-22 16:28:10 +0000

    Dpp 630 suppress retry alerts (#1604)
    * add check for retries before sending message
    
    * add Glue response to tests
    
    * add glue client region
    
    * update job response
    
    * lowercase a for 'attempt' index
    
    * j is for jobName
    
    * patch urllib3
    
    * test message not sent if max retries not met
    
    * alert should not be sent if attempt is same as MaxRetries
    e.g. attempt 2 is retry 1 so, attempt 1 should not send an alert if it fails and MaxRetries is also 1
    
    * remove redundate return
    
    * fix typo
    
    * allow lambda to perform the glue:GetJob action
    
    * test get_max_retries returns correct value
    
    * add job_name attribute and differentiate timestamps
    
    * variable handling test_format_message_returns_correct_message

--- lambdas/glue_failure_gchat_notifications/main.py
+++ lambdas/glue_failure_gchat_notifications/main.py
@@ -25,21 +25,31 @@ def format_message(event) -> dict:
         )
     }
 
+def get_max_retries(job_name, glue_client=None) -> int:
+    glue_client = glue_client or boto3.client("glue")
+    response = glue_client.get_job(JobName=job_name)
+    return response["Job"]["MaxRetries"]
 
-def lambda_handler(event=None, lambda_context=None, secretsManagerClient=None):
+
+def lambda_handler(event=None, lambda_context=None, secretsManagerClient=None, glueClient=None):
     secret_name = getenv("SECRET_NAME")
     secrets_manager_client = secretsManagerClient or boto3.client("secretsmanager")
+    glue_client = glueClient or boto3.client("glue")
 
-    secret = secrets_manager_client.get_secret_value(SecretId=secret_name)
+    max_retries = get_max_retries(event["detail"]["jobName"], glue_client)
+    if event["detail"]["attempt"] <= max_retries:
+        logger.info("Glue job failed, but it is still within the max retries")
+    else:
+        secret = secrets_manager_client.get_secret_value(SecretId=secret_name)
 
-    webhook_url = secret["SecretString"]
-    message = format_message(event)
-    message_headers = {"Content-Type": "application/json; charset=UTF-8"}
-    http = urllib3.PoolManager()
+        webhook_url = secret["SecretString"]
+        message = format_message(event)
+        message_headers = {"Content-Type": "application/json; charset=UTF-8"}
+        http = urllib3.PoolManager()
 
-    http.request("POST", webhook_url, body=json.dumps(message), headers=message_headers)
+        http.request("POST", webhook_url, body=json.dumps(message), headers=message_headers)
 
-    logger.info("Alert sent successfully")
+        logger.info("Alert sent successfully")
 
 
 if __name__ == "__main__":
--- lambdas/glue_failure_gchat_notifications/test.py
+++ lambdas/glue_failure_gchat_notifications/test.py
@@ -2,10 +2,15 @@ import os
 from datetime import datetime
 from json import dumps
 from unittest import TestCase, mock
+from urllib.parse import quote
 
 import botocore.session
 from botocore.stub import Stubber
-from glue_failure_gchat_notifications.main import format_message, lambda_handler
+from glue_failure_gchat_notifications.main import (
+    format_message,
+    lambda_handler,
+    get_max_retries,
+)
 
 
 class TestGlueAlarmsHandler(TestCase):
@@ -33,22 +38,62 @@ class TestGlueAlarmsHandler(TestCase):
             "CreatedDate": datetime(2022, 1, 1),
         }
 
+        self.job_name = "test job name"
+        self.event_time = "2024-01-11T13:51:06Z"
+        self.job_created_on = "2023-01-11T13:51:06Z"
+        self.job_last_modified_on = "2023-01-12T13:51:06Z"
+
         self.cloudwatch_event = {
             "version": "0",
             "id": "test_id",
             "detail-type": "Glue Job State Change",
             "source": "aws.glue",
             "account": "test_account",
-            "time": "2023-01-11T13:51:06Z",
+            "time": self.event_time,
             "region": "test-region-2",
             "resources": [],
             "detail": {
-                "jobName": "test job name",
+                "jobName": self.job_name,
                 "severity": "ERROR",
                 "state": "FAILED",
                 "jobRunId": "test_run_id123",
                 "message": "An error occurred while running the job.",
+                "attempt": 1,
             },
+        }
+
+        self.glue_client = self.boto_session.create_client(
+            "glue",
+            region_name="test-region-2",
+        )
+        self.glue_client_stubber = Stubber(self.glue_client)
+
+        self.get_job_response = {
+            "Job": {
+                "Name": self.job_name,
+                "Role": "test_role",
+                "CreatedOn": self.job_created_on,
+                "LastModifiedOn": self.job_last_modified_on,
+                "MaxRetries": 0,
+                "ExecutionProperty": {
+                    "MaxConcurrentRuns": 1
+                },
+                "Command": {
+                    "Name": "test_name",
+                    "ScriptLocation": "test_location",
+                },
+                "DefaultArguments": {
+                    "test_argument": "test_value",
+                },
+                "Connections": {
+                    "Connections": ["test_connection"],
+                },
+                "Timeout": 360,
+                "SecurityConfiguration": "test_security_configuration",
+                "WorkerType": "Standard",
+                "NumberOfWorkers": 10,
+                "GlueVersion": "test_glue_version",
+            }
         }
 
         self.secret_name = "test_secret_name"
@@ -64,18 +109,24 @@ class TestGlueAlarmsHandler(TestCase):
         )
         self.secretsmanager_client_stubber.activate()
 
+        self.glue_client_stubber.add_response("get_job", self.get_job_response)
+        self.glue_client_stubber.activate()
+
         lambda_handler(
-            self.cloudwatch_event, secretsManagerClient=self.secretsmanager_client
+            event=self.cloudwatch_event,
+            secretsManagerClient=self.secretsmanager_client,
+            glueClient=self.glue_client,
         )
 
         self.secretsmanager_client_stubber.assert_no_pending_responses()
 
     def test_format_message_returns_correct_message(self):
+        job_name_html_encoded = quote(self.job_name)
         expected_message = {
             "text": (
-                "2023-01-11T13:51:06Z \nGlue failure detected for job: *test job name*"
+                f"{self.event_time} \nGlue failure detected for job: *{self.job_name}*"
                 " \nrun:"
-                " https://test-region-2.console.aws.amazon.com/gluestudio/home?region=test-region-2#/job/test%20job%20name/run/test_run_id123"
+                f" https://test-region-2.console.aws.amazon.com/gluestudio/home?region=test-region-2#/job/{job_name_html_encoded}/run/test_run_id123"
                 " \nError message: An error occurred while running the job."
             )
         }
@@ -96,8 +147,13 @@ class TestGlueAlarmsHandler(TestCase):
         )
         self.secretsmanager_client_stubber.activate()
 
+        self.glue_client_stubber.add_response("get_job", self.get_job_response)
+        self.glue_client_stubber.activate()
+
         lambda_handler(
-            event=self.cloudwatch_event, secretsManagerClient=self.secretsmanager_client
+            event=self.cloudwatch_event,
+            secretsManagerClient=self.secretsmanager_client,
+            glueClient=self.glue_client,
         )
 
         mock_pool_manager.request.assert_called_once_with(
@@ -106,3 +162,50 @@ class TestGlueAlarmsHandler(TestCase):
             body=dumps(format_message(self.cloudwatch_event)),
             headers=expected_headers,
         )
+
+    @mock.patch.dict(os.environ, mock_env_vars)
+    @mock.patch("lambda_alarms_handler.main.urllib3.PoolManager", spec=True)
+    def test_does_not_call_poolmanager_on_urllib3_if_attempt_is_less_than_max_retries(
+        self, mock_urllib_poolmanager
+    ):
+        self.get_job_response["Job"]["MaxRetries"] = 3
+        self.glue_client_stubber.add_response("get_job", self.get_job_response)
+        self.glue_client_stubber.activate()
+
+        lambda_handler(
+            event=self.cloudwatch_event,
+            secretsManagerClient=self.secretsmanager_client,
+            glueClient=self.glue_client,
+        )
+
+        mock_urllib_poolmanager.assert_not_called()
+
+    @mock.patch.dict(os.environ, mock_env_vars)
+    @mock.patch("lambda_alarms_handler.main.urllib3.PoolManager", spec=True)
+    def test_does_not_call_poolmanager_on_urllib3_if_attempt_is_equal_to_max_retries(
+        self, mock_urllib_poolmanager
+    ):
+        self.get_job_response["Job"]["MaxRetries"] = 1
+        self.cloudwatch_event["detail"]["attempt"] = 1
+        self.glue_client_stubber.add_response("get_job", self.get_job_response)
+        self.glue_client_stubber.activate()
+
+        lambda_handler(
+            event=self.cloudwatch_event,
+            secretsManagerClient=self.secretsmanager_client,
+            glueClient=self.glue_client,
+        )
+
+        mock_urllib_poolmanager.assert_not_called()
+
+    def test_get_max_retries_returns_correct_max_retries(self):
+        expected_max_retries = 0
+
+        self.glue_client_stubber.add_response("get_job", self.get_job_response)
+        self.glue_client_stubber.activate()
+
+        actual_max_retries = get_max_retries(
+            self.job_name, self.glue_client
+        )
+
+        self.assertEqual(expected_max_retries, actual_max_retries)
