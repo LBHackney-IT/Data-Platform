e453221e356806bdec352babd6106b9c9b9ed1cf
AGibson <4319494+annajgibson@users.noreply.github.com>
2024-02-26 12:16:40 +0000

    Added in arn:aws:lambda:eu-west-2:120038763019:layer:urllib3-1-26-18-layer:1 to ensure correct urllib3 version used.

--- terraform/etl/48-lambda-gov-notify-ingestion.tf
+++ terraform/etl/48-lambda-gov-notify-ingestion.tf
@@ -147,8 +147,9 @@ module "gov-notify-ingestion-housing-repairs" {
     TARGET_S3_BUCKET_NAME = "housing/govnotify/damp_and_mould/"
   }
   layers = [
+    "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python39:13",
     "arn:aws:lambda:eu-west-2:120038763019:layer:notifications-python-client-9-0-0-layer:1",
-    "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python39:13"
+    "arn:aws:lambda:eu-west-2:120038763019:layer:urllib3-1-26-18-layer:1"
   ]
 }
 
