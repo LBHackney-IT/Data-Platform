7880d6652c909b1cbe6366ef409727672a2c709d
timburke-hackit <tim.burke@hackney.gov.uk>
2024-02-28 08:39:49 +0000

    add timeout variable to module

--- terraform/modules/glue-failure-alert-notifications/02-inputs-optional.tf
+++ terraform/modules/glue-failure-alert-notifications/02-inputs-optional.tf
@@ -2,3 +2,9 @@ variable "lambda_environment_variables" {
   description = "An object containing environment variables to be used in the Lambda"
   type        = map(string)
 }
+
+variable "timeout" {
+  description = "The amount of time your Lambda Function has to run in seconds"
+  type        = number
+  default     = 3
+}
