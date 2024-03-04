7812a822446733cfd5d75e9e36adfed91ab26490
timburke-hackit <tim.burke@hackney.gov.uk>
2024-03-04 19:24:11 +0000

    client injection

--- lambdas/team_times/main.py
+++ lambdas/team_times/main.py
@@ -9,9 +9,8 @@ s3_client = boto3.client("s3")
 secrets_manager_client = boto3.client("secretsmanager")
 
 
-def get_secret(secret_name):
-    session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager")
+def get_secret(secret_name, secrets_manager_client=None):
+    client = secrets_manager_client or boto3.client("secretsmanager")
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except Exception as e:
@@ -20,13 +19,15 @@ def get_secret(secret_name):
         return get_secret_value_response["SecretString"]
 
 
-def save_xml_to_s3(xml_data, bucket_name, object_name):
-    s3_client.put_object(Body=xml_data, Bucket=bucket_name, Key=object_name)
+def save_xml_to_s3(xml_data, bucket_name, object_name, s3_client=None):
+    client = s3_client or boto3.client("s3")
+    client.put_object(Body=xml_data, Bucket=bucket_name, Key=object_name)
     print(f"XML saved to s3://{bucket_name}/{object_name}")
 
 
-def save_csv_to_s3(csv_data, bucket_name, object_name):
-    s3_client.put_object(Body=csv_data.getvalue(), Bucket=bucket_name, Key=object_name)
+def save_csv_to_s3(csv_data, bucket_name, object_name, s3_client=None):
+    client = s3_client or boto3.client("s3")
+    client.put_object(Body=csv_data.getvalue(), Bucket=bucket_name, Key=object_name)
     print(f"CSV saved to s3://{bucket_name}/{object_name}")
 
 
@@ -63,10 +64,11 @@ def xml_to_csv(xml_data):
     return csv_data
 
 
-def make_api_request_and_process_data(api_endpoint, api_key, date, data_frequency):
+def make_api_request_and_process_data(api_endpoint, api_key, date, data_frequency, s3_client):
     data = {"apiKey": api_key, "D": date, "E": data_frequency}
     headers = {"Content-Type": "application/x-www-form-urlencoded"}
     response = requests.post(api_endpoint, data=data, headers=headers)
+    client = s3_client or boto3.client("s3")
 
     if response.status_code == 200:
         print("API request successful.")
@@ -79,10 +81,10 @@ def make_api_request_and_process_data(api_endpoint, api_key, date, data_frequenc
         xml_object_name = f"{xml_prefix}/timesheet_{timestamp}.xml"
         csv_object_name = f"{csv_prefix}/timesheet_{timestamp}.csv"
 
-        save_xml_to_s3(response.text, xml_bucket_name, xml_object_name)
+        save_xml_to_s3(response.text, xml_bucket_name, xml_object_name, client)
 
         csv_data = xml_to_csv(response.text)
-        save_csv_to_s3(csv_data, csv_bucket_name, csv_object_name)
+        save_csv_to_s3(csv_data, csv_bucket_name, csv_object_name, client)
     else:
         print("Error:", response.status_code, response.text)
 
