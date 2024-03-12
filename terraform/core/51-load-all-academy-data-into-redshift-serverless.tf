1b2579b8cbb776f98bf37172ecd0c5a79bf80fbf
TianChen17 <107412990+TianChen17@users.noreply.github.com>
2024-03-12 16:54:25 +0000

    use output variable to replace the hardcoded IAM role

--- terraform/core/51-load-all-academy-data-into-redshift-serverless.tf
+++ terraform/core/51-load-all-academy-data-into-redshift-serverless.tf
@@ -19,10 +19,10 @@ module "load_all_academy_data_into_redshift" {
   schedule                       = "cron(15 7 ? * MON-FRI *)"
   job_parameters = {
     "--environment"                      = var.environment
-    "--role_arn"                         = "arn:aws:iam::${var.aws_api_account_id}:role/dataplatform-${var.environment}-redshift-serverless-role",
+    "--role_arn"                         = module.redshift_serverless.redshift_serverless_role_arn
     "--enable-auto-scaling"              = "false"
     "--job-bookmark-option"              = "job-bookmark-disable"
-    "--base_s3_url"                      = "s3://dataplatform-${var.environment}-raw-zone/revenues/"
+    "--base_s3_url"                      = "${module.raw_zone.bucket_url}/revenues"
     "--conf"                             = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
   }
 }
