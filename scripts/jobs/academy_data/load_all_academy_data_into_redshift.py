fbf804df373e9d01e20e840db6d934f56e5a3ffc
Tian CHEN <107412990+TianChen17@users.noreply.github.com>
2024-03-15 17:17:28 +0000

    only shedule it in stg and remove import of rs_command (#1651)

--- scripts/jobs/academy_data/load_all_academy_data_into_redshift.py
+++ scripts/jobs/academy_data/load_all_academy_data_into_redshift.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from typing import List, Dict, Any, Optional
 import boto3
 from scripts.helpers.helpers import get_secret_dict, get_glue_env_var
-from scripts.helpers.redshift_helpers import rs_command
 import redshift_connector
 
 environment = get_glue_env_var("environment")
@@ -11,53 +10,56 @@ role_arn = get_glue_env_var("role_arn")
 base_s3_url = get_glue_env_var("base_s3_url")
 
 def rs_command(query: str, fetch_results: bool = False, allow_commit: bool = True, database_name: str = 'academy') -> Optional[List[Dict]]:
-   """Executes a SQL query against a Redshift database, optionally fetching results.
+    """Executes a SQL query against a Redshift database, optionally fetching results.
 
-   Args:
-       query (str): The SQL query to execute.
-       fetch_results (bool): Whether to fetch and return the query results (default False).
-       allow_commit (bool): Whether to allow committing the transaction (default True).
-       database_name: Name of the database to connect to, defaults to 'academy'.
+    Args:
+        query (str): The SQL query to execute.
+        fetch_results (bool): Whether to fetch and return the query results (default False).
+        allow_commit (bool): Whether to allow committing the transaction (default True).
+        database_name: Name of the database to connect to, defaults to 'academy'.
 
-   Returns:
-       Optional[List[Dict]]: A list of dictionaries representing rows returned by the query if fetch_results is True; otherwise None.
-   """
-   creds = get_secret_dict('/data-and-insight/redshift-serverless-connection', 'eu-west-2')
-   try:
-       # Connects to Redshift cluster using AWS credentials
-       conn = redshift_connector.connect(
-           host=creds['host'],
-           database=database_name,
-           user=creds['user'],
-           password=creds['password']
-       )
-       
-       # autocommit is off by default. 
-       if allow_commit:
-           # Add this line to handle commands like CREATE EXTERNAL TABLE
-           conn.autocommit = True
+    Returns:
+        Optional[List[Dict]]: A list of dictionaries representing rows returned by the query if fetch_results is True; otherwise None.
+    """
+    creds = get_secret_dict('/data-and-insight/redshift-serverless-connection', 'eu-west-2')
+    conn = None
+    cursor = None   
+    try:
+        # Connects to Redshift cluster using AWS credentials
+        conn = redshift_connector.connect(
+            host=creds['host'],
+            database=database_name,
+            user=creds['user'],
+            password=creds['password']
+        )
+        
+        # autocommit is off by default. 
+        if allow_commit:
+            # Add this line to handle commands like CREATE EXTERNAL TABLE
+            conn.autocommit = True
 
-       cursor = conn.cursor()
+        cursor = conn.cursor()
 
-       # Execute the query
-       cursor.execute(query)
-       
-       # Fetch the results if required
-       if fetch_results:
-           result = cursor.fetchall()
-           return [dict(row) for row in result] if result else []
-       elif allow_commit:
-           # Commit the transaction only if allowed and needed
-           conn.commit()
+        # Execute the query
+        cursor.execute(query)
+        
+        # Fetch the results if required
+        if fetch_results:
+            columns = [desc[0] for desc in cursor.description]  # Get column names
+            result = cursor.fetchall()
+            return [dict(zip(columns, row)) for row in result] if result else []
+        elif allow_commit:
+            # Commit the transaction only if allowed and needed
+            conn.commit()
 
-   except redshift_connector.Error as e:
-       raise e
-   finally:
-       if cursor:
-           cursor.close()
-       if conn:
-           conn.close()
-   return None  # Return None if fetch_results is False or if there's an error
+    except redshift_connector.Error as e:
+        raise e
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+    return None  # Return None if fetch_results is False or if there's an error
 
 def get_all_tables(glue_client: Any, database_name: str, pattern: str = '') -> List[Dict]:
     """Retrieve all table metadata from Glue catalog for a specific database."""
