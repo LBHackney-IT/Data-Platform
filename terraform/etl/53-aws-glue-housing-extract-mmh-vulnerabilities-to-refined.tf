9adcb37efc044fb7c1baa6a4f24783d27743fb32
AGibson <4319494+annajgibson@users.noreply.github.com>
2024-04-10 15:51:09 +0100

    Amended the crawler block in 53-aws-glue-housing-extract-mmh-vulnerabilities-to-refined.tf so that it is correctly referenced.

--- terraform/etl/53-aws-glue-housing-extract-mmh-vulnerabilities-to-refined.tf
+++ terraform/etl/53-aws-glue-housing-extract-mmh-vulnerabilities-to-refined.tf
@@ -48,13 +48,15 @@ resource "aws_glue_trigger" "housing_mtfh_case_notes_enriched_to_refined_trigger
 
 }
 
-crawler_details = {
-  database_name      = module.department_housing_data_source.refined_zone_catalog_database_name
-  name               = "${local.short_identifier_prefix}Housing MTFH case notes enrichment to refined"
-  role               = data.aws_iam_role.glue_role.arn
-  s3_target_location = "s3://${module.refined_zone_data_source.bucket_id}/housing/mtfh-case-notes-enriched/"
-  tags               = module.tags.values
-  configuration      = jsonencode({
+resource "aws_glue_crawler" "housing_mtfh_case_notes_enriched_to_refined" {
+  database_name = module.department_housing_data_source.refined_zone_catalog_database_name
+  name          = "${local.short_identifier_prefix}Housing MTFH case notes enrichment to refined"
+  role          = data.aws_iam_role.glue_role.arn
+  s3_target {
+    path = "s3://${module.refined_zone_data_source.bucket_id}/housing/mtfh-case-notes-enriched/"
+  }
+  tags          = module.tags.values
+  configuration = jsonencode({
     Version  = 1.0
     Grouping = {
       TableLevelConfiguration = 8
