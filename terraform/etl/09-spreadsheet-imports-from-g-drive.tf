124f72e319077a335e8ea9dd46ae1b820fffac55
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2024-04-11 14:20:56 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf
    Calendar update

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -712,11 +712,11 @@ module "calendar" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1uB2925sk2oRlxtF_Sjvr7MnCrkPiacHu"
+  google_drive_document_id       = "1TV6GBIYZCWn3xt7YLfQZi9JRGCVS4I23"
   glue_job_name                  = "calendar"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "unmatched_citypay_batches/Calendar Apr 202.csv"
+  input_file_name                = "unmatched_citypay_batches/Calendar Apr 2024.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
