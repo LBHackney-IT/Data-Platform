91666c94c6d2453909338fc15f56c0f851b16f46
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2024-04-23 20:09:41 +0100

    update bailiff return (#1706)
    updated for Bailiff Return

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -747,11 +747,11 @@ module "bailiff_return" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1Nxf2FVKPkAHXz5P80YY8ynwT8L6o-WXI"
+  google_drive_document_id       = "1GhzFODMREWl4mR8tYcLYBfHYJdZfSzKi"
   glue_job_name                  = "bailiff_return"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "bailiff_return/bailiff_return_03_01_2024.csv"
+  input_file_name                = "bailiff_return/bailiff_return_03_04_2024.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
