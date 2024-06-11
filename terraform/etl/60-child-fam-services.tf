f07a726c72a9e1f946cf340371d1659640baf779
timburke-hackit <tim.burke@hackney.gov.uk>
2024-06-11 12:58:34 +0100

    correct data source module name

--- terraform/etl/60-child-fam-services.tf
+++ terraform/etl/60-child-fam-services.tf
@@ -1,45 +1,45 @@
 # mosaic ETL resources
 
 resource "aws_glue_crawler" "mosaic_raw_zone" {
-    count = local.is_live_environment ? 1 : 0
-    name  = "${local.short_identifier_prefix}${module.department_child_and_family_services_data_source.identifier}mosaic-raw-zone"
-    role  = module.department_child_and_family_services_data_source.glue_role_arn
+  count = local.is_live_environment ? 1 : 0
+  name  = "${local.short_identifier_prefix}${module.department_children_family_services_data_source.identifier}mosaic-raw-zone"
+  role  = module.department_child_and_family_services_data_source.glue_role_arn
 
-    s3_target {
-        path = "s3://${module.raw_zone_data_source.bucket_id}/${module.department_child_and_family_services_data_source.identifier}/mosaic/"
+  s3_target {
+    path = "s3://${module.raw_zone_data_source.bucket_id}/${module.department_children_family_services_data_source.identifier}/mosaic/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 4
     }
-
-    configuration = jsonencode({
-        Version = 1.0
-        Grouping = {
-            TableLevelConfiguration = 4
-        }
-        CrawlerOutput = {
-            Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
-            Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
-        }
-    })
+    CrawlerOutput = {
+      Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+      Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
+    }
+  })
 
 }
 
 resource "aws_glue_crawler" "allocations_refined_tables" {
-    count = local.is_live_environment ? 1 : 0
-    name  = "${local.short_identifier_prefix}${module.department_child_and_family_services_data_source.identifier}allocations-refined-tables"
-    role  = module.department_child_and_family_services_data_source.glue_role_arn
+  count = local.is_live_environment ? 1 : 0
+  name  = "${local.short_identifier_prefix}${module.department_children_family_services_data_source.identifier}allocations-refined-tables"
+  role  = module.department_child_and_family_services_data_source.glue_role_arn
 
-    s3_target {
-        path = "s3://${module.refined_zone_data_source.bucket_id}/${module.department_child_and_family_services_data_source.identifier}/allocations/"
+  s3_target {
+    path = "s3://${module.refined_zone_data_source.bucket_id}/${module.department_children_family_services_data_source.identifier}/allocations/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 4
     }
-
-    configuration = jsonencode({
-        Version = 1.0
-        Grouping = {
-            TableLevelConfiguration = 4
-        }
-        CrawlerOutput = {
-            Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
-            Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
-        }
-    })
+    CrawlerOutput = {
+      Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+      Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
+    }
+  })
 
 }
