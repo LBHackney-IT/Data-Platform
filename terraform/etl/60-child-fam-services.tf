955e1417e2f57144eb22db8186ac23254af619c7
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2024-06-11 15:27:23 +0100

    add mosaic crawlers (#1796)
    * add poc crawlers
    
    * correct data source module name
    
    * add database_name parameters
    
    * correct role reference
    
    * add missing delimiters from crawler names

---
+++ terraform/etl/60-child-fam-services.tf
@@ -0,0 +1,47 @@
+# mosaic ETL resources
+
+resource "aws_glue_crawler" "mosaic_raw_zone" {
+  count         = local.is_live_environment ? 1 : 0
+  name          = "${local.short_identifier_prefix}${module.department_children_family_services_data_source.identifier}-mosaic-raw-zone"
+  role          = module.department_children_family_services_data_source.glue_role_arn
+  database_name = module.department_children_family_services_data_source.raw_zone_catalog_database_name
+
+  s3_target {
+    path = "s3://${module.raw_zone_data_source.bucket_id}/${module.department_children_family_services_data_source.identifier}/mosaic/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 4
+    }
+    CrawlerOutput = {
+      Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+      Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
+    }
+  })
+
+}
+
+resource "aws_glue_crawler" "allocations_refined_tables" {
+  count         = local.is_live_environment ? 1 : 0
+  name          = "${local.short_identifier_prefix}${module.department_children_family_services_data_source.identifier}-allocations-refined-tables"
+  role          = module.department_children_family_services_data_source.glue_role_arn
+  database_name = module.department_children_family_services_data_source.refined_zone_catalog_database_name
+
+  s3_target {
+    path = "s3://${module.refined_zone_data_source.bucket_id}/${module.department_children_family_services_data_source.identifier}/allocations/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 4
+    }
+    CrawlerOutput = {
+      Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+      Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
+    }
+  })
+
+}
