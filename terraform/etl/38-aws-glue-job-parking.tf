d85e28077de00342c9e37a40a13030130525444c
Davina Clarke <87067339+daclarkelbh@users.noreply.github.com>
2024-07-10 18:20:19 +0100

    Update 38-aws-glue-job-parking.tf
    Added module parking_vouchers_approved_summary_gds_lbh.py at line 1610

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -1607,3 +1607,24 @@ module "parking_nas_live_manual_updates_with_pcns_box23" {
     "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
   }
 }
+module "parking_vouchers_approved_summary_gds_lbh" {
+  source                         = "../modules/aws-glue-job"
+  is_live_environment            = local.is_live_environment
+  is_production_environment      = local.is_production_environment
+  department                     = module.department_parking_data_source
+  job_name                       = "${local.short_identifier_prefix}parking_vouchers_approved_summary_gds_lbh"
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  script_name                    = "parking_vouchers_approved_summary_gds_lbh"
+  triggered_by_job               = module.parking_voucher_de_normalised.job_name
+  job_description                = "Summary of denormalised voucher approved by FY, Month year for LBH, for use in GDS"
+  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
+  number_of_workers_for_glue_job = 10
+  glue_job_worker_type           = "G.1X"
+  glue_version                   = "4.0"
+  job_parameters = {
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--environment"         = var.environment
+  }
+}
