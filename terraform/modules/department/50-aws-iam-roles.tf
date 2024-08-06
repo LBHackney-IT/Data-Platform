c6d1c53a25eff99dfd23dec180f62c0936196696
timburke-hackit <tim.burke@hackney.gov.uk>
2024-08-06 09:37:35 +0100

    add department catalog access policy

--- terraform/modules/department/50-aws-iam-roles.tf
+++ terraform/modules/department/50-aws-iam-roles.tf
@@ -5,13 +5,15 @@ data "aws_iam_policy_document" "sso_staging_user_policy" {
     data.aws_iam_policy_document.glue_access.json,
     data.aws_iam_policy_document.secrets_manager_read_only.json,
     data.aws_iam_policy_document.redshift_department_read_access.json,
-    data.aws_iam_policy_document.notebook_access[0].json
+    data.aws_iam_policy_document.notebook_access[0].json,
+    data.aws_iam_policy_document.department_catalog_access.json
     ] : [
     data.aws_iam_policy_document.s3_department_access.json,
     data.aws_iam_policy_document.glue_access.json,
     data.aws_iam_policy_document.secrets_manager_read_only.json,
     data.aws_iam_policy_document.redshift_department_read_access.json,
-    data.aws_iam_policy_document.mwaa_department_web_server_access.json
+    data.aws_iam_policy_document.mwaa_department_web_server_access.json,
+    data.aws_iam_policy_document.department_catalog_access.json
   ]
 }
 
