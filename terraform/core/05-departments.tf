4ec2cbd33c2e4c1b9445042067ccf30d0d62095e
Tian-2017 <tian.chen@hackney.gov.uk>
2024-08-13 08:11:21 +0100

    create a parking airflow user

--- terraform/core/05-departments.tf
+++ terraform/core/05-departments.tf
@@ -68,6 +68,7 @@ module "department_parking" {
   identity_store_id               = local.identity_store_id
   google_group_admin_display_name = local.google_group_admin_display_name
   google_group_display_name       = "saml-aws-data-platform-collaborator-parking@hackney.gov.uk"
+  departmental_airflow_user       = true
 }
 
 module "department_finance" {
