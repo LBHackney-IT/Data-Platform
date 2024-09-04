221cc126694204702d83ddf13930e0d5ebeccdda
Davina Clarke <87067339+daclarkelbh@users.noreply.github.com>
2024-09-04 18:27:23 +0100

    Update 38-aws-glue-job-parking.tf

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -1652,3 +1652,25 @@ module "parking_pcn_dvla_response_no_address" {
     "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
   }
 }
+module "parking_motorcycle_permits_480" {
+  source                         = "../modules/aws-glue-job"
+  is_live_environment            = local.is_live_environment
+  is_production_environment      = local.is_production_environment
+  department                     = module.department_parking_data_source
+  job_name                       = "${local.short_identifier_prefix}parking_motorcycle_permits_480"
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  script_name                    = "parking_motorcycle_permits_480"
+  trigger_enabled                = local.is_production_environment
+  triggered_by_job               = module.parking_permit_denormalised_gds_street_llpg.job_name
+  job_description                = "All Permits data in latest import_date matched to all is_motorcycle like 'Y' in the VRM_480 and VRM_update_480 tables"
+  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
+  number_of_workers_for_glue_job = 10
+  glue_job_worker_type           = "G.1X"
+  glue_version                   = "4.0"
+  job_parameters = {
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--environment"         = var.environment
+  }
+}
