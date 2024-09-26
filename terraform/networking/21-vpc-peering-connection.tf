b0d099a40182b8aa71bde6cc21381e86fc903b02
husain_asania <111893387+huasania@users.noreply.github.com>
2024-09-26 13:47:01 +0100

    chore:adding terraform profile

--- terraform/networking/21-vpc-peering-connection.tf
+++ terraform/networking/21-vpc-peering-connection.tf
@@ -16,11 +16,12 @@ module "api_vpc_peering_cross_account" {
   requester_region                          = var.aws_deploy_region
   requester_vpc_id                          = module.core_vpc.vpc_id
   requester_allow_remote_vpc_dns_resolution = "true"
-
-  accepter_aws_assume_role_arn             = local.aws_api_accepter_assume_role_arn
-  accepter_region                          = var.aws_deploy_region
-  accepter_vpc_id                          = var.aws_api_vpc_id
-  accepter_allow_remote_vpc_dns_resolution = "false"
+  accepter_aws_profile                      = "terraform"
+  accepter_aws_assume_role_arn              = local.aws_api_accepter_assume_role_arn
+  accepter_region                           = var.aws_deploy_region
+  accepter_vpc_id                           = var.aws_api_vpc_id
+  accepter_allow_remote_vpc_dns_resolution  = "false"
+  requester_aws_profile                     = "terraform"
 }
 
 module "dp_stg_prod_vpc_peering_cross_account" {
