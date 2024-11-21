e7309b7c6e825426e5d4f031b872bb6b6474174e
Davina Clarke <87067339+daclarkelbh@users.noreply.github.com>
2024-11-21 12:47:04 +0000

    Update 08-google-sheets-imports.tf
    at line 909 added module "abandoned_vehicles_investigation_log_data_load"

--- terraform/etl/08-google-sheets-imports.tf
+++ terraform/etl/08-google-sheets-imports.tf
@@ -905,3 +905,25 @@ module "permits_consultation_survey_data_load" {
   google_sheet_import_schedule    = "cron(0 13 ? * * *)"
   spark_ui_output_storage_id      = module.spark_ui_output_storage_data_source.bucket_id
 }
+
+module "abandoned_vehicles_investigation_log_data_load" {
+  count                           = local.is_live_environment ? 1 : 0
+  source                          = "../modules/google-sheets-glue-job"
+  is_production_environment       = local.is_production_environment
+  identifier_prefix               = local.short_identifier_prefix
+  is_live_environment             = local.is_live_environment
+  glue_scripts_bucket_id          = module.glue_scripts_data_source.bucket_id
+  helper_module_key               = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                 = data.aws_s3_object.pydeequ.key
+  glue_catalog_database_name      = module.department_parking_data_source.raw_zone_catalog_database_name
+  glue_temp_storage_bucket_url    = module.glue_temp_storage_data_source.bucket_url
+  glue_crawler_excluded_blobs     = local.glue_crawler_excluded_blobs
+  google_sheets_import_script_key = aws_s3_object.google_sheets_import_script.key
+  bucket_id                       = module.raw_zone_data_source.bucket_id
+  google_sheets_document_id       = "1hQpFGnDW1bbs66sLiQuNe6N_KLTG78_wMBYrHKMJ5_U"
+  google_sheets_worksheet_name    = "data_import_sheet"
+  department                      = module.department_parking_data_source
+  dataset_name                    = "abandoned_vehicles_investigation_log_data_load"
+  google_sheet_import_schedule    = "cron(0 4 ? * * *)"
+  spark_ui_output_storage_id      = module.spark_ui_output_storage_data_source.bucket_id
+}
