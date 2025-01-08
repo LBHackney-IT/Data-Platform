cab5ec0685f76f1cb34646123e58d2b0166fa9c7
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2025-01-08 11:51:09 +0000

    Update 38-aws-glue-job-parking.tf
    add Parking_interim_cycle_hangar_waiting_list code

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -1752,3 +1752,27 @@ module "parking_cycle_hangar_interim_wait_list" {
     "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
   }
 }
+# MRB 08-11-2024 job created
+module "Parking_interim_cycle_hangar_waiting_list" {
+  source                         = "../modules/aws-glue-job"
+  is_live_environment            = local.is_live_environment
+  is_production_environment      = local.is_production_environment
+  department                     = module.department_parking_data_source
+  job_name                       = "${local.short_identifier_prefix}Parking_interim_cycle_hangar_waiting_list"
+  helper_module_key              = data.aws_s3_object.helpers.key
+  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
+  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
+  script_name                    = "Parking_interim_cycle_hangar_waiting_list"
+  triggered_by_job               = "${local.short_identifier_prefix}Copy parking Liberator landing zone to raw"
+  job_description                = "format the interim cycle hangar waiting list from Michael W. to remove the unwanted waiting list items"
+  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
+  trigger_enabled                = local.is_production_environment
+  number_of_workers_for_glue_job = 2 # 2 minimum which is enough for this job
+  glue_job_worker_type           = "G.1X"
+  glue_version                   = "4.0"
+  job_parameters = {
+    "--job-bookmark-option" = "job-bookmark-disable"
+    "--environment"         = var.environment
+    "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
+  }
+}
