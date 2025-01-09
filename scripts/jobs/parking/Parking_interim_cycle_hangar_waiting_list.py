2e56cba9f7fcf3113e9e06ff13f03e383b7c246a
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-01-09 15:57:34 +0000

    roll back the table with parking prefix

--- scripts/jobs/parking/Parking_interim_cycle_hangar_waiting_list.py
+++ scripts/jobs/parking/Parking_interim_cycle_hangar_waiting_list.py
@@ -18,45 +18,45 @@ query_on_athena = """
 Parking_interim_cycle_hangar_waiting_list
 
 Process the interim cycle hangar waiting list (supplied by Michael W) to add
-additional fields (telephone Number, etc). 
+additional fields (telephone Number, etc).
 
 23/12/2024 - Create SQL
 *********************************************************************************/
 With Interim_Wait as (
-    SELECT 
-        distinct forename	
-        ,surname	
-        ,email	
-        ,party_id_to	
-        ,party_id	
-        ,cast(uprn as decimal) as uprn	
-        ,address1	
-        ,address2	
-        ,post_code	
-        ,x	
-        ,y	
-        ,lat	
+    SELECT
+        distinct forename
+        ,surname
+        ,email
+        ,party_id_to
+        ,party_id
+        ,cast(uprn as decimal) as uprn
+        ,address1
+        ,address2
+        ,post_code
+        ,x
+        ,y
+        ,lat
         ,long
-    FROM "parking-raw-zone".interim_cycle_wait_list 
-    WHERE import_date = (select max(import_date) 
+    FROM "parking-raw-zone".interim_cycle_wait_list
+    WHERE import_date = (select max(import_date)
                     from "parking-raw-zone".interim_cycle_wait_list)),
-                    
+
 /*** Obtain the llpg data ***/
 FULL_LLPG as (
     SELECT * From "parking-refined-zone".spatially_enriched_liberator_permit_llpg
-    WHERE import_date = (select max(import_date) 
+    WHERE import_date = (select max(import_date)
                     from "parking-refined-zone".spatially_enriched_liberator_permit_llpg)
     AND address1 not like '%STREET RECORD%'),
 
 STREET_LLPG as (
     SELECT * From "parking-refined-zone".spatially_enriched_liberator_permit_llpg
-    WHERE import_date = (select max(import_date) 
+    WHERE import_date = (select max(import_date)
                     from "parking-refined-zone".spatially_enriched_liberator_permit_llpg)
     AND address1 like '%STREET RECORD%'),
 
 /*** Obtain the Party details, where available ***/
 Party as (
-    SELECT 
+    SELECT
         *
     From "dataplatform-stg-liberator-raw-zone".liberator_licence_party
     WHERE import_Date = format_datetime(current_date, 'yyyyMMdd')),
@@ -65,11 +65,11 @@ Party as (
 unsubscribed_emails as (
     SELECT *,
         ROW_NUMBER() OVER ( PARTITION BY email_address ORDER BY email_address DESC) row1
-    FROM "parking-raw-zone".parking_cycle_hangar_unsubscribed_emails 
-    WHERE import_date = (select max(import_date) 
-                    from "parking-raw-zone".parking_cycle_hangar_unsubscribed_emails))    
-    
-SELECT 
+    FROM "parking-raw-zone".parking_parking_cycle_hangar_unsubscribed_emails
+    WHERE import_date = (select max(import_date)
+                    from "parking-raw-zone".parking_parking_cycle_hangar_unsubscribed_emails))
+
+SELECT
   A.*, cast(D.telephone_number as varchar) as Telephone_Number,  C.address2 as Street, B.housing_estate, E.email_address,
 
   format_datetime(CAST(CURRENT_TIMESTAMP AS timestamp),
