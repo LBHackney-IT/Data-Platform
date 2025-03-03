86a720783d99032e2642c0709c2c9a4c9f93fcfe
timburke-hackit <tim.burke@hackney.gov.uk>
2025-03-03 09:29:10 +0000

    add tags

--- terraform/etl/60-airflow-variables-and-connnections.tf
+++ terraform/etl/60-airflow-variables-and-connnections.tf
@@ -3,6 +3,7 @@
 resource "aws_secretsmanager_secret" "alloy_api_key" {
   name        = "/airflow/variables/alloy_api_key"
   description = "API key for accessing the Alloy service"
+  tags        = module.tags.values
 }
 
 resource "aws_secretsmanager_secret_version" "alloy_api_key" {
@@ -14,9 +15,11 @@ resource "aws_secretsmanager_secret_version" "alloy_api_key" {
     ignore_changes = [secret_string]
   }
 }
+
 resource "aws_secretsmanager_secret" "alloy_connection" {
   name        = "airflow/connections/alloy_api_connection"
   description = "Connection for the Alloy API"
+  tags        = module.tags.values
 }
 
 
