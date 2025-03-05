c05d5068f4dcc4ae18c95888a0581f65bcf62443
timburke-hackit <tim.burke@hackney.gov.uk>
2025-03-05 11:49:41 +0000

    add airflow alert mapping variable

--- terraform/etl/60-airflow-variables-and-connnections.tf
+++ terraform/etl/60-airflow-variables-and-connnections.tf
@@ -1,3 +1,25 @@
+### Airflow Alerts
+
+resource "aws_secretsmanager_secret" "google_chat_webhook_mapping" {
+  name        = "airflow/variables/google_chat_webhook_mapping"
+  description = "Mapping of department DAG tags to Google Spaces webhooks for failure alerts."
+  tags        = module.tags.values
+}
+
+resource "aws_secretsmanager_secret_version" "google_chat_webhook_mapping" {
+  secret_id = aws_secretsmanager_secret.google_chat_webhook_mapping.id
+  secret_string = jsonencode({
+    lower_department_name = "UPDATE_WITH_WEBHOOK_IN_CONSOLE"
+  })
+
+
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
+
+
+
 ### Alloy
 
 resource "aws_secretsmanager_secret" "alloy_api_key" {
