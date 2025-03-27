b77d40bd590be87114f47a694619469eab8cf6c6
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-03-27 17:02:02 +0000

    create a crawler for the ingested synergy data (#2195)
    * create a crawler for the ingested synergy data
    
    * update the resource name
    
    * add tags

--- terraform/etl/60-airflow-etl-used-crawlers.tf
+++ terraform/etl/60-airflow-etl-used-crawlers.tf
@@ -20,7 +20,7 @@ resource "aws_glue_crawler" "mosaic_raw_zone" {
       Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
     }
   })
-
+  tags = module.tags.values
 }
 
 resource "aws_glue_crawler" "allocations_refined_tables" {
@@ -43,9 +43,31 @@ resource "aws_glue_crawler" "allocations_refined_tables" {
       Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
     }
   })
-
+  tags = module.tags.values
 }
 
+resource "aws_glue_crawler" "synergy_raw_zone" {
+  count         = local.is_live_environment ? 1 : 0
+  name          = "${local.short_identifier_prefix}${module.department_children_family_services_data_source.identifier}-synergy-raw-zone"
+  role          = module.department_children_family_services_data_source.glue_role_arn
+  database_name = module.department_children_family_services_data_source.raw_zone_catalog_database_name
+
+  s3_target {
+    path = "s3://${module.raw_zone_data_source.bucket_id}/${module.department_children_family_services_data_source.identifier}/synergy/"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0
+    Grouping = {
+      TableLevelConfiguration = 4
+    }
+    CrawlerOutput = {
+      Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+      Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
+    }
+  })
+  tags = module.tags.values
+}
 
 resource "aws_glue_crawler" "streetscene_street_systems_raw_zone" {
   count         = local.is_live_environment ? 1 : 0
@@ -67,6 +89,7 @@ resource "aws_glue_crawler" "streetscene_street_systems_raw_zone" {
       Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
     }
   })
+  tags = module.tags.values
 }
 
 resource "aws_glue_crawler" "parking_spatially_enriched_refined_zone" {
@@ -89,6 +112,7 @@ resource "aws_glue_crawler" "parking_spatially_enriched_refined_zone" {
       Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
     }
   })
+  tags = module.tags.values
 }
 
 locals {
@@ -125,4 +149,5 @@ resource "aws_glue_crawler" "google_sheet_ingestion_raw_zone" {
       Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
     }
   })
+  tags = module.tags.values
 }
