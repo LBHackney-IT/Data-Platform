7eca0c06b850cc656e34ae32578b901c83168faa
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-04-10 14:30:36 +0100

    change buffer days to 60 days - tascomi_create_daily_snapshot
    change buffer days to 60 days - tascomi_create_daily_snapshot

--- scripts/jobs/planning/tascomi_create_daily_snapshot.py
+++ scripts/jobs/planning/tascomi_create_daily_snapshot.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
             # snapshot table in glue catalogue
             else:
                 pushDownPredicate = create_pushdown_predicate(
-                    partitionDateColumn="snapshot_date", daysBuffer=30
+                    partitionDateColumn="snapshot_date", daysBuffer=60
                 )
                 #   load latest snpashot
                 snapshot_ddf = glueContext.create_dynamic_frame.from_catalog(
