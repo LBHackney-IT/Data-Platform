382e0f5bd44fb508c0e7b9681b8d040f848e5242
AGibson <4319494+annajgibson@users.noreply.github.com>
2025-04-15 17:34:06 +0100

    Upgrade Great Expectations package to latest version

--- terraform/etl/54-aws-glue-housing-apply-gx-dq-tests.tf
+++ terraform/etl/54-aws-glue-housing-apply-gx-dq-tests.tf
@@ -55,7 +55,7 @@ module "housing_gx_dq_metadata" {
     "--job-bookmark-option"              = "job-bookmark-enable"
     "--enable-glue-datacatalog"          = "true"
     "--enable-continuous-cloudwatch-log" = "true"
-    "--additional-python-modules"        = "great_expectations==1.2.1,numpy==1.26.1,awswrangler==3.10.0"
+    "--additional-python-modules"        = "great_expectations==1.4.0,numpy==1.26.1,awswrangler==3.10.0"
     "--region_name"                      = data.aws_region.current.name
     "--s3_endpoint"                      = "https://s3.${data.aws_region.current.name}.amazonaws.com"
     "--s3_target_location"               = "s3://${module.raw_zone_data_source.bucket_id}/housing/data-quality-test-metadata/"
