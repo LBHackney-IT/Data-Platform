6a57c3f97f2c618f78161cc12f442b1e36964022
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-05-21 09:25:47 +0100

    remove parking_permit_street_cpz_stress_mc

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -491,30 +491,8 @@ module "parking_pcn_dvla_response_no_address" {
 }
 
 # Migrated job "parking_motorcycle_permits_480" to dap-airflow om 20/02/2025
+# Migrated job "parking_permit_street_cpz_stress_mc" to dap-airflow on 21/05/2025
 
-module "parking_permit_street_cpz_stress_mc" {
-  source                         = "../modules/aws-glue-job"
-  is_live_environment            = local.is_live_environment
-  is_production_environment      = local.is_production_environment
-  department                     = module.department_parking_data_source
-  job_name                       = "${local.short_identifier_prefix}parking_permit_street_cpz_stress_mc"
-  helper_module_key              = data.aws_s3_object.helpers.key
-  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
-  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
-  script_name                    = "parking_permit_street_cpz_stress_mc"
-  triggered_by_job               = module.parking_permit_de_normalisation.job_name
-  job_description                = "A new way for Mike to get the parking_permit_street_cpz_stress_mc data"
-  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
-  trigger_enabled                = local.is_production_environment
-  number_of_workers_for_glue_job = 2 # 2 minimum which is enough for this job
-  glue_job_worker_type           = "G.1X"
-  glue_version                   = "4.0"
-  job_parameters = {
-    "--job-bookmark-option" = "job-bookmark-disable"
-    "--environment"         = var.environment
-    "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
-  }
-}
 
 # MRB 18-08-2024 job created
 # Migrated job "parking_permit_denormalisation_mc" to dap-airflow om 01/05/2025
