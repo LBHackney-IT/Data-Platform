7b965d0e8a54e2c8f9b27cae80e37f2261a4e36a
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-05-23 16:51:12 +0100

    resumit the PR - parking group 10 part 3 (#2262)
    * parking group 10 – Part 3
    
    * correct the file
    
    * add missing comments

--- scripts/jobs/parking/parking_open_pcns_vrms_linked_cancelled_ringer.py
+++ scripts/jobs/parking/parking_open_pcns_vrms_linked_cancelled_ringer.py
@@ -1,101 +1,138 @@
-import sys
+"""
+Only need to change the table name and the query prototyped on the Athena UI
+by replacing table_name and query_on_athena
+Note: python file name should be the same as the table name
+"""
 
-from awsglue import DynamicFrame
-from awsglue.context import GlueContext
-from awsglue.job import Job
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
+from scripts.helpers.athena_helpers import create_update_table_with_partition
+from scripts.helpers.helpers import get_glue_env_var
 
-from scripts.helpers.helpers import (
-    PARTITION_KEYS,
-    get_glue_env_var,
-    get_latest_partitions,
-)
-
-
-def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
-    for alias, frame in mapping.items():
-        frame.toDF().createOrReplaceTempView(alias)
-    result = spark.sql(query)
-    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
-
-
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
 environment = get_glue_env_var("environment")
 
-# Script generated for node Amazon S3 - Refined - pcnfoidetails_pcn_foi_full
-AmazonS3Refinedpcnfoidetails_pcn_foi_full_node1708021619806 = glueContext.create_dynamic_frame.from_catalog(
-    database="dataplatform-" + environment + "-liberator-refined-zone",
-    push_down_predicate="to_date(import_date, 'yyyyMMdd') >= date_sub(current_date, 7)",
-    table_name="pcnfoidetails_pcn_foi_full",
-    transformation_ctx="AmazonS3Refinedpcnfoidetails_pcn_foi_full_node1708021619806",
-)
+# The target table in liberator refined zone
+table_name = "parking_open_pcns_vrms_linked_cancelled_ringer"
 
-# Script generated for node SQL Query
-SqlQuery0 = """
+# The exact same query prototyped in pre-prod(stg) or prod Athena
+query_on_athena = """
 /*Open PCNs linked to VRMs cancelled due to being a Ringer or Clone
 Created: 15/02/2024
 11/03/2024 - added extracted post codes and bailiff fields to output
 
 */
 With cancelled_vrm as (
-Select distinct vrm as canx_vrm
-from pcnfoidetails_pcn_foi_full where import_date = (select max(import_date) from pcnfoidetails_pcn_foi_full) and (upper(cancellationgroup) like '%RINGER%' or upper(cancellationreason) like '%RINGER%')
+    Select distinct vrm as canx_vrm
+    from "dataplatform-prod-liberator-refined-zone".pcnfoidetails_pcn_foi_full
+    where import_date = (
+            select max(import_date)
+            from "dataplatform-prod-liberator-refined-zone".pcnfoidetails_pcn_foi_full
+        )
+        and (
+            upper(cancellationgroup) like '%RINGER%'
+            or upper(cancellationreason) like '%RINGER%'
+        )
 )
-Select cancelled_vrm.*, concat(substr(Cast(pcnissuedate as varchar(10)),1, 7), '-01') as MonthYear
-,progressionstage ,debttype ,pcn ,pcnissuedate ,pcnissuedatetime ,street_location ,whereonlocation ,zone ,usrn ,contraventioncode ,contraventionsuffix  ,vrm ,vehiclemake ,vehiclemodel ,vehiclecolour,corresp_dispute_flag ,registered_keeper_address ,current_ticket_address
-
-/*Registered extracted post codes*/
-,case when length(regexp_extract(registered_keeper_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') ) = 0 or regexp_extract(registered_keeper_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') is null or  regexp_extract(registered_keeper_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') like '' or  regexp_extract(registered_keeper_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') like ' '  then 'No Address' else regexp_extract(registered_keeper_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})')  end
-as reg_add_extracted_post_code
-
-/*Current extracted post codes*/
-,case when length(regexp_extract(current_ticket_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') ) = 0 or regexp_extract(current_ticket_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') is null or regexp_extract(current_ticket_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') like '' or regexp_extract(current_ticket_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') like ' ' then 'No Address' else regexp_extract(current_ticket_address, '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})') end
-as curr_add_extracted_post_code
-,bailiff
-
-/*for partiion*/
-,date_format(current_date, 'yyyy') AS import_year
-,date_format(current_date, 'MM') AS import_month
-,date_format(current_date, 'dd') AS import_day
-,date_format(current_date, 'yyyyMMdd') AS import_date
-
-from pcnfoidetails_pcn_foi_full
-left join cancelled_vrm on cancelled_vrm.canx_vrm = pcnfoidetails_pcn_foi_full.vrm
-
-where pcnfoidetails_pcn_foi_full.import_date = (select max(pcnfoidetails_pcn_foi_full.import_date) from pcnfoidetails_pcn_foi_full) and (pcnfoidetails_pcn_foi_full.pcn_canx_date is null and pcnfoidetails_pcn_foi_full.pcn_casecloseddate is null) and cancelled_vrm.canx_vrm is not null and pcnfoidetails_pcn_foi_full.warningflag = 0
-order by cancelled_vrm.canx_vrm ,progressionstage ,debttype ,pcnissuedatetime desc ,pcn ,street_location
+Select cancelled_vrm.*,
+    concat(
+        substr(Cast(pcnissuedate as varchar(10)), 1, 7),
+        '-01'
+    ) as MonthYear,
+    progressionstage,
+    debttype,
+    pcn,
+    pcnissuedate,
+    pcnissuedatetime,
+    street_location,
+    whereonlocation,
+    zone,
+    usrn,
+    contraventioncode,
+    contraventionsuffix,
+    vrm,
+    vehiclemake,
+    vehiclemodel,
+    vehiclecolour,
+    corresp_dispute_flag,
+    registered_keeper_address,
+    current_ticket_address
+    /*Registered extracted post codes*/
+,
+    case
+        when length(
+            regexp_extract(
+                registered_keeper_address,
+                '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+            )
+        ) = 0
+        or regexp_extract(
+            registered_keeper_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        ) is null
+        or regexp_extract(
+            registered_keeper_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        ) like ''
+        or regexp_extract(
+            registered_keeper_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        ) like ' ' then 'No Address'
+        else regexp_extract(
+            registered_keeper_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        )
+    end as reg_add_extracted_post_code
+    /*Current extracted post codes*/
+,
+    case
+        when length(
+            regexp_extract(
+                current_ticket_address,
+                '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+            )
+        ) = 0
+        or regexp_extract(
+            current_ticket_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        ) is null
+        or regexp_extract(
+            current_ticket_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        ) like ''
+        or regexp_extract(
+            current_ticket_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        ) like ' ' then 'No Address'
+        else regexp_extract(
+            current_ticket_address,
+            '([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})'
+        )
+    end as curr_add_extracted_post_code,
+    bailiff
+    /*for partiion*/
+,
+    format_datetime(current_date, 'yyyy') AS import_year,
+    format_datetime(current_date, 'MM') AS import_month,
+    format_datetime(current_date, 'dd') AS import_day,
+    format_datetime(current_date, 'yyyyMMdd') AS import_date
+from "dataplatform-prod-liberator-refined-zone".pcnfoidetails_pcn_foi_full
+    left join cancelled_vrm on cancelled_vrm.canx_vrm = pcnfoidetails_pcn_foi_full.vrm
+where pcnfoidetails_pcn_foi_full.import_date = (
+        select max(pcnfoidetails_pcn_foi_full.import_date)
+        from "dataplatform-prod-liberator-refined-zone".pcnfoidetails_pcn_foi_full
+    )
+    and (
+        pcnfoidetails_pcn_foi_full.pcn_canx_date is null
+        and pcnfoidetails_pcn_foi_full.pcn_casecloseddate is null
+    )
+    and cancelled_vrm.canx_vrm is not null
+    and pcnfoidetails_pcn_foi_full.warningflag = 0
+order by cancelled_vrm.canx_vrm,
+    progressionstage,
+    debttype,
+    pcnissuedatetime desc,
+    pcn,
+    street_location
 """
-SQLQuery_node1708021630710 = sparkSqlQuery(
-    glueContext,
-    query=SqlQuery0,
-    mapping={
-        "pcnfoidetails_pcn_foi_full": AmazonS3Refinedpcnfoidetails_pcn_foi_full_node1708021619806
-    },
-    transformation_ctx="SQLQuery_node1708021630710",
-)
 
-# Script generated for node Amazon S3
-AmazonS3_node1708021659577 = glueContext.getSink(
-    path="s3://dataplatform-"
-    + environment
-    + "-refined-zone/parking/liberator/parking_open_pcns_vrms_linked_cancelled_ringer/",
-    connection_type="s3",
-    updateBehavior="UPDATE_IN_DATABASE",
-    partitionKeys=["import_year", "import_month", "import_day", "import_date"],
-    enableUpdateCatalog=True,
-    transformation_ctx="AmazonS3_node1708021659577",
+create_update_table_with_partition(
+    environment=environment, query_on_athena=query_on_athena, table_name=table_name
 )
-AmazonS3_node1708021659577.setCatalogInfo(
-    catalogDatabase="dataplatform-" + environment + "-liberator-refined-zone",
-    catalogTableName="parking_open_pcns_vrms_linked_cancelled_ringer",
-)
-AmazonS3_node1708021659577.setFormat("glueparquet", compression="snappy")
-AmazonS3_node1708021659577.writeFrame(SQLQuery_node1708021630710)
-job.commit()
--- scripts/jobs/parking/parking_pcn_ltn_report_summary.py
+++ scripts/jobs/parking/parking_pcn_ltn_report_summary.py
@@ -1,108 +1,87 @@
-import sys
+"""
+Only need to change the table name and the query prototyped on the Athena UI
+by replacing table_name and query_on_athena
+Note: python file name should be the same as the table name
+"""
 
-from awsglue import DynamicFrame
-from awsglue.context import GlueContext
-from awsglue.job import Job
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-
+from scripts.helpers.athena_helpers import create_update_table_with_partition
 from scripts.helpers.helpers import get_glue_env_var
 
 environment = get_glue_env_var("environment")
 
+# The target table in liberator refined zone
+table_name = "Parking_PCN_LTN_Report_Summary"
 
-def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
-    for alias, frame in mapping.items():
-        frame.toDF().createOrReplaceTempView(alias)
-    result = spark.sql(query)
-    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
-
-
-args = getResolvedOptions(sys.argv, ["JOB_NAME"])
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
-job.init(args["JOB_NAME"], args)
-
-# Script generated for node Amazon S3
-AmazonS3_node1625732651466 = glueContext.create_dynamic_frame.from_catalog(
-    database="dataplatform-" + environment + "-liberator-refined-zone",
-    table_name="pcnfoidetails_pcn_foi_full",
-    transformation_ctx="AmazonS3_node1625732651466",
-)
-
-# Script generated for node ApplyMapping
-SqlQuery0 = """
+# The exact same query prototyped in pre-prod(stg) or prod Athena
+query_on_athena = """
 /*************************************************************************************************************************
-Parking_PCN_LTN_Report_Summary
+ Parking_PCN_LTN_Report_Summary
 
-The SQL builds the PCN Low Traffic Network figures, for use with the answering of the LTN FOI's
+ The SQL builds the PCN Low Traffic Network figures, for use with the answering of the LTN FOI's
 
-12/07/2021 - Create SQL.
-*************************************************************************************************************************/
-SELECT concat(substr(Cast(pcnissuedate as varchar(10)),1, 7), '-01')    AS IssueMonthYear,
-       street_location,
-       COUNT(distinct pcn)                                              AS PCNs_Issued,
-       CAST(SUM(cast(lib_payment_received as double)) as decimal(11,2)) AS Total_Amount_Paid,
-
-        current_timestamp() as ImportDateTime,
-        date_format(current_date, 'yyyy') AS import_year,
-        date_format(current_date, 'MM') AS import_month,
-        date_format(current_date, 'dd') AS import_day,
-        date_format(current_date, 'yyyyMMdd') AS import_date
-
-FROM pcnfoidetails_pcn_foi_full as A
-WHERE warningflag = 0 and isvda = 0 and isvoid = 0 AND
-vrm != 'T123EST' AND contraventioncode = '52' AND
-street_location IN ('Allen Road',
-'Ashenden Road junction of Glyn Road',
-'Barnabas Road JCT Berger Road','Barnabas Road JCT Oriel Road',
-'Brooke Road (E)',
-'Brooke Road junction of Evering Road',
-'Dove Row',
-'Gore Road junction of Lauriston Road.',
-'Hyde Road',
-'Hyde Road JCT Northport Street',
-'Lee Street junction of Stean Street',
-'Maury Road junction of Evering Road',
-'Meeson Street junction of Kingsmead Way',
-'Nevill Road junction of Osterley Road',
-'Neville Road junction of Osterley Road',
-'Pitfield Street (F)',
-'Pitfield Street JCT Hemsworth Street',
-'Powell Road junction of Kenninghall Road',
-'Pritchard`s Road',
-'Pritchards Road',
-'Richmond Road junction of Greenwood Road',
-'Shepherdess Walk',
-'Ufton Road junction of Downham Road', 'Wilton Way junction of Greenwood Road')
-
-GROUP BY  concat(substr(Cast(pcnissuedate as varchar(10)),1, 7), '-01'), street_location, import_date, import_day, import_month, import_year
+ 12/07/2021 - Create SQL.
+ *************************************************************************************************************************/
+SELECT concat(
+                substr(Cast(pcnissuedate as varchar(10)), 1, 7),
+                '-01'
+        ) AS IssueMonthYear,
+        street_location,
+        COUNT(distinct pcn) AS PCNs_Issued,
+        CAST(
+                SUM(cast(lib_payment_received as double)) as decimal(11, 2)
+        ) AS Total_Amount_Paid,
+        cast(current_timestamp as timestamp) as ImportDateTime,
+        format_datetime(current_date, 'yyyy') AS import_year,
+        format_datetime(current_date, 'MM') AS import_month,
+        format_datetime(current_date, 'dd') AS import_day,
+        format_datetime(current_date, 'yyyyMMdd') AS import_date
+FROM "dataplatform-prod-liberator-refined-zone".pcnfoidetails_pcn_foi_full as A
+WHERE warningflag = 0
+        and isvda = 0
+        and isvoid = 0
+        AND import_date =(
+                select max(import_date)
+                from "dataplatform-prod-liberator-refined-zone".pcnfoidetails_pcn_foi_full
+        )
+        AND vrm != 'T123EST'
+        AND contraventioncode = '52'
+        AND street_location IN (
+                'Allen Road',
+                'Ashenden Road junction of Glyn Road',
+                'Barnabas Road JCT Berger Road',
+                'Barnabas Road JCT Oriel Road',
+                'Brooke Road (E)',
+                'Brooke Road junction of Evering Road',
+                'Dove Row',
+                'Gore Road junction of Lauriston Road.',
+                'Hyde Road',
+                'Hyde Road JCT Northport Street',
+                'Lee Street junction of Stean Street',
+                'Maury Road junction of Evering Road',
+                'Meeson Street junction of Kingsmead Way',
+                'Nevill Road junction of Osterley Road',
+                'Neville Road junction of Osterley Road',
+                'Pitfield Street (F)',
+                'Pitfield Street JCT Hemsworth Street',
+                'Powell Road junction of Kenninghall Road',
+                'Pritchard`s Road',
+                'Pritchards Road',
+                'Richmond Road junction of Greenwood Road',
+                'Shepherdess Walk',
+                'Ufton Road junction of Downham Road',
+                'Wilton Way junction of Greenwood Road'
+        )
+GROUP BY concat(
+                substr(Cast(pcnissuedate as varchar(10)), 1, 7),
+                '-01'
+        ),
+        street_location,
+        import_date,
+        import_day,
+        import_month,
+        import_year;
 """
-ApplyMapping_node2 = sparkSqlQuery(
-    glueContext,
-    query=SqlQuery0,
-    mapping={"pcnfoidetails_pcn_foi_full": AmazonS3_node1625732651466},
-    transformation_ctx="ApplyMapping_node2",
-)
 
-# Script generated for node S3 bucket
-S3bucket_node3 = glueContext.getSink(
-    path="s3://dataplatform-"
-    + environment
-    + "-refined-zone/parking/liberator/Parking_PCN_LTN_Report_Summary/",
-    connection_type="s3",
-    updateBehavior="UPDATE_IN_DATABASE",
-    partitionKeys=["import_year", "import_month", "import_day", "import_date"],
-    enableUpdateCatalog=True,
-    transformation_ctx="S3bucket_node3",
+create_update_table_with_partition(
+    environment=environment, query_on_athena=query_on_athena, table_name=table_name
 )
-S3bucket_node3.setCatalogInfo(
-    catalogDatabase="dataplatform-" + environment + "-liberator-refined-zone",
-    catalogTableName="Parking_PCN_LTN_Report_Summary",
-)
-S3bucket_node3.setFormat("glueparquet")
-S3bucket_node3.writeFrame(ApplyMapping_node2)
-job.commit()
