5fd693406beb93996c6149dd3db01d88fc397b59
Steve Farr <36167148+stevefarrhackneygovuk@users.noreply.github.com>
2025-05-30 11:54:02 +0100

    Part 3: parking_pcn_dvla_response_no_address

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -201,30 +201,7 @@ module "parking_pcn_ltn_report_summary" {
 # migrated job "Parking_Suspensions_Processed" to dap-airflow on 19/05/2025
 # parking_suspensions_processed_with_finyear migrated to dap-airflow on 19/05/2025
 
-
-module "parking_pcn_dvla_response_no_address" {
-  source                         = "../modules/aws-glue-job"
-  is_live_environment            = local.is_live_environment
-  is_production_environment      = local.is_production_environment
-  department                     = module.department_parking_data_source
-  job_name                       = "${local.short_identifier_prefix}parking_pcn_dvla_response_no_address"
-  helper_module_key              = data.aws_s3_object.helpers.key
-  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
-  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
-  script_name                    = "parking_pcn_dvla_response_no_address"
-  triggered_by_job               = module.parking_pcn_denormalisation.job_name
-  job_description                = "All VRMs with PCNs response from DVLA has no address still open and not due to be written off"
-  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
-  trigger_enabled                = local.is_production_environment
-  number_of_workers_for_glue_job = 2
-  glue_job_worker_type           = "G.1X"
-  glue_version                   = "4.0"
-  job_parameters = {
-    "--job-bookmark-option" = "job-bookmark-disable"
-    "--environment"         = var.environment
-    "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
-  }
-}
+# Migrated "parking_pcn_dvla_response_no_address" to dap-airflow on 30/05/2025
 
 # Migrated job "parking_motorcycle_permits_480" to dap-airflow om 20/02/2025
 # Migrated job "parking_permit_street_cpz_stress_mc" to dap-airflow on 21/05/2025
