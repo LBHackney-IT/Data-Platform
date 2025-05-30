0636f05ae8ab23eba8b822da4b2a84ee339b3be5
Steve Farr <36167148+stevefarrhackneygovuk@users.noreply.github.com>
2025-05-30 11:51:31 +0100

    Part 3: parking_open_pcns_vrms_linked_cancelled_ringer

--- terraform/etl/38-aws-glue-job-parking.tf
+++ terraform/etl/38-aws-glue-job-parking.tf
@@ -195,31 +195,9 @@ module "parking_pcn_ltn_report_summary" {
 # Migrated "parking_correspondence_performance_records_with_pcn_downtime" to dap-airflow on 30/05/2025
 # Migrated "parking_correspondence_performance_records_with_pcn_downtime_gds" to dap-airflow on 30/05/2025
 
-module "parking_open_pcns_vrms_linked_cancelled_ringer" {
-  source                         = "../modules/aws-glue-job"
-  is_live_environment            = local.is_live_environment
-  is_production_environment      = local.is_production_environment
-  department                     = module.department_parking_data_source
-  job_name                       = "${local.short_identifier_prefix}parking_open_pcns_vrms_linked_cancelled_ringer"
-  helper_module_key              = data.aws_s3_object.helpers.key
-  pydeequ_zip_key                = data.aws_s3_object.pydeequ.key
-  spark_ui_output_storage_id     = module.spark_ui_output_storage_data_source.bucket_id
-  script_name                    = "parking_open_pcns_vrms_linked_cancelled_ringer"
-  triggered_by_job               = module.parking_pcn_denormalisation.job_name
-  job_description                = "Parking Open PCNs linked to VRMs cancelled due to being a Ringer or Clone"
-  workflow_name                  = "${local.short_identifier_prefix}parking-liberator-data-workflow"
-  trigger_enabled                = local.is_production_environment
-  number_of_workers_for_glue_job = 2
-  glue_job_worker_type           = "G.1X"
-  glue_version                   = "4.0"
-  job_parameters = {
-    "--job-bookmark-option" = "job-bookmark-disable"
-    "--environment"         = var.environment
-    "--conf"                = "spark.sql.legacy.timeParserPolicy=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.int96RebaseModeInWrite=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInRead=LEGACY --conf spark.sql.legacy.parquet.datetimeRebaseModeInWrite=LEGACY"
-  }
-}
-# MRB 15-02-2024 job created
+# Migrated "parking_open_pcns_vrms_linked_cancelled_ringer" to dap-airflow on 30/05/2025
 
+# MRB 15-02-2024 job created
 # migrated job "Parking_Suspensions_Processed" to dap-airflow on 19/05/2025
 # parking_suspensions_processed_with_finyear migrated to dap-airflow on 19/05/2025
 
