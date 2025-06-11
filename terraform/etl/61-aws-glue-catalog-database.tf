5fae7bcdd22d9514a0560467e9946852c27f67de
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-06-11 15:25:36 +0100

    feature/add-glue-database-child_edu_refined (#2285)
    add this databases to store the transforamtion results from child and edu users.

--- terraform/etl/61-aws-glue-catalog-database.tf
+++ terraform/etl/61-aws-glue-catalog-database.tf
@@ -14,6 +14,14 @@ resource "aws_glue_catalog_database" "hackney_casemanagement_live" {
   }
 }
 
+resource "aws_glue_catalog_database" "child_edu_refined" {
+  name = "child_edu_refined"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 resource "aws_glue_catalog_database" "housing_nec_migration_database" {
   name = "housing_nec_migration"
 
