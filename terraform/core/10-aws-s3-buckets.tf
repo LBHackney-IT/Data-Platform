d6a08958673cdefdce659657416c532ecbb81f58
Tim Burke <tim.burke@hackney.gov.uk>
2025-07-07 17:22:52 +0100

    fixup: bucket key statements

--- terraform/core/10-aws-s3-buckets.tf
+++ terraform/core/10-aws-s3-buckets.tf
@@ -65,13 +65,13 @@ module "raw_zone" {
     ] : []
   )
   bucket_key_policy_statements = concat(
+    [local.allow_s3_batch_copy_kms_access_raw_zone],
     local.is_production_environment ? [
       local.s3_to_s3_copier_for_addresses_api_raw_zone_key_statement
     ] : [],
     local.is_preprod_env ? [
       local.prod_to_pre_prod_data_sync_access_to_raw_zone_key_statement_for_pre_prod
-    ] : [],
-    local.allow_s3_batch_copy_kms_access_raw_zone
+    ] : []
   )
   include_backup_policy_tags = false
 }
@@ -93,11 +93,13 @@ module "refined_zone" {
   )
 
   bucket_key_policy_statements = concat(
-    [local.rentsense_refined_zone_key_statement],
+    [
+      local.rentsense_refined_zone_key_statement,
+      local.allow_s3_batch_copy_kms_access_refined_zone
+    ],
     local.is_preprod_env ? [
       local.prod_to_pre_prod_data_sync_access_to_refined_zone_key_statement_for_pre_prod
-    ] : [],
-    local.allow_s3_batch_copy_kms_access_refined_zone
+    ] : []
   )
   include_backup_policy_tags = false
 }
@@ -116,10 +118,10 @@ module "trusted_zone" {
   ] : []
 
   bucket_key_policy_statements = concat(
+    [local.allow_s3_batch_copy_kms_access_trusted_zone],
     local.is_preprod_env ? [
       local.prod_to_pre_prod_data_sync_access_to_trusted_zone_key_statement_for_pre_prod
-    ] : [],
-    local.allow_s3_batch_copy_kms_access_trusted_zone
+    ] : []
   )
   include_backup_policy_tags = false
 }
