3b66e8d05f1ee4733d78aec6001a4aa84b3414bb
Tim Burke <timburke55@gmail.com>
2025-07-10 14:06:21 +0100

    change to aws provider v 5

--- terraform/core/00-init.tf
+++ terraform/core/00-init.tf
@@ -65,7 +65,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
     google = {
       source  = "hashicorp/google"
