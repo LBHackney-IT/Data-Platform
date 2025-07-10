f7e4fc9216ef05f6f90b4552f6c333a56e538062
Tim Burke <timburke55@gmail.com>
2025-07-10 11:46:30 +0100

    add athena budget alert

--- terraform/core/34-aws-budget-alerting.tf
+++ terraform/core/34-aws-budget-alerting.tf
@@ -9,3 +9,26 @@ module "set_budget_limit_amount" {
   account_id                     = data.aws_caller_identity.data_platform.account_id
   emails_to_notify               = var.emails_to_notify_with_budget_alerts
 }
+
+
+module "aws_budget" {
+  source = "github.com/LBHackney-IT/ce-aws-budgets-lbh"
+
+  budget_name               = "Athena Daily Budget Alert"
+  budget_type               = "COST"
+  limit_amount              = "3"
+  time_unit                 = "DAILY"
+
+  cost_filter = [
+    {
+      name   = "Service"
+      values = ["Amazon Athena"]
+    }
+  ]
+
+  comparison_operator       = "GREATER_THAN"
+  threshold                 = 100
+  threshold_type            = "PERCENTAGE"
+  notification_type         = "ACTUAL"
+  subscriber_email_address  = var.emails_to_notify_with_budget_alerts
+}
