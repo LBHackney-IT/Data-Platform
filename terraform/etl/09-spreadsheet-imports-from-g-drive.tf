da7a0bf87f97d973a1ae59dcc2bfec4f0fe9aa10
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2025-07-18 14:17:30 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf
    Ops dashboard button updated to version 1.13

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -1243,11 +1243,11 @@ module "ops_dashboard_link" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1uIlovxeujHsTvZ2O36DewtUOMI1GFEJD"
+  google_drive_document_id       = "1M-f_neC71jSptfAp349ZSw-1YpCkMpLd"
   glue_job_name                  = "ops_dashboard_link"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "ops_button/ops_dashboard_link_13_06_2025.csv"
+  input_file_name                = "ops_button/ops_dashboard_link_18_07_2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   tags                           = module.tags.values
