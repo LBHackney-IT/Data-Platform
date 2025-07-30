30dac887134368fd34d4c997cea3eb8bb2923b7d
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-07-30 12:44:23 +0100

    Remove-academy-ingestion-related-resources (#2359)
    * Remove academy ingestion‑related resources
    
    * remove the academy related lambda alert

--- terraform/core/41-lambda-failure-alarms.tf
+++ terraform/core/41-lambda-failure-alarms.tf
@@ -22,17 +22,6 @@ module "icaseworks_api_ingestion_lambda_alarm" {
   alarms_handler_lambda_arn  = module.lambda_alarms_handler[0].lambda_arn
 }
 
-module "max_concurrency_lambda_alarm" {
-  count                      = local.is_production_environment ? 1 : 0
-  source                     = "../modules/lambda-alarms-and-monitoring"
-  tags                       = module.tags.values
-  identifier_prefix          = local.short_identifier_prefix
-  lambda_name                = module.max_concurrency_lambda[0].lambda_name
-  project                    = var.project
-  environment                = var.environment
-  alarms_handler_lambda_name = module.lambda_alarms_handler[0].lambda_name
-  alarms_handler_lambda_arn  = module.lambda_alarms_handler[0].lambda_arn
-}
 
 module "rds_snapshot_s3_to_s3_copie_lambda_alarm" {
   count                      = local.is_production_environment ? 1 : 0
