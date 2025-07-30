f3d9849d89b2a73db985524796388fd5d6a8f54d
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2025-07-30 14:12:13 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf (#2360)
    cycle hangar waiting list @ 30-07-2025

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -992,11 +992,11 @@ module "interim_cycle_wait_list" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "19KfD7oGXRGdCsamKqJpVhGW9BqWcIlIB"
+  google_drive_document_id       = "10fsxCIxsCU8JSVddrZHoxAFtsZeN64ou"
   glue_job_name                  = "interim_cycle_wait_list"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "Interim_Cycle_Hangar_Wait_List/interim_cycle_wait_list @ 29-07-2025.csv"
+  input_file_name                = "Interim_Cycle_Hangar_Wait_List/interim_cycle_wait_list @ 30-07-2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
