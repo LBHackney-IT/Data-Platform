88614860ae7bdd130db974f69c22343564c1ca53
Tim Burke <tim.burke@hackney.gov.uk>
2025-08-04 18:24:36 +0100

    add additional tables

--- terraform/core/39-housing-interim-finance-db-ingestion.tf
+++ terraform/core/39-housing-interim-finance-db-ingestion.tf
@@ -15,7 +15,7 @@ module "housing_interim_finance_database_ingestion" {
 }
 
 locals {
-  table_filter_expressions_housing_interim_finance = local.is_live_environment ? "(^sow2b_dbo_matenancyagreement$|^sow2b_dbo_uharaction$|^sow2b_dbo_maproperty$|^sow2b_dbo_ssminitransaction$|^sow2b_dbo_uhproperty$|^sow2b_dbo_calculatedcurrentbalance$|^sow2b_dbo_mamember$)" : ""
+  table_filter_expressions_housing_interim_finance = local.is_live_environment ? "(^sow2b_dbo_matenancyagreement$|^sow2b_dbo_uharaction$|^sow2b_dbo_maproperty$|^sow2b_dbo_ssminitransaction$|^sow2b_dbo_uhproperty$|^sow2b_dbo_calculatedcurrentbalance$|^sow2b_dbo_mamember$|^sow2b_dbo_uhhousehold$|^sow2b_dbo_uhtennancyagreement$|^sow2b_dbo_uhmember$)" : ""
 }
 
 resource "aws_glue_trigger" "housing_interim_finance_filter_ingestion_tables" {
