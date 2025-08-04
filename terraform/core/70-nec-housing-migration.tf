602f50c838708c7ad4eb4e6a28e83f8c15f477a1
Tim Burke <tim.burke@hackney.gov.uk>
2025-08-04 14:57:24 +0100

    configuration
    use earlier verions until dotnet changes are made
    add vpc configs
    add aws_security_group

--- terraform/core/70-nec-housing-migration.tf
+++ terraform/core/70-nec-housing-migration.tf
@@ -1,5 +1,6 @@
 module "file_sync_destination_nec" {
-  source = "git::https://github.com/LBHackney-IT/ce-file-sync-modules.git//destination?ref=82daf9901f3cb49b851213813ceab32ce88edb81" # v.0.4.0
+  # TODO: use previous version
+  source = "git::https://github.com/LBHackney-IT/ce-file-sync-modules.git//destination?ref=caa59d0852914b52245295c57695a52940aa4a31" # v.0.3.2
 
   tags        = module.tags.values
   application = "nec_housing"
@@ -7,6 +8,18 @@ module "file_sync_destination_nec" {
   sftp_host   = "172.26.130.37"
   sftp_port   = 22
   lambda_vpc_config = {
-    # TODO: think this needs to be set up?
+    subnet_ids         = local.subnet_ids_list
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+}
+
+resource "aws_security_group" "lambda_sg" {
+  name   = "NEC sftp lambda"
+  vpc_id = data.aws_vpc.network.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
