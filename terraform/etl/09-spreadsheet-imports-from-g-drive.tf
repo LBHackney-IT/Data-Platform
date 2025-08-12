360d6b4564514925a312d86e9014c896621e5d48
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2025-08-12 13:56:52 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf (#2388)
    cycle hangar waiting list @ 12-08-2025 & Ops button update

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -992,11 +992,11 @@ module "interim_cycle_wait_list" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "10G6vF-2T2EHslVzKrS3402YEzKQyPgyn"
+  google_drive_document_id       = "174QnQnLCgkfum1SZEvDShFCW01eFoBTV"
   glue_job_name                  = "interim_cycle_wait_list"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "Interim_Cycle_Hangar_Wait_List/interim_cycle_wait_list @ 11-08-2025.csv"
+  input_file_name                = "Interim_Cycle_Hangar_Wait_List/interim_cycle_wait_list @ 12-08-2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
@@ -1243,11 +1243,11 @@ module "ops_dashboard_link" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1xjMwTWH2C2cW_Te8ErHRm3ICluv7c9TC"
+  google_drive_document_id       = "1BodTRmWYKrwwOqjiD-MR5hZtGuGlfIpw"
   glue_job_name                  = "ops_dashboard_link"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "ops_button/ops_dashboard_link_08_08_2025.csv"
+  input_file_name                = "ops_button/ops_dashboard_link_12_08_2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   tags                           = module.tags.values
