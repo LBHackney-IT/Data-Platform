1836f6dcd338361cb2d91bc49ab765d8830558b4
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-08-18 12:08:51 +0100

    remove the academy landing database (#2398)

--- terraform/core/13-mssql-ingestion.tf
+++
@@ -1,8 +0,0 @@
-# As Daro suggested, this will be kept until he returns on 11 August 2025
-resource "aws_glue_catalog_database" "landing_zone_academy" {
-  name = "${local.short_identifier_prefix}academy-landing-zone"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
