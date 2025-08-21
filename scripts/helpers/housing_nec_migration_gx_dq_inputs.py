f4d05f4376bc43a7ba3e2f97c4bc5b204f17949a
AGibson <4319494+annajgibson@users.noreply.github.com>
2025-08-21 15:08:05 +0100

    Remove indentation

--- scripts/helpers/housing_nec_migration_gx_dq_inputs.py
+++ scripts/helpers/housing_nec_migration_gx_dq_inputs.py
@@ -5,18 +5,18 @@ sql_config = {
         "id_field": "LPRO_PROPREF",
     },
     "properties_1b": {
-            "sql": """ SELECT *
+        "sql": """ SELECT *
                        FROM "housing_nec_migration"."properties_1b" """,
-            "id_field": "LPRO_PROPREF",
-        },
+        "id_field": "LPRO_PROPREF",
+    },
     "properties_1c": {
-            "sql": """ SELECT *
+        "sql": """ SELECT *
                        FROM "housing_nec_migration"."properties_1c" """,
-            "id_field": "LPRO_PROPREF",
-        }
+        "id_field": "LPRO_PROPREF",
+    },
 }
 
 
-table_list = ['properties_1a', 'properties_1b', 'properties_1c']
+table_list = ["properties_1a", "properties_1b", "properties_1c"]
 
-partition_keys = ['import_date']
+partition_keys = ["import_date"]
