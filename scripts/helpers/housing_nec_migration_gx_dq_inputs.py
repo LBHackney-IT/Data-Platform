431334cd7a0134ffca304e3a804cb45939e08857
AGibson <4319494+annajgibson@users.noreply.github.com>
2025-08-21 15:06:52 +0100

    Add more data quality tests for properties data load. Add two more tables for testing.

--- scripts/helpers/housing_nec_migration_gx_dq_inputs.py
+++ scripts/helpers/housing_nec_migration_gx_dq_inputs.py
@@ -4,9 +4,19 @@ sql_config = {
                    FROM "housing_nec_migration"."properties_1a" """,
         "id_field": "LPRO_PROPREF",
     },
+    "properties_1b": {
+            "sql": """ SELECT *
+                       FROM "housing_nec_migration"."properties_1b" """,
+            "id_field": "LPRO_PROPREF",
+        },
+    "properties_1c": {
+            "sql": """ SELECT *
+                       FROM "housing_nec_migration"."properties_1c" """,
+            "id_field": "LPRO_PROPREF",
+        }
 }
 
 
-table_list = ['properties_1a']
+table_list = ['properties_1a', 'properties_1b', 'properties_1c']
 
 partition_keys = ['import_date']
