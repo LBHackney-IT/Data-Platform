063b5975eea651bee981c705b40951dad328484e
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2025-08-26 10:14:46 +0100

    ignore changes to parameter value (#2407)
    * ignore changes to parameter value
    
    * use parameter name for data source

--- terraform/core/34-aws-budget-alerting.tf
+++ terraform/core/34-aws-budget-alerting.tf
@@ -11,14 +11,21 @@ module "set_budget_limit_amount" {
 }
 
 resource "aws_ssm_parameter" "budget_alert_recipients" {
-  name  = "/data-and-insight/budget-alert-recipients"
-  type  = "StringList"
-  value = "value"
-  tags  = module.tags.values
+  name      = "/data-and-insight/budget-alert-recipients"
+  type      = "StringList"
+  value     = "update_in_console@example.com"
+  tags      = module.tags.values
+  overwrite = true
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
 }
 
 data "aws_ssm_parameter" "budget_alert_recipients" {
-  name = aws_ssm_parameter.budget_alert_recipients.value
+  name = aws_ssm_parameter.budget_alert_recipients.name
 }
 
 module "aws_budget_athena" {
