7cff5f023e923a0f134849c65614013f64f40f49
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2025-09-01 15:34:04 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf (#2420)
    ringgo update august 2025

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -359,11 +359,11 @@ module "Ringgo_Daily_Transactions" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1lcYXdGEdlM9X6mJVvFEZBfGvKzmSwdvm"
+  google_drive_document_id       = "15rxmV-Bnin2u91oikZvxoRKzm-u7iDaU"
   glue_job_name                  = "Ringgo Daily Transactions"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "Ringgo_Daily/ringgo_july_2025.csv"
+  input_file_name                = "Ringgo_Daily/ringgo_aug_2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
