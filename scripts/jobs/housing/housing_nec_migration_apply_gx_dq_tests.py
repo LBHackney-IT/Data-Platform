ffb38e6b8bc29329c79b68eaf66b480d12a0ae8a
AGibson <4319494+annajgibson@users.noreply.github.com>
2025-09-05 14:11:11 +0100

    Remove partition reference

--- scripts/jobs/housing/housing_nec_migration_apply_gx_dq_tests.py
+++ scripts/jobs/housing/housing_nec_migration_apply_gx_dq_tests.py
@@ -10,7 +10,7 @@ from awsglue.utils import getResolvedOptions
 import great_expectations as gx
 import pandas as pd
 from pyathena import connect
-from scripts.helpers.housing_nec_migration_gx_dq_inputs import sql_config, table_list, partition_keys
+from scripts.helpers.housing_nec_migration_gx_dq_inputs import sql_config, table_list
 import scripts.jobs.housing.housing_nec_migration_properties_data_load_gx_suite
 
 logging.basicConfig(level=logging.INFO)
@@ -126,7 +126,6 @@ def main():
         database=target_database,
         table=target_table,
         mode="overwrite",
-        partition_cols=partition_keys,
         dtype=dtype_dict,
         schema_evolution=True
     )
