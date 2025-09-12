163e0a71175442e93d6bd918a8a50a8dbb25bfa3
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2025-09-12 12:50:18 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf
    Bailiff button update @ 12-09-2025

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -1315,11 +1315,11 @@ module "bailiff_dashboard_link" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1LfpDuBDaHncUFnTYNSaSC6S4IWBE0noP"
+  google_drive_document_id       = "1Tb1QkLCowPUOrJTHkGjY1UAngqhlJ4Hm"
   glue_job_name                  = "bailiff_dashboard_link"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "bailiff_button/bailiff_dashboard_link.csv"
+  input_file_name                = "bailiff_button/bailiff_dashboard_link_12_09_2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   tags                           = module.tags.values
