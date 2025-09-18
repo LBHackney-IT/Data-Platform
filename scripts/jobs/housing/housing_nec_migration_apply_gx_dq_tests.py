3d6eb7b96db768fd2c3e448843a6069c926d4976
annajgibson <4319494+annajgibson@users.noreply.github.com>
2025-09-18 11:03:18 +0100

    Add GX DQ suite for tenancies (fix typo in imports) (#2454)

--- scripts/jobs/housing/housing_nec_migration_apply_gx_dq_tests.py
+++ scripts/jobs/housing/housing_nec_migration_apply_gx_dq_tests.py
@@ -16,7 +16,7 @@ from scripts.helpers.housing_nec_migration_gx_dq_inputs import (
     table_list,
 )
 import scripts.jobs.housing.housing_nec_migration_properties_data_load_gx_suite
-import scripts.jobs.housing.housing_nec_migration_properties_data_load_gx_suite
+import scripts.jobs.housing.housing_nec_migration_tenancies_data_load_gx_suite
 
 
 logging.basicConfig(level=logging.INFO)
