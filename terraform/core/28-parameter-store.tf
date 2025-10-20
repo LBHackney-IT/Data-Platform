637744063e87170e18912fb4db6c3d399af4aec3
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2025-10-20 13:24:20 +0100

    (dl-31) add uh parameters (#2514)
    * add uh parameters
    
    * change type

---
+++ terraform/core/28-parameter-store.tf
@@ -0,0 +1,39 @@
+resource "aws_ssm_parameter" "uh_mirror_address" {
+  name  = "/${local.identifier_prefix}/${module.department_housing.identifier}/uh_mirror_prod/address"
+  type  = "String"
+  value = "UPDATE_IN_CONSOLE"
+  tags  = module.tags.values
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "uh_mirror_username" {
+  name  = "/${local.identifier_prefix}/${module.department_housing.identifier}/uh_mirror_prod/username"
+  type  = "String"
+  value = "UPDATE_IN_CONSOLE"
+  tags  = module.tags.values
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "uh_mirror_password" {
+  name  = "/${local.identifier_prefix}/${module.department_housing.identifier}/uh_mirror_prod/password"
+  type  = "SecureString"
+  value = "UPDATE_IN_CONSOLE"
+  tags  = module.tags.values
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
