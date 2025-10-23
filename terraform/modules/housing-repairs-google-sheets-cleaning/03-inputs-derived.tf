d1d05a37e3614ea33433f70900eecd93bb85baf5
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2025-10-23 09:52:30 +0100

    Chore dl 11 remove housing repairs (#2479)
    * remove housing repairs resources
    
    * fixup
    
    * fixup

--- terraform/modules/housing-repairs-google-sheets-cleaning/03-inputs-derived.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  glue_job_name                      = "${var.short_identifier_prefix}Housing Repairs - ${title(replace(var.dataset_name, "-", " "))}"
-  raw_zone_catalog_database_name     = var.department.raw_zone_catalog_database_name
-  refined_zone_catalog_database_name = var.department.refined_zone_catalog_database_name
-}
