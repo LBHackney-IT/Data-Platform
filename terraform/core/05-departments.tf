09d3849c213d83ea216e53d49fe332701f13628e
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2025-10-23 09:37:52 +0100

    change it to read only

--- terraform/core/05-departments.tf
+++ terraform/core/05-departments.tf
@@ -452,8 +452,8 @@ module "department_housing" {
     }
   ]
   additional_glue_database_access = {
-    read_only  = []
-    read_write = ["housing_nec_migration", "housing_nec_migration_outputs", "housing_service_requests_ieg4"]
+    read_only  = ["housing_service_requests_ieg4"]
+    read_write = ["housing_nec_migration", "housing_nec_migration_outputs"]
   }
 }
 
