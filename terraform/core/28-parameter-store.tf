9ee33a9bfcb80bc7e53595ad533d4a8860d6f8df
timburke-hackit <61045197+timburke-hackit@users.noreply.github.com>
2025-11-06 16:51:12 +0000

    add repairs hub parameters (#2535)

--- terraform/core/28-parameter-store.tf
+++ terraform/core/28-parameter-store.tf
@@ -37,3 +37,42 @@ resource "aws_ssm_parameter" "uh_mirror_password" {
   }
 }
 
+resource "aws_ssm_parameter" "repairs_hub_address" {
+  name  = "/${local.identifier_prefix}/${module.department_housing.identifier}/repairs_hub_prod/address"
+  type  = "String"
+  value = "UPDATE_IN_CONSOLE"
+  tags  = module.tags.values
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "repairs_hub_username" {
+  name  = "/${local.identifier_prefix}/${module.department_housing.identifier}/repairs_hub_prod/username"
+  type  = "String"
+  value = "UPDATE_IN_CONSOLE"
+  tags  = module.tags.values
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+resource "aws_ssm_parameter" "repairs_hub_password" {
+  name  = "/${local.identifier_prefix}/${module.department_housing.identifier}/repairs_hub_prod/password"
+  type  = "SecureString"
+  value = "UPDATE_IN_CONSOLE"
+  tags  = module.tags.values
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
