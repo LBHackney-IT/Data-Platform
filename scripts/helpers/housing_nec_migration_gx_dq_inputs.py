8d16638901ec20c2ed9f2b15f9221a647eece426
AGibson <4319494+annajgibson@users.noreply.github.com>
2025-11-13 12:22:31 +0000

    update sql scripts according to changes in MTFH reshape files

--- scripts/helpers/housing_nec_migration_gx_dq_inputs.py
+++ scripts/helpers/housing_nec_migration_gx_dq_inputs.py
@@ -3,9 +3,22 @@ sql_config = {
     "tenancies": {"id_field": "LTCY_ALT_REF"},
     "people": {"id_field": "LPAR_PER_ALT_REF"},
     "contacts": {"id_field": "LCDE_LEGACY_REF"},
+    "arrears_actions": {"id_field": "LACA_PAY_REF"},
+    "revenue_accounts": {"id_field": "LRAC_PAY_REF"},
+    "transactions": {"id_field": "LTRN_ALT_REF"},
+    "addresses": {"id_field": "LAUS_LEGACY_REF"}
 }
 
-data_load_list = ["properties", "tenancies", "people", "contacts", "arrears_actions"]
+data_load_list = [
+    "properties",
+    "tenancies",
+    "people",
+    "contacts",
+    "arrears_actions",
+    "revenue_accounts",
+    "transactions",
+    "addresses",
+]
 
 table_list = {
     "properties": [
@@ -31,7 +44,26 @@ table_list = {
     ],
     "people": ["people_1a", "people_1b", "people_1c", "people_2a", "people_all"],
     "contacts": ["contacts_1a", "contacts_1b", "contacts_2a", "contacts_all"],
-    "arrears_actions": ["arrears_actions_1a", "arrears_actions_1c", "arrears_actions_2a"],
+    "arrears_actions": [
+        "arrears_actions_1a",
+        "arrears_actions_1c",
+        "arrears_actions_2a",
+    ],
+    "revenue_accounts": [
+        "revenue_accounts_1a",
+        "revenue_accounts_1b_sc",
+        "revenue_accounts_1c",
+        "revenue_accounts_2a",
+        "revenue_accounts_other",
+    ],
+    "transactions": [
+        "transactions_1a",
+        "transactions_1c",
+        "transactions_2a",
+        "transactions_other",
+        "transactions_all",
+    ],
+    "addresses": ["addresses_1a", "addresses_2a"],
 }
 
 partition_keys = ["import_date"]
