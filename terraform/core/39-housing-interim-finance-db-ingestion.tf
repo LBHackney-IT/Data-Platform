7a4f3062bceabbcb584f658e656a7540c2adafda
daroclark <daro.clark@hackney.gov.uk>
2025-12-11 15:32:03 +0000

    Update 39-housing-interim-finance-db-ingestion.tf
    Adding another table to get the balances for NEC migration

--- terraform/core/39-housing-interim-finance-db-ingestion.tf
+++ terraform/core/39-housing-interim-finance-db-ingestion.tf
@@ -15,7 +15,7 @@ module "housing_interim_finance_database_ingestion" {
 }
 
 locals {
-  table_filter_expressions_housing_interim_finance = local.is_live_environment ? "(^sow2b_dbo_matenancyagreement$|^sow2b_dbo_uharaction$|^sow2b_dbo_maproperty$|^sow2b_dbo_ssminitransaction$|^sow2b_dbo_uhproperty$|^sow2b_dbo_calculatedcurrentbalance$|^sow2b_dbo_mamember$|^sow2b_dbo_uhhousehold$|^sow2b_dbo_uhtennancyagreement$|^sow2b_dbo_uhmember$|^sow2b_dbo_uhdebtype$|^sow2b_dbo_charges$|^sow2b_dbo_chargeshistory$)" : ""
+  table_filter_expressions_housing_interim_finance = local.is_live_environment ? "(^sow2b_dbo_matenancyagreement$|^sow2b_dbo_uharaction$|^sow2b_dbo_maproperty$|^sow2b_dbo_ssminitransaction$|^sow2b_dbo_uhproperty$|^sow2b_dbo_calculatedcurrentbalance$|^sow2b_dbo_mamember$|^sow2b_dbo_uhhousehold$|^sow2b_dbo_uhtenancyagreement$|^sow2b_dbo_uhmember$|^sow2b_dbo_uhdebtype$|^sow2b_dbo_charges$|^sow2b_dbo_chargeshistory$|^sow2b_dbo_vitransaction$)" : ""
 }
 
 resource "aws_glue_trigger" "housing_interim_finance_filter_ingestion_tables" {
