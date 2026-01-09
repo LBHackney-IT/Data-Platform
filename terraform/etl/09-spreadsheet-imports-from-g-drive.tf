4e78f1df66e765116968a2ceea813e0260712b3e
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2026-01-09 17:03:39 +0000

    Update 09-spreadsheet-imports-from-g-drive.tf (#2623)
    SMT & Ops button update @ 09-01-2026

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -1164,11 +1164,11 @@ module "ops_dashboard_link" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1No-lK6sAVL0rRMwbmwzf8ZoPHGZHZ4l6"
+  google_drive_document_id       = "1Rb3fYnFnGtyGTqlz-Qbku3md4TaMoDq6"
   glue_job_name                  = "ops_dashboard_link"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "ops_button/ops_dashboard_link_23_12_2025.csv"
+  input_file_name                = "ops_button/ops_dashboard_link_09_01_2026.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   tags                           = module.tags.values
@@ -1416,11 +1416,11 @@ module "smt_dashboard_link" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1ytuLSxiCCgRLyPxR5bxhjH3oCHcQc0p5"
+  google_drive_document_id       = "16gp11Qlu-39CTh18hhN6PwPIlKtovjHt"
   glue_job_name                  = "smt_dashboard_link"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "smt_button/smt_dashboard_link_23_12_2025.csv"
+  input_file_name                = "smt_button/smt_dashboard_link_09_01_2026.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   tags                           = module.tags.values
