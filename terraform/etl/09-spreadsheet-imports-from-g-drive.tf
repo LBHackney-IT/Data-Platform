367aef5cf2335a51a26580da20398d7fe51baf4d
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2026-01-09 10:50:15 +0000

    Update 09-spreadsheet-imports-from-g-drive.tf (#2622)
    Bailiff data update @ 07/01/2026

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -668,11 +668,11 @@ module "bailiff_return" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "199qT4AH9AqFt5-Zb5yfvwmXj30lsAyL0"
+  google_drive_document_id       = "1SOH1YJWHHBr5SVzsnUWQVt75t273WDyi"
   glue_job_name                  = "bailiff_return"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "bailiff_return/Bailiff_return @ 01-12-2025.csv"
+  input_file_name                = "bailiff_return/Bailiff_return @ 07-01-2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
@@ -703,11 +703,11 @@ module "bailiff_allocation" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1H1ED0ln286heWtajqCm0eIZTdbA0cnw9"
+  google_drive_document_id       = "1jwQQjf3g4E_Fd2rIXw5TAOSo36yV5GpX"
   glue_job_name                  = "bailiff_allocation"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "bailiff_allocation/Bailiff Allocation @ 01-12-2025.csv"
+  input_file_name                = "bailiff_allocation/Bailiff Allocation @ 07-01-2025.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
