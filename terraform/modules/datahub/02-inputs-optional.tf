2adeccf445a71ecdbe620be92a14d8ce0a78f6c0
Wayne Greeff <61699419+LBH-wgreeff@users.noreply.github.com>
2026-02-26 17:21:35 +0000

    Decommission old Datahub in Pre-Prod and Prod (#2666)

--- terraform/modules/datahub/02-inputs-optional.tf
+++
@@ -1,9 +0,0 @@
-variable "enable_load_balancer" {
-  type    = bool
-  default = false
-}
-
-variable "hub_firewall_ips" {
-  type    = list(string)
-  default = ["192.168.20.0/28", "192.168.21.0/28"]
-}
