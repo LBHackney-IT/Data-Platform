c1e510bd987dd615026d6a1d5b09688e4d8d9019
Wayne Greeff <61699419+LBH-wgreeff@users.noreply.github.com>
2026-03-02 19:00:25 +0000

    Revert "Revert "Revert "Revert "Decommission old Datahub in Pre-Prod and Prod…" (#2676)

--- terraform/modules/datahub/06-ecs-cluster.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecs_cluster" "datahub" {
-  tags = var.tags
-  name = "${var.short_identifier_prefix}datahub"
-}
--- terraform/modules/datahub/07-cloudwatch-logs.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "datahub" {
-  name = "${var.short_identifier_prefix}datahub"
-  tags = var.tags
-}
