db5906bbb9ef178868a76d52684babb04679049c
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2026-03-27 09:24:15 +0000

    Enable departmental Airflow user for planning (#2703)

--- terraform/core/05-departments.tf
+++ terraform/core/05-departments.tf
@@ -239,6 +239,7 @@ module "department_planning" {
   identity_store_id               = local.identity_store_id
   google_group_admin_display_name = local.google_group_admin_display_name
   google_group_display_name       = "saml-aws-data-platform-collaborator-planning@hackney.gov.uk"
+  departmental_airflow_user       = true
   mwaa_etl_scripts_bucket_arn     = aws_s3_bucket.mwaa_etl_scripts_bucket.arn
   mwaa_key_arn                    = aws_kms_key.mwaa_key.arn
   user_uploads_bucket             = module.user_uploads
