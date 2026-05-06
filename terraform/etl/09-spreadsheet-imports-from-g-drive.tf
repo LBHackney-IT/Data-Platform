edaa3e3ee68e673b69d47cba7cf2f36fa751eeb3
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2026-05-06 16:30:39 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf
    06-05-2026 update Qlik links

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -1164,11 +1164,11 @@ module "ops_dashboard_link" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1tn_pj4j6n8fi4oiNoGYKTuZ-YMx9DCkm"
+  google_drive_document_id       = "1EzQWYkKTayKYiD12RD-iPOpAYNEbq92c"
   glue_job_name                  = "ops_dashboard_link"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "ops_button/ops_dashboard_link_01-05-2026.csv"
+  input_file_name                = "ops_button/ops_dashboard_link_06-05-2026.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   tags                           = module.tags.values
