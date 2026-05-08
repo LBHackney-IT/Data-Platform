421ce1a8df357e8674ad387a8e987745e93c91d3
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2026-05-08 11:08:22 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf (#2740)
    Apr Cedar backing data ingestion 08-05-2026

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -172,11 +172,11 @@ module "Cedar_Backing_Data" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1M3ZCgEJLshUSMZgt_sjOF1W04tX-sxKT"
+  google_drive_document_id       = "13I-gnoXVCEJtqP_lM4VL2HNLNL6qnXrd"
   glue_job_name                  = "Cedar Backing Data"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "Cedar_Backing_Data/cedarbacking_mar_2026_1.csv"
+  input_file_name                = "Cedar_Backing_Data/cedarbacking_apr_2026.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
