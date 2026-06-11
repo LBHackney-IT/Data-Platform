adbd186fcf4983d39d41815b4616f0431458f8ef
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2026-06-11 10:37:44 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf
    Financial data update for May 11-06-2026

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -208,11 +208,11 @@ module "Cedar_Parking_Payments" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "10lrxiLN1Rw87xwwjDAz5nyNwGMSTMljj"
+  google_drive_document_id       = "1GzjUWEMp9g3_SzF_JJHPV9nsJm2WWKvm"
   glue_job_name                  = "Cedar Parking Payments"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "Cedar_Parking_Payments/Cedar_Parking_Payments_apr_2026.csv"
+  input_file_name                = "Cedar_Parking_Payments/Cedar_Parking_Payments_may_2026.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
@@ -244,11 +244,11 @@ module "Citypay_Import" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1NaKczUik8E1ThK5oaRrqHXmbvxEjqtFV"
+  google_drive_document_id       = "1gLGkaXePO4YZdniWwtUujIWeAcacuxmm"
   glue_job_name                  = "Citypay Import"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "CityPay_Payments/Citypay_import_apr_2026.csv"
+  input_file_name                = "CityPay_Payments/Citypay_import_may_2026.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   worksheets = {
