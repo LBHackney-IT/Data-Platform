58ab9d861a08cf72c80b454d7bc133f373827271
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2026-06-15 16:10:23 +0100

    add raw_private_sector_housing database to redshift locals

--- terraform/etl/42-redshift.tf
+++ terraform/etl/42-redshift.tf
@@ -69,6 +69,7 @@ locals {
     replace(module.department_data_and_insight_data_source.raw_zone_catalog_database_name, "-", "_")     = module.department_data_and_insight_data_source.raw_zone_catalog_database_name,
     replace(module.department_data_and_insight_data_source.refined_zone_catalog_database_name, "-", "_") = module.department_data_and_insight_data_source.refined_zone_catalog_database_name,
     replace(module.department_data_and_insight_data_source.trusted_zone_catalog_database_name, "-", "_") = module.department_data_and_insight_data_source.trusted_zone_catalog_database_name,
+    replace(aws_glue_catalog_database.raw_private_sector_housing.name, "-", "_")                        = aws_glue_catalog_database.raw_private_sector_housing.name,
 
     replace(module.department_env_enforcement_data_source.raw_zone_catalog_database_name, "-", "_")     = module.department_env_enforcement_data_source.raw_zone_catalog_database_name,
     replace(module.department_env_enforcement_data_source.refined_zone_catalog_database_name, "-", "_") = module.department_env_enforcement_data_source.refined_zone_catalog_database_name,
@@ -181,6 +182,7 @@ locals {
         replace(module.department_data_and_insight_data_source.raw_zone_catalog_database_name, "-", "_"),
         replace(module.department_data_and_insight_data_source.refined_zone_catalog_database_name, "-", "_"),
         replace(module.department_data_and_insight_data_source.trusted_zone_catalog_database_name, "-", "_"),
+        replace(aws_glue_catalog_database.raw_private_sector_housing.name, "-", "_"),
 
         replace(module.department_env_enforcement_data_source.raw_zone_catalog_database_name, "-", "_"),
         replace(module.department_env_enforcement_data_source.refined_zone_catalog_database_name, "-", "_"),
@@ -364,6 +366,7 @@ locals {
         replace(module.department_data_and_insight_data_source.raw_zone_catalog_database_name, "-", "_"),
         replace(module.department_data_and_insight_data_source.refined_zone_catalog_database_name, "-", "_"),
         replace(module.department_data_and_insight_data_source.trusted_zone_catalog_database_name, "-", "_"),
+        replace(aws_glue_catalog_database.raw_private_sector_housing.name, "-", "_"),
 
         replace(module.department_env_enforcement_data_source.raw_zone_catalog_database_name, "-", "_"),
         replace(module.department_env_enforcement_data_source.refined_zone_catalog_database_name, "-", "_"),
