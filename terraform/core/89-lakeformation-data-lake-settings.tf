3152adfeae79b0b961107cd1dbfed65285ceb2ef
Tim Burke <tim.burke@hackney.gov.uk>
2026-06-22 10:20:17 +0100

    test

--- terraform/core/89-lakeformation-data-lake-settings.tf
+++ terraform/core/89-lakeformation-data-lake-settings.tf
@@ -1,3 +1,5 @@
+# test for broken workflow
+
 resource "aws_lakeformation_data_lake_settings" "data_platform" {
   admins = [
     "arn:aws:iam::${var.aws_deploy_account_id}:role/${var.aws_deploy_iam_role_name}"
