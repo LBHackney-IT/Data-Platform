d1f08e2d96d31b21128f80add1afe789fd31345f
barnesm707 <87021322+barnesm707@users.noreply.github.com>
2026-07-30 13:43:47 +0100

    Update 09-spreadsheet-imports-from-g-drive.tf
    forecasting button update 30-07-2026

--- terraform/etl/09-spreadsheet-imports-from-g-drive.tf
+++ terraform/etl/09-spreadsheet-imports-from-g-drive.tf
@@ -1200,11 +1200,11 @@ module "forecast_dashboard_link" {
   landing_zone_bucket_id         = module.landing_zone_data_source.bucket_id
   landing_zone_kms_key_arn       = module.landing_zone_data_source.kms_key_arn
   landing_zone_bucket_arn        = module.landing_zone_data_source.bucket_arn
-  google_drive_document_id       = "1u2CSitcJ3M05LNrZkUN6QRyeg8dCUiOe"
+  google_drive_document_id       = "1pt9cfVsgTV92_BfcuAyDJF8vOBPOk9b0"
   glue_job_name                  = "forecast_dashboard_link"
   output_folder_name             = "g-drive"
   raw_zone_bucket_id             = module.raw_zone_data_source.bucket_id
-  input_file_name                = "forecasting_button/forecasting_dashboard_link_23_07_2026.csv"
+  input_file_name                = "forecasting_button/forecasting_dashboard_link_30_07_2026.csv"
   ingestion_schedule             = "cron(0 21 * * ? *)"
   enable_bookmarking             = true
   tags                           = module.tags.values
