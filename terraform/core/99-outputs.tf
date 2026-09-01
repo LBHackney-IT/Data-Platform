81a149944f2ff6b3794b1a14f050f0004f2a0d19
Tim Burke <tim.burke@hackney.gov.uk>
2026-09-01 11:38:29 +0100

    remove weekly pre-production copy cleanup

--- terraform/core/99-outputs.tf
+++ terraform/core/99-outputs.tf
@@ -17,10 +17,6 @@ output "liberator_dump_to_rds_snapshot_ecr_repository_worker_endpoint" {
   value = try(module.liberator_dump_to_rds_snapshot[0].ecr_repository_worker_endpoint, null)
 }
 
-output "pre_prod_data_cleanup_ecr_repository_endpoint" {
-  value = try(module.pre_production_data_cleanup[0].ecr_repository_worker_endpoint, null)
-}
-
 output "ssl_connection_resources_bucket_id" {
   value = try(aws_s3_bucket.ssl_connection_resources[0].id, "")
 }
