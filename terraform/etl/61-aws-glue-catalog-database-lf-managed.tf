3d7fc3e449c0185b11119223fa178d668174e69d
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2026-09-11 11:23:32 +0100

    Create mosaic catalog databases and secrets (#2824)
    * create catalog and secrets for mosaic ingestion
    
    * update description catalog database
    
    * update location_uri for mosaic_raw and mosaic_refined Glue catalog databases

---
+++ terraform/etl/61-aws-glue-catalog-database-lf-managed.tf
@@ -0,0 +1,22 @@
+# Lake Formation registration, grants and hybrid opt-ins are managed in dap-infrastructure
+resource "aws_glue_catalog_database" "mosaic_raw" {
+  name         = "mosaic_raw"
+  description  = "Mosaic SQL Server full-ingestion history and latest datasets."
+  location_uri = "s3://${module.raw_zone_data_source.bucket_id}/projects/mosaic/"
+  tags         = module.tags.values
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_glue_catalog_database" "mosaic_refined" {
+  name         = "mosaic_refined"
+  description  = "Refined Mosaic datasets."
+  location_uri = "s3://${module.refined_zone_data_source.bucket_id}/projects/mosaic/"
+  tags         = module.tags.values
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
