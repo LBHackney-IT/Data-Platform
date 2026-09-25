2c8342aef20c27c5a5463a3da4a59bf2f7eff420
Tian Chen <38001883+Tian-2017@users.noreply.github.com>
2026-09-25 09:16:58 +0100

    Standardize naming for Environmental Health resources and update references across modules (#2842)

--- terraform/modules/department/20-aws-glue-databases.tf
+++ terraform/modules/department/20-aws-glue-databases.tf
@@ -14,10 +14,9 @@ resource "aws_ssm_parameter" "raw_zone_catalog_database_name" {
 resource "aws_glue_catalog_database" "refined_zone_catalog_database" {
   name = "${var.short_identifier_prefix}${local.department_identifier}-refined-zone"
 
-  # Temporarily disabled to allow the Environmental Health database rename.
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_ssm_parameter" "refined_zone_catalog_database_name" {
@@ -32,10 +31,9 @@ resource "aws_ssm_parameter" "refined_zone_catalog_database_name" {
 resource "aws_glue_catalog_database" "trusted_zone_catalog_database" {
   name = "${var.short_identifier_prefix}${local.department_identifier}-trusted-zone"
 
-  # Temporarily disabled to allow the Environmental Health database rename.
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "aws_ssm_parameter" "trusted_zone_catalog_database_name" {
